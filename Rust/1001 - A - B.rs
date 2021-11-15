fbd421dbc15879c1cd81a5aeb2379ea6ed412cb7
Chris Ohk <utilForever@gmail.com>
2021-11-15 18:44:33 +0900

    Upload 1001 - A - B

---
+++ Rust/1001 - A - B.rs
@@ -0,0 +1,15 @@
+use std::io;
+
+fn main() {
+    let mut s = String::new();
+
+    io::stdin().read_line(&mut s).unwrap();
+
+    let values: Vec<i32> = s
+        .as_mut_str()
+        .split_whitespace()
+        .map(|s| s.parse().unwrap())
+        .collect();
+
+    println!("{}", values[0] - values[1]);
+}

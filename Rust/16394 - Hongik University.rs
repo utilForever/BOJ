887f6e643ebc49c21b209a8bd9926590acc8e5b3
Chris Ohk <utilforever@gmail.com>
2021-12-03 01:29:38 +0900

    Upload 16394 - Hongik University

---
+++ Rust/16394 - Hongik University.rs
@@ -0,0 +1,21 @@
+use std::io;
+
+fn input_integers() -> Vec<i32> {
+    let mut s = String::new();
+
+    io::stdin().read_line(&mut s).unwrap();
+
+    let values: Vec<i32> = s
+        .as_mut_str()
+        .split_whitespace()
+        .map(|s| s.parse().unwrap())
+        .collect();
+
+    values
+}
+
+fn main() {
+    let n = input_integers()[0];
+
+    println!("{}", n - 1946);
+}

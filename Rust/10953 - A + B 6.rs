0b4d103eba3ecbd62a1f7690a53094bfbe2c7cae
Chris Ohk <utilforever@gmail.com>
2021-12-16 03:07:35 +0900

    Upload 10953 - A + B 6

---
+++ Rust/10953 - A + B 6.rs
@@ -0,0 +1,35 @@
+use std::io;
+
+fn input_integers() -> Vec<i32> {
+    let mut s = String::new();
+
+    io::stdin().read_line(&mut s).unwrap();
+
+    let values: Vec<i32> = s
+        .as_mut_str()
+        .split_whitespace()
+        .map(|s| s.parse().unwrap())
+        .collect();
+
+    values
+}
+
+fn main() {
+    let t = input_integers()[0];
+
+    for _ in 0..t {
+        let mut s = String::new();
+        io::stdin().read_line(&mut s).unwrap();
+        let nums: Vec<i32> = s
+            .as_mut_str()
+            .trim()
+            .split(',')
+            .map(|s| s.parse().unwrap())
+            .collect();
+
+        let a = nums[0];
+        let b = nums[1];
+
+        println!("{}", a + b);
+    }
+}

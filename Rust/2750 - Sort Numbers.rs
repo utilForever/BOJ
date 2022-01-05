cd3ec0e9dc4aaf02f4579e9e8dd2f8722ddd30db
Chris Ohk <utilforever@gmail.com>
2022-01-06 01:31:56 +0900

    Upload 2750 - Sort Numbers

---
+++ Rust/2750 - Sort Numbers.rs
@@ -0,0 +1,31 @@
+use std::io;
+
+fn input_integers() -> Vec<i64> {
+    let mut s = String::new();
+
+    io::stdin().read_line(&mut s).unwrap();
+
+    let values: Vec<i64> = s
+        .as_mut_str()
+        .split_whitespace()
+        .map(|s| s.parse().unwrap())
+        .collect();
+
+    values
+}
+
+fn main() {
+    let n = input_integers()[0] as usize;
+
+    let mut nums = vec![0; n];
+
+    for i in 0..n {
+        nums[i] = input_integers()[0];
+    }
+
+    nums.sort();
+
+    for num in nums {
+        println!("{}", num);
+    }
+}

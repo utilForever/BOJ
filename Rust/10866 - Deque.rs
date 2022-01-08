12f7f9a6cd18f271c7028117674039674b35ab01
Chris Ohk <utilforever@gmail.com>
2022-01-09 02:08:44 +0900

    Upload 10866 - Deque

---
+++ Rust/10866 - Deque.rs
@@ -0,0 +1,89 @@
+use std::{collections::VecDeque, io};
+
+fn input_strings() -> Vec<String> {
+    let mut s = String::new();
+
+    io::stdin().read_line(&mut s).unwrap();
+
+    let values: Vec<String> = s
+        .as_mut_str()
+        .split_whitespace()
+        .map(|s| s.parse().unwrap())
+        .collect();
+
+    values
+}
+
+fn input_integers() -> Vec<i64> {
+    let mut s = String::new();
+
+    io::stdin().read_line(&mut s).unwrap();
+
+    let values: Vec<i64> = s
+        .as_mut_str()
+        .split_whitespace()
+        .map(|s| s.parse().unwrap())
+        .collect();
+
+    values
+}
+
+fn main() {
+    let mut queue = VecDeque::new();
+
+    let n = input_integers()[0];
+
+    for _ in 0..n {
+        let order = input_strings();
+
+        match order[0].as_str() {
+            "push_front" => {
+                let num = order[1].parse::<i64>().unwrap();
+                queue.push_front(num);
+            }
+            "push_back" => {
+                let num = order[1].parse::<i64>().unwrap();
+                queue.push_back(num);
+            }
+            "pop_front" => {
+                if queue.is_empty() {
+                    println!("-1");
+                } else {
+                    println!("{}", queue.pop_front().unwrap());
+                }
+            }
+            "pop_back" => {
+                if queue.is_empty() {
+                    println!("-1");
+                } else {
+                    println!("{}", queue.pop_back().unwrap());
+                }
+            }
+            "size" => {
+                println!("{}", queue.len());
+            }
+            "empty" => {
+                if queue.is_empty() {
+                    println!("1");
+                } else {
+                    println!("0");
+                }
+            }
+            "front" => {
+                if queue.is_empty() {
+                    println!("-1");
+                } else {
+                    println!("{}", queue.front().unwrap());
+                }
+            }
+            "back" => {
+                if queue.is_empty() {
+                    println!("-1");
+                } else {
+                    println!("{}", queue.back().unwrap());
+                }
+            }
+            _ => {}
+        }
+    }
+}

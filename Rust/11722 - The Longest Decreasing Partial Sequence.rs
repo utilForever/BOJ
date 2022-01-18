4fe9940e8f8191ed43150ff6de0c7a643ef3f948
Chris Ohk <utilforever@gmail.com>
2022-01-19 08:42:19 +0900

    Upload 11722 - The Longest Decreasing Partial Sequence

---
+++ Rust/11722 - The Longest Decreasing Partial Sequence.rs
@@ -0,0 +1,34 @@
+use std::io;
+
+fn input_integers() -> Vec<i32> {
+    let mut s = String::new();
+
+    io::stdin().read_line(&mut s).unwrap();
+
+    let values: Vec<i32> = s
+        .as_mut_str()
+        .split_whitespace()
+        .map(|s| s.parse().unwrap())
+        .collect();
+
+    values
+}
+
+fn main() {
+    let n = input_integers()[0] as usize;
+
+    let sequence = input_integers();
+    let mut longest = vec![0; n];
+
+    for i in (0..=n - 1).rev() {
+        longest[i] = 1;
+
+        for j in (i + 1..=n - 1).rev() {
+            if sequence[i] > sequence[j] && longest[i] < longest[j] + 1 {
+                longest[i] = longest[j] + 1;
+            }
+        }
+    }
+
+    println!("{}", longest.iter().max().unwrap());
+}

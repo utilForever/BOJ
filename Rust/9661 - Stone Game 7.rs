c6ab664f7787869d299ce250956d596fc6aed6a2
Chris Ohk <utilforever@gmail.com>
2022-02-01 16:15:04 +0900

    Upload 9661 - Stone Game 7

---
+++ Rust/9661 - Stone Game 7.rs
@@ -0,0 +1,21 @@
+use std::io;
+
+fn input_integers() -> Vec<i64> {
+    let mut s = String::new();
+
+    io::stdin().read_line(&mut s).unwrap();
+
+    let values: Vec<i64> = s
+        .as_mut_str()
+        .split_whitespace()
+        .map(|s| s.parse().unwrap())
+        .collect();
+
+    values
+}
+
+fn main() {
+    let n = input_integers()[0] as usize;
+
+    println!("{}", if n % 5 == 0 || n % 5 == 2 { "CY" } else { "SK" });
+}

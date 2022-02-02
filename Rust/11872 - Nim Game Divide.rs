05e008c993b44ee5ba65a26750dcfa27cd2a89d4
Chris Ohk <utilforever@gmail.com>
2022-02-02 16:27:14 +0900

    Upload 11872 - Nim Game Divide

---
+++ Rust/11872 - Nim Game Divide.rs
@@ -0,0 +1,37 @@
+use std::io;
+
+fn input_integers() -> Vec<i64> {
+    let mut s = String::new();
+
+    io::stdin().read_line(&mut s).unwrap();
+
+    let values: Vec<i64> = s
+        .as_mut_str()
+        .split_whitespace()
+        .map(|s| s.parse().unwrap())
+        .collect();
+
+    values
+}
+
+fn main() {
+    let _ = input_integers()[0] as usize;
+    let mut stones = input_integers();
+
+    let mut ret = 0;
+    for stone in stones.iter_mut() {
+        if *stone % 4 == 0 {
+            *stone -= 1;
+        } else if *stone % 4 == 3 {
+            *stone += 1;
+        }
+
+        ret ^= *stone;
+    }
+
+    if ret == 0 {
+        println!("cubelover");
+    } else {
+        println!("koosaga");
+    }
+}

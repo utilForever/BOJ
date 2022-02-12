f43953ca9e60b81037fedfca895cfb10cb725685
Chris Ohk <utilforever@gmail.com>
2022-02-12 20:14:20 +0900

    Upload 11656 - Suffix Array

---
+++ Rust/11656 - Suffix Array.rs
@@ -0,0 +1,54 @@
+use io::Write;
+use std::{io, str};
+
+pub struct UnsafeScanner<R> {
+    reader: R,
+    buf_str: Vec<u8>,
+    buf_iter: str::SplitAsciiWhitespace<'static>,
+}
+
+impl<R: io::BufRead> UnsafeScanner<R> {
+    pub fn new(reader: R) -> Self {
+        Self {
+            reader,
+            buf_str: vec![],
+            buf_iter: "".split_ascii_whitespace(),
+        }
+    }
+
+    pub fn token<T: str::FromStr>(&mut self) -> T {
+        loop {
+            if let Some(token) = self.buf_iter.next() {
+                return token.parse().ok().expect("Failed parse");
+            }
+            self.buf_str.clear();
+            self.reader
+                .read_until(b'\n', &mut self.buf_str)
+                .expect("Failed read");
+            self.buf_iter = unsafe {
+                let slice = str::from_utf8_unchecked(&self.buf_str);
+                std::mem::transmute(slice.split_ascii_whitespace())
+            }
+        }
+    }
+}
+
+fn main() {
+    let (stdin, stdout) = (io::stdin(), io::stdout());
+    let mut scan = UnsafeScanner::new(stdin.lock());
+    let mut out = io::BufWriter::new(stdout.lock());
+
+    let s = scan.token::<String>();
+    let chars = s.as_str();
+    let mut suffix_array = vec![String::new(); chars.len()];
+
+    for i in 0..suffix_array.len() {
+        suffix_array[i] = chars[i..].to_string();
+    }
+
+    suffix_array.sort();
+
+    for suffix in suffix_array {
+        writeln!(out, "{}", suffix).unwrap();
+    }
+}

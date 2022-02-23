0c56c575947155aa91b2ec4e56c569544dd971cf
Chris Ohk <utilforever@gmail.com>
2022-02-23 12:58:27 +0900

    Update 9252 - LCS 2

--- Rust/9252 - LCS 2.rs
+++ Rust/9252 - LCS 2.rs
@@ -10,32 +10,57 @@ fn main() {
     t = t.trim().to_string();
 
     let mut lcs_count = vec![vec![0; 1001]; 1001];
-    let mut lcs_str = vec![vec![String::new(); 1001]; 1001];
     let mut i = 0;
     let mut j;
 
-    for s in s.chars() {
+    let s_chars = s.as_bytes();
+    let t_chars = t.as_bytes();
+    let s_len = s_chars.len();
+    let t_len = t_chars.len();
+
+    for s_idx in 0..s_len {
         i += 1;
         j = 0;
 
-        for t in t.chars() {
+        for t_idx in 0..t_len {
             j += 1;
 
-            if s != t {
+            if s_chars[s_idx] != t_chars[t_idx] {
                 if lcs_count[i - 1][j] > lcs_count[i][j - 1] {
                     lcs_count[i][j] = lcs_count[i - 1][j];
-                    lcs_str[i][j] = lcs_str[i - 1][j].clone();
                 } else {
                     lcs_count[i][j] = lcs_count[i][j - 1];
-                    lcs_str[i][j] = lcs_str[i][j - 1].clone();
                 }
             } else {
                 lcs_count[i][j] = lcs_count[i - 1][j - 1] + 1;
-                lcs_str[i][j] = lcs_str[i - 1][j - 1].clone() + &s.to_string();
             }
         }
     }
 
-    println!("{}", lcs_count[s.chars().count()][t.chars().count()]);
-    println!("{}", lcs_str[s.chars().count()][t.chars().count()]);
+    let mut lcs_str = String::new();
+    let (mut i, mut j) = (s_len, t_len);
+
+    while i != 0 && j != 0 {
+        let now = lcs_count[i][j];
+
+        if now != lcs_count[i - 1][j] && now != lcs_count[i][j - 1] {
+            lcs_str.push(s_chars[i - 1] as char);
+        }
+
+        if now == lcs_count[i - 1][j] {
+            i -= 1;
+        } else if now == lcs_count[i][j - 1] {
+            j -= 1;
+        } else {
+            i -= 1;
+            j -= 1;
+        }
+
+        if now == 0 {
+            break;
+        }
+    }
+
+    println!("{}", lcs_count[s_len][t_len]);
+    println!("{}", lcs_str.chars().rev().collect::<String>());
 }

1d344dccdabadd4b9d990dd58565850942c17735
Chris Ohk <utilforever@gmail.com>
2022-03-21 23:09:05 +0900

    Upload 11404 - Floyd

---
+++ Rust/11404 - Floyd.rs
@@ -0,0 +1,83 @@
+use io::Write;
+use std::{io, str};
+
+pub struct UnsafeScanner<R> {
+    reader: R,
+    buf_str: Vec<u8>,
+    buf_iter: str::SplitAsciiWhitespace<'static>,
+}
+
+impl<R: io::BufRead> UnsafeScanner<R> {
+    pub fn new(reader: R) -> Self {
+        Self {
+            reader,
+            buf_str: vec![],
+            buf_iter: "".split_ascii_whitespace(),
+        }
+    }
+
+    pub fn token<T: str::FromStr>(&mut self) -> T {
+        loop {
+            if let Some(token) = self.buf_iter.next() {
+                return token.parse().ok().expect("Failed parse");
+            }
+            self.buf_str.clear();
+            self.reader
+                .read_until(b'\n', &mut self.buf_str)
+                .expect("Failed read");
+            self.buf_iter = unsafe {
+                let slice = str::from_utf8_unchecked(&self.buf_str);
+                std::mem::transmute(slice.split_ascii_whitespace())
+            }
+        }
+    }
+}
+
+fn main() {
+    let (stdin, stdout) = (io::stdin(), io::stdout());
+    let mut scan = UnsafeScanner::new(stdin.lock());
+    let mut out = io::BufWriter::new(stdout.lock());
+
+    let (n, m) = (scan.token::<usize>(), scan.token::<usize>());
+    let mut cost = vec![vec![0; n + 1]; n + 1];
+
+    for _ in 0..m {
+        let (a, b, c) = (
+            scan.token::<usize>(),
+            scan.token::<usize>(),
+            scan.token::<usize>(),
+        );
+
+        if cost[a][b] != 0 {
+            cost[a][b] = std::cmp::min(cost[a][b], c);
+        } else {
+            cost[a][b] = c;
+        }
+    }
+
+    for k in 1..=n {
+        for i in 1..=n {
+            for j in 1..=n {
+                if i == j {
+                    continue;
+                }
+
+                if cost[i][k] != 0 && cost[k][j] != 0 {
+                    if cost[i][j] == 0 {
+                        cost[i][j] = cost[i][k] + cost[k][j];
+                    } else {
+                        cost[i][j] = std::cmp::min(cost[i][j], cost[i][k] + cost[k][j]);
+                    }
+                }
+            }
+        }
+    }
+
+    for i in 1..=n {
+        for j in 1..=n {
+            write!(out, "{} ", cost[i][j]).unwrap();
+        }
+
+        writeln!(out).unwrap();
+    }
+}

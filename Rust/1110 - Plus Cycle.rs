818e8906811e4a36ca806107aaf263512ec62bad
Chris Ohk <utilforever@gmail.com>
2022-03-29 01:48:37 +0900

    Upload 1110 - Plus Cycle

---
+++ Rust/1110 - Plus Cycle.rs
@@ -0,0 +1,57 @@
+use io::Write;
+use std::{io, str};
+
+pub struct UnsafeScanner<R> {
+    reader: R,
+    buf_str: Vec<u8>,
+    buf_iter: str::SplitAsciiWhitespace<'static>,
+}
+
+impl<R: io::BufRead> UnsafeScanner<R> {
+    pub fn new(reader: R) -> Self {
+        Self {
+            reader,
+            buf_str: vec![],
+            buf_iter: "".split_ascii_whitespace(),
+        }
+    }
+
+    pub fn token<T: str::FromStr>(&mut self) -> T {
+        loop {
+            if let Some(token) = self.buf_iter.next() {
+                return token.parse().ok().expect("Failed parse");
+            }
+            self.buf_str.clear();
+            self.reader
+                .read_until(b'\n', &mut self.buf_str)
+                .expect("Failed read");
+            self.buf_iter = unsafe {
+                let slice = str::from_utf8_unchecked(&self.buf_str);
+                std::mem::transmute(slice.split_ascii_whitespace())
+            }
+        }
+    }
+}
+
+fn main() {
+    let (stdin, stdout) = (io::stdin(), io::stdout());
+    let mut scan = UnsafeScanner::new(stdin.lock());
+    let mut out = io::BufWriter::new(stdout.lock());
+
+    let n = scan.token::<usize>();
+    let mut new_n = n;
+    let mut len_cycle = 0;
+
+    loop {
+        len_cycle += 1;
+
+        let temp = (new_n / 10) + (new_n % 10);
+        new_n = (new_n % 10) * 10 + (temp % 10);
+
+        if new_n == n {
+            break;
+        }
+    }
+
+    writeln!(out, "{}", len_cycle).unwrap();
+}

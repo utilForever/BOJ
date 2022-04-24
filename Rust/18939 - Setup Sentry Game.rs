2536ccbfd89f476dccd8b24e8f2590a5f96006f1
Chris Ohk <utilforever@gmail.com>
2022-04-24 23:34:16 +0900

    Upload 18939 - Setup Sentry Game

---
+++ Rust/18939 - Setup Sentry Game.rs
@@ -0,0 +1,56 @@
+use io::Write;
+use std::{cmp, io, str};
+
+pub struct UnsafeScanner<R> {
+    reader: R,
+    buf_str: Vec<u8>,
+    buf_iter: str::SplitAsciiWhitespace<'static>,
+}
+
+impl<R: io::BufRead> UnsafeScanner<R> {
+    pub fn new(reader: R) -> Self {
+        Self {
+            reader,
+            buf_str: vec![],
+            buf_iter: "".split_ascii_whitespace(),
+        }
+    }
+
+    pub fn token<T: str::FromStr>(&mut self) -> T {
+        loop {
+            if let Some(token) = self.buf_iter.next() {
+                return token.parse().ok().expect("Failed parse");
+            }
+            self.buf_str.clear();
+            self.reader
+                .read_until(b'\n', &mut self.buf_str)
+                .expect("Failed read");
+            self.buf_iter = unsafe {
+                let slice = str::from_utf8_unchecked(&self.buf_str);
+                std::mem::transmute(slice.split_ascii_whitespace())
+            }
+        }
+    }
+}
+
+fn main() {
+    let (stdin, stdout) = (io::stdin(), io::stdout());
+    let mut scan = UnsafeScanner::new(stdin.lock());
+    let mut out = io::BufWriter::new(stdout.lock());
+
+    let t = scan.token::<usize>();
+
+    for _ in 0..t {
+        let (n, m, k) = (
+            scan.token::<usize>(),
+            scan.token::<usize>(),
+            scan.token::<usize>(),
+        );
+
+        if cmp::max(n, m) < k * 2 {
+            writeln!(out, "Yuto").unwrap();
+        } else {
+            writeln!(out, "{}", if (n * m) % 2 == 0 { "Platina" } else { "Yuto" }).unwrap();
+        }
+    }
+}

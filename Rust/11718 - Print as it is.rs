2d38982edc94c4e0d7511c50a470d8dba4af6610
Chris Ohk <utilforever@gmail.com>
2022-05-05 18:49:29 +0900

    Upload 11718 - Print as it is

---
+++ Rust/11718 - Print as it is.rs
@@ -0,0 +1,15 @@
+use std::io;
+
+fn main() {
+    loop {
+        let mut s = String::new();
+        io::stdin().read_line(&mut s).unwrap();
+        s = s.trim().to_string();
+
+        if s.is_empty() {
+            break;
+        }
+
+        println!("{s}");
+    }
+}

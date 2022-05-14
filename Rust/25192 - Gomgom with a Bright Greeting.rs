ed3d0f5e45d1b1630d3db5f453c7cb5386dc20fb
Chris Ohk <utilforever@gmail.com>
2022-05-15 00:09:44 +0900

    Upload 25192 - Gomgom with a Bright Greeting

---
+++ Rust/25192 - Gomgom with a Bright Greeting.rs
@@ -0,0 +1,60 @@
+use io::Write;
+use std::{io, str, collections::HashSet};
+
+pub struct UnsafeScanner<R> {
+    reader: R,
+    buf_str: Vec<u8>,
+    buf_iter: str::SplitAsciiWhitespace<'static>,
+}
+
+impl<R: io::BufRead> UnsafeScanner<R> {
+    pub fn new(reader: R) -> Self {
+        Self {
+            reader,
+            buf_str: vec![],
+            buf_iter: "".split_ascii_whitespace(),
+        }
+    }
+
+    pub fn token<T: str::FromStr>(&mut self) -> T {
+        loop {
+            if let Some(token) = self.buf_iter.next() {
+                return token.parse().ok().expect("Failed parse");
+            }
+            self.buf_str.clear();
+            self.reader
+                .read_until(b'\n', &mut self.buf_str)
+                .expect("Failed read");
+            self.buf_iter = unsafe {
+                let slice = str::from_utf8_unchecked(&self.buf_str);
+                std::mem::transmute(slice.split_ascii_whitespace())
+            }
+        }
+    }
+}
+
+fn main() {
+    let (stdin, stdout) = (io::stdin(), io::stdout());
+    let mut scan = UnsafeScanner::new(stdin.lock());
+    let mut out = io::BufWriter::new(stdout.lock());
+
+    let n = scan.token::<usize>();
+    let mut nicknames = HashSet::new();
+    let mut ret = 0;
+
+    for _ in 0..n {
+        let s = scan.token::<String>();
+
+        if s == "ENTER" {
+            ret += nicknames.len();
+            nicknames.clear();
+        } else {
+            if !nicknames.contains(&s) {
+                nicknames.insert(s);
+            }
+        }
+    }
+
+    ret += nicknames.len();
+    writeln!(out, "{}", ret).unwrap();
+}

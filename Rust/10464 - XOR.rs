379b8971e6950c071d5b5d0e7170aa9f9a3ca616
Chris Ohk <utilforever@gmail.com>
2022-06-08 12:24:13 +0900

    Upload 10464 - XOR

---
+++ Rust/10464 - XOR.rs
@@ -0,0 +1,59 @@
+use io::Write;
+use std::{io, str};
+
+pub struct UnsafeScanner<R> {
+    reader: R,
+    buf_str: Vec<u8>,
+    buf_iter: str::SplitAsciiWhitespace<'static>,
+}
+
+impl<R: io::BufRead> UnsafeScanner<R> {
+    pub fn new(reader: R) -> Self {
+        Self {
+            reader,
+            buf_str: vec![],
+            buf_iter: "".split_ascii_whitespace(),
+        }
+    }
+
+    pub fn token<T: str::FromStr>(&mut self) -> T {
+        loop {
+            if let Some(token) = self.buf_iter.next() {
+                return token.parse().ok().expect("Failed parse");
+            }
+            self.buf_str.clear();
+            self.reader
+                .read_until(b'\n', &mut self.buf_str)
+                .expect("Failed read");
+            self.buf_iter = unsafe {
+                let slice = str::from_utf8_unchecked(&self.buf_str);
+                std::mem::transmute(slice.split_ascii_whitespace())
+            }
+        }
+    }
+}
+
+fn calculate(val: i64) -> i64 {
+    match val % 4 {
+        0 => val,
+        1 => 1,
+        2 => val + 1,
+        3 => 0,
+        _ => unreachable!(),
+    }
+}
+
+fn main() {
+    let (stdin, stdout) = (io::stdin(), io::stdout());
+    let mut scan = UnsafeScanner::new(stdin.lock());
+    let mut out = io::BufWriter::new(stdout.lock());
+
+    let t = scan.token::<usize>();
+
+    for _ in 0..t {
+        let (s, f) = (scan.token::<i64>(), scan.token::<i64>());
+        let ret = calculate(f) ^ calculate(s - 1);
+
+        writeln!(out, "{}", ret).unwrap();
+    }
+}

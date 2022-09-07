ba6822de6859cf5cbea366721ae3ca06432708e6
Chris Ohk <utilforever@gmail.com>
2022-09-07 23:39:08 +0900

    Upload 2720 - Quick Change

---
+++ Rust/2720 - Quick Change.rs
@@ -0,0 +1,52 @@
+use io::Write;
+use std::{io, str};
+
+pub struct UnsafeScanner<R> {
+    reader: R,
+    buf_str: Vec<u8>,
+    buf_iter: str::SplitAsciiWhitespace<'static>,
+}
+
+impl<R: io::BufRead> UnsafeScanner<R> {
+    pub fn new(reader: R) -> Self {
+        Self {
+            reader,
+            buf_str: vec![],
+            buf_iter: "".split_ascii_whitespace(),
+        }
+    }
+
+    pub fn token<T: str::FromStr>(&mut self) -> T {
+        loop {
+            if let Some(token) = self.buf_iter.next() {
+                return token.parse().ok().expect("Failed parse");
+            }
+            self.buf_str.clear();
+            self.reader
+                .read_until(b'\n', &mut self.buf_str)
+                .expect("Failed read");
+            self.buf_iter = unsafe {
+                let slice = str::from_utf8_unchecked(&self.buf_str);
+                std::mem::transmute(slice.split_ascii_whitespace())
+            }
+        }
+    }
+}
+
+fn main() {
+    let (stdin, stdout) = (io::stdin(), io::stdout());
+    let mut scan = UnsafeScanner::new(stdin.lock());
+    let mut out = io::BufWriter::new(stdout.lock());
+
+    let t = scan.token::<i64>();
+
+    for _ in 0..t {
+        let c = scan.token::<i64>();
+        let num_quarter = c / 25;
+        let num_dime = (c - num_quarter * 25) / 10;
+        let num_nickel = (c - num_quarter * 25 - num_dime * 10) / 5;
+        let num_penny = c - num_quarter * 25 - num_dime * 10 - num_nickel * 5;
+
+        writeln!(out, "{num_quarter} {num_dime} {num_nickel} {num_penny}").unwrap();
+    }
+}

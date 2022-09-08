3b29545beff05f307440a06041c641d07ff80d04
Chris Ohk <utilforever@gmail.com>
2022-09-08 23:59:17 +0900

    Upload 10824 - Four Numbers

---
+++ Rust/10824 - Four Numbers.rs
@@ -0,0 +1,54 @@
+use io::Write;
+use std::{io, str};
+
+pub struct UnsafeScanner<R> {
+    reader: R,
+    buf_str: Vec<u8>,
+    buf_iter: str::SplitAsciiWhitespace<'static>,
+}
+
+impl<R: io::BufRead> UnsafeScanner<R> {
+    pub fn new(reader: R) -> Self {
+        Self {
+            reader,
+            buf_str: vec![],
+            buf_iter: "".split_ascii_whitespace(),
+        }
+    }
+
+    pub fn token<T: str::FromStr>(&mut self) -> T {
+        loop {
+            if let Some(token) = self.buf_iter.next() {
+                return token.parse().ok().expect("Failed parse");
+            }
+            self.buf_str.clear();
+            self.reader
+                .read_until(b'\n', &mut self.buf_str)
+                .expect("Failed read");
+            self.buf_iter = unsafe {
+                let slice = str::from_utf8_unchecked(&self.buf_str);
+                std::mem::transmute(slice.split_ascii_whitespace())
+            }
+        }
+    }
+}
+
+fn main() {
+    let (stdin, stdout) = (io::stdin(), io::stdout());
+    let mut scan = UnsafeScanner::new(stdin.lock());
+    let mut out = io::BufWriter::new(stdout.lock());
+
+    let (a, b, c, d) = (
+        scan.token::<String>(),
+        scan.token::<String>(),
+        scan.token::<String>(),
+        scan.token::<String>(),
+    );
+    
+    let ab = a + &b;
+    let cd = c + &d;
+    let ab = ab.parse::<i64>().unwrap();
+    let cd = cd.parse::<i64>().unwrap();
+
+    writeln!(out, "{}", ab + cd).unwrap();
+}

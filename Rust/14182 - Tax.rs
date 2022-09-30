5cb44fd4d2bf16086e1912d3ebed4027ecc954b8
Chris Ohk <utilforever@gmail.com>
2022-09-30 23:46:23 +0900

    Upload 14182 - Tax

---
+++ Rust/14182 - Tax.rs
@@ -0,0 +1,61 @@
+use io::Write;
+use std::{io, str};
+
+pub struct UnsafeScanner<R> {
+    reader: R,
+    buf_str: Vec<u8>,
+    buf_iter: str::SplitAsciiWhitespace<'static>,
+}
+
+impl<R: io::BufRead> UnsafeScanner<R> {
+    pub fn new(reader: R) -> Self {
+        Self {
+            reader,
+            buf_str: vec![],
+            buf_iter: "".split_ascii_whitespace(),
+        }
+    }
+
+    pub fn token<T: str::FromStr>(&mut self) -> T {
+        loop {
+            if let Some(token) = self.buf_iter.next() {
+                return token.parse().ok().expect("Failed parse");
+            }
+            self.buf_str.clear();
+            self.reader
+                .read_until(b'\n', &mut self.buf_str)
+                .expect("Failed read");
+            self.buf_iter = unsafe {
+                let slice = str::from_utf8_unchecked(&self.buf_str);
+                std::mem::transmute(slice.split_ascii_whitespace())
+            }
+        }
+    }
+}
+
+fn main() {
+    let (stdin, stdout) = (io::stdin(), io::stdout());
+    let mut scan = UnsafeScanner::new(stdin.lock());
+    let mut out = io::BufWriter::new(stdout.lock());
+
+    loop {
+        let income = scan.token::<f64>();
+
+        if income == 0.0 {
+            break;
+        }
+
+        writeln!(
+            out,
+            "{}",
+            if income > 5000000.0 {
+                income * 0.8
+            } else if income > 1000000.0 {
+                income * 0.9
+            } else {
+                income
+            }
+        )
+        .unwrap();
+    }
+}

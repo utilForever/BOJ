5a5ae01622f3e94e6de752086617ea270426202b
Chris Ohk <utilforever@gmail.com>
2022-11-20 10:11:26 +0900

    Upload 5220 - Error Detection

---
+++ Rust/5220 - Error Detection.rs
@@ -0,0 +1,66 @@
+use io::Write;
+use std::{io, str};
+
+pub struct UnsafeScanner<R> {
+    reader: R,
+    buf_str: Vec<u8>,
+    buf_iter: str::SplitAsciiWhitespace<'static>,
+}
+
+impl<R: io::BufRead> UnsafeScanner<R> {
+    pub fn new(reader: R) -> Self {
+        Self {
+            reader,
+            buf_str: vec![],
+            buf_iter: "".split_ascii_whitespace(),
+        }
+    }
+
+    pub fn token<T: str::FromStr>(&mut self) -> T {
+        loop {
+            if let Some(token) = self.buf_iter.next() {
+                return token.parse().ok().expect("Failed parse");
+            }
+            self.buf_str.clear();
+            self.reader
+                .read_until(b'\n', &mut self.buf_str)
+                .expect("Failed read");
+            self.buf_iter = unsafe {
+                let slice = str::from_utf8_unchecked(&self.buf_str);
+                std::mem::transmute(slice.split_ascii_whitespace())
+            }
+        }
+    }
+}
+
+fn main() {
+    let (stdin, stdout) = (io::stdin(), io::stdout());
+    let mut scan = UnsafeScanner::new(stdin.lock());
+    let mut out = io::BufWriter::new(stdout.lock());
+
+    let n = scan.token::<i64>();
+
+    for _ in 0..n {
+        let (mut a, b) = (scan.token::<i64>(), scan.token::<i64>());
+        let mut count = 0;
+
+        while a > 0 {
+            if a % 2 == 1 {
+                count += 1;
+            }
+
+            a /= 2;
+        }
+
+        writeln!(
+            out,
+            "{}",
+            if (count % 2 == 0 && b == 0) || (count % 2 == 1 && b == 1) {
+                "Valid"
+            } else {
+                "Corrupt"
+            }
+        )
+        .unwrap();
+    }
+}

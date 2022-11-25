74b62737214d2877ee2cd989d0d4ee7b9603983b
Chris Ohk <utilforever@gmail.com>
2022-11-26 00:48:52 +0900

    Upload 13773 - Olympic Games

---
+++ Rust/13773 - Olympic Games.rs
@@ -0,0 +1,63 @@
+use io::Write;
+use std::{io, str};
+
+pub struct UnsafeScanner<R> {
+    reader: R,
+    buf_str: Vec<u8>,
+    buf_iter: str::SplitAsciiWhitespace<'static>,
+}
+
+impl<R: io::BufRead> UnsafeScanner<R> {
+    pub fn new(reader: R) -> Self {
+        Self {
+            reader,
+            buf_str: vec![],
+            buf_iter: "".split_ascii_whitespace(),
+        }
+    }
+
+    pub fn token<T: str::FromStr>(&mut self) -> T {
+        loop {
+            if let Some(token) = self.buf_iter.next() {
+                return token.parse().ok().expect("Failed parse");
+            }
+            self.buf_str.clear();
+            self.reader
+                .read_until(b'\n', &mut self.buf_str)
+                .expect("Failed read");
+            self.buf_iter = unsafe {
+                let slice = str::from_utf8_unchecked(&self.buf_str);
+                std::mem::transmute(slice.split_ascii_whitespace())
+            }
+        }
+    }
+}
+
+fn main() {
+    let (stdin, stdout) = (io::stdin(), io::stdout());
+    let mut scan = UnsafeScanner::new(stdin.lock());
+    let mut out = io::BufWriter::new(stdout.lock());
+
+    loop {
+        let year = scan.token::<i64>();
+
+        if year == 0 {
+            break;
+        }
+
+        writeln!(
+            out,
+            "{year} {}",
+            if year == 1916 || year == 1940 || year == 1944 {
+                "Games cancelled"
+            } else if year == 2024 || year == 2028 {
+                "No city yet chosen"
+            } else if year >= 1896 && year % 4 == 0 {
+                "Summer Olympics"
+            } else {
+                "No summer games"
+            }
+        )
+        .unwrap();
+    }
+}

fe2e9dce60957be0716ad742c95fcd150312d826
Chris Ohk <utilforever@gmail.com>
2022-12-19 03:18:05 +0900

    Upload 26264 - Bigdata, Security

---
+++ Rust/26264 - Bigdata, Security.rs
@@ -0,0 +1,70 @@
+use io::Write;
+use std::{io, str};
+
+pub struct UnsafeScanner<R> {
+    reader: R,
+    buf_str: Vec<u8>,
+    buf_iter: str::SplitAsciiWhitespace<'static>,
+}
+
+impl<R: io::BufRead> UnsafeScanner<R> {
+    pub fn new(reader: R) -> Self {
+        Self {
+            reader,
+            buf_str: vec![],
+            buf_iter: "".split_ascii_whitespace(),
+        }
+    }
+
+    pub fn token<T: str::FromStr>(&mut self) -> T {
+        loop {
+            if let Some(token) = self.buf_iter.next() {
+                return token.parse().ok().expect("Failed parse");
+            }
+            self.buf_str.clear();
+            self.reader
+                .read_until(b'\n', &mut self.buf_str)
+                .expect("Failed read");
+            self.buf_iter = unsafe {
+                let slice = str::from_utf8_unchecked(&self.buf_str);
+                std::mem::transmute(slice.split_ascii_whitespace())
+            }
+        }
+    }
+}
+
+fn main() {
+    let (stdin, stdout) = (io::stdin(), io::stdout());
+    let mut scan = UnsafeScanner::new(stdin.lock());
+    let mut out = io::BufWriter::new(stdout.lock());
+
+    let _ = scan.token::<i64>();
+    let s = scan.token::<String>();
+    let s = s.chars().collect::<Vec<_>>();
+    let mut cnt_security = 0;
+    let mut cnt_bigdata = 0;
+    let mut idx = 0;
+
+    while idx < s.len() {
+        if s[idx] == 's' {
+            cnt_security += 1;
+            idx += 8;
+        } else {
+            cnt_bigdata += 1;
+            idx += 7;
+        }
+    }
+
+    writeln!(
+        out,
+        "{}",
+        if cnt_security > cnt_bigdata {
+            "security!"
+        } else if cnt_security < cnt_bigdata {
+            "bigdata?"
+        } else {
+            "bigdata? security!"
+        }
+    )
+    .unwrap();
+}

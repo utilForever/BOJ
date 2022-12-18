97b25187c2b0c7cd0226dbe19a3a08603b248c7d
Chris Ohk <utilforever@gmail.com>
2022-12-18 14:06:53 +0900

    Upload 14337 - Helicopter

---
+++ Visual Basic/14337 - Helicopter.vb
@@ -0,0 +1,11 @@
+Module Main
+  Sub Main()
+    Console.WriteLine("      ===================")
+    Console.WriteLine("          ____||___")
+    Console.WriteLine("\ /      /       []\")
+    Console.WriteLine(" X=======           \__")
+    Console.WriteLine("/ \      \____________|")
+    Console.WriteLine("            ||  ||")
+    Console.WriteLine("         \-----------/")
+  End Sub
+End Module

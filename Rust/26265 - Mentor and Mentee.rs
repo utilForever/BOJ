447055eecadf6f1f95e4b570b9db5c2f9dcf00bd
Chris Ohk <utilforever@gmail.com>
2022-12-19 03:23:43 +0900

    Upload 26265 - Mentor and Mentee

---
+++ Rust/26265 - Mentor and Mentee.rs
@@ -0,0 +1,62 @@
+use io::Write;
+use std::{collections::HashMap, io, str};
+
+pub struct UnsafeScanner<R> {
+    reader: R,
+    buf_str: Vec<u8>,
+    buf_iter: str::SplitAsciiWhitespace<'static>,
+}
+
+impl<R: io::BufRead> UnsafeScanner<R> {
+    pub fn new(reader: R) -> Self {
+        Self {
+            reader,
+            buf_str: vec![],
+            buf_iter: "".split_ascii_whitespace(),
+        }
+    }
+
+    pub fn token<T: str::FromStr>(&mut self) -> T {
+        loop {
+            if let Some(token) = self.buf_iter.next() {
+                return token.parse().ok().expect("Failed parse");
+            }
+            self.buf_str.clear();
+            self.reader
+                .read_until(b'\n', &mut self.buf_str)
+                .expect("Failed read");
+            self.buf_iter = unsafe {
+                let slice = str::from_utf8_unchecked(&self.buf_str);
+                std::mem::transmute(slice.split_ascii_whitespace())
+            }
+        }
+    }
+}
+
+fn main() {
+    let (stdin, stdout) = (io::stdin(), io::stdout());
+    let mut scan = UnsafeScanner::new(stdin.lock());
+    let mut out = io::BufWriter::new(stdout.lock());
+
+    let n = scan.token::<i64>();
+    let mut pairs = HashMap::new();
+
+    for _ in 0..n {
+        let (mentor, mentee) = (scan.token::<String>(), scan.token::<String>());
+
+        pairs.entry(mentor).or_insert(Vec::new()).push(mentee);
+    }
+
+    let mut pairs = pairs.into_iter().collect::<Vec<_>>();
+    pairs.sort_by(|a, b| a.0.cmp(&b.0));
+
+    for pair in pairs.iter_mut() {
+        pair.1.sort_by(|a, b| b.cmp(&a));
+    }
+
+    for pair in pairs.iter() {
+        for mentee in pair.1.iter() {
+            writeln!(out, "{} {}", pair.0, mentee).unwrap();
+        }
+    }
+}

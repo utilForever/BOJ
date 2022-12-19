c85da36b58d928eb5777cb7329ac255d4ba8786e
Chris Ohk <utilforever@gmail.com>
2022-12-19 10:23:27 +0900

    Upload 2321 - Crowing

---
+++ Fortran/2321 - Crowing.f95
@@ -0,0 +1,12 @@
+      PROGRAM P1000
+
+      IMPLICIT NONE
+
+      PRINT '(a)', ',~'
+      PRINT '(a)', '( 9> //)'
+      PRINT '(a)', ' )(_///'
+      PRINT '(a)', '( \_>/'
+      PRINT '(a)', ' \__/'
+      PRINT '(a)', '  ^^'
+
+      END PROGRAM P1000

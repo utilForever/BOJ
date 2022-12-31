33308517fe4a493a6f9816778da1b5e87799c764
Chris Ohk <utilforever@gmail.com>
2023-01-01 02:44:48 +0900

    Upload 4909 - Judging Olympia

---
+++ Rust/4909 - Judging Olympia.rs
@@ -0,0 +1,62 @@
+use io::Write;
+use std::{io, str};
+
+pub struct UnsafeScanner<R> {
+    reader: R,
+    buf_str: Vec<u8>,
+    buf_iter: str::SplitAsciiWhitespace<'static>,
+}
+
+impl<R: io::BufRead> UnsafeScanner<R> {
+    pub fn new(reader: R) -> Self {
+        Self {
+            reader,
+            buf_str: vec![],
+            buf_iter: "".split_ascii_whitespace(),
+        }
+    }
+
+    pub fn token<T: str::FromStr>(&mut self) -> T {
+        loop {
+            if let Some(token) = self.buf_iter.next() {
+                return token.parse().ok().expect("Failed parse");
+            }
+            self.buf_str.clear();
+            self.reader
+                .read_until(b'\n', &mut self.buf_str)
+                .expect("Failed read");
+            self.buf_iter = unsafe {
+                let slice = str::from_utf8_unchecked(&self.buf_str);
+                std::mem::transmute(slice.split_ascii_whitespace())
+            }
+        }
+    }
+}
+
+fn main() {
+    let (stdin, stdout) = (io::stdin(), io::stdout());
+    let mut scan = UnsafeScanner::new(stdin.lock());
+    let mut out = io::BufWriter::new(stdout.lock());
+
+    loop {
+        let mut scores = vec![0; 6];
+
+        for i in 0..6 {
+            scores[i] = scan.token::<i64>();
+        }
+
+        if scores.iter().all(|&x| x == 0) {
+            break;
+        }
+
+        writeln!(
+            out,
+            "{}",
+            (scores.iter().sum::<i64>()
+                - scores.iter().min().unwrap()
+                - scores.iter().max().unwrap()) as f64
+                / 4.0
+        )
+        .unwrap();
+    }
+}

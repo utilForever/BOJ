d0d1bc53ba5999566884694eb3658ff2fba2fad1
Chris Ohk <utilforever@gmail.com>
2023-01-03 09:45:46 +0900

    Upload 27106 - Making Change

---
+++ Rust/27106 - Making Change.rs
@@ -0,0 +1,53 @@
+use io::Write;
+use std::{io, str};
+
+pub struct UnsafeScanner<R> {
+    reader: R,
+    buf_str: Vec<u8>,
+    buf_iter: str::SplitAsciiWhitespace<'static>,
+}
+
+impl<R: io::BufRead> UnsafeScanner<R> {
+    pub fn new(reader: R) -> Self {
+        Self {
+            reader,
+            buf_str: vec![],
+            buf_iter: "".split_ascii_whitespace(),
+        }
+    }
+
+    pub fn token<T: str::FromStr>(&mut self) -> T {
+        loop {
+            if let Some(token) = self.buf_iter.next() {
+                return token.parse().ok().expect("Failed parse");
+            }
+            self.buf_str.clear();
+            self.reader
+                .read_until(b'\n', &mut self.buf_str)
+                .expect("Failed read");
+            self.buf_iter = unsafe {
+                let slice = str::from_utf8_unchecked(&self.buf_str);
+                std::mem::transmute(slice.split_ascii_whitespace())
+            }
+        }
+    }
+}
+
+fn main() {
+    let (stdin, stdout) = (io::stdin(), io::stdout());
+    let mut scan = UnsafeScanner::new(stdin.lock());
+    let mut out = io::BufWriter::new(stdout.lock());
+
+    let mut p = scan.token::<i64>();
+    p = 100 - p;
+
+    let cent1 = p / 25;
+    p %= 25;
+    let cent2 = p / 10;
+    p %= 10;
+    let cent3 = p / 5;
+    p %= 5;
+    let cent4 = p;
+
+    writeln!(out, "{cent1} {cent2} {cent3} {cent4}").unwrap();
+}

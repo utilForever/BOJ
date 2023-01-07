6dd51da0d46b3991d460090e8a63c6a81a8ec0b7
Chris Ohk <utilforever@gmail.com>
2023-01-07 20:08:24 +0900

    Upload 9020 - Goldbach’s Conjecture

---
+++ Rust/9020 - Goldbach’s Conjecture.rs
@@ -0,0 +1,71 @@
+use io::Write;
+use std::{io, str};
+
+pub struct UnsafeScanner<R> {
+    reader: R,
+    buf_str: Vec<u8>,
+    buf_iter: str::SplitAsciiWhitespace<'static>,
+}
+
+impl<R: io::BufRead> UnsafeScanner<R> {
+    pub fn new(reader: R) -> Self {
+        Self {
+            reader,
+            buf_str: vec![],
+            buf_iter: "".split_ascii_whitespace(),
+        }
+    }
+
+    pub fn token<T: str::FromStr>(&mut self) -> T {
+        loop {
+            if let Some(token) = self.buf_iter.next() {
+                return token.parse().ok().expect("Failed parse");
+            }
+            self.buf_str.clear();
+            self.reader
+                .read_until(b'\n', &mut self.buf_str)
+                .expect("Failed read");
+            self.buf_iter = unsafe {
+                let slice = str::from_utf8_unchecked(&self.buf_str);
+                std::mem::transmute(slice.split_ascii_whitespace())
+            }
+        }
+    }
+}
+
+fn is_prime(n: i64) -> bool {
+    if n < 2 {
+        return true;
+    }
+
+    let mut i = 2;
+
+    while i * i <= n {
+        if n % i == 0 {
+            return false;
+        }
+
+        i += 1;
+    }
+
+    true
+}
+
+fn main() {
+    let (stdin, stdout) = (io::stdin(), io::stdout());
+    let mut scan = UnsafeScanner::new(stdin.lock());
+    let mut out = io::BufWriter::new(stdout.lock());
+
+    let t = scan.token::<i64>();
+
+    for _ in 0..t {
+        let n = scan.token::<i64>();
+
+        for i in (2..=(n / 2)).rev() {
+            if is_prime(i) && is_prime(n - i) {
+                writeln!(out, "{} {}", i, n - i).unwrap();
+                break;
+            }
+        }
+    }
+}

d6d8db73e7c47134c8bb78819564a82c205e0793
Chris Ohk <utilforever@gmail.com>
2023-02-12 23:17:49 +0900

    Upload 9773 - ID Key

---
+++ Rust/9773 - ID Key.rs
@@ -0,0 +1,65 @@
+use io::Write;
+use std::{io, str};
+
+pub struct UnsafeScanner<R> {
+    reader: R,
+    buf_str: Vec<u8>,
+    buf_iter: str::SplitAsciiWhitespace<'static>,
+}
+
+impl<R: io::BufRead> UnsafeScanner<R> {
+    pub fn new(reader: R) -> Self {
+        Self {
+            reader,
+            buf_str: vec![],
+            buf_iter: "".split_ascii_whitespace(),
+        }
+    }
+
+    pub fn token<T: str::FromStr>(&mut self) -> T {
+        loop {
+            if let Some(token) = self.buf_iter.next() {
+                return token.parse().ok().expect("Failed parse");
+            }
+            self.buf_str.clear();
+            self.reader
+                .read_until(b'\n', &mut self.buf_str)
+                .expect("Failed read");
+            self.buf_iter = unsafe {
+                let slice = str::from_utf8_unchecked(&self.buf_str);
+                std::mem::transmute(slice.split_ascii_whitespace())
+            }
+        }
+    }
+}
+
+fn main() {
+    let (stdin, stdout) = (io::stdin(), io::stdout());
+    let mut scan = UnsafeScanner::new(stdin.lock());
+    let mut out = io::BufWriter::new(stdout.lock());
+
+    let n = scan.token::<i64>();
+
+    for _ in 0..n {
+        let mut id = scan.token::<i64>();
+        let mut ret = (id % 1000) * 10;
+        let mut sum = 0;
+
+        while id > 0 {
+            sum += id % 10;
+            id /= 10;
+        }
+
+        ret += sum;
+
+        if ret < 1000 {
+            ret += 1000;
+        }
+
+        if ret >= 10000 {
+            ret -= 10000;
+        }
+
+        writeln!(out, "{:04}", ret).unwrap();
+    }
+}

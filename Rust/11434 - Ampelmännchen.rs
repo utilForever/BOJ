33f63d0b3843e3e0a44f5daf6f70f433f08df3b1
Chris Ohk <utilforever@gmail.com>
2023-02-18 19:19:42 +0900

    Upload 11434 - Ampelmännchen

---
+++ Rust/11434 - Ampelmännchen.rs
@@ -0,0 +1,69 @@
+use io::Write;
+use std::{io, str};
+
+pub struct UnsafeScanner<R> {
+    reader: R,
+    buf_str: Vec<u8>,
+    buf_iter: str::SplitAsciiWhitespace<'static>,
+}
+
+impl<R: io::BufRead> UnsafeScanner<R> {
+    pub fn new(reader: R) -> Self {
+        Self {
+            reader,
+            buf_str: vec![],
+            buf_iter: "".split_ascii_whitespace(),
+        }
+    }
+
+    pub fn token<T: str::FromStr>(&mut self) -> T {
+        loop {
+            if let Some(token) = self.buf_iter.next() {
+                return token.parse().ok().expect("Failed parse");
+            }
+            self.buf_str.clear();
+            self.reader
+                .read_until(b'\n', &mut self.buf_str)
+                .expect("Failed read");
+            self.buf_iter = unsafe {
+                let slice = str::from_utf8_unchecked(&self.buf_str);
+                std::mem::transmute(slice.split_ascii_whitespace())
+            }
+        }
+    }
+}
+
+fn main() {
+    let (stdin, stdout) = (io::stdin(), io::stdout());
+    let mut scan = UnsafeScanner::new(stdin.lock());
+    let mut out = io::BufWriter::new(stdout.lock());
+
+    let k = scan.token::<i64>();
+
+    for i in 1..=k {
+        let (n, w, e) = (
+            scan.token::<i64>(),
+            scan.token::<i64>(),
+            scan.token::<i64>(),
+        );
+        let mut ret = 0;
+
+        for _ in 0..n {
+            let (ww, we, ew, ee) = (
+                scan.token::<i64>(),
+                scan.token::<i64>(),
+                scan.token::<i64>(),
+                scan.token::<i64>(),
+            );
+
+            ret += (ww * w + ew * e).max(we * w + ee * e);
+        }
+
+        writeln!(out, "Data Set {i}:").unwrap();
+        writeln!(out, "{ret}").unwrap();
+
+        if i != k {
+            writeln!(out).unwrap();
+        }
+    }
+}

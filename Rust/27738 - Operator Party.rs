ac42c2e1dcb217dc30106197d618fb8f1ab2edb2
Chris Ohk <utilforever@gmail.com>
2023-03-12 14:10:26 +0900

    Upload 27738 - Operator Party

---
+++ Rust/27738 - Operator Party.rs
@@ -0,0 +1,80 @@
+use io::Write;
+use std::{io, str};
+
+pub struct UnsafeScanner<R> {
+    reader: R,
+    buf_str: Vec<u8>,
+    buf_iter: str::SplitAsciiWhitespace<'static>,
+}
+
+impl<R: io::BufRead> UnsafeScanner<R> {
+    pub fn new(reader: R) -> Self {
+        Self {
+            reader,
+            buf_str: vec![],
+            buf_iter: "".split_ascii_whitespace(),
+        }
+    }
+
+    pub fn token<T: str::FromStr>(&mut self) -> T {
+        loop {
+            if let Some(token) = self.buf_iter.next() {
+                return token.parse().ok().expect("Failed parse");
+            }
+            self.buf_str.clear();
+            self.reader
+                .read_until(b'\n', &mut self.buf_str)
+                .expect("Failed read");
+            self.buf_iter = unsafe {
+                let slice = str::from_utf8_unchecked(&self.buf_str);
+                std::mem::transmute(slice.split_ascii_whitespace())
+            }
+        }
+    }
+}
+
+fn main() {
+    let (stdin, stdout) = (io::stdin(), io::stdout());
+    let mut scan = UnsafeScanner::new(stdin.lock());
+    let mut out = io::BufWriter::new(stdout.lock());
+
+    let n = scan.token::<i64>();
+    let (a, b, c, d, e, f) = (
+        scan.token::<i64>(),
+        scan.token::<i64>(),
+        scan.token::<i64>(),
+        scan.token::<i64>(),
+        scan.token::<i64>(),
+        scan.token::<i64>(),
+    );
+    let start = n / f * f + 1;
+    let mut x = 0;
+
+    for i in start..=n {
+        if i % a == 0 {
+            x += i;
+        }
+
+        if i % b == 0 {
+            x %= i;
+        }
+
+        if i % c == 0 {
+            x &= i;
+        }
+
+        if i % d == 0 {
+            x ^= i;
+        }
+
+        if i % e == 0 {
+            x |= i;
+        }
+
+        if i % f == 0 {
+            x >>= i;
+        }
+    }
+
+    writeln!(out, "{x}").unwrap();
+}

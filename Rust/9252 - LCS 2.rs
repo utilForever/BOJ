4a6bc153e7dac90a3852eb7dac5b2554b9c644f2
Chris Ohk <utilforever@gmail.com>
2023-04-04 20:10:46 +0900

    Update 9252 - LCS 2

--- Rust/9252 - LCS 2.rs
+++ Rust/9252 - LCS 2.rs
@@ -1,13 +1,45 @@
-use std::io;
+use io::Write;
+use std::{io, str};
+
+pub struct UnsafeScanner<R> {
+    reader: R,
+    buf_str: Vec<u8>,
+    buf_iter: str::SplitAsciiWhitespace<'static>,
+}
+
+impl<R: io::BufRead> UnsafeScanner<R> {
+    pub fn new(reader: R) -> Self {
+        Self {
+            reader,
+            buf_str: vec![],
+            buf_iter: "".split_ascii_whitespace(),
+        }
+    }
+
+    pub fn token<T: str::FromStr>(&mut self) -> T {
+        loop {
+            if let Some(token) = self.buf_iter.next() {
+                return token.parse().ok().expect("Failed parse");
+            }
+            self.buf_str.clear();
+            self.reader
+                .read_until(b'\n', &mut self.buf_str)
+                .expect("Failed read");
+            self.buf_iter = unsafe {
+                let slice = str::from_utf8_unchecked(&self.buf_str);
+                std::mem::transmute(slice.split_ascii_whitespace())
+            }
+        }
+    }
+}
 
 fn main() {
-    let mut s = String::new();
-    io::stdin().read_line(&mut s).unwrap();
-    s = s.trim().to_string();
+    let (stdin, stdout) = (io::stdin(), io::stdout());
+    let mut scan = UnsafeScanner::new(stdin.lock());
+    let mut out = io::BufWriter::new(stdout.lock());
 
-    let mut t = String::new();
-    io::stdin().read_line(&mut t).unwrap();
-    t = t.trim().to_string();
+    let s = scan.token::<String>();
+    let t = scan.token::<String>();
 
     let mut lcs_count = vec![vec![0; 1001]; 1001];
     let mut i = 0;
@@ -61,6 +93,6 @@ fn main() {
         }
     }
 
-    println!("{}", lcs_count[s_len][t_len]);
-    println!("{}", lcs_str.chars().rev().collect::<String>());
+    writeln!(out, "{}", lcs_count[s_len][t_len]).unwrap();
+    writeln!(out, "{}", lcs_str.chars().rev().collect::<String>()).unwrap();
 }

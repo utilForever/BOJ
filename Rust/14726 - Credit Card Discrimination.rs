23806fbeb372a2b4df4360386d7d9ec66a96a3e2
Chris Ohk <utilforever@gmail.com>
2023-04-07 02:06:53 +0900

    Upload 14726 - Credit Card Discrimination

---
+++ Rust/14726 - Credit Card Discrimination.rs
@@ -0,0 +1,66 @@
+use io::Write;
+use std::{io, str};
+
+pub struct UnsafeScanner<R> {
+    reader: R,
+    buf_str: Vec<u8>,
+    buf_iter: str::SplitAsciiWhitespace<'static>,
+}
+
+impl<R: io::BufRead> UnsafeScanner<R> {
+    pub fn new(reader: R) -> Self {
+        Self {
+            reader,
+            buf_str: vec![],
+            buf_iter: "".split_ascii_whitespace(),
+        }
+    }
+
+    pub fn token<T: str::FromStr>(&mut self) -> T {
+        loop {
+            if let Some(token) = self.buf_iter.next() {
+                return token.parse().ok().expect("Failed parse");
+            }
+            self.buf_str.clear();
+            self.reader
+                .read_until(b'\n', &mut self.buf_str)
+                .expect("Failed read");
+            self.buf_iter = unsafe {
+                let slice = str::from_utf8_unchecked(&self.buf_str);
+                std::mem::transmute(slice.split_ascii_whitespace())
+            }
+        }
+    }
+}
+
+fn main() {
+    let (stdin, stdout) = (io::stdin(), io::stdout());
+    let mut scan = UnsafeScanner::new(stdin.lock());
+    let mut out = io::BufWriter::new(stdout.lock());
+
+    let t = scan.token::<i64>();
+
+    for _ in 0..t {
+        let mut num = scan.token::<i64>();
+        let mut is_even = false;
+        let mut ret = 0;
+
+        while num > 0 {
+            let mut r = num % 10;
+
+            if is_even {
+                r *= 2;
+
+                if r >= 10 {
+                    r -= 9;
+                }
+            }
+
+            ret += r;
+            num /= 10;
+            is_even = !is_even;
+        }
+
+        writeln!(out, "{}", if ret % 10 == 0 { "T" } else { "F" }).unwrap();
+    }
+}

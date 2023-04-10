84a12dca588f8bd8304394ea1c1f593973625d36
Chris Ohk <utilforever@gmail.com>
2023-04-11 02:34:30 +0900

    Update 3970 - Security Zone

--- Rust/3970 - Security Zone.rs
+++ Rust/3970 - Security Zone.rs
@@ -1,5 +1,5 @@
 use io::Write;
-use std::{cmp::Ordering, io, ops::Sub, str};
+use std::{cmp::Ordering, collections::BTreeSet, io, ops::Sub, str};
 use Ordering::{Equal, Greater, Less};
 
 pub struct UnsafeScanner<R> {
@@ -53,10 +53,6 @@ impl Disc {
         square > (r1 - r2).powi(2)
     }
 
-    pub fn get_leftmost_point(&self) -> (f64, f64) {
-        (self.center.0 - self.radius, self.center.1)
-    }
-
     pub fn compute_common_support_line_to(&self, other: &Disc) -> Edge {
         let (x1, y1, r1) = (self.center.0, self.center.1, self.radius);
         let (x2, y2, r2) = (other.center.0, other.center.1, other.radius);
@@ -67,7 +63,7 @@ impl Disc {
         let vy = (y2 - y1) / d;
 
         let c = (r1 - r2) / d;
-        let h = (1.0 - c.powi(2)).sqrt();
+        let h = (1.0 - c.powi(2)).max(0.0).sqrt();
 
         let nx = vx * c - vy * h;
         let ny = vx * h + vy * c;
@@ -87,8 +83,8 @@ impl Disc {
     pub fn compute_parallel_support_line(&self, line: &Edge) -> Edge {
         let line_unit = line.normalize().unit();
         let p = Point {
-            x: self.center.0 - (line_unit.p2.x - line_unit.p1.x),
-            y: self.center.1 - (line_unit.p2.y - line_unit.p1.y),
+            x: self.center.0 - self.radius * (line_unit.p2.x - line_unit.p1.x),
+            y: self.center.1 - self.radius * (line_unit.p2.y - line_unit.p1.y),
         };
         let od = Edge {
             p1: Point { x: p.x, y: p.y },
@@ -104,11 +100,203 @@ impl Disc {
             p2: od.p1,
         }
     }
+
+    fn calculate_ccw(&self, p1: Point, p2: Point, p3: Point) -> i64 {
+        let (x1, y1) = (p1.x as i64, p1.y as i64);
+        let (x2, y2) = (p2.x as i64, p2.y as i64);
+        let (x3, y3) = (p3.x as i64, p3.y as i64);
+
+        let res = (x2 - x1) * (y3 - y1) - (x3 - x1) * (y2 - y1);
+        if res > 0 {
+            1
+        } else if res < 0 {
+            -1
+        } else {
+            0
+        }
+    }
+
+    fn get_points(&self, other: &Disc, points: &mut Vec<Point>, val: f64) {
+        let sign = |val: f64| -> i64 { if val.abs() < 1e-10 { 0 } else if val > 0.0 { 1 } else { -1 } };
+        let calculate_ab = |x1: f64, y1: f64, x2: f64, y2: f64| -> Point {
+            let a = (y2 - y1) / (x1 - x2);
+            let b = x1 * a + y1;
+
+            Point { x: a, y: b }
+        };
+
+        let (x1, y1, r1) = (self.center.0, self.center.1, self.radius);
+        let (x2, y2, r2) = (other.center.0, other.center.1, other.radius);
+
+        if val == 0.0 {
+            if r1 + y1 == r2 + y2 {
+                points.push(Point { x: x1, y: r1 + y1 });
+                points.push(Point { x: x2, y: r2 + y2 });
+            }
+
+            if y1 - r1 == y2 - r2 {
+                points.push(Point { x: x1, y: y1 - r1 });
+                points.push(Point { x: x2, y: y2 - r2 });
+            }
+
+            return;
+        }
+
+        if r1 == r2 {
+            let val1 = val;
+            let val2 = r1 * (val * val + 1.0).sqrt() + y1 - val * x1;
+            let val3 = val;
+            let val4 = r2 * (val * val + 1.0).sqrt() + y2 - val * x2;
+            let val5 = -1.0 / val;
+            let val6 = -val5 * x1 + y1;
+            let val7 = -1.0 / val;
+            let val8 = -val7 * x2 + y2;
+
+            points.push(calculate_ab(val1, val2, val5, val6));
+            points.push(calculate_ab(val3, val4, val7, val8));
+
+            let val2 = -r1 * (val * val + 1.0).sqrt() + y1 - val * x1;
+            let val4 = -r2 * (val * val + 1.0).sqrt() + y2 - val * x2;
+
+            points.push(calculate_ab(val1, val2, val5, val6));
+            points.push(calculate_ab(val3, val4, val7, val8));
+        }
+
+        if sign(r1 - r2) == sign(val * (x1 - x2) - (y1 - y2)) {
+            let val1 = val;
+            let val2 = r1 * (val * val + 1.0).sqrt() + y1 - val * x1;
+            let val3 = val;
+            let val4 = r2 * (val * val + 1.0).sqrt() + y2 - val * x2;
+            let val5 = -1.0 / val;
+            let val6 = -val5 * x1 + y1;
+            let val7 = -1.0 / val;
+            let val8 = -val7 * x2 + y2;
+
+            points.push(calculate_ab(val1, val2, val5, val6));
+            points.push(calculate_ab(val3, val4, val7, val8));
+        }
+
+        if sign(r2 - r1) == sign(val * (x1 - x2) - (y1 - y2)) {
+            let val1 = val;
+            let val2 = -r1 * (val * val + 1.0).sqrt() + y1 - val * x1;
+            let val3 = val;
+            let val4 = -r2 * (val * val + 1.0).sqrt() + y2 - val * x2;
+            let val5 = -1.0 / val;
+            let val6 = -val5 * x1 + y1;
+            let val7 = -1.0 / val;
+            let val8 = -val7 * x2 + y2;
+
+            points.push(calculate_ab(val1, val2, val5, val6));
+            points.push(calculate_ab(val3, val4, val7, val8));
+        }
+    }
+
+    pub fn calculate_angle(&self, other: &Disc) -> (f64, Vec<Point>) {
+        let (x1, y1, r1) = (self.center.0, self.center.1, self.radius);
+        let (x2, y2, r2) = (other.center.0, other.center.1, other.radius);
+        let mut points = Vec::new();
+
+        if (x1 - x2).powi(2) + (y1 - y2).powi(2) <= (r1 - r2).powi(2) {
+            return (f64::MAX / 2.0, Vec::new());
+        }
+
+        if x1 == x2 && y1 == y2 && r1 == r2 {
+            return (f64::MAX / 2.0, Vec::new());
+        }
+
+        if x1 + r1 == x2 + r2 {
+            points.push(Point { x: x1 + r1, y: y1 });
+            points.push(Point { x: x2 + r2, y: y2 });
+        }
+
+        if x1 - r1 == x2 - r2 {
+            points.push(Point { x: x1 - r1, y: y1 });
+            points.push(Point { x: x2 - r2, y: y2 });
+        }
+
+        let val1 = (r1 - r2).powi(2) - (x1 - x2).powi(2);
+        let val2 = 2.0 * (x1 - x2) * (y1 - y2);
+        let val3 = (r1 - r2).powi(2) - (y1 - y2).powi(2);
+
+        if val1 == 0.0 {
+            if val2 != 0.0 {
+                self.get_points(other, &mut points, -val3 / val2);
+            }
+        } else if val2 * val2 / 4.0 >= val1 * val3 {
+            let val = (-val2 + (val2 * val2 - 4.0 * val1 * val3).sqrt()) / (2.0 * val1);
+            self.get_points(other, &mut points, val);
+            let val = (-val2 - (val2 * val2 - 4.0 * val1 * val3).sqrt()) / (2.0 * val1);
+            self.get_points(other, &mut points, val);
+        }
+
+        while points.len() > 4 {
+            points.pop();
+        }
+
+        let mut points_new = Vec::new();
+
+        if points.len() == 4 {
+            if self.calculate_ccw(points[0], points[1], points[2]) == -1 {
+                points_new.push(points[0]);
+                points_new.push(points[1]);
+            } else {
+                points_new.push(points[2]);
+                points_new.push(points[3]);
+            }
+        } else if points.len() == 2 {
+            points_new.push(points[0]);
+            points_new.push(points[1]);
+        }
+
+        points = points_new;
+
+        if points[0].x == points[1].x {
+            if points[0].y < points[1].y {
+                return (1.5 * std::f64::consts::PI, points);
+            } else {
+                return (0.5 * std::f64::consts::PI, points);
+            }
+        }
+
+        if points[0].y == points[1].y {
+            if points[0].x < points[1].x {
+                return (2.0 * std::f64::consts::PI, points);
+            } else {
+                return (std::f64::consts::PI, points);
+            }
+        }
+
+        let val = ((points[1].y - points[0].y) / (points[1].x - points[0].x)).abs();
+
+        if points[0].x < points[1].x && points[0].y < points[1].y {
+            (2.0 * std::f64::consts::PI - val.atan(), points)
+        } else if points[0].x > points[1].x && points[0].y < points[1].y {
+            (std::f64::consts::PI + val.atan(), points)
+        } else if points[0].x < points[1].x && points[0].y > points[1].y {
+            (val.atan(), points)
+        } else {
+            (std::f64::consts::PI - val.atan(), points)
+        }
+    }
 }
+
+impl Eq for Disc {}
 
 impl PartialEq for Disc {
     fn eq(&self, other: &Self) -> bool {
         self.center == other.center && self.radius == other.radius
+    }
+}
+
+impl Ord for Disc {
+    fn cmp(&self, other: &Self) -> Ordering {
+        if self.radius < other.radius {
+            Less
+        } else if self.radius > other.radius {
+            Greater
+        } else {
+            Equal
+        }
     }
 }
 
@@ -130,12 +318,6 @@ struct Point {
     y: f64,
 }
 
-impl Point {
-    fn new(x: f64, y: f64) -> Self {
-        Self { x, y }
-    }
-}
-
 impl Sub for Point {
     type Output = Self;
 
@@ -145,57 +327,6 @@ impl Sub for Point {
             y: self.y - other.y,
         }
     }
-}
-
-fn is_ccw(p1: &Point, p2: &Point, p3: &Point) -> bool {
-    let area = (p2.x - p1.x) * (p3.y - p1.y) - (p2.y - p1.y) * (p3.x - p1.x);
-    area > 0.0
-}
-
-fn convex_hull(mut points: Vec<Point>) -> Vec<Point> {
-    let n = points.len();
-
-    if n <= 1 {
-        return points;
-    }
-
-    points.sort_by(|a, b| {
-        if a.y == b.y {
-            a.x.partial_cmp(&b.x).unwrap()
-        } else {
-            a.y.partial_cmp(&b.y).unwrap()
-        }
-    });
-
-    let mut hull = Vec::with_capacity(n);
-
-    for i in 0..n {
-        while hull.len() >= 2 {
-            let m = hull.len() - 1;
-            if is_ccw(&hull[m - 1], &hull[m], &points[i]) {
-                break;
-            }
-            hull.pop();
-        }
-        hull.push(points[i]);
-    }
-
-    let lower_hull_len = hull.len();
-
-    for i in (0..(n - 1)).rev() {
-        while hull.len() >= lower_hull_len + 1 {
-            let m = hull.len() - 1;
-            if is_ccw(&hull[m - 1], &hull[m], &points[i]) {
-                break;
-            }
-            hull.pop();
-        }
-        hull.push(points[i]);
-    }
-
-    hull.pop();
-
-    hull
 }
 
 #[derive(Clone, Debug, Default)]
@@ -250,7 +381,7 @@ impl Edge {
         let theta = radians_to_degrees(-dy.atan2(dx));
         let theta_norm = if theta < 0.0 { theta + 360.0 } else { theta };
 
-        if (theta_norm - 360.0).abs() < 1e-6 {
+        if (theta_norm - 360.0).abs() < 1e-12 {
             0.0
         } else {
             theta_norm
@@ -263,7 +394,7 @@ impl Edge {
         let delta = a2 - a1;
         let delta_norm = if delta < 0.0 { delta + 360.0 } else { delta };
 
-        if (delta_norm - 360.0).abs() < 1e-6 {
+        if (delta_norm - 360.0).abs() < 1e-12 {
             0.0
         } else {
             delta_norm
@@ -279,27 +410,50 @@ struct ConvexHull {
 
 impl ConvexHull {
     fn length(&self) -> f64 {
-        let mut points = Vec::new();
-
-        for disc in &self.discs {
-            let mut angle = 0.0_f64;
-
-            for _ in 0..=5000 {
-                points.push(Point::new(
-                    disc.center.0 + disc.radius * angle.cos(),
-                    disc.center.1 + disc.radius * angle.sin(),
-                ));
-
-                angle += std::f64::consts::PI / 2500.0;
-            }
+        if self.discs.len() == 1 {
+            return 2.0 * std::f64::consts::PI * self.discs[0].radius;
         }
 
-        let hull = convex_hull(points);
+        let mut set = BTreeSet::new();
+        let mut circles = Vec::new();
+
+        for i in 0..self.discs.len() {
+            let j = (i + 1) % self.discs.len();
+
+            if set
+                .iter()
+                .find(|(a, b)| *a == self.discs[i] && *b == self.discs[j])
+                .is_some()
+            {
+                break;
+            }
+
+            set.insert((self.discs[i].clone(), self.discs[j].clone()));
+            circles.push(self.discs[i].clone());
+        }
+
+        println!("{:?}", self.discs);
+        println!("{:?}", circles);
+
         let mut ret = 0.0;
 
-        for i in 0..hull.len() {
-            let j = (i + 1) % hull.len();
-            ret += (hull[i].x - hull[j].x).hypot(hull[i].y - hull[j].y);
+        for j in 0..circles.len() {
+            let i = (j + circles.len() - 1) % circles.len();
+            let k = (j + 1) % circles.len();
+
+            let (angle1, _) = circles[i].calculate_angle(&circles[j]);
+            let (angle2, points2) = circles[j].calculate_angle(&circles[k]);
+            let mut angle = angle2 - angle1;
+
+            if angle < 0.0 {
+                angle += 2.0 * std::f64::consts::PI;
+            }
+
+            ret += angle * circles[j].radius;
+
+            if !points2.is_empty() {
+                ret += (points2[0].x - points2[1].x).hypot(points2[0].y - points2[1].y);
+            }
         }
 
         ret
@@ -316,81 +470,33 @@ impl ConvexHullAlgorithm {
         self.discs.push(disc);
     }
 
-    pub fn find(&self, start: usize, end: usize) -> ConvexHull {
-        let len = end - start + 1;
+    pub fn sort(&mut self) {
+        self.discs.sort_by(|a, b| {
+            if a.center.1 + a.radius == b.center.1 + b.radius {
+                a.center.0.partial_cmp(&b.center.0).unwrap()
+            } else {
+                (a.center.1 + a.radius).partial_cmp(&(b.center.1 + b.radius)).unwrap()
+            }
+        });
+    }
 
-        if len == 1 {
+    pub fn find(&self, start: usize, end: usize) -> ConvexHull {
+        if start == end {
             return ConvexHull {
                 discs: self.discs.clone(),
                 edges: Vec::new(),
             };
         }
 
-        if len == 2 {
-            return self.merge_two_discs(&self.discs[0], &self.discs[1]);
-        }
-
         let mid = (start + end) / 2;
         let p = self.find(start, mid);
         let q = self.find(mid + 1, end);
-
-        if p.discs.len() == 1 && q.discs.len() == 1 {
-            return self.merge_two_discs(&p.discs[0], &q.discs[0]);
-        }
 
         self.merge(&p, &q)
     }
 }
 
 impl ConvexHullAlgorithm {
-    fn merge_two_discs(&self, disc1: &Disc, disc2: &Disc) -> ConvexHull {
-        let x = disc1.clone();
-        let y = disc2.clone();
-
-        if !x.exist_common_support_line(&y) {
-            if x.partial_cmp(&y) == Some(Less) {
-                return ConvexHull {
-                    discs: vec![x],
-                    edges: Vec::new(),
-                };
-            } else {
-                return ConvexHull {
-                    discs: vec![y],
-                    edges: Vec::new(),
-                };
-            }
-        }
-
-        let p1 = x.get_leftmost_point().0;
-        let p2 = y.get_leftmost_point().0;
-        let e1 = x.compute_common_support_line_to(&y);
-        let e2 = y.compute_common_support_line_to(&x);
-
-        if p1 < p2 {
-            ConvexHull {
-                discs: vec![x.clone(), y, x],
-                edges: vec![e1, e2],
-            }
-        } else if p1 > p2 {
-            ConvexHull {
-                discs: vec![y.clone(), x, y],
-                edges: vec![e2, e1],
-            }
-        } else {
-            if x.center.1 < y.center.1 {
-                ConvexHull {
-                    discs: vec![x.clone(), y, x],
-                    edges: vec![e1, e2],
-                }
-            } else {
-                ConvexHull {
-                    discs: vec![y.clone(), x, y],
-                    edges: vec![e2, e1],
-                }
-            }
-        }
-    }
-
     fn triangle_orientation(&self, p: &Point, q: &Point, r: &Point) -> f64 {
         (q.x - p.x) * (r.y - p.y) - (q.y - p.y) * (r.x - p.x)
     }
@@ -438,7 +544,7 @@ impl ConvexHullAlgorithm {
     }
 
     fn add(&self, list: &mut Vec<Disc>, disc: &Disc) -> bool {
-        if list.is_empty() || list.last().unwrap() != disc {
+        if list.is_empty() || *list.last().unwrap() != *disc {
             list.push(disc.clone());
             true
         } else {
@@ -601,7 +707,8 @@ fn main() {
     let mut scan = UnsafeScanner::new(stdin.lock());
     let mut out = io::BufWriter::new(stdout.lock());
 
-    let c = scan.token::<i64>();
+    // let c = scan.token::<i64>();
+    let c = 1;
 
     for _ in 0..c {
         let n = scan.token::<usize>();
@@ -611,11 +718,13 @@ fn main() {
             let (x, y, r) = (
                 scan.token::<f64>(),
                 scan.token::<f64>(),
-                scan.token::<f64>() + 10.0,
+                scan.token::<f64>(),
             );
             convex_hull_algorithm.add_disc(Disc::new((x, y), r));
         }
 
-        writeln!(out, "{:.7}", convex_hull_algorithm.find(1, n).length()).unwrap();
+        convex_hull_algorithm.sort();
+
+        writeln!(out, "{:.7}", convex_hull_algorithm.find(0, n - 1).length()).unwrap();
     }
 }

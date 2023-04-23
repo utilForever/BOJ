77a98e532214f2ea751a15cb4a0342b295aae05d
Chris Ohk <utilforever@gmail.com>
2023-04-23 23:02:32 +0900

    Update 6171 - Ddangddaemokgi

--- Rust/6171 - Ddangddaemokgi.rs
+++ Rust/6171 - Ddangddaemokgi.rs
@@ -33,30 +33,30 @@ impl<R: io::BufRead> UnsafeScanner<R> {
     }
 }
 
-fn cross(stack: &Vec<(i64, i64)>, x: i64, y: i64) -> f64 {
-    let t1 = (stack[y as usize].1 - stack[x as usize].1) as f64;
-    let t2 = (stack[x as usize].0 - stack[y as usize].0) as f64;
+fn cross(stack: &Vec<(i64, i64)>, x: usize, y: usize) -> f64 {
+    let t1 = stack[y].1 - stack[x].1;
+    let t2 = stack[x].0 - stack[y].0;
 
-    t1 / t2
+    t1 as f64 / t2 as f64
 }
 
-fn insert(stack: &mut Vec<(i64, i64)>, size: &mut i64, x: i64, y: i64) {
-    stack[*size as usize] = (x, y);
+fn insert(stack: &mut Vec<(i64, i64)>, size: &mut usize, x: i64, y: i64) {
+    stack[*size] = (x, y);
 
     while *size > 1 && cross(stack, *size - 2, *size - 1) > cross(stack, *size - 1, *size) {
-        stack.swap(*size as usize - 1, *size as usize);
+        stack[*size - 1] = stack[*size];
         *size -= 1;
     }
 
     *size += 1;
 }
 
-fn query(stack: &Vec<(i64, i64)>, size: i64, last: &mut i64, x: i64) -> i64 {
-    while *last + 1 < size && cross(stack, *last, *last + 1) <= x as f64 {
+fn query(stack: &Vec<(i64, i64)>, size: &usize, last: &mut usize, x: i64) -> i64 {
+    while *last + 1 < *size && cross(stack, *last, *last + 1) <= x as f64 {
         *last += 1;
     }
 
-    x * stack[*last as usize].0 + stack[*last as usize].1
+    x * stack[*last].0 as i64 + stack[*last].1 as i64
 }
 
 fn main() {
@@ -64,38 +64,38 @@ fn main() {
     let mut scan = UnsafeScanner::new(stdin.lock());
     let mut out = io::BufWriter::new(stdout.lock());
 
-    let mut n = scan.token::<usize>();
-    let mut arr = vec![(0, 0); n];
+    let n = scan.token::<usize>();
+    let mut areas = vec![(0, 0); n];
 
     for i in 0..n {
-        arr[i].0 = scan.token::<i64>();
-        arr[i].1 = scan.token::<i64>();
+        areas[i] = (scan.token::<i64>(), scan.token::<i64>());
     }
 
-    arr.sort();
-    arr.reverse();
+    areas.sort();
+    areas.reverse();
 
-    let mut new_arr = Vec::new();
-    new_arr.push(arr[0]);
+    let mut areas_new = Vec::new();
+    areas_new.push(areas[0]);
 
     for i in 1..n {
-        if new_arr.last().unwrap().1 >= arr[i].1 {
+        if areas_new.last().unwrap().1 >= areas[i].1 {
             continue;
         }
 
-        new_arr.push(arr[i]);
+        areas_new.push(areas[i]);
     }
 
-    new_arr.sort();
-    n = new_arr.len();
+    areas_new.sort();
+    let n = areas_new.len();
 
     let mut stack = vec![(0, 0); n + 1];
+    let mut size = 0;
     let mut cost = vec![0; n + 1];
-    let (mut size, mut last) = (0, 0);
+    let mut last = 0;
 
     for i in 1..=n {
-        insert(&mut stack, &mut size, new_arr[i - 1].1, cost[i - 1]);
-        cost[i] = query(&stack, size, &mut last, new_arr[i - 1].0);
+        insert(&mut stack, &mut size, areas_new[i - 1].1, cost[i - 1]);
+        cost[i] = query(&stack, &size, &mut last, areas_new[i - 1].0);
     }
 
     writeln!(out, "{}", cost[n]).unwrap();

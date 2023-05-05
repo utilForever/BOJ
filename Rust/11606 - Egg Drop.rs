caf204a1b894c218a6da3bab57f241d587921d44
Chris Ohk <utilforever@gmail.com>
2023-05-05 16:08:25 +0900

    Upload 11606 - Egg Drop

---
+++ Rust/11606 - Egg Drop.rs
@@ -0,0 +1,56 @@
+use io::Write;
+use std::{io, str};
+
+pub struct UnsafeScanner<R> {
+    reader: R,
+    buf_str: Vec<u8>,
+    buf_iter: str::SplitAsciiWhitespace<'static>,
+}
+
+impl<R: io::BufRead> UnsafeScanner<R> {
+    pub fn new(reader: R) -> Self {
+        Self {
+            reader,
+            buf_str: vec![],
+            buf_iter: "".split_ascii_whitespace(),
+        }
+    }
+
+    pub fn token<T: str::FromStr>(&mut self) -> T {
+        loop {
+            if let Some(token) = self.buf_iter.next() {
+                return token.parse().ok().expect("Failed parse");
+            }
+            self.buf_str.clear();
+            self.reader
+                .read_until(b'\n', &mut self.buf_str)
+                .expect("Failed read");
+            self.buf_iter = unsafe {
+                let slice = str::from_utf8_unchecked(&self.buf_str);
+                std::mem::transmute(slice.split_ascii_whitespace())
+            }
+        }
+    }
+}
+
+fn main() {
+    let (stdin, stdout) = (io::stdin(), io::stdout());
+    let mut scan = UnsafeScanner::new(stdin.lock());
+    let mut out = io::BufWriter::new(stdout.lock());
+
+    let (n, k) = (scan.token::<i64>(), scan.token::<i64>());
+    let mut min = k;
+    let mut max = 1;
+
+    for _ in 0..n {
+        let (floor, result) = (scan.token::<i64>(), scan.token::<String>());
+
+        if result == "SAFE" {
+            max = max.max(floor);
+        } else {
+            min = min.min(floor);
+        }
+    }
+
+    writeln!(out, "{} {}", max + 1, min - 1).unwrap();
+}

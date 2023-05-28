c435dd8111b3a62ed94465163e08b181b200f79a
Chris Ohk <utilforever@gmail.com>
2023-05-28 21:11:06 +0900

    Upload 20152 - Game Addiction (Catalan)

---
+++ Rust/20152 - Game Addiction (Catalan).rs
@@ -0,0 +1,67 @@
+use io::Write;
+use std::{io, str};
+
+pub struct UnsafeScanner<R> {
+    reader: R,
+    buf_str: Vec<u8>,
+    buf_iter: str::SplitAsciiWhitespace<'static>,
+}
+
+impl<R: io::BufRead> UnsafeScanner<R> {
+    pub fn new(reader: R) -> Self {
+        Self {
+            reader,
+            buf_str: vec![],
+            buf_iter: "".split_ascii_whitespace(),
+        }
+    }
+
+    pub fn token<T: str::FromStr>(&mut self) -> T {
+        loop {
+            if let Some(token) = self.buf_iter.next() {
+                return token.parse().ok().expect("Failed parse");
+            }
+            self.buf_str.clear();
+            self.reader
+                .read_until(b'\n', &mut self.buf_str)
+                .expect("Failed read");
+            self.buf_iter = unsafe {
+                let slice = str::from_utf8_unchecked(&self.buf_str);
+                std::mem::transmute(slice.split_ascii_whitespace())
+            }
+        }
+    }
+}
+
+fn binomial_coefficient(n: u128, mut k: u128) -> u128 {
+    let mut ret = 1;
+
+    if k > n - k {
+        k = n - k;
+    }
+
+    for i in 0..k {
+        ret *= n - i;
+        ret /= i + 1;
+    }
+
+    ret
+}
+
+fn catalan(n: u128) -> u128 {
+    let c = binomial_coefficient(2 * n, n);
+
+    c / (n + 1)
+}
+
+// Reference: https://www.geeksforgeeks.org/program-nth-catalan-number/
+fn main() {
+    let (stdin, stdout) = (io::stdin(), io::stdout());
+    let mut scan = UnsafeScanner::new(stdin.lock());
+    let mut out = io::BufWriter::new(stdout.lock());
+
+    let (mut h, n) = (scan.token::<i128>(), scan.token::<i128>());
+    h = (h - n).abs();
+
+    writeln!(out, "{}", catalan(h as u128)).unwrap();
+}

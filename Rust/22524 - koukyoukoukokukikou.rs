d958e75a88862815cd20560e63a7892fcabe3619
Chris Ohk <utilforever@gmail.com>
2023-06-20 18:20:18 +0900

    Upload 22524 - koukyoukoukokukikou

---
+++ Rust/22524 - koukyoukoukokukikou.rs
@@ -0,0 +1,67 @@
+use io::Write;
+use std::{io, str};
+
+pub struct UnsafeScanner<R> {
+    reader: R,
+    buf_str: Vec<u8>,
+    buf_iter: str::SplitAsciiWhitespace<'static>,
+}
+
+impl<R: io::BufRead> UnsafeScanner<R> {
+    pub fn new(reader: R) -> Self {
+        Self {
+            reader,
+            buf_str: vec![],
+            buf_iter: "".split_ascii_whitespace(),
+        }
+    }
+
+    pub fn token<T: str::FromStr>(&mut self) -> T {
+        loop {
+            if let Some(token) = self.buf_iter.next() {
+                return token.parse().ok().expect("Failed parse");
+            }
+            self.buf_str.clear();
+            self.reader
+                .read_until(b'\n', &mut self.buf_str)
+                .expect("Failed read");
+            self.buf_iter = unsafe {
+                let slice = str::from_utf8_unchecked(&self.buf_str);
+                std::mem::transmute(slice.split_ascii_whitespace())
+            }
+        }
+    }
+}
+
+fn main() {
+    let (stdin, stdout) = (io::stdin(), io::stdout());
+    let mut scan = UnsafeScanner::new(stdin.lock());
+    let mut out = io::BufWriter::new(stdout.lock());
+
+    let left = ['q', 'w', 'e', 'r', 't', 'a', 's', 'd', 'f', 'g', 'z', 'x', 'c', 'v', 'b'];
+
+    loop {
+        let s = scan.token::<String>();
+
+        if s == "#" {
+            break;
+        }
+
+        let s = s.chars().collect::<Vec<_>>();
+        let mut direction = None;
+        let mut ret = 0;
+
+        for c in s.iter() {
+            let is_left = left.contains(c);
+
+            if direction.is_none() {
+                direction = Some(is_left);
+            } else if direction.unwrap() != is_left {
+                ret += 1;
+                direction = Some(is_left);
+            }
+        }
+
+        writeln!(out, "{ret}").unwrap();
+    }
+}

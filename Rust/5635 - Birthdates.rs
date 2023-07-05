c08405aae7163ff3785020410e2d3982360ce10a
Chris Ohk <utilforever@gmail.com>
2023-07-05 22:26:00 +0900

    Upload 5635 - Birthdates

---
+++ Rust/5635 - Birthdates.rs
@@ -0,0 +1,57 @@
+use io::Write;
+use std::{io, str};
+
+pub struct UnsafeScanner<R> {
+    reader: R,
+    buf_str: Vec<u8>,
+    buf_iter: str::SplitAsciiWhitespace<'static>,
+}
+
+impl<R: io::BufRead> UnsafeScanner<R> {
+    pub fn new(reader: R) -> Self {
+        Self {
+            reader,
+            buf_str: vec![],
+            buf_iter: "".split_ascii_whitespace(),
+        }
+    }
+
+    pub fn token<T: str::FromStr>(&mut self) -> T {
+        loop {
+            if let Some(token) = self.buf_iter.next() {
+                return token.parse().ok().expect("Failed parse");
+            }
+            self.buf_str.clear();
+            self.reader
+                .read_until(b'\n', &mut self.buf_str)
+                .expect("Failed read");
+            self.buf_iter = unsafe {
+                let slice = str::from_utf8_unchecked(&self.buf_str);
+                std::mem::transmute(slice.split_ascii_whitespace())
+            }
+        }
+    }
+}
+
+fn main() {
+    let (stdin, stdout) = (io::stdin(), io::stdout());
+    let mut scan = UnsafeScanner::new(stdin.lock());
+    let mut out = io::BufWriter::new(stdout.lock());
+
+    let n = scan.token::<usize>();
+    let mut people = vec![(0, 0, 0, String::new()); n];
+
+    for i in 0..n {
+        let name = scan.token::<String>();
+        let day = scan.token::<i64>();
+        let month = scan.token::<i64>();
+        let year = scan.token::<i64>();
+
+        people[i] = (year, month, day, name);
+    }
+
+    people.sort();
+
+    writeln!(out, "{}", people.last().unwrap().3).unwrap();
+    writeln!(out, "{}", people.first().unwrap().3).unwrap();
+}

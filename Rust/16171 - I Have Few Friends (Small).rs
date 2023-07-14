ec7f1995eee610548cdc796bca735c0571b0f10e
Chris Ohk <utilforever@gmail.com>
2023-07-14 13:39:55 +0900

    Update 16171 - I Have Few Friends (Small)

--- Rust/16171 - I Have Few Friends (Small).rs
+++ Rust/16171 - I Have Few Friends (Small).rs
@@ -1,17 +1,49 @@
-use std::io;
+use io::Write;
+use std::{io, str};
+
+pub struct UnsafeScanner<R> {
+    reader: R,
+    buf_str: Vec<u8>,
+    buf_iter: str::SplitAsciiWhitespace<'static>,
+}
+
+impl<R: io::BufRead> UnsafeScanner<R> {
+    pub fn new(reader: R) -> Self {
+        Self {
+            reader,
+            buf_str: vec![],
+            buf_iter: "".split_ascii_whitespace(),
+        }
+    }
+
+    pub fn token<T: str::FromStr>(&mut self) -> T {
+        loop {
+            if let Some(token) = self.buf_iter.next() {
+                return token.parse().ok().expect("Failed parse");
+            }
+            self.buf_str.clear();
+            self.reader
+                .read_until(b'\n', &mut self.buf_str)
+                .expect("Failed read");
+            self.buf_iter = unsafe {
+                let slice = str::from_utf8_unchecked(&self.buf_str);
+                std::mem::transmute(slice.split_ascii_whitespace())
+            }
+        }
+    }
+}
 
 fn main() {
-    let mut s = String::new();
-    io::stdin().read_line(&mut s).unwrap();
+    let (stdin, stdout) = (io::stdin(), io::stdout());
+    let mut scan = UnsafeScanner::new(stdin.lock());
+    let mut out = io::BufWriter::new(stdout.lock());
 
-    let mut k = String::new();
-    io::stdin().read_line(&mut k).unwrap();
-
+    let s = scan.token::<String>();
     let s = s
         .chars()
         .filter(|&c| c.is_ascii_alphabetic())
         .collect::<String>();
-    let k = k.trim().to_string();
+    let k = scan.token::<String>();
 
     let s_chars = s.as_bytes();
     let s_len = s_chars.len();
@@ -41,7 +73,7 @@ fn main() {
 
         if s_chars[i] == k_chars[cmp] {
             if cmp == k_len - 1 {
-                println!("1");
+                writeln!(out, "1").unwrap();
                 return;
             } else {
                 cmp += 1;
@@ -49,5 +81,5 @@ fn main() {
         }
     }
 
-    println!("0");
+    writeln!(out, "0").unwrap();
 }

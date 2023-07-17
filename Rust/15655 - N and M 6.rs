9dd9eb9040b20dfb35092fea0cb1d06359aa99d5
Chris Ohk <utilforever@gmail.com>
2023-07-18 08:55:55 +0900

    Update 15655 - N and M 6

--- Rust/15655 - N and M 6.rs
+++ Rust/15655 - N and M 6.rs
@@ -1,56 +1,79 @@
 use io::Write;
-use std::io::{self, BufWriter, StdoutLock};
+use std::io::{BufWriter, StdoutLock};
+use std::{io, str};
 
-fn input_integers() -> Vec<usize> {
-    let mut s = String::new();
+pub struct UnsafeScanner<R> {
+    reader: R,
+    buf_str: Vec<u8>,
+    buf_iter: str::SplitAsciiWhitespace<'static>,
+}
 
-    io::stdin().read_line(&mut s).unwrap();
+impl<R: io::BufRead> UnsafeScanner<R> {
+    pub fn new(reader: R) -> Self {
+        Self {
+            reader,
+            buf_str: vec![],
+            buf_iter: "".split_ascii_whitespace(),
+        }
+    }
 
-    let values: Vec<usize> = s
-        .as_mut_str()
-        .split_whitespace()
-        .map(|s| s.parse().unwrap())
-        .collect();
-
-    values
+    pub fn token<T: str::FromStr>(&mut self) -> T {
+        loop {
+            if let Some(token) = self.buf_iter.next() {
+                return token.parse().ok().expect("Failed parse");
+            }
+            self.buf_str.clear();
+            self.reader
+                .read_until(b'\n', &mut self.buf_str)
+                .expect("Failed read");
+            self.buf_iter = unsafe {
+                let slice = str::from_utf8_unchecked(&self.buf_str);
+                std::mem::transmute(slice.split_ascii_whitespace())
+            }
+        }
+    }
 }
 
 fn check(
     out: &mut BufWriter<StdoutLock>,
-    nums: &mut Vec<usize>,
-    arr: &mut [i32; 9],
+    nums: &mut Vec<i64>,
+    ret: &mut [i64; 9],
     num: usize,
     n: usize,
     m: usize,
 ) {
     if num == m + 1 {
         for i in 1..=m {
-            write!(out, "{} ", arr[i]).unwrap();
+            write!(out, "{} ", ret[i]).unwrap();
         }
 
         writeln!(out).unwrap();
     } else {
         for i in 1..=n {
-            if arr.iter().max().unwrap() < &(nums[i - 1] as i32) {
-                arr[num] = nums[i - 1] as i32;
-                check(out, nums, arr, num + 1, n, m);
-                arr[num] = 0;
+            if *ret.iter().max().unwrap() < nums[i - 1] {
+                ret[num] = nums[i - 1];
+                check(out, nums, ret, num + 1, n, m);
+                ret[num] = 0;
             }
         }
     }
 }
 
 fn main() {
-    let stdout = io::stdout();
+    let (stdin, stdout) = (io::stdin(), io::stdout());
+    let mut scan = UnsafeScanner::new(stdin.lock());
     let mut out = io::BufWriter::new(stdout.lock());
 
-    let nums = input_integers();
-    let (n, m) = (nums[0], nums[1]);
+    let (n, m) = (scan.token::<usize>(), scan.token::<usize>());
+    let mut nums = vec![0; n];
 
-    let mut nums = input_integers();
-    let mut arr = [0; 9];
+    for i in 0..n {
+        nums[i] = scan.token::<i64>();
+    }
 
     nums.sort();
 
-    check(&mut out, &mut nums, &mut arr, 1, n, m);
+    let mut ret = [0; 9];
+
+    check(&mut out, &mut nums, &mut ret, 1, n, m);
 }

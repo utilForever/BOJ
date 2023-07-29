940ec42c7c91e2f77dfb22fe234b8cc430d3fabb
Chris Ohk <utilforever@gmail.com>
2023-07-29 15:07:16 +0900

    Upload 3417 - Vigenère Cipher Encryption

---
+++ Rust/3417 - Vigenère Cipher Encryption.rs
@@ -0,0 +1,60 @@
+use io::Write;
+use std::{io, str};
+
+pub struct UnsafeScanner<R> {
+    reader: R,
+    buf_str: Vec<u8>,
+    buf_iter: str::SplitAsciiWhitespace<'static>,
+}
+
+impl<R: io::BufRead> UnsafeScanner<R> {
+    pub fn new(reader: R) -> Self {
+        Self {
+            reader,
+            buf_str: vec![],
+            buf_iter: "".split_ascii_whitespace(),
+        }
+    }
+
+    pub fn token<T: str::FromStr>(&mut self) -> T {
+        loop {
+            if let Some(token) = self.buf_iter.next() {
+                return token.parse().ok().expect("Failed parse");
+            }
+            self.buf_str.clear();
+            self.reader
+                .read_until(b'\n', &mut self.buf_str)
+                .expect("Failed read");
+            self.buf_iter = unsafe {
+                let slice = str::from_utf8_unchecked(&self.buf_str);
+                std::mem::transmute(slice.split_ascii_whitespace())
+            }
+        }
+    }
+}
+
+fn main() {
+    let (stdin, stdout) = (io::stdin(), io::stdout());
+    let mut scan = UnsafeScanner::new(stdin.lock());
+    let mut out = io::BufWriter::new(stdout.lock());
+
+    loop {
+        let encryption_key = scan.token::<String>();
+
+        if encryption_key == "0" {
+            break;
+        }
+
+        let plaintext = scan.token::<String>();
+        let mut idx = 0;
+        let mut ret = String::new();
+
+        for c in plaintext.chars() {
+            let c = c as u8 - b'A' + (encryption_key.chars().nth(idx).unwrap() as u8 - b'A') + 1;
+            ret.push((c % 26 + b'A') as char);
+            idx = (idx + 1) % encryption_key.len();
+        }
+
+        writeln!(out, "{ret}").unwrap();
+    }
+}

dfc16944d7493d3799a089c5c7c8d3d1440e2d0d
Chris Ohk <utilforever@gmail.com>
2023-08-01 23:45:51 +0900

    Upload 28422 - XOR Card Game

---
+++ Rust/28422 - XOR Card Game.rs
@@ -0,0 +1,64 @@
+use io::Write;
+use std::{io, str};
+
+pub struct UnsafeScanner<R> {
+    reader: R,
+    buf_str: Vec<u8>,
+    buf_iter: str::SplitAsciiWhitespace<'static>,
+}
+
+impl<R: io::BufRead> UnsafeScanner<R> {
+    pub fn new(reader: R) -> Self {
+        Self {
+            reader,
+            buf_str: vec![],
+            buf_iter: "".split_ascii_whitespace(),
+        }
+    }
+
+    pub fn token<T: str::FromStr>(&mut self) -> T {
+        loop {
+            if let Some(token) = self.buf_iter.next() {
+                return token.parse().ok().expect("Failed parse");
+            }
+            self.buf_str.clear();
+            self.reader
+                .read_until(b'\n', &mut self.buf_str)
+                .expect("Failed read");
+            self.buf_iter = unsafe {
+                let slice = str::from_utf8_unchecked(&self.buf_str);
+                std::mem::transmute(slice.split_ascii_whitespace())
+            }
+        }
+    }
+}
+
+fn main() {
+    let (stdin, stdout) = (io::stdin(), io::stdout());
+    let mut scan = UnsafeScanner::new(stdin.lock());
+    let mut out = io::BufWriter::new(stdout.lock());
+
+    let n = scan.token::<usize>();
+    let mut cards = vec![0; n];
+
+    for i in 0..n {
+        cards[i] = scan.token::<i64>();
+    }
+
+    let mut dp = vec![-1; n + 1];
+    dp[0] = 0;
+    dp[n] = 0;
+
+    for i in 1..=n {
+        if i >= 2 && dp[i - 2] != -1 {
+            dp[i] = dp[i].max(dp[i - 2] + (cards[i - 2] ^ cards[i - 1]).count_ones() as i64);
+        }
+
+        if i >= 3 && dp[i - 3] != -1 {
+            dp[i] = dp[i]
+                .max(dp[i - 3] + (cards[i - 3] ^ cards[i - 2] ^ cards[i - 1]).count_ones() as i64);
+        }
+    }
+
+    writeln!(out, "{}", dp[n]).unwrap();
+}

4ae9c166bebf5037adcf932d20e84b715faee707
Chris Ohk <utilforever@gmail.com>
2023-09-06 14:56:42 +0900

    Update 9012 - Parenthesis

--- Rust/9012 - Parenthesis.rs
+++ Rust/9012 - Parenthesis.rs
@@ -1,27 +1,35 @@
-use std::io;
+use io::Write;
+use std::{io, str};
 
-fn input_integers() -> Vec<i64> {
-    let mut s = String::new();
-
-    io::stdin().read_line(&mut s).unwrap();
-
-    let values: Vec<i64> = s
-        .as_mut_str()
-        .split_whitespace()
-        .map(|s| s.parse().unwrap())
-        .collect();
-
-    values
+pub struct UnsafeScanner<R> {
+    reader: R,
+    buf_str: Vec<u8>,
+    buf_iter: str::SplitAsciiWhitespace<'static>,
 }
 
-fn main() {
-    let t = input_integers()[0];
+impl<R: io::BufRead> UnsafeScanner<R> {
+    pub fn new(reader: R) -> Self {
+        Self {
+            reader,
+            buf_str: vec![],
+            buf_iter: "".split_ascii_whitespace(),
+        }
+    }
 
-    for _ in 0..t {
-        let mut s = String::new();
-        io::stdin().read_line(&mut s).unwrap();
-
-        println!("{}", if is_valid(s) { "YES" } else { "NO" });
+    pub fn token<T: str::FromStr>(&mut self) -> T {
+        loop {
+            if let Some(token) = self.buf_iter.next() {
+                return token.parse().ok().expect("Failed parse");
+            }
+            self.buf_str.clear();
+            self.reader
+                .read_until(b'\n', &mut self.buf_str)
+                .expect("Failed read");
+            self.buf_iter = unsafe {
+                let slice = str::from_utf8_unchecked(&self.buf_str);
+                std::mem::transmute(slice.split_ascii_whitespace())
+            }
+        }
     }
 }
 
@@ -45,4 +53,17 @@ fn is_valid(s: String) -> bool {
     }
 
     false
+}
+
+fn main() {
+    let (stdin, stdout) = (io::stdin(), io::stdout());
+    let mut scan = UnsafeScanner::new(stdin.lock());
+    let mut out = io::BufWriter::new(stdout.lock());
+
+    let t = scan.token::<i64>();
+
+    for _ in 0..t {
+        let s = scan.token::<String>();
+        writeln!(out, "{}", if is_valid(s) { "YES" } else { "NO" }).unwrap();
+    }
 }

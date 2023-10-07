079114a29bde8f716cd9f9d49f873834206fb0e1
Chris Ohk <utilforever@gmail.com>
2023-10-07 11:05:33 +0900

    Upload 15486 - Resignation 2

---
+++ Rust/15486 - Resignation 2.rs
@@ -0,0 +1,59 @@
+use io::Write;
+use std::{io, str};
+
+pub struct UnsafeScanner<R> {
+    reader: R,
+    buf_str: Vec<u8>,
+    buf_iter: str::SplitAsciiWhitespace<'static>,
+}
+
+impl<R: io::BufRead> UnsafeScanner<R> {
+    pub fn new(reader: R) -> Self {
+        Self {
+            reader,
+            buf_str: vec![],
+            buf_iter: "".split_ascii_whitespace(),
+        }
+    }
+
+    pub fn token<T: str::FromStr>(&mut self) -> T {
+        loop {
+            if let Some(token) = self.buf_iter.next() {
+                return token.parse().ok().expect("Failed parse");
+            }
+            self.buf_str.clear();
+            self.reader
+                .read_until(b'\n', &mut self.buf_str)
+                .expect("Failed read");
+            self.buf_iter = unsafe {
+                let slice = str::from_utf8_unchecked(&self.buf_str);
+                std::mem::transmute(slice.split_ascii_whitespace())
+            }
+        }
+    }
+}
+
+fn main() {
+    let (stdin, stdout) = (io::stdin(), io::stdout());
+    let mut scan = UnsafeScanner::new(stdin.lock());
+    let mut out = io::BufWriter::new(stdout.lock());
+
+    let n = scan.token::<usize>();
+    let mut periods = vec![0; n];
+    let mut prices = vec![0; n];
+    let mut dp = vec![0; n + 1];
+
+    for i in 0..n {
+        (periods[i], prices[i]) = (scan.token::<usize>(), scan.token::<i64>());
+    }
+
+    for i in (0..n).rev() {
+        dp[i] = if i + periods[i] > n {
+            dp[i + 1]
+        } else {
+            dp[i + 1].max(dp[i + periods[i]] + prices[i])
+        }
+    }
+
+    writeln!(out, "{}", dp[0]).unwrap();
+}

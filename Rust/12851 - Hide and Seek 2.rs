89ec77d5cdefa5b149a007e0c77173663d203b5d
Chris Ohk <utilforever@gmail.com>
2023-10-07 11:55:45 +0900

    Update 12851 - Hide and Seek 2

--- Rust/12851 - Hide and Seek 2.rs
+++ Rust/12851 - Hide and Seek 2.rs
@@ -1,64 +1,78 @@
-use std::{collections::VecDeque, io};
+use io::Write;
+use std::{collections::VecDeque, io, str};
 
-fn input_integers() -> Vec<i64> {
-    let mut s = String::new();
-
-    io::stdin().read_line(&mut s).unwrap();
-
-    let values: Vec<i64> = s
-        .as_mut_str()
-        .split_whitespace()
-        .map(|s| s.parse().unwrap())
-        .collect();
-
-    values
+pub struct UnsafeScanner<R> {
+    reader: R,
+    buf_str: Vec<u8>,
+    buf_iter: str::SplitAsciiWhitespace<'static>,
 }
 
-fn process_bfs(visited: &mut Vec<bool>, n: usize, k: usize) -> (i64, i64) {
-    let mut time_ans = -1;
-    let mut count_ans = 0;
-
-    let mut queue = VecDeque::new();
-    queue.push_back((n, 0));
-    visited[n] = true;
-
-    while !queue.is_empty() {
-        let (cur_n, time) = queue.pop_front().unwrap();
-        visited[cur_n] = true;
-
-        if cur_n == k {
-            if time_ans == -1 {
-                time_ans = time;
-                count_ans += 1;    
-            } else if time_ans == time {
-                count_ans += 1;
-            }
-
-            visited[k] = false;
-            continue;
-        }
-
-        if cur_n as i64 - 1 >= 0 && !visited[cur_n - 1] {
-            queue.push_back((cur_n - 1, time + 1));
-        }
-        if cur_n + 1 <= 100_000 && !visited[cur_n + 1] {
-            queue.push_back((cur_n + 1, time + 1));
-        }
-        if cur_n * 2 <= 100_000 && !visited[cur_n * 2] {
-            queue.push_back((cur_n * 2, time + 1));
+impl<R: io::BufRead> UnsafeScanner<R> {
+    pub fn new(reader: R) -> Self {
+        Self {
+            reader,
+            buf_str: vec![],
+            buf_iter: "".split_ascii_whitespace(),
         }
     }
 
-    (time_ans, count_ans)
+    pub fn token<T: str::FromStr>(&mut self) -> T {
+        loop {
+            if let Some(token) = self.buf_iter.next() {
+                return token.parse().ok().expect("Failed parse");
+            }
+            self.buf_str.clear();
+            self.reader
+                .read_until(b'\n', &mut self.buf_str)
+                .expect("Failed read");
+            self.buf_iter = unsafe {
+                let slice = str::from_utf8_unchecked(&self.buf_str);
+                std::mem::transmute(slice.split_ascii_whitespace())
+            }
+        }
+    }
 }
 
 fn main() {
-    let nums = input_integers();
-    let (n, k) = (nums[0] as usize, nums[1] as usize);
+    let (stdin, stdout) = (io::stdin(), io::stdout());
+    let mut scan = UnsafeScanner::new(stdin.lock());
+    let mut out = io::BufWriter::new(stdout.lock());
 
-    let mut visited = vec![false; 100_001];
+    let (n, k) = (scan.token::<usize>(), scan.token::<usize>());
+    let mut positions = vec![false; 100001];
+    let mut ret_time = -1;
+    let mut ret_count = 0;
 
-    let (time_ans, count_ans) = process_bfs(&mut visited, n, k);
-    println!("{}", time_ans);
-    println!("{}", count_ans);
+    let mut queue = VecDeque::new();
+    queue.push_back((n, 0));
+
+    while !queue.is_empty() {
+        let (curr_position, curr_time) = queue.pop_front().unwrap();
+        positions[curr_position] = true;
+
+        if curr_position == k {
+            if ret_time == -1 {
+                ret_time = curr_time;
+                ret_count += 1;
+            } else if ret_time == curr_time {
+                ret_count += 1;
+            }
+
+            positions[k] = false;
+            continue;
+        }
+
+        if curr_position as i64 - 1 >= 0 && !positions[curr_position - 1] {
+            queue.push_back((curr_position - 1, curr_time + 1));
+        }
+        if curr_position + 1 <= 100_000 && !positions[curr_position + 1] {
+            queue.push_back((curr_position + 1, curr_time + 1));
+        }
+        if curr_position * 2 <= 100_000 && !positions[curr_position * 2] {
+            queue.push_back((curr_position * 2, curr_time + 1));
+        }
+    }
+
+    writeln!(out, "{ret_time}").unwrap();
+    writeln!(out, "{ret_count}").unwrap();
 }

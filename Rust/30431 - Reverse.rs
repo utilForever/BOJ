af6004fcb8a526d97f142eaf9b3286969c9f41ae
Chris Ohk <utilforever@gmail.com>
2023-10-31 00:36:02 +0900

    Upload 30431 - Reverse

---
+++ Rust/30431 - Reverse.rs
@@ -0,0 +1,135 @@
+use io::Write;
+use std::{io, str};
+
+pub struct UnsafeScanner<R> {
+    reader: R,
+    buf_str: Vec<u8>,
+    buf_iter: str::SplitAsciiWhitespace<'static>,
+}
+
+impl<R: io::BufRead> UnsafeScanner<R> {
+    pub fn new(reader: R) -> Self {
+        Self {
+            reader,
+            buf_str: vec![],
+            buf_iter: "".split_ascii_whitespace(),
+        }
+    }
+
+    pub fn token<T: str::FromStr>(&mut self) -> T {
+        loop {
+            if let Some(token) = self.buf_iter.next() {
+                return token.parse().ok().expect("Failed parse");
+            }
+            self.buf_str.clear();
+            self.reader
+                .read_until(b'\n', &mut self.buf_str)
+                .expect("Failed read");
+            self.buf_iter = unsafe {
+                let slice = str::from_utf8_unchecked(&self.buf_str);
+                std::mem::transmute(slice.split_ascii_whitespace())
+            }
+        }
+    }
+}
+
+fn main() {
+    let (stdin, stdout) = (io::stdin(), io::stdout());
+    let mut scan = UnsafeScanner::new(stdin.lock());
+    let mut out = io::BufWriter::new(stdout.lock());
+
+    // A
+    let n = scan.token::<i64>();
+
+    writeln!(out, "{n}").unwrap();
+
+    for _ in 0..n {
+        write!(out, "A").unwrap();
+    }
+    writeln!(out).unwrap();
+
+    // B
+    let s = scan.token::<String>();
+
+    if s == "utopia" {
+        writeln!(out, "3 2 3").unwrap();
+        writeln!(out, "1 2").unwrap();
+        writeln!(out, "0 1").unwrap();
+        writeln!(out, "2 1").unwrap();
+        writeln!(out, "0").unwrap();
+    } else {
+        writeln!(out, "3 2 3").unwrap();
+        writeln!(out, "1 2").unwrap();
+        writeln!(out, "0 1").unwrap();
+        writeln!(out, "2 1").unwrap();
+        writeln!(out, "1").unwrap();
+        writeln!(out, "1").unwrap();
+    }
+
+    // C
+    let s = scan.token::<String>();
+
+    writeln!(out, "swi's cake is missing!").unwrap();
+
+    if s == "swi" {
+        writeln!(out, "0").unwrap();
+        writeln!(out, "0").unwrap();
+    } else {
+        writeln!(out, "1").unwrap();
+        writeln!(out, "{s}").unwrap();
+        writeln!(out, "0").unwrap();
+    }
+
+    // D
+    let n = scan.token::<i64>();
+
+    if n == 0 {
+        writeln!(out, "1 1 1 1").unwrap();
+        writeln!(out, "1").unwrap();
+    } else {
+        writeln!(out, "{n} {n} {n} {n}").unwrap();
+
+        for _ in 1..=n {
+            for _ in 1..=n {
+                for _ in 1..=n {
+                    for _ in 1..=n {
+                        write!(out, "0 ").unwrap();
+                    }
+
+                    writeln!(out).unwrap();
+                }
+            }
+        }
+    }
+
+    // E
+    let n = scan.token::<i64>();
+
+    if n == 0 {
+        writeln!(out, "2").unwrap();
+        writeln!(out, "2 0").unwrap();
+        writeln!(out, "1 0").unwrap();
+    } else {
+        writeln!(out, "{}", n * 2).unwrap();
+
+        for i in 1..=n {
+            writeln!(out, "{} 1", i * 2).unwrap();
+            writeln!(out, "{} 1", i * 2 - 1).unwrap();
+        }
+    }
+
+    // F
+    let s = scan.token::<String>();
+
+    writeln!(out, "{}", s.len()).unwrap();
+
+    for c in s.chars() {
+        if c == 'W' {
+            writeln!(out, "1 1").unwrap();
+        } else if c == 'D' {
+            writeln!(out, "2 2").unwrap();
+        } else {
+            writeln!(out, "1 3").unwrap();
+        }
+    }
+}

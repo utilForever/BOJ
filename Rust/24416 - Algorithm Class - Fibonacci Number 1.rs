d9f01ab6e26d7bc2646be3135b8a8ad5d15a2131
Chris Ohk <utilforever@gmail.com>
2023-12-04 20:33:05 +0900

    Update 24416 - Algorithm Class - Fibonacci Number 1

--- Rust/24416 - Algorithm Class - Fibonacci Number 1.rs
+++ Rust/24416 - Algorithm Class - Fibonacci Number 1.rs
@@ -1,22 +1,44 @@
-use std::io;
+use io::Write;
+use std::{io, str};
 
-fn input_integers() -> Vec<i64> {
-    let mut s = String::new();
+pub struct UnsafeScanner<R> {
+    reader: R,
+    buf_str: Vec<u8>,
+    buf_iter: str::SplitAsciiWhitespace<'static>,
+}
 
-    io::stdin().read_line(&mut s).unwrap();
+impl<R: io::BufRead> UnsafeScanner<R> {
+    pub fn new(reader: R) -> Self {
+        Self {
+            reader,
+            buf_str: vec![],
+            buf_iter: "".split_ascii_whitespace(),
+        }
+    }
 
-    let values: Vec<i64> = s
-        .as_mut_str()
-        .split_whitespace()
-        .map(|s| s.parse().unwrap())
-        .collect();
-
-    values
+    pub fn token<T: str::FromStr>(&mut self) -> T {
+        loop {
+            if let Some(token) = self.buf_iter.next() {
+                return token.parse().ok().expect("Failed parse");
+            }
+            self.buf_str.clear();
+            self.reader
+                .read_until(b'\n', &mut self.buf_str)
+                .expect("Failed read");
+            self.buf_iter = unsafe {
+                let slice: &str = str::from_utf8_unchecked(&self.buf_str);
+                std::mem::transmute(slice.split_ascii_whitespace())
+            }
+        }
+    }
 }
 
 fn main() {
-    let mut fibonacci = vec![vec![0_i64; 2]; 41];
+    let (stdin, stdout) = (io::stdin(), io::stdout());
+    let mut scan = UnsafeScanner::new(stdin.lock());
+    let mut out = io::BufWriter::new(stdout.lock());
 
+    let mut fibonacci = vec![vec![0_i64; 2]; 41];
     fibonacci[0][0] = 1;
     fibonacci[0][1] = 0;
     fibonacci[1][0] = 0;
@@ -27,6 +49,7 @@ fn main() {
         fibonacci[i][1] = fibonacci[i - 1][1] + fibonacci[i - 2][1];
     }
 
-    let n = input_integers()[0] as usize;
-    println!("{} {}", fibonacci[n][1], n - 2);
+    let n = scan.token::<usize>();
+
+    writeln!(out, "{} {}", fibonacci[n][1], n - 2).unwrap();
 }

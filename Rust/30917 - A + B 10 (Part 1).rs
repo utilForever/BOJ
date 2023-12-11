caa8f54694edc9fbc96541305a652628dbfc43f3
Chris Ohk <utilforever@gmail.com>
2023-12-12 02:37:43 +0900

    Upload 30917 - A + B 10 (Part 1)

---
+++ Rust/30917 - A + B 10 (Part 1).rs
@@ -0,0 +1,67 @@
+use std::{io, str};
+
+pub struct UnsafeScanner<R> {
+    reader: R,
+    buf_str: Vec<u8>,
+    buf_iter: str::SplitAsciiWhitespace<'static>,
+}
+
+impl<R: io::BufRead> UnsafeScanner<R> {
+    pub fn new(reader: R) -> Self {
+        Self {
+            reader,
+            buf_str: vec![],
+            buf_iter: "".split_ascii_whitespace(),
+        }
+    }
+
+    pub fn token<T: str::FromStr>(&mut self) -> T {
+        loop {
+            if let Some(token) = self.buf_iter.next() {
+                return token.parse().ok().expect("Failed parse");
+            }
+            self.buf_str.clear();
+            self.reader
+                .read_until(b'\n', &mut self.buf_str)
+                .expect("Failed read");
+            self.buf_iter = unsafe {
+                let slice = str::from_utf8_unchecked(&self.buf_str);
+                std::mem::transmute(slice.split_ascii_whitespace())
+            }
+        }
+    }
+}
+
+fn main() {
+    let stdin = io::stdin();
+    let mut scan = UnsafeScanner::new(stdin.lock());
+
+    let mut a = 1;
+    let mut b = 1;
+
+    loop {
+        println!("? A {a}");
+
+        let ret = scan.token::<i64>();
+
+        if ret == 1 {
+            break;
+        } else {
+            a += 1;
+        }
+    }
+
+    loop {
+        println!("? B {b}");
+
+        let ret = scan.token::<i64>();
+
+        if ret == 1 {
+            break;
+        } else {
+            b += 1;
+        }
+    }
+
+    println!("! {}", a + b);
+}

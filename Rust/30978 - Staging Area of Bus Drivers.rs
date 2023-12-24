be470d49d3f585cd0cab5708b88db31848e675f6
Chris Ohk <utilforever@gmail.com>
2023-12-24 21:55:33 +0900

    Upload 30978 - Staging Area of Bus Drivers

---
+++ Rust/30978 - Staging Area of Bus Drivers.rs
@@ -0,0 +1,401 @@
+use io::Write;
+use std::{io, str};
+
+pub struct UnsafeScanner<R> {
+    reader: R,
+    buf_str: Vec<u8>,
+    buf_iter: str::SplitAsciiWhitespace<'static>,
+}
+
+impl<R: io::BufRead> UnsafeScanner<R> {
+    pub fn new(reader: R) -> Self {
+        Self {
+            reader,
+            buf_str: vec![],
+            buf_iter: "".split_ascii_whitespace(),
+        }
+    }
+
+    pub fn token<T: str::FromStr>(&mut self) -> T {
+        loop {
+            if let Some(token) = self.buf_iter.next() {
+                return token.parse().ok().expect("Failed parse");
+            }
+            self.buf_str.clear();
+            self.reader
+                .read_until(b'\n', &mut self.buf_str)
+                .expect("Failed read");
+            self.buf_iter = unsafe {
+                let slice = str::from_utf8_unchecked(&self.buf_str);
+                std::mem::transmute(slice.split_ascii_whitespace())
+            }
+        }
+    }
+}
+
+#[derive(Clone, Default)]
+struct Node {
+    one: i64,
+    zero: i64,
+    min: i64,
+    max: i64,
+}
+
+struct LazySegmentTree {
+    size: usize,
+    data: Vec<Node>,
+    lazy_and: Vec<i64>,
+    lazy_or: Vec<i64>,
+}
+
+impl LazySegmentTree {
+    pub fn new(n: usize) -> Self {
+        let mut real_n = 1;
+        while real_n < n {
+            real_n *= 2;
+        }
+
+        Self {
+            size: n,
+            data: vec![Node::default(); real_n * 4],
+            lazy_and: vec![0; real_n * 4],
+            lazy_or: vec![0; real_n * 4],
+        }
+    }
+
+    fn merge(a: &Node, b: &Node) -> Node {
+        Node {
+            one: a.one & b.one,
+            zero: a.zero & b.zero,
+            min: a.min.min(b.min),
+            max: a.max.max(b.max),
+        }
+    }
+
+    pub fn construct(&mut self, arr: &[i64], start: usize, end: usize) {
+        self.construct_internal(arr, 1, start, end);
+    }
+
+    fn construct_internal(&mut self, arr: &[i64], node: usize, start: usize, end: usize) -> Node {
+        if start == end {
+            self.data[node] = Node {
+                one: arr[start],
+                zero: !arr[start],
+                min: arr[start],
+                max: arr[start],
+            };
+            self.data[node].clone()
+        } else {
+            let mid = (start + end) / 2;
+
+            let left = self.construct_internal(arr, node * 2, start, mid);
+            let right = self.construct_internal(arr, node * 2 + 1, mid + 1, end);
+
+            self.data[node] = LazySegmentTree::merge(&left, &right);
+            self.data[node].clone()
+        }
+    }
+
+    fn propagate(&mut self, node: usize, start: usize, end: usize) {
+        self.data[node].one |= self.lazy_or[node];
+        self.data[node].one &= !self.lazy_and[node];
+
+        self.data[node].zero &= !self.lazy_or[node];
+        self.data[node].zero |= self.lazy_and[node];
+
+        self.data[node].min |= self.lazy_or[node];
+        self.data[node].min &= !self.lazy_and[node];
+
+        self.data[node].max |= self.lazy_or[node];
+        self.data[node].max &= !self.lazy_and[node];
+
+        if start == end {
+            self.lazy_and[node] = 0;
+            self.lazy_or[node] = 0;
+            return;
+        }
+
+        self.lazy_or[node * 2] &= !self.lazy_and[node];
+        self.lazy_or[node * 2] |= self.lazy_or[node];
+
+        self.lazy_and[node * 2] &= !self.lazy_or[node];
+        self.lazy_and[node * 2] |= self.lazy_and[node];
+
+        self.lazy_or[node * 2 + 1] &= !self.lazy_and[node];
+        self.lazy_or[node * 2 + 1] |= self.lazy_or[node];
+
+        self.lazy_and[node * 2 + 1] &= !self.lazy_or[node];
+        self.lazy_and[node * 2 + 1] |= self.lazy_and[node];
+
+        self.lazy_and[node] = 0;
+        self.lazy_or[node] = 0;
+    }
+
+    pub fn update_and(&mut self, start: usize, end: usize, val: i64) {
+        self.update_and_internal(start, end, val, 1, 1, self.size);
+    }
+
+    fn update_and_internal(
+        &mut self,
+        start: usize,
+        end: usize,
+        mut val: i64,
+        node: usize,
+        node_start: usize,
+        node_end: usize,
+    ) {
+        self.propagate(node, node_start, node_end);
+
+        if end < node_start || node_end < start {
+            return;
+        }
+
+        val &= !self.data[node].zero;
+
+        if start <= node_start && node_end <= end && (val & self.data[node].one) != 0 {
+            self.lazy_and[node] = val & self.data[node].one;
+            val &= !self.data[node].one;
+            self.propagate(node, node_start, node_end);
+        }
+
+        if val == 0 {
+            return;
+        }
+
+        let mid = (node_start + node_end) / 2;
+        self.update_and_internal(start, end, val, node * 2, node_start, mid);
+        self.update_and_internal(start, end, val, node * 2 + 1, mid + 1, node_end);
+
+        self.data[node] = LazySegmentTree::merge(&self.data[node * 2], &self.data[node * 2 + 1]);
+    }
+
+    pub fn update_or(&mut self, start: usize, end: usize, val: i64) {
+        self.update_or_internal(start, end, val, 1, 1, self.size);
+    }
+
+    fn update_or_internal(
+        &mut self,
+        start: usize,
+        end: usize,
+        mut val: i64,
+        node: usize,
+        node_start: usize,
+        node_end: usize,
+    ) {
+        self.propagate(node, node_start, node_end);
+
+        if end < node_start || node_end < start {
+            return;
+        }
+
+        val &= !self.data[node].one;
+
+        if start <= node_start && node_end <= end && (val & self.data[node].zero) != 0 {
+            self.lazy_or[node] = val & self.data[node].zero;
+            val &= !self.data[node].zero;
+            self.propagate(node, node_start, node_end);
+        }
+
+        if val == 0 {
+            return;
+        }
+
+        let mid = (node_start + node_end) / 2;
+        self.update_or_internal(start, end, val, node * 2, node_start, mid);
+        self.update_or_internal(start, end, val, node * 2 + 1, mid + 1, node_end);
+
+        self.data[node] = LazySegmentTree::merge(&self.data[node * 2], &self.data[node * 2 + 1]);
+    }
+
+    pub fn query_min(&mut self, start: usize, end: usize) -> i64 {
+        self.query_min_internal(start, end, 1, 1, self.size)
+    }
+
+    fn query_min_internal(
+        &mut self,
+        start: usize,
+        end: usize,
+        node: usize,
+        node_start: usize,
+        node_end: usize,
+    ) -> i64 {
+        self.propagate(node, node_start, node_end);
+
+        if end < node_start || node_end < start {
+            return i64::MAX;
+        }
+
+        if start <= node_start && node_end <= end {
+            return self.data[node].min;
+        }
+
+        let mid = (node_start + node_end) / 2;
+        let left = self.query_min_internal(start, end, node * 2, node_start, mid);
+        let right = self.query_min_internal(start, end, node * 2 + 1, mid + 1, node_end);
+
+        left.min(right)
+    }
+
+    pub fn query_max(&mut self, start: usize, end: usize) -> i64 {
+        self.query_max_internal(start, end, 1, 1, self.size)
+    }
+
+    fn query_max_internal(
+        &mut self,
+        start: usize,
+        end: usize,
+        node: usize,
+        node_start: usize,
+        node_end: usize,
+    ) -> i64 {
+        self.propagate(node, node_start, node_end);
+
+        if end < node_start || node_end < start {
+            return 0;
+        }
+
+        if start <= node_start && node_end <= end {
+            return self.data[node].max;
+        }
+
+        let mid = (node_start + node_end) / 2;
+        let left = self.query_max_internal(start, end, node * 2, node_start, mid);
+        let right = self.query_max_internal(start, end, node * 2 + 1, mid + 1, node_end);
+
+        left.max(right)
+    }
+}
+
+fn main() {
+    let (stdin, stdout) = (io::stdin(), io::stdout());
+    let mut scan = UnsafeScanner::new(stdin.lock());
+    let mut out = io::BufWriter::new(stdout.lock());
+
+    let n = scan.token::<usize>();
+
+    let mut tree = LazySegmentTree::new(n);
+    let mut arr = vec![0; n + 1];
+
+    for i in 1..=n {
+        let m = scan.token::<i64>();
+
+        for _ in 0..m {
+            let l = scan.token::<i64>();
+            arr[i] |= 1 << l;
+        }
+    }
+
+    tree.construct(&arr, 1, n);
+
+    let q = scan.token::<i64>();
+
+    for _ in 0..q {
+        let command = scan.token::<i64>();
+
+        if command == 1 {
+            let (a, b, m) = (
+                scan.token::<usize>(),
+                scan.token::<usize>(),
+                scan.token::<i64>(),
+            );
+            let mut val = 0;
+
+            for _ in 0..m {
+                let l = scan.token::<i64>();
+                val |= 1 << l;
+            }
+
+            tree.update_or(a, b, val);
+        } else if command == 2 {
+            let (a, b, m) = (
+                scan.token::<usize>(),
+                scan.token::<usize>(),
+                scan.token::<i64>(),
+            );
+            let mut val = 0;
+
+            for _ in 0..m {
+                let l = scan.token::<i64>();
+                val |= 1 << l;
+            }
+
+            tree.update_and(a, b, !val);
+        } else if command == 3 {
+            let (a, m) = (scan.token::<usize>(), scan.token::<i64>());
+            let mut val = 0;
+
+            for _ in 0..m {
+                let l = scan.token::<i64>();
+                val |= 1 << l;
+            }
+
+            tree.update_and(a, a, !0);
+            tree.update_or(a, a, val);
+        } else if command == 4 {
+            let (a, b) = (scan.token::<usize>(), scan.token::<usize>());
+            let val_a = tree.query_min(a, a);
+            let val_b = tree.query_min(b, b);
+
+            tree.update_and(a, a, !0);
+            tree.update_or(a, a, val_b);
+            tree.update_and(b, b, !0);
+            tree.update_or(b, b, val_a);
+        } else if command == 5 {
+            let (a, b) = (scan.token::<usize>(), scan.token::<usize>());
+            let val = tree.query_min(a, b);
+
+            if val == 0 {
+                writeln!(out, "-1").unwrap();
+            } else {
+                for i in 0..=50 {
+                    if (val & (1 << i)) != 0 {
+                        write!(out, "{i} ").unwrap();
+                    }
+                }
+
+                writeln!(out).unwrap();
+            }
+        } else if command == 6 {
+            let (a, b) = (scan.token::<usize>(), scan.token::<usize>());
+            let val = tree.query_max(a, b);
+
+            if val == 0 {
+                writeln!(out, "-1").unwrap();
+            } else {
+                for i in 0..=50 {
+                    if (val & (1 << i)) != 0 {
+                        write!(out, "{i} ").unwrap();
+                    }
+                }
+
+                writeln!(out).unwrap();
+            }
+        } else {
+            let (a, b) = (scan.token::<usize>(), scan.token::<usize>());
+            let val_min = tree.query_min(a, b);
+            let val_max = tree.query_max(a, b);
+
+            if val_min == val_max {
+                writeln!(out, "-1").unwrap();
+            } else {
+                let mut ret = Vec::new();
+
+                for i in (0..=50).rev() {
+                    if val_max & (1 << i) != 0 {
+                        if val_min & (1 << i) == 0 {
+                            ret.push(i);
+                        }
+                    } else if val_min & (1 << i) != 0 {
+                        break;
+                    }
+                }
+
+                for val in ret.iter().rev() {
+                    write!(out, "{val} ").unwrap();
+                }
+
+                writeln!(out).unwrap();
+            }
+        }
+    }
+}

7028616cffd152ad0c7c169b6f3dc8ac0a17b305
Chirs Ohk <utilforever@gmail.com>
2023-12-26 14:18:01 +0900

    Upload 30970 - Crossroad

---
+++ Rust/30970 - Crossroad.rs
@@ -0,0 +1,77 @@
+use io::Write;
+use std::{io, str};
+
+pub struct UnsafeScanner<R> {
+    reader: R,
+    buf_str: Vec<u8>,
+    buf_iter: str::SplitAsciiWhitespace<'static>,
+}
+
+impl<R: io::BufRead> UnsafeScanner<R> {
+    pub fn new(reader: R) -> Self {
+        Self {
+            reader,
+            buf_str: vec![],
+            buf_iter: "".split_ascii_whitespace(),
+        }
+    }
+
+    pub fn token<T: str::FromStr>(&mut self) -> T {
+        loop {
+            if let Some(token) = self.buf_iter.next() {
+                return token.parse().ok().expect("Failed parse");
+            }
+            self.buf_str.clear();
+            self.reader
+                .read_until(b'\n', &mut self.buf_str)
+                .expect("Failed read");
+            self.buf_iter = unsafe {
+                let slice = str::from_utf8_unchecked(&self.buf_str);
+                std::mem::transmute(slice.split_ascii_whitespace())
+            }
+        }
+    }
+}
+
+fn main() {
+    let (stdin, stdout) = (io::stdin(), io::stdout());
+    let mut scan = UnsafeScanner::new(stdin.lock());
+    let mut out = io::BufWriter::new(stdout.lock());
+
+    let n = scan.token::<usize>();
+    let mut dolls = vec![(0, 0); n];
+
+    for i in 0..n {
+        dolls[i] = (scan.token::<i64>(), scan.token::<i64>());
+    }
+
+    // Method 1
+    dolls.sort_by(|a, b| {
+        if a.0 == b.0 {
+            a.1.cmp(&b.1)
+        } else {
+            b.0.cmp(&a.0)
+        }
+    });
+    writeln!(
+        out,
+        "{} {} {} {}",
+        dolls[0].0, dolls[0].1, dolls[1].0, dolls[1].1
+    )
+    .unwrap();
+
+    // Method 2
+    dolls.sort_by(|a, b| {
+        if a.1 == b.1 {
+            b.0.cmp(&a.0)
+        } else {
+            a.1.cmp(&b.1)
+        }
+    });
+    writeln!(
+        out,
+        "{} {} {} {}",
+        dolls[0].0, dolls[0].1, dolls[1].0, dolls[1].1
+    )
+    .unwrap();
+}

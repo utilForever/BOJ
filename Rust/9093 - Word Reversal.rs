3aa320dc91d2a063ee007aa5b5039c0e99b5e824
Chris Ohk <utilforever@gmail.com>
2024-01-13 22:08:37 +0900

    Update 9093 - Word Reversal

--- Rust/9093 - Word Reversal.rs
+++ Rust/9093 - Word Reversal.rs
@@ -1,25 +1,15 @@
 use io::Write;
 use std::{io, str};
 
-fn input_integers() -> Vec<i64> {
-    let mut s = String::new();
-
-    io::stdin().read_line(&mut s).unwrap();
-
-    let values: Vec<i64> = s
-        .as_mut_str()
-        .split_whitespace()
-        .map(|s| s.parse().unwrap())
-        .collect();
-
-    values
-}
-
 fn main() {
     let stdout = io::stdout();
     let mut out = io::BufWriter::new(stdout.lock());
 
-    let n = input_integers()[0];
+    let mut s = String::new();
+    io::stdin().read_line(&mut s).unwrap();
+    s = s.trim().to_string();
+    
+    let n = s.parse::<i64>().unwrap();
 
     for _ in 0..n {
         let mut s = String::new();

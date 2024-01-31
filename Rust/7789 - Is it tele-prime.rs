16a769e32ebdced704537419e6d3eb4b0136b0cd
Chris Ohk <utilforever@gmail.com>
2024-02-01 00:53:49 +0900

    Upload 7789 - Is it tele-prime

---
+++ Rust/7789 - Is it tele-prime.rs
@@ -0,0 +1,73 @@
+use io::Write;
+use std::{io, str};
+
+pub struct UnsafeScanner<R> {
+    reader: R,
+    buf_str: Vec<u8>,
+    buf_iter: str::SplitAsciiWhitespace<'static>,
+}
+
+impl<R: io::BufRead> UnsafeScanner<R> {
+    pub fn new(reader: R) -> Self {
+        Self {
+            reader,
+            buf_str: vec![],
+            buf_iter: "".split_ascii_whitespace(),
+        }
+    }
+
+    pub fn token<T: str::FromStr>(&mut self) -> T {
+        loop {
+            if let Some(token) = self.buf_iter.next() {
+                return token.parse().ok().expect("Failed parse");
+            }
+            self.buf_str.clear();
+            self.reader
+                .read_until(b'\n', &mut self.buf_str)
+                .expect("Failed read");
+            self.buf_iter = unsafe {
+                let slice = str::from_utf8_unchecked(&self.buf_str);
+                std::mem::transmute(slice.split_ascii_whitespace())
+            }
+        }
+    }
+}
+
+fn is_prime(n: i64) -> bool {
+    if n < 2 {
+        return true;
+    }
+
+    let mut i = 2;
+
+    while i * i <= n {
+        if n % i == 0 {
+            return false;
+        }
+
+        i += 1;
+    }
+
+    true
+}
+
+fn main() {
+    let (stdin, stdout) = (io::stdin(), io::stdout());
+    let mut scan = UnsafeScanner::new(stdin.lock());
+    let mut out = io::BufWriter::new(stdout.lock());
+
+    let (phone_number, digit) = (scan.token::<String>(), scan.token::<char>());
+    let mut phone_number_new = phone_number.clone();
+    phone_number_new.insert(0, digit);
+
+    writeln!(
+        out,
+        "{}",
+        if is_prime(phone_number.parse().unwrap()) && is_prime(phone_number_new.parse().unwrap()) {
+            "Yes"
+        } else {
+            "No"
+        }
+    )
+    .unwrap();
+}

e6c8fa2a1c1977fec3b85ab8b47a11dc025d9d12
Chirs Ohk <utilforever@gmail.com>
2024-02-06 14:02:29 +0900

    Upload 10819 - Maximize the Difference

---
+++ Rust/10819 - Maximize the Difference.rs
@@ -0,0 +1,89 @@
+use io::Write;
+use std::{cmp::Ordering, io, str};
+
+pub struct UnsafeScanner<R> {
+    reader: R,
+    buf_str: Vec<u8>,
+    buf_iter: str::SplitAsciiWhitespace<'static>,
+}
+
+impl<R: io::BufRead> UnsafeScanner<R> {
+    pub fn new(reader: R) -> Self {
+        Self {
+            reader,
+            buf_str: vec![],
+            buf_iter: "".split_ascii_whitespace(),
+        }
+    }
+
+    pub fn token<T: str::FromStr>(&mut self) -> T {
+        loop {
+            if let Some(token) = self.buf_iter.next() {
+                return token.parse().ok().expect("Failed parse");
+            }
+            self.buf_str.clear();
+            self.reader
+                .read_until(b'\n', &mut self.buf_str)
+                .expect("Failed read");
+            self.buf_iter = unsafe {
+                let slice = str::from_utf8_unchecked(&self.buf_str);
+                std::mem::transmute(slice.split_ascii_whitespace())
+            }
+        }
+    }
+}
+
+pub fn next_permutation(nums: &mut Vec<usize>) -> bool {
+    let last_ascending = match nums.windows(2).rposition(|w| w[0] < w[1]) {
+        Some(i) => i,
+        None => {
+            nums.reverse();
+            return false;
+        }
+    };
+
+    let swap_with = nums[last_ascending + 1..]
+        .binary_search_by(|n| usize::cmp(&nums[last_ascending], n).then(Ordering::Less))
+        .unwrap_err();
+    nums.swap(last_ascending, last_ascending + swap_with);
+    nums[last_ascending + 1..].reverse();
+
+    true
+}
+
+fn main() {
+    let (stdin, stdout) = (io::stdin(), io::stdout());
+    let mut scan = UnsafeScanner::new(stdin.lock());
+    let mut out = io::BufWriter::new(stdout.lock());
+
+    let n = scan.token::<usize>();
+    let mut nums = vec![0; n];
+
+    for i in 0..n {
+        nums[i] = scan.token::<i64>();
+    }
+
+    let mut permutation = (0..n).collect::<Vec<_>>();
+    let mut ret = nums
+        .windows(2)
+        .map(|vals| (vals[0] - vals[1]).abs())
+        .sum::<i64>();
+
+    loop {
+        let is_exist = next_permutation(&mut permutation);
+
+        if !is_exist {
+            break;
+        }
+
+        let mut sum = 0;
+
+        for i in 0..permutation.len() - 1 {
+            sum += (nums[permutation[i]] - nums[permutation[i + 1]]).abs();
+        }
+
+        ret = ret.max(sum);
+    }
+
+    writeln!(out, "{ret}").unwrap();
+}

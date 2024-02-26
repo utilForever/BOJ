4eb7b4b95996fdac7f4f790a26d8eee3c610b64c
Chris Ohk <utilforever@gmail.com>
2024-02-26 09:14:56 +0900

    Upload 31433 - KSA String

---
+++ Rust/31433 - KSA String.rs
@@ -0,0 +1,62 @@
+use io::Write;
+use std::{io, str};
+
+pub struct UnsafeScanner<R> {
+    reader: R,
+    buf_str: Vec<u8>,
+    buf_iter: str::SplitAsciiWhitespace<'static>,
+}
+
+impl<R: io::BufRead> UnsafeScanner<R> {
+    pub fn new(reader: R) -> Self {
+        Self {
+            reader,
+            buf_str: vec![],
+            buf_iter: "".split_ascii_whitespace(),
+        }
+    }
+
+    pub fn token<T: str::FromStr>(&mut self) -> T {
+        loop {
+            if let Some(token) = self.buf_iter.next() {
+                return token.parse().ok().expect("Failed parse");
+            }
+            self.buf_str.clear();
+            self.reader
+                .read_until(b'\n', &mut self.buf_str)
+                .expect("Failed read");
+            self.buf_iter = unsafe {
+                let slice = str::from_utf8_unchecked(&self.buf_str);
+                std::mem::transmute(slice.split_ascii_whitespace())
+            }
+        }
+    }
+}
+
+fn main() {
+    let (stdin, stdout) = (io::stdin(), io::stdout());
+    let mut scan = UnsafeScanner::new(stdin.lock());
+    let mut out = io::BufWriter::new(stdout.lock());
+
+    let s = scan.token::<String>();
+    let s = s.chars().collect::<Vec<_>>();
+
+    let word = ['K', 'S', 'A'];
+    let len = s.len() as i64;
+    let mut ret = 2 * len;
+
+    for i in 0..3 {
+        let mut cnt = 0;
+
+        for ch in s.iter() {
+            if *ch == word[(cnt as usize + i) % 3] {
+                cnt += 1;
+            }
+        }
+
+        cnt = cnt.min(len - i as i64);
+        ret = ret.min(2 * (len - cnt));
+    }
+
+    writeln!(out, "{ret}").unwrap();
+}

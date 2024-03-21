33d9c08928cf8d37f7341c364ebcf27c8d7ae54f
Chris Ohk <utilforever@gmail.com>
2024-03-21 22:02:37 +0900

    Upload 31547 - DAGame Insane

---
+++ Rust/31547 - DAGame Insane.rs
@@ -0,0 +1,409 @@
+use io::Write;
+use std::{io, ops::Mul, str};
+
+pub struct UnsafeScanner<R> {
+    reader: R,
+    buf_str: Vec<u8>,
+    buf_iter: str::SplitAsciiWhitespace<'static>,
+}
+
+impl<R: io::BufRead> UnsafeScanner<R> {
+    pub fn new(reader: R) -> Self {
+        Self {
+            reader,
+            buf_str: vec![],
+            buf_iter: "".split_ascii_whitespace(),
+        }
+    }
+
+    pub fn token<T: str::FromStr>(&mut self) -> T {
+        loop {
+            if let Some(token) = self.buf_iter.next() {
+                return token.parse().ok().expect("Failed parse");
+            }
+            self.buf_str.clear();
+            self.reader
+                .read_until(b'\n', &mut self.buf_str)
+                .expect("Failed read");
+            self.buf_iter = unsafe {
+                let slice = str::from_utf8_unchecked(&self.buf_str);
+                std::mem::transmute(slice.split_ascii_whitespace())
+            }
+        }
+    }
+}
+
+fn multiply(x: i64, y: i64, modular: i64) -> i64 {
+    (x as i128 * y as i128 % modular as i128) as i64
+}
+
+fn pow(x: i64, mut y: i64, p: i64) -> i64 {
+    let mut ret = 1;
+    let mut piv = x % p;
+
+    while y != 0 {
+        if y & 1 != 0 {
+            ret = multiply(ret, piv, p);
+        }
+
+        piv = multiply(piv, piv, p);
+        y >>= 1;
+    }
+
+    ret
+}
+
+fn calculate_mex(check: &Vec<bool>) -> i64 {
+    for i in 0..check.len() {
+        if !check[i] {
+            return i as i64;
+        }
+    }
+
+    return check.len() as i64;
+}
+
+fn process_dfs(
+    grundy: &mut Vec<Vec<i64>>,
+    edges: &Vec<Vec<usize>>,
+    visited: &mut Vec<bool>,
+    visited_vertices: &mut Vec<usize>,
+    idx: usize,
+) {
+    visited[idx] = true;
+
+    for &next in edges[idx].iter() {
+        if !visited[next] {
+            process_dfs(grundy, edges, visited, visited_vertices, next);
+        }
+    }
+
+    let mut check = vec![false; 100];
+
+    for &next in edges[idx].iter() {
+        check[grundy[next][next] as usize] = true;
+    }
+
+    grundy[idx][idx] = calculate_mex(&check);
+
+    for vertex in visited_vertices.iter() {
+        check.clear();
+        check.resize(100, false);
+
+        for &next in edges[idx].iter() {
+            check[grundy[*vertex][next] as usize] = true;
+        }
+
+        for &next in edges[*vertex].iter() {
+            check[grundy[idx][next] as usize] = true;
+        }
+
+        grundy[idx][*vertex] = calculate_mex(&check);
+        grundy[*vertex][idx] = grundy[idx][*vertex];
+    }
+
+    visited_vertices.push(idx);
+}
+
+#[derive(Clone)]
+struct Matrix {
+    data: Vec<Vec<i64>>,
+}
+
+impl Matrix {
+    pub fn new() -> Self {
+        Self {
+            data: vec![vec![0; 32]; 32],
+        }
+    }
+
+    pub fn merge(&self, other: &Matrix) -> Matrix {
+        self.clone() * other.clone()
+    }
+}
+
+impl Mul for Matrix {
+    type Output = Self;
+
+    fn mul(self, rhs: Self) -> Self {
+        let mut ret = Self::new();
+
+        for i in 0..32 {
+            for j in 0..32 {
+                for k in 0..32 {
+                    ret.data[i][j] = (ret.data[i][j] + self.data[i][k] * rhs.data[k][j]) % MOD;
+                }
+            }
+        }
+
+        ret
+    }
+}
+
+struct SegmentTree {
+    matrix_init: Vec<Matrix>,
+    matrix_unit: Matrix,
+    data: Vec<Matrix>,
+}
+
+impl SegmentTree {
+    pub fn new(matrix_init: Vec<Matrix>, matrix_unit: Matrix) -> Self {
+        Self {
+            matrix_init,
+            matrix_unit,
+            data: vec![Matrix::new(); 1025],
+        }
+    }
+
+    pub fn init(&mut self, node: usize, start: usize, end: usize) {
+        if start == end {
+            self.data[node] = self.matrix_init[start].clone();
+            return;
+        }
+
+        let mid = (start + end) / 2;
+        self.init(node * 2, start, mid);
+        self.init(node * 2 + 1, mid + 1, end);
+
+        self.data[node] = self.data[node * 2].merge(&self.data[node * 2 + 1]);
+    }
+
+    pub fn update(&mut self, index: usize, val: &Matrix) {
+        self.update_internal(index, val, 1, 0, 255);
+    }
+
+    fn update_internal(
+        &mut self,
+        index: usize,
+        val: &Matrix,
+        node: usize,
+        node_start: usize,
+        node_end: usize,
+    ) {
+        if index > node_end || index < node_start {
+            return;
+        }
+
+        if node_start == node_end {
+            self.data[node] = val.clone();
+            return;
+        }
+
+        let mid = (node_start + node_end) / 2;
+        self.update_internal(index, val, node * 2, node_start, mid);
+        self.update_internal(index, val, node * 2 + 1, mid + 1, node_end);
+
+        let left = self.data[node * 2].clone();
+        let right = self.data[node * 2 + 1].clone();
+        self.data[node] = left.merge(&right);
+    }
+
+    fn query(&mut self, start: usize, end: usize) -> Matrix {
+        self.query_internal(start, end, 1, 0, 255)
+    }
+
+    fn query_internal(
+        &mut self,
+        start: usize,
+        end: usize,
+        node: usize,
+        node_start: usize,
+        node_end: usize,
+    ) -> Matrix {
+        if end < node_start || node_end < start {
+            return self.matrix_unit.clone();
+        }
+
+        if start <= node_start && node_end <= end {
+            return self.data[node].clone();
+        }
+
+        let mid = (node_start + node_end) / 2;
+        let left = self.query_internal(start, end, node * 2, node_start, mid);
+        let right = self.query_internal(start, end, node * 2 + 1, mid + 1, node_end);
+
+        left.clone() * right.clone()
+    }
+}
+
+static MOD: i64 = 1_000_000_007;
+
+fn main() {
+    let (stdin, stdout) = (io::stdin(), io::stdout());
+    let mut scan = UnsafeScanner::new(stdin.lock());
+    let mut out = io::BufWriter::new(stdout.lock());
+
+    let mut a = vec![vec![0; 32]; 256];
+    let mut matrix_init = vec![Matrix::new(); 256];
+    let mut matrix_unit = Matrix::new();
+
+    for i in 0..32 {
+        matrix_unit.data[i][i] = 1;
+    }
+
+    for i in 0..256 {
+        for j in 0..32 {
+            a[i][j] = scan.token::<usize>();
+        }
+
+        if i == 0 {
+            matrix_init[i] = matrix_unit.clone();
+        } else {
+            for j in 0..32 {
+                for k in 0..32 {
+                    matrix_init[i].data[a[i - 1][j] ^ a[i - 1][k]]
+                        [a[i][a[i - 1][j]] ^ a[i][a[i - 1][k]]] += 1;
+                }
+            }
+        }
+    }
+
+    let mut tree = SegmentTree::new(matrix_init, matrix_unit);
+    tree.init(1, 0, 255);
+
+    let t = scan.token::<i64>();
+
+    for _ in 0..t {
+        let l = scan.token::<usize>();
+
+        for i in 0..32 {
+            let val = scan.token::<usize>();
+            a[l][i] = val;
+        }
+
+        if l != 0 {
+            let mut vals = Matrix::new();
+
+            for i in 0..32 {
+                for j in 0..32 {
+                    vals.data[a[l - 1][i] ^ a[l - 1][j]][a[l][a[l - 1][i]] ^ a[l][a[l - 1][j]]] +=
+                        1;
+                }
+            }
+
+            tree.update(l, &vals);
+        }
+
+        if l != 255 {
+            let mut vals = Matrix::new();
+
+            for i in 0..32 {
+                for j in 0..32 {
+                    vals.data[a[l][i] ^ a[l][j]][a[l + 1][a[l][i]] ^ a[l + 1][a[l][j]]] += 1;
+                }
+            }
+
+            tree.update(l + 1, &vals);
+        }
+
+        let (x, y) = (scan.token::<usize>(), scan.token::<usize>());
+        let (n, m) = (scan.token::<usize>(), scan.token::<usize>());
+        let mut grundy = vec![vec![-1; n]; n];
+        let mut edges = vec![Vec::new(); n];
+        let mut nums_xor1 = Matrix::new();
+        let mut nums_xor2 = vec![vec![0; 64]; 32];
+        let mut sum_xor = vec![0; 32];
+        let mut visited: Vec<bool> = vec![false; n];
+        let mut visited_vertices = Vec::new();
+        let mut is_minus_one = false;
+
+        for _ in 0..m {
+            let (p, q) = (scan.token::<usize>(), scan.token::<usize>());
+            edges[p].push(q);
+        }
+
+        for i in 0..n {
+            if !visited[i] {
+                process_dfs(&mut grundy, &edges, &mut visited, &mut visited_vertices, i);
+            }
+        }
+
+        let k = scan.token::<usize>();
+        let mut colors = vec![Vec::new(); 32];
+
+        for _ in 0..k {
+            let (c, e) = (scan.token::<usize>(), scan.token::<usize>());
+            colors[c].push(e);
+        }
+
+        for i in 0..32 {
+            for j in 0..32 {
+                nums_xor1.data[i ^ j][a[x][i] ^ a[x][j]] += 1;
+            }
+        }
+
+        if x != y {
+            let ret = tree.query(x + 1, y);
+            nums_xor1 = nums_xor1 * ret;
+        }
+
+        for i in 0..n {
+            for j in 0..n {
+                for k in 0..32 {
+                    nums_xor2[k][grundy[i][j] as usize] =
+                        (nums_xor2[k][grundy[i][j] as usize] + nums_xor1.data[i ^ j][k]) % MOD;
+                    sum_xor[k] = (sum_xor[k] + nums_xor1.data[i ^ j][k]) % MOD;
+                }
+            }
+        }
+
+        let mut ret = vec![0; 64];
+        let mut is_initialized = false;
+
+        for i in 0..32 {
+            if colors[i].is_empty() {
+                continue;
+            }
+
+            let mut check = vec![0; 64];
+
+            if colors[i].len() == 1 {
+                for j in 0..n {
+                    check[grundy[j][j] as usize] += 1;
+                }
+
+                let p = pow(n as i64, MOD - 2, MOD);
+
+                for j in 0..64 {
+                    check[j] = (check[j] * p) % MOD;
+                }
+            } else {
+                let p = pow(sum_xor[colors[i][0] ^ colors[i][1]], MOD - 2, MOD);
+
+                if sum_xor[colors[i][0] ^ colors[i][1]] == 0 {
+                    is_minus_one = true;
+                }
+
+                for j in 0..64 {
+                    check[j] = (nums_xor2[colors[i][0] ^ colors[i][1]][j] * p) % MOD;
+                }
+            }
+
+            if is_initialized {
+                let mut ret_new = vec![0; 64];
+
+                for j in 0..64 {
+                    for k in 0..64 {
+                        ret_new[j ^ k] = (ret_new[j ^ k] + ret[j] * check[k]) % MOD;
+                    }
+                }
+
+                ret = ret_new;
+            } else {
+                ret = check.clone();
+                is_initialized = true;
+            }
+        }
+
+        writeln!(
+            out,
+            "{}",
+            if is_minus_one {
+                -1
+            } else {
+                (1 - ret[0] + MOD) % MOD
+            }
+        )
+        .unwrap();
+    }
+}

b9c8aaf0644259d88cf1269fe85f25809e9c4c5c
Chris Ohk <utilforever@gmail.com>
2024-04-04 02:14:52 +0900

    Update 1806 - Partial Sum

--- Rust/1806 - Partial Sum.rs
+++ Rust/1806 - Partial Sum.rs
@@ -1,46 +1,70 @@
-use std::io;
+use io::Write;
+use std::{io, str};
 
-fn input_integers() -> Vec<i32> {
-    let mut s = String::new();
-
-    io::stdin().read_line(&mut s).unwrap();
-
-    let values: Vec<i32> = s
-        .as_mut_str()
-        .split_whitespace()
-        .map(|s| s.parse().unwrap())
-        .collect();
-
-    values
+pub struct UnsafeScanner<R> {
+    reader: R,
+    buf_str: Vec<u8>,
+    buf_iter: str::SplitAsciiWhitespace<'static>,
 }
 
-fn main() {
-    let nums = input_integers();
-    let (n, s) = (nums[0], nums[1]);
-
-    let arr = input_integers();
-
-    let (mut left, mut right) = (0_i32, 0_i32);
-    let (mut sum, mut len, mut ans) = (0, 0, 1_000_000_000);
-
-    loop {
-        if sum >= s {
-            if len < ans {
-                ans = len;
-            }
-            sum -= arr[left as usize];
-            left += 1;
-            len -= 1;
-        } else {
-            if right == n {
-                break;
-            }
-
-            sum += arr[right as usize];
-            right += 1;
-            len += 1;
+impl<R: io::BufRead> UnsafeScanner<R> {
+    pub fn new(reader: R) -> Self {
+        Self {
+            reader,
+            buf_str: vec![],
+            buf_iter: "".split_ascii_whitespace(),
         }
     }
 
-    println!("{}", if ans == 1_000_000_000 { 0 } else { ans });
+    pub fn token<T: str::FromStr>(&mut self) -> T {
+        loop {
+            if let Some(token) = self.buf_iter.next() {
+                return token.parse().ok().expect("Failed parse");
+            }
+            self.buf_str.clear();
+            self.reader
+                .read_until(b'\n', &mut self.buf_str)
+                .expect("Failed read");
+            self.buf_iter = unsafe {
+                let slice = str::from_utf8_unchecked(&self.buf_str);
+                std::mem::transmute(slice.split_ascii_whitespace())
+            }
+        }
+    }
+}
+
+fn main() {
+    let (stdin, stdout) = (io::stdin(), io::stdout());
+    let mut scan = UnsafeScanner::new(stdin.lock());
+    let mut out = io::BufWriter::new(stdout.lock());
+
+    let (n, s) = (scan.token::<usize>(), scan.token::<i64>());
+    let mut nums = vec![0; n];
+
+    for i in 0..n {
+        nums[i] = scan.token::<i64>();
+    }
+
+    let mut left = 0;
+    let mut right = 0;
+    let mut sum = nums[0];
+    let mut ret = n + 1;
+
+    while left <= right {
+        if sum >= s {
+            ret = ret.min(right - left + 1);
+            sum -= nums[left];
+            left += 1;
+        } else {
+            right += 1;
+
+            if right >= n {
+                break;
+            }
+            
+            sum += nums[right];
+        }
+    }
+
+    writeln!(out, "{}", if ret == n + 1 { 0 } else { ret }).unwrap();
 }

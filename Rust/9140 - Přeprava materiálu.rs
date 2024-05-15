c205902b3c10785e7e11bf93f2feb24ba0ad1e2f
Chris Ohk <utilforever@gmail.com>
2024-05-15 14:08:10 +0900

    Upload 9140 - Přeprava materiálu

---
+++ Rust/9140 - Přeprava materiálu.rs
@@ -0,0 +1,98 @@
+use io::Write;
+use std::{collections::BinaryHeap, io, str};
+
+pub struct UnsafeScanner<R> {
+    reader: R,
+    buf_str: Vec<u8>,
+    buf_iter: str::SplitAsciiWhitespace<'static>,
+}
+
+impl<R: io::BufRead> UnsafeScanner<R> {
+    pub fn new(reader: R) -> Self {
+        Self {
+            reader,
+            buf_str: vec![],
+            buf_iter: "".split_ascii_whitespace(),
+        }
+    }
+
+    pub fn token<T: str::FromStr>(&mut self) -> T {
+        loop {
+            if let Some(token) = self.buf_iter.next() {
+                return token.parse().ok().expect("Failed parse");
+            }
+            self.buf_str.clear();
+            self.reader
+                .read_until(b'\n', &mut self.buf_str)
+                .expect("Failed read");
+            self.buf_iter = unsafe {
+                let slice = str::from_utf8_unchecked(&self.buf_str);
+                std::mem::transmute(slice.split_ascii_whitespace())
+            }
+        }
+    }
+}
+
+fn process_dijkstra(graph: &Vec<Vec<(usize, i64)>>, from: usize) -> Vec<i64> {
+    let mut ret = vec![i64::MAX / 4; graph.len()];
+    ret[from] = 0;
+
+    let mut queue = BinaryHeap::new();
+    queue.push((0, from));
+
+    while !queue.is_empty() {
+        let (mut cost_curr, vertex_curr) = queue.pop().unwrap();
+        cost_curr *= -1;
+
+        if ret[vertex_curr] < cost_curr {
+            continue;
+        }
+
+        for info in graph[vertex_curr].iter() {
+            let (vertex_next, mut cost_next) = *info;
+
+            cost_next += cost_curr;
+
+            if ret[vertex_next] > cost_next {
+                ret[vertex_next] = cost_next;
+                queue.push((-cost_next, vertex_next));
+            }
+        }
+    }
+
+    ret
+}
+
+fn main() {
+    let (stdin, stdout) = (io::stdin(), io::stdout());
+    let mut scan = UnsafeScanner::new(stdin.lock());
+    let mut out = io::BufWriter::new(stdout.lock());
+
+    loop {
+        let (n, m, s, c) = (
+            scan.token::<usize>(),
+            scan.token::<i64>(),
+            scan.token::<usize>(),
+            scan.token::<usize>(),
+        );
+
+        if n == 0 && m == 0 && s == 0 && c == 0 {
+            break;
+        }
+
+        let mut graph = vec![Vec::new(); n + 1];
+
+        for _ in 0..m {
+            let (a, b, v) = (
+                scan.token::<usize>(),
+                scan.token::<usize>(),
+                scan.token::<i64>(),
+            );
+            graph[a].push((b, v));
+        }
+
+        let ret = process_dijkstra(&graph, s);
+
+        writeln!(out, "{}", ret[c]).unwrap();
+    }
+}

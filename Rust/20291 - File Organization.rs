27a983c3cbfa97c41aeb1faae5ff5949bdc5f92b
Chris Ohk <utilforever@gmail.com>
2024-05-21 11:18:53 +0900

    Upload 20291 - File Organization

---
+++ Rust/20291 - File Organization.rs
@@ -0,0 +1,54 @@
+use io::Write;
+use std::{collections::BTreeMap, io, str};
+
+pub struct UnsafeScanner<R> {
+    reader: R,
+    buf_str: Vec<u8>,
+    buf_iter: str::SplitAsciiWhitespace<'static>,
+}
+
+impl<R: io::BufRead> UnsafeScanner<R> {
+    pub fn new(reader: R) -> Self {
+        Self {
+            reader,
+            buf_str: vec![],
+            buf_iter: "".split_ascii_whitespace(),
+        }
+    }
+
+    pub fn token<T: str::FromStr>(&mut self) -> T {
+        loop {
+            if let Some(token) = self.buf_iter.next() {
+                return token.parse().ok().expect("Failed parse");
+            }
+            self.buf_str.clear();
+            self.reader
+                .read_until(b'\n', &mut self.buf_str)
+                .expect("Failed read");
+            self.buf_iter = unsafe {
+                let slice = str::from_utf8_unchecked(&self.buf_str);
+                std::mem::transmute(slice.split_ascii_whitespace())
+            }
+        }
+    }
+}
+
+fn main() {
+    let (stdin, stdout) = (io::stdin(), io::stdout());
+    let mut scan = UnsafeScanner::new(stdin.lock());
+    let mut out = io::BufWriter::new(stdout.lock());
+
+    let n = scan.token::<i64>();
+    let mut ret = BTreeMap::new();
+
+    for _ in 0..n {
+        let file = scan.token::<String>();
+        let ext = file.split('.').last().unwrap().to_string();
+
+        ret.entry(ext).and_modify(|e| *e += 1).or_insert(1);
+    }
+
+    for (k, v) in ret {
+        writeln!(out, "{k} {v}").unwrap();
+    }
+}

de7f9d5fd0b296f5612478eddedead72357d8a0b
Chris Ohk <utilforever@gmail.com>
2024-05-31 17:52:05 +0900

    Upload 16946 - Break Down the Wall and Move 4

--- Rust/16946 - Break Down the Wall and Move 4.rs
+++ Rust/16946 - Break Down the Wall and Move 4.rs
@@ -1,157 +1,165 @@
-use std::{collections::HashSet, io};
+use io::Write;
+use std::{collections::HashSet, io, str};
 
-fn input_integers() -> Vec<i32> {
-    let mut s = String::new();
+pub struct UnsafeScanner<R> {
+    reader: R,
+    buf_str: Vec<u8>,
+    buf_iter: str::SplitAsciiWhitespace<'static>,
+}
 
-    io::stdin().read_line(&mut s).unwrap();
+impl<R: io::BufRead> UnsafeScanner<R> {
+    pub fn new(reader: R) -> Self {
+        Self {
+            reader,
+            buf_str: vec![],
+            buf_iter: "".split_ascii_whitespace(),
+        }
+    }
 
-    let values: Vec<i32> = s
-        .as_mut_str()
-        .split_whitespace()
-        .map(|s| s.parse().unwrap())
-        .collect();
-
-    values
+    pub fn token<T: str::FromStr>(&mut self) -> T {
+        loop {
+            if let Some(token) = self.buf_iter.next() {
+                return token.parse().ok().expect("Failed parse");
+            }
+            self.buf_str.clear();
+            self.reader
+                .read_until(b'\n', &mut self.buf_str)
+                .expect("Failed read");
+            self.buf_iter = unsafe {
+                let slice = str::from_utf8_unchecked(&self.buf_str);
+                std::mem::transmute(slice.split_ascii_whitespace())
+            }
+        }
+    }
 }
 
 fn process_dfs(
     maze: &Vec<Vec<i32>>,
     numbered_maze: &mut Vec<Vec<i32>>,
-    n: i32,
-    m: i32,
-    x: i32,
-    y: i32,
-    num_empty: i32,
     visited: &mut Vec<Vec<bool>>,
+    y_curr: usize,
+    x_curr: usize,
+    cnt_empty: i32,
+    n: usize,
+    m: usize,
 ) -> i32 {
+    numbered_maze[y_curr][x_curr] = cnt_empty;
+    visited[y_curr][x_curr] = true;
+
+    let dy = [-1, 1, 0, 0];
+    let dx = [0, 0, -1, 1];
     let mut ret = 1;
 
-    numbered_maze[y as usize][x as usize] = num_empty;
-    visited[y as usize][x as usize] = true;
-
     for i in 0..4 {
-        let next_x = x + if i == 0 {
-            -1
-        } else if i == 1 {
-            1
-        } else {
-            0
-        };
-        let next_y = y + if i == 2 {
-            -1
-        } else if i == 3 {
-            1
-        } else {
-            0
-        };
+        let y_next = y_curr as i32 + dy[i];
+        let x_next = x_curr as i32 + dx[i];
 
-        if next_x < 0 || next_x >= m || next_y < 0 || next_y >= n {
+        if y_next < 0 || y_next >= n as i32 || x_next < 0 || x_next >= m as i32 {
             continue;
         }
 
-        if !visited[next_y as usize][next_x as usize] && maze[next_y as usize][next_x as usize] == 0
-        {
-            ret += process_dfs(
-                maze,
-                numbered_maze,
-                n,
-                m,
-                next_x,
-                next_y,
-                num_empty,
-                visited,
-            )
+        let y_next = y_next as usize;
+        let x_next = x_next as usize;
+
+        if visited[y_next][x_next] || maze[y_next][x_next] == 1 {
+            continue;
         }
+
+        ret += process_dfs(
+            maze,
+            numbered_maze,
+            visited,
+            y_next,
+            x_next,
+            cnt_empty,
+            n,
+            m,
+        )
     }
 
     ret
 }
 
-fn process_flood_fill(maze: &mut Vec<Vec<i32>>, n: i32, m: i32) {
-    let mut numbered_maze = vec![vec![0; m as usize]; n as usize];
-    let mut visited = vec![vec![false; m as usize]; n as usize];
-    let mut vec = Vec::new();
-    let mut num_empty = 1;
-
-    vec.push(0);
-
-    for i in 0..n {
-        for j in 0..m {
-            if maze[i as usize][j as usize] == 0 && !visited[i as usize][j as usize] {
-                vec.push(process_dfs(
-                    &maze,
-                    &mut numbered_maze,
-                    n,
-                    m,
-                    j,
-                    i,
-                    num_empty,
-                    &mut visited,
-                ));
-                num_empty += 1;
-            }
-        }
-    }
-
-    for i in 0..n {
-        for j in 0..m {
-            if maze[i as usize][j as usize] == 1 {
-                let mut set = HashSet::new();
-
-                for k in 0..4 {
-                    let next_x = j + if k == 0 {
-                        -1
-                    } else if k == 1 {
-                        1
-                    } else {
-                        0
-                    };
-                    let next_y = i + if k == 2 {
-                        -1
-                    } else if k == 3 {
-                        1
-                    } else {
-                        0
-                    };
-
-                    if next_x < 0 || next_x >= m || next_y < 0 || next_y >= n {
-                        continue;
-                    }
-
-                    set.insert(numbered_maze[next_y as usize][next_x as usize]);
-                }
-
-                for k in set.iter() {
-                    maze[i as usize][j as usize] += vec[*k as usize];
-                }
-            }
-        }
-    }
-
-    for i in 0..n {
-        for j in 0..m {
-            print!("{}", maze[i as usize][j as usize] % 10);
-        }
-        println!();
-    }
-}
-
 fn main() {
-    let nums = input_integers();
-    let (n, m) = (nums[0] as usize, nums[1] as usize);
+    let (stdin, stdout) = (io::stdin(), io::stdout());
+    let mut scan = UnsafeScanner::new(stdin.lock());
+    let mut out = io::BufWriter::new(stdout.lock());
 
+    let (n, m) = (scan.token::<usize>(), scan.token::<usize>());
     let mut maze = vec![vec![0; m]; n];
 
     for i in 0..n {
-        let mut s = String::new();
-        io::stdin().read_line(&mut s).unwrap();
-        s = s.trim().to_string();
-        let mut chars = s.chars();
+        let line = scan.token::<String>();
 
-        for j in 0..m {
-            maze[i][j] = chars.next().unwrap() as i32 - '0' as i32;
+        for (j, c) in line.chars().enumerate() {
+            maze[i][j] = (c as u8 - b'0') as i32;
         }
     }
 
-    process_flood_fill(&mut maze, n as i32, m as i32);
+    let mut maze_numbered = vec![vec![0; m]; n];
+    let mut visited = vec![vec![false; m]; n];
+    let mut cnt = Vec::new();
+    let mut cnt_empty = 1;
+
+    cnt.push(0);
+
+    for i in 0..n {
+        for j in 0..m {
+            if visited[i][j] || maze[i][j] == 1 {
+                continue;
+            }
+
+            cnt.push(process_dfs(
+                &mut maze,
+                &mut maze_numbered,
+                &mut visited,
+                i,
+                j,
+                cnt_empty,
+                n,
+                m,
+            ));
+
+            cnt_empty += 1;
+        }
+    }
+
+    let dy = [-1, 1, 0, 0];
+    let dx = [0, 0, -1, 1];
+
+    for i in 0..n {
+        for j in 0..m {
+            if maze[i][j] == 0 {
+                continue;
+            }
+
+            let mut set = HashSet::new();
+
+            for k in 0..4 {
+                let y_next = i as i32 + dy[k];
+                let x_next = j as i32 + dx[k];
+
+                if y_next < 0 || y_next >= n as i32 || x_next < 0 || x_next >= m as i32 {
+                    continue;
+                }
+
+                let y_next = y_next as usize;
+                let x_next = x_next as usize;
+
+                set.insert(maze_numbered[y_next][x_next]);
+            }
+
+            for &idx in set.iter() {
+                maze[i][j] += cnt[idx as usize];
+            }
+        }
+    }
+
+    for i in 0..n {
+        for j in 0..m {
+            write!(out, "{}", maze[i][j] % 10).unwrap();
+        }
+
+        writeln!(out).unwrap();
+    }
 }

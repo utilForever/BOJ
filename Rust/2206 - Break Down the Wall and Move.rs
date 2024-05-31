5357f7a3bdb7ed2b818e2373e7eaaaa88c05bfd1
Chris Ohk <utilforever@gmail.com>
2024-05-31 17:03:18 +0900

    Update 2206 - Break Down the Wall and Move

--- Rust/2206 - Break Down the Wall and Move.rs
+++ Rust/2206 - Break Down the Wall and Move.rs
@@ -33,63 +33,69 @@ impl<R: io::BufRead> UnsafeScanner<R> {
     }
 }
 
-fn explore(maze: &Vec<Vec<char>>, visited: &mut Vec<Vec<Vec<i64>>>, n: usize, m: usize) -> i64 {
-    let dy: [i64; 4] = [0, 1, 0, -1];
-    let dx: [i64; 4] = [1, 0, -1, 0];
-
-    let mut queue = VecDeque::new();
-    queue.push_back((0, 0, 0));
-    visited[0][0][0] = 1;
-
-    while !queue.is_empty() {
-        let (y, x, num_break) = queue.pop_front().unwrap();
-
-        if y == n - 1 && x == m - 1 {
-            return visited[y as usize][x as usize][num_break];
-        }
-
-        for i in 0..4 {
-            let y_next: i64 = y as i64 + dy[i];
-            let x_next = x as i64 + dx[i];
-
-            if y_next < 0 || y_next >= n as i64 || x_next < 0 || x_next >= m as i64 {
-                continue;
-            }
-
-            let x_next = x_next as usize;
-            let y_next = y_next as usize;
-
-            if visited[y_next][x_next][num_break] == 0 {
-                if maze[y_next][x_next] == '0' {
-                    visited[y_next][x_next][num_break] = visited[y][x][num_break] + 1;
-                    queue.push_back((y_next, x_next, num_break));
-                } else if maze[y_next][x_next] == '1' && num_break == 0 {
-                    visited[y_next][x_next][num_break + 1] = visited[y][x][num_break] + 1;
-                    queue.push_back((y_next, x_next, num_break + 1));
-                }
-            }
-        }
-    }
-
-    -1
-}
-
 fn main() {
     let (stdin, stdout) = (io::stdin(), io::stdout());
     let mut scan = UnsafeScanner::new(stdin.lock());
     let mut out = io::BufWriter::new(stdout.lock());
 
-    let (n, m) = (scan.token::<usize>(), scan.token::<usize>());
-    let mut maze = vec![vec![' '; m]; n];
+    let (n, m) = (
+        scan.token::<usize>(),
+        scan.token::<usize>(),
+    );
+    let mut maze = vec![vec![0; m]; n];
     let mut visited = vec![vec![vec![0; 2]; m]; n];
 
     for i in 0..n {
-        let s = scan.token::<String>();
+        let line = scan.token::<String>();
 
-        for (j, c) in s.chars().enumerate() {
-            maze[i][j] = c;
+        for (j, c) in line.chars().enumerate() {
+            maze[i][j] = c as u8 - b'0';
         }
     }
 
-    writeln!(out, "{}", explore(&maze, &mut visited, n, m)).unwrap();
+    let dy = [-1, 1, 0, 0];
+    let dx = [0, 0, -1, 1];
+    let mut queue = VecDeque::new();
+
+    queue.push_back((0, 0, 0));
+    visited[0][0][0] = 1;
+
+    while !queue.is_empty() {
+        let (y_curr, x_curr, cnt_break) = queue.pop_front().unwrap();
+
+        if y_curr == n - 1 && x_curr == m - 1 {
+            writeln!(out, "{}", visited[y_curr][x_curr][cnt_break]).unwrap();
+            return;
+        }
+
+        for i in 0..4 {
+            let y_next = y_curr as i32 + dy[i];
+            let x_next = x_curr as i32 + dx[i];
+
+            if y_next < 0 || y_next >= n as i32 || x_next < 0 || x_next >= m as i32 {
+                continue;
+            }
+
+            let y_next = y_next as usize;
+            let x_next = x_next as usize;
+
+            if maze[y_next][x_next] == 0 {
+                if visited[y_next][x_next][cnt_break] > 0 {
+                    continue;
+                }
+
+                visited[y_next][x_next][cnt_break] = visited[y_curr][x_curr][cnt_break] + 1;
+                queue.push_back((y_next, x_next, cnt_break));
+            } else if maze[y_next][x_next] == 1 && cnt_break == 0 {
+                if visited[y_next][x_next][cnt_break + 1] > 0 {
+                    continue;
+                }
+
+                visited[y_next][x_next][cnt_break + 1] = visited[y_curr][x_curr][cnt_break] + 1;
+                queue.push_back((y_next, x_next, cnt_break + 1));
+            }
+        }
+    }
+
+    writeln!(out, "-1").unwrap();
 }

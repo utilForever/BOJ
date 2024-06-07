7177b5c73667a869bf12d8db012a9b406c7dbaf1
Chris Ohk <utilforever@gmail.com>
2024-06-08 02:22:04 +0900

    Update 1865 - Wormhole

--- Rust/1865 - Wormhole.rs
+++ Rust/1865 - Wormhole.rs
@@ -1,60 +1,105 @@
-use std::io;
+use io::Write;
+use std::{io, str};
 
-fn input_integers() -> Vec<i32> {
-    let mut s = String::new();
-
-    io::stdin().read_line(&mut s).unwrap();
-
-    let values: Vec<i32> = s
-        .as_mut_str()
-        .split_whitespace()
-        .map(|s| s.parse().unwrap())
-        .collect();
-
-    values
+pub struct UnsafeScanner<R> {
+    reader: R,
+    buf_str: Vec<u8>,
+    buf_iter: str::SplitAsciiWhitespace<'static>,
 }
 
-fn main() {
-    let tc = input_integers()[0];
-
-    for _ in 0..tc {
-        let nums = input_integers();
-        let (n, m, w) = (nums[0] as usize, nums[1] as usize, nums[2] as usize);
-
-        let mut vertex_info = vec![Vec::new(); n + 1];
-
-        for _ in 0..m {
-            let nums = input_integers();
-            let (s, e, t) = (nums[0], nums[1], nums[2]);
-
-            vertex_info[s as usize].push((e, t));
-            vertex_info[e as usize].push((s, t));
+impl<R: io::BufRead> UnsafeScanner<R> {
+    pub fn new(reader: R) -> Self {
+        Self {
+            reader,
+            buf_str: vec![],
+            buf_iter: "".split_ascii_whitespace(),
         }
+    }
 
-        for _ in 0..w {
-            let nums = input_integers();
-            let (s, e, t) = (nums[0], nums[1], nums[2]);
-
-            vertex_info[s as usize].push((e, -t));
+    pub fn token<T: str::FromStr>(&mut self) -> T {
+        loop {
+            if let Some(token) = self.buf_iter.next() {
+                return token.parse().ok().expect("Failed parse");
+            }
+            self.buf_str.clear();
+            self.reader
+                .read_until(b'\n', &mut self.buf_str)
+                .expect("Failed read");
+            self.buf_iter = unsafe {
+                let slice = str::from_utf8_unchecked(&self.buf_str);
+                std::mem::transmute(slice.split_ascii_whitespace())
+            }
         }
+    }
+}
 
-        let mut times = vec![0; n + 1];
-        let mut has_cycle = false;
+fn process_bellman_ford(graph: &Vec<Vec<(usize, i64)>>, from: usize) -> Option<Vec<i64>> {
+    let n: usize = graph.len() - 1;
+    let mut ret = vec![i64::MAX / 4; graph.len()];
 
-        for i in 1..=n {
-            for j in 1..=n {
-                for (to_vertex, time) in vertex_info[j].iter() {
-                    if times[*to_vertex as usize] > times[j] + time {
-                        times[*to_vertex as usize] = times[j] + time;
+    ret[from] = 0;
 
-                        if i == n {
-                            has_cycle = true;
-                        }
+    for i in 1..=n {
+        for j in 1..=n {
+            for info in graph[j].iter() {
+                let (vertex_next, cost_next) = *info;
+
+                if ret[vertex_next] > ret[j] + cost_next {
+                    ret[vertex_next] = ret[j] + cost_next;
+
+                    if i == n {
+                        return None;
                     }
                 }
             }
         }
+    }
 
-        println!("{}", if has_cycle { "YES" } else { "NO" });
+    Some(ret)
+}
+
+fn main() {
+    let (stdin, stdout) = (io::stdin(), io::stdout());
+    let mut scan = UnsafeScanner::new(stdin.lock());
+    let mut out = io::BufWriter::new(stdout.lock());
+
+    let tc = scan.token::<i64>();
+
+    for _ in 0..tc {
+        let (n, m, w) = (
+            scan.token::<usize>(),
+            scan.token::<i64>(),
+            scan.token::<i64>(),
+        );
+        let mut graph = vec![Vec::new(); n + 1];
+
+        for _ in 0..m {
+            let (s, e, t) = (
+                scan.token::<usize>(),
+                scan.token::<usize>(),
+                scan.token::<i64>(),
+            );
+            graph[s].push((e, t));
+            graph[e].push((s, t));
+        }
+
+        for _ in 0..w {
+            let (s, e, t) = (
+                scan.token::<usize>(),
+                scan.token::<usize>(),
+                scan.token::<i64>(),
+            );
+            graph[s].push((e, -t));
+        }
+
+        writeln!(
+            out,
+            "{}",
+            match process_bellman_ford(&graph, 1) {
+                Some(_) => "NO",
+                None => "YES",
+            }
+        )
+        .unwrap();
     }
 }

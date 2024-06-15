8d2da1ac39b5306c15f7ac93cbb703728ad72b43
Chris Ohk <utilforever@gmail.com>
2024-06-15 16:26:10 +0900

    Update 30039 - Queueueue

--- Rust/30039 - Queueueue.rs
+++ Rust/30039 - Queueueue.rs
@@ -54,13 +54,125 @@ impl Queueueue {
         }
     }
 
-    fn hpush(&mut self, x: i64) {}
+    fn check_shared_element_horizontal(&mut self) {
+        let len_front = self.queue_horizontal_front.len();
+        let len_back = self.queue_horizontal_back.len();
 
-    fn hpop(&mut self) -> i64 {}
+        // The shared element is middle of the queue (front + back)
 
-    fn hfront(&self) -> i64 {}
+        // In case of push, if the difference between the front and back is 2,
+        // all elements should be moved from back to front.
+        if len_front + 2 == len_back {
+            self.queue_horizontal_front.push_back(self.shared_element);
+            self.shared_element = self.queue_horizontal_back.pop_front().unwrap();
+            return;
+        }
 
-    fn hback(&self) -> i64 {}
+        // In case of pop, if the difference between the front and back is 1,
+        // all elements should be moved from back to front.
+        if len_front == len_back + 1 {
+            self.queue_horizontal_back.push_front(self.shared_element);
+            self.shared_element = self.queue_horizontal_front.pop_back().unwrap();
+            return;
+        }
+    }
+
+    fn check_shared_element_vertical(&mut self) {
+        let len_front = self.queue_vertical_front.len();
+        let len_back = self.queue_vertical_back.len();
+
+        // The shared element is middle of the queue (front + back)
+
+        // In case of push, if the difference between the front and back is 2,
+        // all elements should be moved from back to front.
+        if len_front + 2 == len_back {
+            self.queue_vertical_front.push_back(self.shared_element);
+            self.shared_element = self.queue_vertical_back.pop_front().unwrap();
+            return;
+        }
+
+        // In case of pop, if the difference between the front and back is 1,
+        // all elements should be moved from back to front.
+        if len_front == len_back + 1 {
+            self.queue_vertical_back.push_front(self.shared_element);
+            self.shared_element = self.queue_vertical_front.pop_back().unwrap();
+            return;
+        }
+    }
+
+    fn hpush(&mut self, x: i64) {
+        if self.size == 0 {
+            self.shared_element = x;
+            self.size = 1;
+            return;
+        }
+
+        self.queue_horizontal_back.push_back(x);
+        self.check_shared_element_horizontal();
+
+        self.size += 1;
+    }
+
+    fn hpop(&mut self) -> i64 {
+        if self.size == 0 {
+            -1
+        } else if self.queue_horizontal_front.is_empty() && self.queue_horizontal_back.is_empty() {
+            let val = self.shared_element;
+
+            if !self.queue_vertical_front.is_empty() {
+                self.shared_element = self.queue_vertical_front.pop_back().unwrap();
+                self.check_shared_element_vertical();
+            } else if !self.queue_vertical_back.is_empty() {
+                self.shared_element = self.queue_vertical_back.pop_front().unwrap();
+                self.check_shared_element_vertical();
+            } else {
+                self.shared_element = 0;
+            }
+
+            self.size -= 1;
+
+            val
+        } else if self.queue_horizontal_front.is_empty() {
+            let val = self.shared_element;
+
+            self.shared_element = self.queue_horizontal_back.pop_front().unwrap();
+            self.check_shared_element_horizontal();
+            self.size -= 1;
+
+            val
+        } else {
+            let val = self.queue_horizontal_front.pop_front().unwrap();
+
+            self.check_shared_element_horizontal();
+            self.size -= 1;
+
+            val
+        }
+    }
+
+    fn hfront(&self) -> i64 {
+        if self.size == 0 {
+            -1
+        } else if self.queue_horizontal_front.is_empty() && self.queue_horizontal_back.is_empty() {
+            self.shared_element
+        } else if self.queue_horizontal_front.is_empty() {
+            self.shared_element
+        } else {
+            *self.queue_horizontal_front.front().unwrap()
+        }
+    }
+
+    fn hback(&self) -> i64 {
+        if self.size == 0 {
+            -1
+        } else if self.queue_horizontal_front.is_empty() && self.queue_horizontal_back.is_empty() {
+            self.shared_element
+        } else if self.queue_horizontal_back.is_empty() {
+            self.shared_element
+        } else {
+            *self.queue_horizontal_back.back().unwrap()
+        }
+    }
 
     fn hsize(&self) -> usize {
         self.queue_horizontal_front.len()
@@ -68,13 +180,79 @@ impl Queueueue {
             + if self.size == 0 { 0 } else { 1 }
     }
 
-    fn vpush(&mut self, x: i64) {}
+    fn vpush(&mut self, x: i64) {
+        if self.size == 0 {
+            self.shared_element = x;
+            self.size = 1;
+            return;
+        }
 
-    fn vpop(&mut self) -> i64 {}
+        self.queue_vertical_back.push_back(x);
+        self.check_shared_element_vertical();
 
-    fn vfront(&self) -> i64 {}
+        self.size += 1;
+    }
 
-    fn vback(&self) -> i64 {}
+    fn vpop(&mut self) -> i64 {
+        if self.size == 0 {
+            -1
+        } else if self.queue_vertical_front.is_empty() && self.queue_vertical_back.is_empty() {
+            let val = self.shared_element;
+
+            if !self.queue_horizontal_front.is_empty() {
+                self.shared_element = self.queue_horizontal_front.pop_back().unwrap();
+                self.check_shared_element_horizontal();
+            } else if !self.queue_horizontal_back.is_empty() {
+                self.shared_element = self.queue_horizontal_back.pop_front().unwrap();
+                self.check_shared_element_horizontal();
+            } else {
+                self.shared_element = 0;
+            }
+
+            self.size -= 1;
+
+            val
+        } else if self.queue_vertical_front.is_empty() {
+            let val = self.shared_element;
+
+            self.shared_element = self.queue_vertical_back.pop_front().unwrap();
+            self.check_shared_element_vertical();
+            self.size -= 1;
+
+            val
+        } else {
+            let val = self.queue_vertical_front.pop_front().unwrap();
+
+            self.check_shared_element_vertical();
+            self.size -= 1;
+
+            val
+        }
+    }
+
+    fn vfront(&self) -> i64 {
+        if self.size == 0 {
+            -1
+        } else if self.queue_vertical_front.is_empty() && self.queue_vertical_back.is_empty() {
+            self.shared_element
+        } else if self.queue_vertical_front.is_empty() {
+            self.shared_element
+        } else {
+            *self.queue_vertical_front.front().unwrap()
+        }
+    }
+
+    fn vback(&self) -> i64 {
+        if self.size == 0 {
+            -1
+        } else if self.queue_vertical_front.is_empty() && self.queue_vertical_back.is_empty() {
+            self.shared_element
+        } else if self.queue_vertical_back.is_empty() {
+            self.shared_element
+        } else {
+            *self.queue_vertical_back.back().unwrap()
+        }
+    }
 
     fn vsize(&self) -> usize {
         self.queue_vertical_front.len()

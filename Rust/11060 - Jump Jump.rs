565683c89a3634217e5baff09790c56eb9cc24ea
Chris Ohk <utilforever@gmail.com>
2024-07-01 13:54:17 +0900

    Upload 11060 - Jump Jump

---
+++ Rust/11060 - Jump Jump.rs
@@ -0,0 +1,84 @@
+use io::Write;
+use std::{collections::VecDeque, io, str};
+
+pub struct UnsafeScanner<R> {
+    reader: R,
+    buf_str: Vec<u8>,
+    buf_iter: str::SplitAsciiWhitespace<'static>,
+}
+
+impl<R: io::BufRead> UnsafeScanner<R> {
+    pub fn new(reader: R) -> Self {
+        Self {
+            reader,
+            buf_str: vec![],
+            buf_iter: "".split_ascii_whitespace(),
+        }
+    }
+
+    pub fn token<T: str::FromStr>(&mut self) -> T {
+        loop {
+            if let Some(token) = self.buf_iter.next() {
+                return token.parse().ok().expect("Failed parse");
+            }
+            self.buf_str.clear();
+            self.reader
+                .read_until(b'\n', &mut self.buf_str)
+                .expect("Failed read");
+            self.buf_iter = unsafe {
+                let slice = str::from_utf8_unchecked(&self.buf_str);
+                std::mem::transmute(slice.split_ascii_whitespace())
+            }
+        }
+    }
+}
+
+fn main() {
+    let (stdin, stdout) = (io::stdin(), io::stdout());
+    let mut scan = UnsafeScanner::new(stdin.lock());
+    let mut out = io::BufWriter::new(stdout.lock());
+
+    let n = scan.token::<usize>();
+    let mut jumps = vec![0; n];
+    let mut positions = vec![0; n];
+
+    for i in 0..n {
+        jumps[i] = scan.token::<usize>();
+    }
+
+    if n == 1 {
+        writeln!(out, "0").unwrap();
+        return;
+    }
+
+    let mut queue = VecDeque::new();
+    queue.push_back(0);
+
+    while !queue.is_empty() {
+        let curr = queue.pop_front().unwrap();
+
+        if curr == n - 1 {
+            break;
+        }
+
+        for i in 1..=jumps[curr] {
+            if curr + i >= n || positions[curr + i] != 0 {
+                continue;
+            }
+
+            positions[curr + i] = positions[curr] + 1;
+            queue.push_back(curr + i);
+        }
+    }
+
+    writeln!(
+        out,
+        "{}",
+        if positions[n - 1] == 0 {
+            -1
+        } else {
+            positions[n - 1]
+        }
+    )
+    .unwrap();
+}

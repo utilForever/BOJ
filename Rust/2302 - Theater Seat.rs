956925ac6b3f77e7fe3ca58ec68d7bc80df105a1
Chris Ohk <utilforever@gmail.com>
2024-07-08 01:51:03 +0900

    Upload 2302 - Theater Seat

---
+++ Rust/2302 - Theater Seat.rs
@@ -0,0 +1,69 @@
+use io::Write;
+use std::{io, str};
+
+pub struct UnsafeScanner<R> {
+    reader: R,
+    buf_str: Vec<u8>,
+    buf_iter: str::SplitAsciiWhitespace<'static>,
+}
+
+impl<R: io::BufRead> UnsafeScanner<R> {
+    pub fn new(reader: R) -> Self {
+        Self {
+            reader,
+            buf_str: vec![],
+            buf_iter: "".split_ascii_whitespace(),
+        }
+    }
+
+    pub fn token<T: str::FromStr>(&mut self) -> T {
+        loop {
+            if let Some(token) = self.buf_iter.next() {
+                return token.parse().ok().expect("Failed parse");
+            }
+            self.buf_str.clear();
+            self.reader
+                .read_until(b'\n', &mut self.buf_str)
+                .expect("Failed read");
+            self.buf_iter = unsafe {
+                let slice = str::from_utf8_unchecked(&self.buf_str);
+                std::mem::transmute(slice.split_ascii_whitespace())
+            }
+        }
+    }
+}
+
+fn main() {
+    let (stdin, stdout) = (io::stdin(), io::stdout());
+    let mut scan = UnsafeScanner::new(stdin.lock());
+    let mut out = io::BufWriter::new(stdout.lock());
+
+    let (n, m) = (scan.token::<usize>(), scan.token::<usize>());
+    let mut seats = vec![0; m];
+
+    for i in 0..m {
+        seats[i] = scan.token::<usize>();
+    }
+
+    let mut dp = vec![0; n + 1];
+    dp[0] = 1;
+    dp[1] = 1;
+
+    for i in 2..=n {
+        dp[i] = dp[i - 1] + dp[i - 2];
+    }
+
+    let mut prev = 0;
+    let mut ret = 1;
+
+    for curr in seats {
+        let diff = curr - prev - 1;
+
+        ret *= dp[diff];
+        prev = curr;
+    }
+
+    ret *= dp[n - prev];
+
+    writeln!(out, "{ret}").unwrap();
+}

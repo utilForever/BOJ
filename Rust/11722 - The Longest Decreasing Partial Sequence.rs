d6dd8532f24cb999afe34f854ddcbb59d77fe438
Chris Ohk <utilforever@gmail.com>
2024-07-12 18:16:21 +0900

    Update 11722 - The Longest Decreasing Partial Sequence

--- Rust/11722 - The Longest Decreasing Partial Sequence.rs
+++ Rust/11722 - The Longest Decreasing Partial Sequence.rs
@@ -1,34 +1,61 @@
-use std::io;
+use io::Write;
+use std::{io, str};
 
-fn input_integers() -> Vec<i32> {
-    let mut s = String::new();
+pub struct UnsafeScanner<R> {
+    reader: R,
+    buf_str: Vec<u8>,
+    buf_iter: str::SplitAsciiWhitespace<'static>,
+}
 
-    io::stdin().read_line(&mut s).unwrap();
+impl<R: io::BufRead> UnsafeScanner<R> {
+    pub fn new(reader: R) -> Self {
+        Self {
+            reader,
+            buf_str: vec![],
+            buf_iter: "".split_ascii_whitespace(),
+        }
+    }
 
-    let values: Vec<i32> = s
-        .as_mut_str()
-        .split_whitespace()
-        .map(|s| s.parse().unwrap())
-        .collect();
-
-    values
+    pub fn token<T: str::FromStr>(&mut self) -> T {
+        loop {
+            if let Some(token) = self.buf_iter.next() {
+                return token.parse().ok().expect("Failed parse");
+            }
+            self.buf_str.clear();
+            self.reader
+                .read_until(b'\n', &mut self.buf_str)
+                .expect("Failed read");
+            self.buf_iter = unsafe {
+                let slice = str::from_utf8_unchecked(&self.buf_str);
+                std::mem::transmute(slice.split_ascii_whitespace())
+            }
+        }
+    }
 }
 
 fn main() {
-    let n = input_integers()[0] as usize;
+    let (stdin, stdout) = (io::stdin(), io::stdout());
+    let mut scan = UnsafeScanner::new(stdin.lock());
+    let mut out = io::BufWriter::new(stdout.lock());
 
-    let sequence = input_integers();
+    let n = scan.token::<usize>();
+    let mut nums = vec![0; n];
+
+    for i in 0..n {
+        nums[i] = scan.token::<i64>();
+    }
+
     let mut longest = vec![0; n];
 
     for i in (0..=n - 1).rev() {
         longest[i] = 1;
 
         for j in (i + 1..=n - 1).rev() {
-            if sequence[i] > sequence[j] && longest[i] < longest[j] + 1 {
+            if nums[i] > nums[j] && longest[i] < longest[j] + 1 {
                 longest[i] = longest[j] + 1;
             }
         }
     }
 
-    println!("{}", longest.iter().max().unwrap());
+    writeln!(out, "{}", longest.iter().max().unwrap()).unwrap();
 }

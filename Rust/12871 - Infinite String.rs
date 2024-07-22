b7d0c97e758a62450393f0ef8be7f7785fb58e57
Chris Ohk <utilforever@gmail.com>
2024-07-22 14:20:20 +0900

    Upload 12871 - Infinite String

---
+++ Rust/12871 - Infinite String.rs
@@ -0,0 +1,55 @@
+use io::Write;
+use std::{io, str};
+
+pub struct UnsafeScanner<R> {
+    reader: R,
+    buf_str: Vec<u8>,
+    buf_iter: str::SplitAsciiWhitespace<'static>,
+}
+
+impl<R: io::BufRead> UnsafeScanner<R> {
+    pub fn new(reader: R) -> Self {
+        Self {
+            reader,
+            buf_str: vec![],
+            buf_iter: "".split_ascii_whitespace(),
+        }
+    }
+
+    pub fn token<T: str::FromStr>(&mut self) -> T {
+        loop {
+            if let Some(token) = self.buf_iter.next() {
+                return token.parse().ok().expect("Failed parse");
+            }
+            self.buf_str.clear();
+            self.reader
+                .read_until(b'\n', &mut self.buf_str)
+                .expect("Failed read");
+            self.buf_iter = unsafe {
+                let slice = str::from_utf8_unchecked(&self.buf_str);
+                std::mem::transmute(slice.split_ascii_whitespace())
+            }
+        }
+    }
+}
+
+fn main() {
+    let (stdin, stdout) = (io::stdin(), io::stdout());
+    let mut scan = UnsafeScanner::new(stdin.lock());
+    let mut out = io::BufWriter::new(stdout.lock());
+
+    let (s, t) = (scan.token::<String>(), scan.token::<String>());
+    let (len_s, len_t) = (s.len(), t.len());
+    let mut ret_s = String::new();
+    let mut ret_t = String::new();
+
+    for _ in 0..len_t {
+        ret_s.push_str(&s);
+    }
+
+    for _ in 0..len_s {
+        ret_t.push_str(&t);
+    }
+
+    writeln!(out, "{}", if ret_s == ret_t { 1 } else { 0 }).unwrap();
+}

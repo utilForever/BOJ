5a176fdfd8cb7f717368c145424f3668affc3e73
Chris Ohk <utilforever@gmail.com>
2024-07-30 11:02:35 +0900

    Upload 17509 - And the Winner Is... Ourselves!

---
+++ Rust/17509 - And the Winner Is... Ourselves!.rs
@@ -0,0 +1,64 @@
+use io::Write;
+use std::{io, str};
+
+pub struct UnsafeScanner<R> {
+    reader: R,
+    buf_str: Vec<u8>,
+    buf_iter: str::SplitAsciiWhitespace<'static>,
+}
+
+impl<R: io::BufRead> UnsafeScanner<R> {
+    pub fn new(reader: R) -> Self {
+        Self {
+            reader,
+            buf_str: vec![],
+            buf_iter: "".split_ascii_whitespace(),
+        }
+    }
+
+    pub fn token<T: str::FromStr>(&mut self) -> T {
+        loop {
+            if let Some(token) = self.buf_iter.next() {
+                return token.parse().ok().expect("Failed parse");
+            }
+            self.buf_str.clear();
+            self.reader
+                .read_until(b'\n', &mut self.buf_str)
+                .expect("Failed read");
+            self.buf_iter = unsafe {
+                let slice = str::from_utf8_unchecked(&self.buf_str);
+                std::mem::transmute(slice.split_ascii_whitespace())
+            }
+        }
+    }
+
+    pub fn line(&mut self) -> String {
+        let mut input = String::new();
+        self.reader.read_line(&mut input).expect("Failed read");
+        input
+    }
+}
+
+fn main() {
+    let (stdin, stdout) = (io::stdin(), io::stdout());
+    let mut scan = UnsafeScanner::new(stdin.lock());
+    let mut out = io::BufWriter::new(stdout.lock());
+
+    let mut problems = [(0, 0); 11];
+
+    for i in 0..11 {
+        problems[i] = (scan.token::<i64>(), scan.token::<i64>());
+    }
+
+    problems.sort_by(|a, b| a.0.cmp(&b.0));
+
+    let mut time = 0;
+    let mut ret = 0;
+
+    for i in 0..11 {
+        time += problems[i].0;
+        ret += time + 20 * problems[i].1;
+    }
+
+    writeln!(out, "{ret}").unwrap();
+}

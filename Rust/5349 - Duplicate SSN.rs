ad05a544e46150c681296a6431711e245effc408
Chris Ohk <utilforever@gmail.com>
2024-07-31 14:43:11 +0900

    Upload 5349 - Duplicate SSN

---
+++ Rust/5349 - Duplicate SSN.rs
@@ -0,0 +1,70 @@
+use io::Write;
+use std::{collections::HashSet, io, str};
+
+pub struct UnsafeScanner<R> {
+    reader: R,
+    buf_str: Vec<u8>,
+    buf_iter: str::SplitAsciiWhitespace<'static>,
+}
+
+impl<R: io::BufRead> UnsafeScanner<R> {
+    pub fn new(reader: R) -> Self {
+        Self {
+            reader,
+            buf_str: vec![],
+            buf_iter: "".split_ascii_whitespace(),
+        }
+    }
+
+    pub fn token<T: str::FromStr>(&mut self) -> T {
+        loop {
+            if let Some(token) = self.buf_iter.next() {
+                return token.parse().ok().expect("Failed parse");
+            }
+            self.buf_str.clear();
+            self.reader
+                .read_until(b'\n', &mut self.buf_str)
+                .expect("Failed read");
+            self.buf_iter = unsafe {
+                let slice = str::from_utf8_unchecked(&self.buf_str);
+                std::mem::transmute(slice.split_ascii_whitespace())
+            }
+        }
+    }
+
+    pub fn line(&mut self) -> String {
+        let mut input = String::new();
+        self.reader.read_line(&mut input).expect("Failed read");
+        input
+    }
+}
+
+fn main() {
+    let (stdin, stdout) = (io::stdin(), io::stdout());
+    let mut scan = UnsafeScanner::new(stdin.lock());
+    let mut out = io::BufWriter::new(stdout.lock());
+
+    let mut set = HashSet::new();
+    let mut ret = Vec::new();
+
+    loop {
+        let ssn = scan.token::<String>();
+
+        if ssn == "000-00-0000" {
+            break;
+        }
+
+        if set.contains(&ssn) {
+            ret.push(ssn);
+        } else {
+            set.insert(ssn);
+        }
+    }
+
+    ret.sort();
+    ret.dedup();
+
+    for val in ret {
+        writeln!(out, "{val}").unwrap();
+    }
+}

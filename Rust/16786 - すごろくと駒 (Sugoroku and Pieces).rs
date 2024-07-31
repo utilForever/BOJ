1169582e54005549f0ab0e8cc46fbefa5ab5fcb0
Chris Ohk <utilforever@gmail.com>
2024-07-31 10:49:11 +0900

    Upload 16786 - すごろくと駒 (Sugoroku and Pieces)

---
+++ Rust/16786 - すごろくと駒 (Sugoroku and Pieces).rs
@@ -0,0 +1,78 @@
+use io::Write;
+use std::{io, str};
+
+pub struct UnsafeScanner<R> {
+    reader: R,
+    buf_str: Vec<u8>,
+    buf_iter: str::SplitAsciiWhitespace<'static>,
+}
+
+impl<R: io::BufRead> UnsafeScanner<R> {
+    pub fn new(reader: R) -> Self {
+        Self {
+            reader,
+            buf_str: vec![],
+            buf_iter: "".split_ascii_whitespace(),
+        }
+    }
+
+    pub fn token<T: str::FromStr>(&mut self) -> T {
+        loop {
+            if let Some(token) = self.buf_iter.next() {
+                return token.parse().ok().expect("Failed parse");
+            }
+            self.buf_str.clear();
+            self.reader
+                .read_until(b'\n', &mut self.buf_str)
+                .expect("Failed read");
+            self.buf_iter = unsafe {
+                let slice = str::from_utf8_unchecked(&self.buf_str);
+                std::mem::transmute(slice.split_ascii_whitespace())
+            }
+        }
+    }
+
+    pub fn line(&mut self) -> String {
+        let mut input = String::new();
+        self.reader.read_line(&mut input).expect("Failed read");
+        input
+    }
+}
+
+fn main() {
+    let (stdin, stdout) = (io::stdin(), io::stdout());
+    let mut scan = UnsafeScanner::new(stdin.lock());
+    let mut out = io::BufWriter::new(stdout.lock());
+
+    let n = scan.token::<usize>();
+    let mut positions = vec![0; n];
+
+    for i in 0..n {
+        let x = scan.token::<usize>();
+        positions[i] = x;
+    }
+
+    let m = scan.token::<i64>();
+
+    for _ in 0..m {
+        let a = scan.token::<usize>();
+
+        if a == n {
+            if positions[n - 1] < 2019 {
+                positions[n - 1] += 1;
+            }
+
+            continue;
+        }
+
+        if positions[a - 1] + 1 == positions[a] {
+            continue;
+        }
+
+        positions[a - 1] += 1;
+    }
+
+    for val in positions {
+        writeln!(out, "{val}").unwrap();
+    }
+}

f6f60d3837326b596795f38312870530795f2206
Chris Ohk <utilforever@gmail.com>
2024-08-08 12:50:49 +0900

    Upload 18766 - Card Game

---
+++ 18000/18766 - Card Game.rs
@@ -0,0 +1,75 @@
+use io::Write;
+use std::{collections::BTreeMap, io, str};
+
+pub struct UnsafeScanner<R> {
+    reader: R,
+    buf_str: Vec<u8>,
+    buf_iter: str::SplitAsciiWhitespace<'static>,
+}
+
+impl<R: io::BufRead> UnsafeScanner<R> {
+    pub fn new(reader: R) -> Self {
+        Self {
+            reader,
+            buf_str: vec![],
+            buf_iter: "".split_ascii_whitespace(),
+        }
+    }
+
+    pub fn token<T: str::FromStr>(&mut self) -> T {
+        loop {
+            if let Some(token) = self.buf_iter.next() {
+                return token.parse().ok().expect("Failed parse");
+            }
+            self.buf_str.clear();
+            self.reader
+                .read_until(b'\n', &mut self.buf_str)
+                .expect("Failed read");
+            self.buf_iter = unsafe {
+                let slice = str::from_utf8_unchecked(&self.buf_str);
+                std::mem::transmute(slice.split_ascii_whitespace())
+            }
+        }
+    }
+
+    pub fn line(&mut self) -> String {
+        let mut input = String::new();
+        self.reader.read_line(&mut input).expect("Failed read");
+        input
+    }
+}
+
+fn main() {
+    let (stdin, stdout) = (io::stdin(), io::stdout());
+    let mut scan = UnsafeScanner::new(stdin.lock());
+    let mut out = io::BufWriter::new(stdout.lock());
+
+    let t = scan.token::<i64>();
+
+    for _ in 0..t {
+        let n = scan.token::<i64>();
+        let mut cards1 = BTreeMap::new();
+        let mut cards2 = BTreeMap::new();
+
+        for _ in 0..n {
+            let card = scan.token::<String>();
+            *cards1.entry(card).or_insert(0) += 1;
+        }
+
+        for _ in 0..n {
+            let card = scan.token::<String>();
+            *cards2.entry(card).or_insert(0) += 1;
+        }
+
+        writeln!(
+            out,
+            "{}",
+            if cards1 == cards2 {
+                "NOT CHEATER"
+            } else {
+                "CHEATER"
+            }
+        )
+        .unwrap();
+    }
+}

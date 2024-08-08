47212ed080569867395ad2391cd7cc518bf2b494
Chris Ohk <utilforever@gmail.com>
2024-08-08 11:29:20 +0900

    Upload 4482 - Tetrahedral Stacks of Cannonballs

---
+++ 04000/4482 - Tetrahedral Stacks of Cannonballs.rs
@@ -0,0 +1,60 @@
+use io::Write;
+use std::{io, str};
+
+pub struct UnsafeScanner<R> {
+    reader: R,
+    buf_str: Vec<u8>,
+    buf_iter: str::SplitAsciiWhitespace<'static>,
+}
+
+impl<R: io::BufRead> UnsafeScanner<R> {
+    pub fn new(reader: R) -> Self {
+        Self {
+            reader,
+            buf_str: vec![],
+            buf_iter: "".split_ascii_whitespace(),
+        }
+    }
+
+    pub fn token<T: str::FromStr>(&mut self) -> T {
+        loop {
+            if let Some(token) = self.buf_iter.next() {
+                return token.parse().ok().expect("Failed parse");
+            }
+            self.buf_str.clear();
+            self.reader
+                .read_until(b'\n', &mut self.buf_str)
+                .expect("Failed read");
+            self.buf_iter = unsafe {
+                let slice = str::from_utf8_unchecked(&self.buf_str);
+                std::mem::transmute(slice.split_ascii_whitespace())
+            }
+        }
+    }
+
+    pub fn line(&mut self) -> String {
+        let mut input = String::new();
+        self.reader.read_line(&mut input).expect("Failed read");
+        input
+    }
+}
+
+fn main() {
+    let (stdin, stdout) = (io::stdin(), io::stdout());
+    let mut scan = UnsafeScanner::new(stdin.lock());
+    let mut out = io::BufWriter::new(stdout.lock());
+
+    let n = scan.token::<i64>();
+    let mut sum = vec![0; 1001];
+    let mut val = 0;
+
+    for i in 1..=1000 {
+        val += i * (i + 1) / 2;
+        sum[i] = val;
+    }
+
+    for i in 1..=n {
+        let num = scan.token::<usize>();
+        writeln!(out, "{i}: {num} {}", sum[num]).unwrap();
+    }
+}

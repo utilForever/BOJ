2953faa89bb4421e087ee9e49cf59f44f8e245f2
Chris Ohk <utilforever@gmail.com>
2024-08-08 23:57:51 +0900

    Upload 30527 - Cornhusker

---
+++ 30000/30527 - Cornhusker.rs
@@ -0,0 +1,61 @@
+use io::Write;
+use std::{io, str};
+
+pub struct UnsafeScanner<R> {
+    reader: R,
+    buf_str: Vec<u8>,
+    buf_iter: str::SplitAsciiWhitespace<'static>,
+}
+
+impl<R: io::BufRead> UnsafeScanner<R> {
+    pub fn new(reader: R) -> Self {
+        Self {
+            reader,
+            buf_str: vec![],
+            buf_iter: "".split_ascii_whitespace(),
+        }
+    }
+
+    pub fn token<T: str::FromStr>(&mut self) -> T {
+        loop {
+            if let Some(token) = self.buf_iter.next() {
+                return token.parse().ok().expect("Failed parse");
+            }
+            self.buf_str.clear();
+            self.reader
+                .read_until(b'\n', &mut self.buf_str)
+                .expect("Failed read");
+            self.buf_iter = unsafe {
+                let slice = str::from_utf8_unchecked(&self.buf_str);
+                std::mem::transmute(slice.split_ascii_whitespace())
+            }
+        }
+    }
+
+    pub fn line(&mut self) -> String {
+        let mut input = String::new();
+        self.reader.read_line(&mut input).expect("Failed read");
+        input
+    }
+}
+
+fn main() {
+    let (stdin, stdout) = (io::stdin(), io::stdout());
+    let mut scan = UnsafeScanner::new(stdin.lock());
+    let mut out = io::BufWriter::new(stdout.lock());
+
+    let mut corns = [(0, 0); 5];
+
+    for i in 0..5 {
+        corns[i] = (scan.token::<i64>(), scan.token::<i64>());
+    }
+
+    let (n, kwf) = (scan.token::<i64>(), scan.token::<i64>());
+    let mut sum = 0;
+
+    for i in 0..5 {
+        sum += corns[i].0 * corns[i].1;
+    }
+
+    writeln!(out, "{}", n * (sum / 5) / kwf).unwrap();
+}

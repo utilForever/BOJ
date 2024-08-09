3e797108ddc9ecd50bba1c001d64b8bbaa20c58f
Chris Ohk <utilforever@gmail.com>
2024-08-10 02:38:16 +0900

    Update 25563 - AND, OR, XOR

--- 25000/25563 - AND, OR, XOR.rs
+++ 25000/25563 - AND, OR, XOR.rs
@@ -31,48 +31,140 @@ impl<R: io::BufRead> UnsafeScanner<R> {
             }
         }
     }
+
+    pub fn line(&mut self) -> String {
+        let mut input = String::new();
+        self.reader.read_line(&mut input).expect("Failed read");
+        input
+    }
 }
 
+pub mod fwht {
+    pub fn or(nums: &mut Vec<i64>, inv: bool) {
+        let n = nums.len();
+        let dir = if inv { -1 } else { 1 };
+        let mut s = 2;
+        let mut h = 1;
+
+        while s <= n {
+            for i in (0..n).step_by(s) {
+                for j in 0..h {
+                    nums[i + h + j] += dir * nums[i + j];
+                }
+            }
+
+            s *= 2;
+            h *= 2;
+        }
+    }
+
+    pub fn and(nums: &mut Vec<i64>, inv: bool) {
+        let n = nums.len();
+        let dir = if inv { -1 } else { 1 };
+        let mut s = 2;
+        let mut h = 1;
+
+        while s <= n {
+            for i in (0..n).step_by(s) {
+                for j in 0..h {
+                    nums[i + j] += dir * nums[i + h + j];
+                }
+            }
+
+            s *= 2;
+            h *= 2;
+        }
+    }
+
+    pub fn xor(nums: &mut Vec<i64>, inv: bool) {
+        let n = nums.len();
+        let mut s = 2;
+        let mut h = 1;
+
+        while s <= n {
+            for i in (0..n).step_by(s) {
+                for j in 0..h {
+                    let t = nums[i + h + j];
+                    nums[i + h + j] = nums[i + j] - t;
+                    nums[i + j] += t;
+
+                    if inv {
+                        nums[i + h + j] /= 2;
+                        nums[i + j] /= 2;
+                    }
+                }
+            }
+
+            s *= 2;
+            h *= 2;
+        }
+    }
+
+    pub fn convolution_or(nums: &Vec<i64>, idx: usize) -> i64 {
+        let mut nums_cloned = nums.clone();
+
+        or(&mut nums_cloned, false);
+
+        for i in 0..nums.len() {
+            nums_cloned[i] *= nums_cloned[i];
+        }
+
+        or(&mut nums_cloned, true);
+
+        (nums_cloned[idx] - nums[idx]) / 2
+    }
+
+    pub fn convolution_and(nums: &Vec<i64>, idx: usize) -> i64 {
+        let mut nums_cloned = nums.clone();
+
+        and(&mut nums_cloned, false);
+
+        for i in 0..nums.len() {
+            nums_cloned[i] *= nums_cloned[i];
+        }
+
+        and(&mut nums_cloned, true);
+
+        (nums_cloned[idx] - nums[idx]) / 2
+    }
+
+    pub fn convolution_xor(nums: &Vec<i64>, idx: usize) -> i64 {
+        let mut nums_cloned = nums.clone();
+
+        xor(&mut nums_cloned, false);
+
+        for i in 0..nums_cloned.len() {
+            nums_cloned[i] *= nums_cloned[i];
+        }
+
+        xor(&mut nums_cloned, true);
+
+        if idx == 0 {
+            nums_cloned[0] -= nums.iter().sum::<i64>();
+        }
+
+        nums_cloned[idx] / 2
+    }
+}
+
+// Reference: https://gina65.tistory.com/30
+// Reference: SUAPC 2022 Summer Editorial
 fn main() {
     let (stdin, stdout) = (io::stdin(), io::stdout());
     let mut scan = UnsafeScanner::new(stdin.lock());
     let mut out = io::BufWriter::new(stdout.lock());
 
-    let (n, k) = (scan.token::<usize>(), scan.token::<i64>());
-    let mut nums = Vec::new();
-    let mut dp = vec![0; 1000001];
-    let mut ret_and = 0;
-    
+    let (n, k) = (scan.token::<i64>(), scan.token::<usize>());
+    let mut nums = vec![0; 1048576];
+
     for _ in 0..n {
-        let a = scan.token::<i64>();
-
-        nums.push(a);
-        dp[a as usize] += 1;
+        let idx = scan.token::<usize>();
+        nums[idx] += 1;
     }
 
-    for i in 0..n {
-        if (nums[i] & k) == k {
-            dp[i - k as usize] += nums[i];
-        }
-    }
+    let and = fwht::convolution_and(&nums, k);
+    let or = fwht::convolution_or(&nums, k);
+    let xor = fwht::convolution_xor(&nums, k);
 
-    for i in 0..20 {
-        for j in 1..=1000000 {
-            if j & (1 << i) != 0 {
-                dp[j] += dp[j ^ (1 << i)];
-            }
-        }
-    }
-
-    for i in 0..n {
-        if (nums[i] & k) == k {
-            ret_and += dp[(1000000 + k - nums[i]) as usize];
-
-            if nums[i] == k {
-                ret_and -= nums[i];
-            }
-        }
-    }
-
-    writeln!(out, "{ret_and}").unwrap();
+    writeln!(out, "{and} {or} {xor}").unwrap();
 }

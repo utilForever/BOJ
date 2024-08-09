09cdeb894046ca7ba28d9e3eb38d83160abd70f1
Chris Ohk <utilforever@gmail.com>
2024-08-09 13:38:01 +0900

    Upload 11292 - Tallest

---
+++ 11000/11292 - Tallest.rs
@@ -0,0 +1,70 @@
+use io::Write;
+use std::{io, str};
+
+pub struct UnsafeScanner<R> {
+    reader: R,
+    buf_str: Vec<u8>,
+    buf_iter: str::SplitAsciiWhitespace<'static>,
+}
+
+impl<R: io::BufRead> UnsafeScanner<R> {
+    pub fn new(reader: R) -> Self {
+        Self {
+            reader,
+            buf_str: vec![],
+            buf_iter: "".split_ascii_whitespace(),
+        }
+    }
+
+    pub fn token<T: str::FromStr>(&mut self) -> T {
+        loop {
+            if let Some(token) = self.buf_iter.next() {
+                return token.parse().ok().expect("Failed parse");
+            }
+            self.buf_str.clear();
+            self.reader
+                .read_until(b'\n', &mut self.buf_str)
+                .expect("Failed read");
+            self.buf_iter = unsafe {
+                let slice = str::from_utf8_unchecked(&self.buf_str);
+                std::mem::transmute(slice.split_ascii_whitespace())
+            }
+        }
+    }
+
+    pub fn line(&mut self) -> String {
+        let mut input = String::new();
+        self.reader.read_line(&mut input).expect("Failed read");
+        input
+    }
+}
+
+fn main() {
+    let (stdin, stdout) = (io::stdin(), io::stdout());
+    let mut scan = UnsafeScanner::new(stdin.lock());
+    let mut out = io::BufWriter::new(stdout.lock());
+
+    loop {
+        let n = scan.token::<usize>();
+
+        if n == 0 {
+            break;
+        }
+
+        let mut heights = vec![(String::new(), 0.0); n];
+        let mut height_max = 0.0f64;
+
+        for i in 0..n {
+            heights[i] = (scan.token::<String>(), scan.token::<f64>());
+            height_max = height_max.max(heights[i].1);
+        }
+
+        for (name, height) in heights {
+            if height == height_max {
+                write!(out, "{name} ").unwrap();
+            }
+        }
+
+        writeln!(out).unwrap();
+    }
+}

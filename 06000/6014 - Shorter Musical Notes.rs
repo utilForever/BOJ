395362420426e39d74e07866bb60896c63f2d41b
Chris Ohk <utilforever@gmail.com>
2024-08-11 15:40:33 +0900

    Upload 6014 - Shorter Musical Notes

---
+++ 06000/6014 - Shorter Musical Notes.rs
@@ -0,0 +1,67 @@
+use io::Write;
+use std::{io, str};
+
+pub struct UnsafeScanner<R> {
+    reader: R,
+    buf_str: Vec<u8>,
+    buf_iter: str::SplitAsciiWhitespace<'static>,
+}
+
+impl<R: io::BufRead> UnsafeScanner<R> {
+    pub fn new(reader: R) -> Self {
+        Self {
+            reader,
+            buf_str: vec![],
+            buf_iter: "".split_ascii_whitespace(),
+        }
+    }
+
+    pub fn token<T: str::FromStr>(&mut self) -> T {
+        loop {
+            if let Some(token) = self.buf_iter.next() {
+                return token.parse().ok().expect("Failed parse");
+            }
+            self.buf_str.clear();
+            self.reader
+                .read_until(b'\n', &mut self.buf_str)
+                .expect("Failed read");
+            self.buf_iter = unsafe {
+                let slice = str::from_utf8_unchecked(&self.buf_str);
+                std::mem::transmute(slice.split_ascii_whitespace())
+            }
+        }
+    }
+
+    pub fn line(&mut self) -> String {
+        let mut input = String::new();
+        self.reader.read_line(&mut input).expect("Failed read");
+        input
+    }
+}
+
+fn main() {
+    let (stdin, stdout) = (io::stdin(), io::stdout());
+    let mut scan = UnsafeScanner::new(stdin.lock());
+    let mut out = io::BufWriter::new(stdout.lock());
+
+    let (n, q) = (scan.token::<usize>(), scan.token::<i64>());
+    let mut notes = vec![0; 1_200_001];
+    let mut idx = 0;
+    let mut val = 1;
+
+    for _ in 0..n {
+        let b = scan.token::<usize>();
+
+        for _ in idx..idx + b {
+            notes[idx] = val;
+            idx += 1;
+        }
+
+        val += 1;
+    }
+
+    for _ in 0..q {
+        let t = scan.token::<usize>();
+        writeln!(out, "{}", notes[t]).unwrap();
+    }
+}

57a54ee1ac0abdae0d0d660e863adb4312ec47d4
Chris Ohk <utilforever@gmail.com>
2024-08-18 02:22:03 +0900

    Upload 3770 - Japan

---
+++ 03000/3770 - Japan.rs
@@ -0,0 +1,210 @@
+use io::Write;
+use std::{cmp::Ordering, io, str};
+use Ordering::Less;
+
+pub struct UnsafeScanner<R> {
+    reader: R,
+    buf_str: Vec<u8>,
+    buf_iter: str::SplitAsciiWhitespace<'static>,
+}
+
+impl<R: io::BufRead> UnsafeScanner<R> {
+    pub fn new(reader: R) -> Self {
+        Self {
+            reader,
+            buf_str: vec![],
+            buf_iter: "".split_ascii_whitespace(),
+        }
+    }
+
+    pub fn token<T: str::FromStr>(&mut self) -> T {
+        loop {
+            if let Some(token) = self.buf_iter.next() {
+                return token.parse().ok().expect("Failed parse");
+            }
+            self.buf_str.clear();
+            self.reader
+                .read_until(b'\n', &mut self.buf_str)
+                .expect("Failed read");
+            self.buf_iter = unsafe {
+                let slice = str::from_utf8_unchecked(&self.buf_str);
+                std::mem::transmute(slice.split_ascii_whitespace())
+            }
+        }
+    }
+}
+
+pub trait Ext {
+    type Item;
+
+    fn lower_bound(&self, x: &Self::Item) -> usize
+    where
+        Self::Item: Ord;
+
+    fn lower_bound_by<'a, F>(&'a self, f: F) -> usize
+    where
+        F: FnMut(&'a Self::Item) -> Ordering;
+}
+
+impl<T> Ext for [T] {
+    type Item = T;
+    fn lower_bound(&self, x: &Self::Item) -> usize
+    where
+        T: Ord,
+    {
+        self.lower_bound_by(|y| y.cmp(x))
+    }
+    fn lower_bound_by<'a, F>(&'a self, mut f: F) -> usize
+    where
+        F: FnMut(&'a Self::Item) -> Ordering,
+    {
+        let s = self;
+        let mut size = s.len();
+        if size == 0 {
+            return 0;
+        }
+        let mut base = 0usize;
+        while size > 1 {
+            let half = size / 2;
+            let mid = base + half;
+            let cmp = f(unsafe { s.get_unchecked(mid) });
+            base = if cmp == Less { mid } else { base };
+            size -= half;
+        }
+        let cmp = f(unsafe { s.get_unchecked(base) });
+        base + (cmp == Less) as usize
+    }
+}
+
+#[derive(Clone, Debug)]
+struct Node {
+    sum: i64,
+}
+
+impl Node {
+    fn new(val: i64) -> Self {
+        Self { sum: val }
+    }
+
+    fn merge(&mut self, other: &Self) -> Node {
+        let mut ret = Node::new(0);
+        ret.sum = self.sum + other.sum;
+
+        ret
+    }
+}
+
+struct SegmentTree {
+    size: usize,
+    data: Vec<Node>,
+}
+
+impl SegmentTree {
+    pub fn new(n: usize) -> Self {
+        let mut real_n = 1;
+        while real_n < n {
+            real_n *= 2;
+        }
+
+        Self {
+            size: n,
+            data: vec![Node::new(0); real_n * 4],
+        }
+    }
+
+    pub fn update(&mut self, index: usize, val: i64) {
+        self.update_internal(index, val, 1, 1, self.size);
+    }
+
+    fn update_internal(
+        &mut self,
+        index: usize,
+        val: i64,
+        node: usize,
+        node_start: usize,
+        node_end: usize,
+    ) {
+        if index > node_end || index < node_start {
+            return;
+        }
+
+        if node_start == node_end {
+            self.data[node].sum += 1;
+            return;
+        }
+
+        let mid = (node_start + node_end) / 2;
+        self.update_internal(index, val, node * 2, node_start, mid);
+        self.update_internal(index, val, node * 2 + 1, mid + 1, node_end);
+
+        let mut left = self.data[node * 2].clone();
+        let right = self.data[node * 2 + 1].clone();
+        self.data[node] = left.merge(&right);
+    }
+
+    fn query(&mut self, start: usize, end: usize) -> Node {
+        self.query_internal(start, end, 1, 1, self.size)
+    }
+
+    fn query_internal(
+        &mut self,
+        start: usize,
+        end: usize,
+        node: usize,
+        node_start: usize,
+        node_end: usize,
+    ) -> Node {
+        if end < node_start || node_end < start {
+            return Node { sum: 0 };
+        }
+
+        if start <= node_start && node_end <= end {
+            return self.data[node].clone();
+        }
+
+        let mid = (node_start + node_end) / 2;
+        let left = self.query_internal(start, end, node * 2, node_start, mid);
+        let right = self.query_internal(start, end, node * 2 + 1, mid + 1, node_end);
+
+        let sum = left.sum + right.sum;
+
+        Node { sum }
+    }
+}
+
+fn main() {
+    let (stdin, stdout) = (io::stdin(), io::stdout());
+    let mut scan = UnsafeScanner::new(stdin.lock());
+    let mut out = io::BufWriter::new(stdout.lock());
+
+    let t = scan.token::<i64>();
+
+    for i in 1..=t {
+        let (n, m, k) = (
+            scan.token::<usize>(),
+            scan.token::<usize>(),
+            scan.token::<usize>(),
+        );
+        let mut nums = vec![Vec::new(); n + 1];
+
+        for _ in 0..k {
+            let (a, b) = (scan.token::<usize>(), scan.token::<usize>());
+            nums[a].push(b);
+        }
+
+        let mut tree = SegmentTree::new(m + 1);
+        let mut ret = 0;
+
+        for j in 1..=n {
+            for val in nums[j].iter() {
+                ret += tree.query(*val + 1, m).sum;
+            }
+
+            for val in nums[j].iter() {
+                tree.update(*val, 1);
+            }
+        }
+
+        writeln!(out, "Test case {i}: {ret}").unwrap();
+    }
+}

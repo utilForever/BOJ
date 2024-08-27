2fde4d8d92355aa603c54c55a0333ad3993560de
Chris Ohk <utilforever@gmail.com>
2024-08-28 08:14:51 +0900

    Upload 13398 - Continuous Sum 2

---
+++ 13000/13398 - Continuous Sum 2.rs
@@ -0,0 +1,67 @@
+use io::Write;
+use std::{io, str};
+
+pub struct UnsafeScanner<R> {
+    reader: R,
+    buf_str: Vec<u8>,
+    buf_iter: str::SplitAsciiWhitespace<'static>,
+}
+
+impl<R: io::BufRead> UnsafeScanner<R> {
+    pub fn new(reader: R) -> Self {
+        Self {
+            reader,
+            buf_str: vec![],
+            buf_iter: "".split_ascii_whitespace(),
+        }
+    }
+
+    pub fn token<T: str::FromStr>(&mut self) -> T {
+        loop {
+            if let Some(token) = self.buf_iter.next() {
+                return token.parse().ok().expect("Failed parse");
+            }
+            self.buf_str.clear();
+            self.reader
+                .read_until(b'\n', &mut self.buf_str)
+                .expect("Failed read");
+            self.buf_iter = unsafe {
+                let slice = str::from_utf8_unchecked(&self.buf_str);
+                std::mem::transmute(slice.split_ascii_whitespace())
+            }
+        }
+    }
+
+    pub fn line(&mut self) -> String {
+        let mut input = String::new();
+        self.reader.read_line(&mut input).expect("Failed read");
+        input
+    }
+}
+
+fn main() {
+    let (stdin, stdout) = (io::stdin(), io::stdout());
+    let mut scan = UnsafeScanner::new(stdin.lock());
+    let mut out = io::BufWriter::new(stdout.lock());
+
+    let n = scan.token::<usize>();
+    let mut nums = vec![0; n + 1];
+
+    for i in 1..=n {
+        nums[i] = scan.token::<i64>();
+    }
+
+    let mut dp = vec![vec![0; 2]; n + 1];
+    dp[1][0] = nums[1];
+    dp[1][1] = nums[1];
+
+    let mut ret = nums[1];
+
+    for i in 2..=n {
+        dp[i][0] = nums[i].max(dp[i - 1][0] + nums[i]);
+        dp[i][1] = dp[i - 1][0].max(dp[i - 1][1] + nums[i]);
+        ret = ret.max(dp[i][0]).max(dp[i][1]);
+    }
+
+    writeln!(out, "{ret}").unwrap();
+}

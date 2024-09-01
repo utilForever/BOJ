82a1d474737d1ac5a7eff2137813dfa96fe41a8f
Chris Ohk <utilforever@gmail.com>
2024-09-01 23:10:23 +0900

    Upload 32171 - Fence Construction

---
+++ 32000/32171 - Fence Construction.rs
@@ -0,0 +1,68 @@
+use io::Write;
+use std::{io, str};
+
+pub struct UnsafeScanner<R> {
+    reader: R,
+    buf_str: Vec<u8>,
+    buf_iter: str::SplitAsciiWhitespace<'static>,
+}
+
+impl<R: io::BufRead> UnsafeScanner<R> {
+    pub fn new(reader: R) -> Self {
+        Self {
+            reader,
+            buf_str: vec![],
+            buf_iter: "".split_ascii_whitespace(),
+        }
+    }
+
+    pub fn token<T: str::FromStr>(&mut self) -> T {
+        loop {
+            if let Some(token) = self.buf_iter.next() {
+                return token.parse().ok().expect("Failed parse");
+            }
+            self.buf_str.clear();
+            self.reader
+                .read_until(b'\n', &mut self.buf_str)
+                .expect("Failed read");
+            self.buf_iter = unsafe {
+                let slice = str::from_utf8_unchecked(&self.buf_str);
+                std::mem::transmute(slice.split_ascii_whitespace())
+            }
+        }
+    }
+
+    pub fn line(&mut self) -> String {
+        let mut input = String::new();
+        self.reader.read_line(&mut input).expect("Failed read");
+        input
+    }
+}
+
+fn main() {
+    let (stdin, stdout) = (io::stdin(), io::stdout());
+    let mut scan = UnsafeScanner::new(stdin.lock());
+    let mut out = io::BufWriter::new(stdout.lock());
+
+    let n = scan.token::<i64>();
+    let mut pos_min = (i64::MAX, i64::MAX);
+    let mut pos_max = (i64::MIN, i64::MIN);
+
+    for _ in 0..n {
+        let (a, b, c, d) = (
+            scan.token::<i64>(),
+            scan.token::<i64>(),
+            scan.token::<i64>(),
+            scan.token::<i64>(),
+        );
+        pos_min = (pos_min.0.min(a), pos_min.1.min(b));
+        pos_max = (pos_max.0.max(c), pos_max.1.max(d));
+
+        writeln!(
+            out,
+            "{}",
+            2 * ((pos_max.0 - pos_min.0) + (pos_max.1 - pos_min.1))
+        )
+        .unwrap();
+    }
+}

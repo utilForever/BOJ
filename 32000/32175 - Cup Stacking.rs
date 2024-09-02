4f1b973776298a5a22c04a3b18c51f0779e3e846
Chris Ohk <utilforever@gmail.com>
2024-09-02 10:20:06 +0900

    Upload 32175 - Cup Stacking

---
+++ 32000/32175 - Cup Stacking.rs
@@ -0,0 +1,68 @@
+use io::Write;
+use std::{io, str};
+
+pub struct UnsafeScanner<R> {
+    reader: R,
+    buf_str: Vec<u8>,
+    buf_iter: str::SplitAsciiWhitespace<'static>,
+}
+
+impl<R: io::BufRead> UnsafeScanner<R> {
+    pub fn new(reader: R) -> Self {
+        Self {
+            reader,
+            buf_str: vec![],
+            buf_iter: "".split_ascii_whitespace(),
+        }
+    }
+
+    pub fn token<T: str::FromStr>(&mut self) -> T {
+        loop {
+            if let Some(token) = self.buf_iter.next() {
+                return token.parse().ok().expect("Failed parse");
+            }
+            self.buf_str.clear();
+            self.reader
+                .read_until(b'\n', &mut self.buf_str)
+                .expect("Failed read");
+            self.buf_iter = unsafe {
+                let slice = str::from_utf8_unchecked(&self.buf_str);
+                std::mem::transmute(slice.split_ascii_whitespace())
+            }
+        }
+    }
+
+    pub fn line(&mut self) -> String {
+        let mut input = String::new();
+        self.reader.read_line(&mut input).expect("Failed read");
+        input
+    }
+}
+
+const MOD: i64 = 1_000_000_007;
+
+fn main() {
+    let (stdin, stdout) = (io::stdin(), io::stdout());
+    let mut scan = UnsafeScanner::new(stdin.lock());
+    let mut out = io::BufWriter::new(stdout.lock());
+
+    let (n, h) = (scan.token::<usize>(), scan.token::<usize>());
+    let mut heights = vec![0; n];
+
+    for i in 0..n {
+        heights[i] = scan.token::<usize>();
+    }
+
+    let mut dp = vec![0; h + 1];
+    dp[0] = 1;
+
+    for i in 1..=h {
+        for &height in heights.iter() {
+            if i >= height {
+                dp[i] = (dp[i] + dp[i - height]) % MOD;
+            }
+        }
+    }
+
+    writeln!(out, "{}", dp[h]).unwrap();
+}

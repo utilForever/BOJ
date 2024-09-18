6ad3787391de268537f3421322f80fb47c47e4ba
Chris Ohk <utilforever@gmail.com>
2024-09-18 23:42:32 +0900

    Upload 32315 - Cool Phone Numbers

---
+++ 32000/32315 - Cool Phone Numbers.rs
@@ -0,0 +1,59 @@
+use io::Write;
+use std::{io, str};
+
+pub struct UnsafeScanner<R> {
+    reader: R,
+    buf_str: Vec<u8>,
+    buf_iter: str::SplitAsciiWhitespace<'static>,
+}
+
+impl<R: io::BufRead> UnsafeScanner<R> {
+    pub fn new(reader: R) -> Self {
+        Self {
+            reader,
+            buf_str: vec![],
+            buf_iter: "".split_ascii_whitespace(),
+        }
+    }
+
+    pub fn token<T: str::FromStr>(&mut self) -> T {
+        loop {
+            if let Some(token) = self.buf_iter.next() {
+                return token.parse().ok().expect("Failed parse");
+            }
+            self.buf_str.clear();
+            self.reader
+                .read_until(b'\n', &mut self.buf_str)
+                .expect("Failed read");
+            self.buf_iter = unsafe {
+                let slice = str::from_utf8_unchecked(&self.buf_str);
+                std::mem::transmute(slice.split_ascii_whitespace())
+            }
+        }
+    }
+
+    pub fn line(&mut self) -> String {
+        let mut input = String::new();
+        self.reader.read_line(&mut input).expect("Failed read");
+        input
+    }
+}
+
+fn main() {
+    let (stdin, stdout) = (io::stdin(), io::stdout());
+    let mut scan = UnsafeScanner::new(stdin.lock());
+    let mut out = io::BufWriter::new(stdout.lock());
+
+    let phone_number = scan.token::<String>();
+    let mut numbers = [0; 10];
+
+    for c in phone_number.chars() {
+        if !c.is_numeric() {
+            continue;
+        }
+
+        numbers[c.to_digit(10).unwrap() as usize] += 1;
+    }
+
+    writeln!(out, "{}", numbers.iter().filter(|&x| *x > 0).count()).unwrap();
+}

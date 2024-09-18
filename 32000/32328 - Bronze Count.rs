cb4141b22a040e4b37726484f54a2f59f24cdf1c
Chris Ohk <utilforever@gmail.com>
2024-09-19 00:59:18 +0900

    Upload 32328 - Bronze Count

---
+++ 32000/32328 - Bronze Count.rs
@@ -0,0 +1,59 @@
+use io::Write;
+use std::{collections::BTreeMap, io, str};
+
+pub struct UnsafeScanner<R> {
+    reader: R,
+    buf_str: Vec<u8>,
+    buf_iter: str::SplitAsciiWhitespace<'static>,
+}
+
+impl<R: io::BufRead> UnsafeScanner<R> {
+    pub fn new(reader: R) -> Self {
+        Self {
+            reader,
+            buf_str: vec![],
+            buf_iter: "".split_ascii_whitespace(),
+        }
+    }
+
+    pub fn token<T: str::FromStr>(&mut self) -> T {
+        loop {
+            if let Some(token) = self.buf_iter.next() {
+                return token.parse().ok().expect("Failed parse");
+            }
+            self.buf_str.clear();
+            self.reader
+                .read_until(b'\n', &mut self.buf_str)
+                .expect("Failed read");
+            self.buf_iter = unsafe {
+                let slice = str::from_utf8_unchecked(&self.buf_str);
+                std::mem::transmute(slice.split_ascii_whitespace())
+            }
+        }
+    }
+
+    pub fn line(&mut self) -> String {
+        let mut input = String::new();
+        self.reader.read_line(&mut input).expect("Failed read");
+        input
+    }
+}
+
+fn main() {
+    let (stdin, stdout) = (io::stdin(), io::stdout());
+    let mut scan = UnsafeScanner::new(stdin.lock());
+    let mut out = io::BufWriter::new(stdout.lock());
+
+    let n = scan.token::<usize>();
+    let mut scores = BTreeMap::new();
+
+    for _ in 0..n {
+        let score = scan.token::<i64>();
+        scores.entry(score).and_modify(|e| *e += 1).or_insert(1);
+    }
+
+    let mut scores = scores.into_iter().collect::<Vec<_>>();
+    scores.sort_by(|a, b| b.0.cmp(&a.0));
+
+    writeln!(out, "{} {}", scores[2].0, scores[2].1).unwrap();
+}

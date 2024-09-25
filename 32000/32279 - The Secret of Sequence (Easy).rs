1bd03439ba92f0673cf741e492887d301e0ad148
Chris Ohk <utilforever@gmail.com>
2024-09-25 20:52:50 +0900

    Upload 32279 - The Secret of Sequence (Easy)

---
+++ 32000/32279 - The Secret of Sequence (Easy).rs
@@ -0,0 +1,67 @@
+use io::Write;
+use std::{io, str};
+
+pub struct UnsafeScanner<R> {
+    reader: R,
+    buf_str: Vec<u8>,
+    buf_iter: str::SplitAsciiWhitespace<'static>,
+}
+
+impl<R: io::BufRead> UnsafeScanner<R> {
+    pub fn new(reader: R) -> Self {
+        Self {
+            reader,
+            buf_str: vec![],
+            buf_iter: "".split_ascii_whitespace(),
+        }
+    }
+
+    pub fn token<T: str::FromStr>(&mut self) -> T {
+        loop {
+            if let Some(token) = self.buf_iter.next() {
+                return token.parse().ok().expect("Failed parse");
+            }
+            self.buf_str.clear();
+            self.reader
+                .read_until(b'\n', &mut self.buf_str)
+                .expect("Failed read");
+            self.buf_iter = unsafe {
+                let slice = str::from_utf8_unchecked(&self.buf_str);
+                std::mem::transmute(slice.split_ascii_whitespace())
+            }
+        }
+    }
+
+    pub fn line(&mut self) -> String {
+        let mut input = String::new();
+        self.reader.read_line(&mut input).expect("Failed read");
+        input
+    }
+}
+
+fn main() {
+    let (stdin, stdout) = (io::stdin(), io::stdout());
+    let mut scan = UnsafeScanner::new(stdin.lock());
+    let mut out = io::BufWriter::new(stdout.lock());
+
+    let n = scan.token::<usize>();
+    let mut nums = vec![0; n + 1];
+
+    let (p, q, r, s) = (
+        scan.token::<i32>(),
+        scan.token::<i32>(),
+        scan.token::<i32>(),
+        scan.token::<i32>(),
+    );
+    nums[1] = scan.token::<i32>();
+
+    for i in 2..=n {
+        nums[i] = if i % 2 == 0 {
+            p * nums[i / 2] + q
+        } else {
+            r * nums[(i - 1) / 2] + s
+        }
+    }
+
+    writeln!(out, "{}", nums.iter().sum::<i32>()).unwrap();
+}

739e03411baec43a5c8c3d05a58f74cf49b1856e
Chris Ohk <utilforever@gmail.com>
2024-09-26 22:09:03 +0900

    Upload 32236 - The Expectation of LCS

---
+++ 32000/32236 - The Expectation of LCS.rs
@@ -0,0 +1,76 @@
+use io::Write;
+use std::{io, str};
+
+pub struct UnsafeScanner<R> {
+    reader: R,
+    buf_str: Vec<u8>,
+    buf_iter: str::SplitAsciiWhitespace<'static>,
+}
+
+impl<R: io::BufRead> UnsafeScanner<R> {
+    pub fn new(reader: R) -> Self {
+        Self {
+            reader,
+            buf_str: vec![],
+            buf_iter: "".split_ascii_whitespace(),
+        }
+    }
+
+    pub fn token<T: str::FromStr>(&mut self) -> T {
+        loop {
+            if let Some(token) = self.buf_iter.next() {
+                return token.parse().ok().expect("Failed parse");
+            }
+            self.buf_str.clear();
+            self.reader
+                .read_until(b'\n', &mut self.buf_str)
+                .expect("Failed read");
+            self.buf_iter = unsafe {
+                let slice = str::from_utf8_unchecked(&self.buf_str);
+                std::mem::transmute(slice.split_ascii_whitespace())
+            }
+        }
+    }
+
+    pub fn line(&mut self) -> String {
+        let mut input = String::new();
+        self.reader.read_line(&mut input).expect("Failed read");
+        input
+    }
+}
+
+const MOD: i64 = 1_000_000_007;
+
+fn pow(x: i64, mut y: i64) -> i64 {
+    let mut ret = 1;
+    let mut piv = x % MOD;
+
+    while y != 0 {
+        if y & 1 != 0 {
+            ret = ret * piv % MOD
+        }
+
+        piv = piv * piv % MOD;
+        y >>= 1;
+    }
+
+    ret
+}
+
+fn main() {
+    let (stdin, stdout) = (io::stdin(), io::stdout());
+    let mut scan = UnsafeScanner::new(stdin.lock());
+    let mut out = io::BufWriter::new(stdout.lock());
+
+    let (n, m) = (scan.token::<i64>(), scan.token::<i64>());
+    let mut ret = 1;
+
+    write!(out, "{ret} ").unwrap();
+
+    for i in 2..=n {
+        ret = (ret + pow(pow(i, MOD - 2), m)) % MOD;
+        write!(out, "{ret} ").unwrap();
+    }
+
+    writeln!(out).unwrap();
+}

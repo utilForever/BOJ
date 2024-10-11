85a0fa000dc3af44225c207466cf07bd1671f6ee
Chris Ohk <utilforever@gmail.com>
2024-10-11 10:02:12 +0900

    Upload 2981 - GRANICA

---
+++ 02000/2981 - GRANICA.rs
@@ -0,0 +1,121 @@
+use io::Write;
+use std::{io, str};
+
+pub struct UnsafeScanner<R> {
+    reader: R,
+    buf_str: Vec<u8>,
+    buf_iter: str::SplitAsciiWhitespace<'static>,
+}
+
+impl<R: io::BufRead> UnsafeScanner<R> {
+    pub fn new(reader: R) -> Self {
+        Self {
+            reader,
+            buf_str: vec![],
+            buf_iter: "".split_ascii_whitespace(),
+        }
+    }
+
+    pub fn token<T: str::FromStr>(&mut self) -> T {
+        loop {
+            if let Some(token) = self.buf_iter.next() {
+                return token.parse().ok().expect("Failed parse");
+            }
+            self.buf_str.clear();
+            self.reader
+                .read_until(b'\n', &mut self.buf_str)
+                .expect("Failed read");
+            self.buf_iter = unsafe {
+                let slice = str::from_utf8_unchecked(&self.buf_str);
+                std::mem::transmute(slice.split_ascii_whitespace())
+            }
+        }
+    }
+
+    pub fn line(&mut self) -> String {
+        let mut input = String::new();
+        self.reader.read_line(&mut input).expect("Failed read");
+        input
+    }
+}
+
+fn gcd(first: i64, second: i64) -> i64 {
+    let mut max = first;
+    let mut min = second;
+
+    if min == 0 && max == 0 {
+        return 0;
+    } else if min == 0 {
+        return max;
+    } else if max == 0 {
+        return min;
+    }
+
+    if min > max {
+        let val = max;
+
+        max = min;
+        min = val;
+    }
+
+    loop {
+        let res = max % min;
+
+        if res == 0 {
+            return min;
+        }
+
+        max = min;
+        min = res;
+    }
+}
+
+fn main() {
+    let (stdin, stdout) = (io::stdin(), io::stdout());
+    let mut scan = UnsafeScanner::new(stdin.lock());
+    let mut out = io::BufWriter::new(stdout.lock());
+
+    let n = scan.token::<usize>();
+    let mut nums = vec![0; n];
+
+    for i in 0..n {
+        nums[i] = scan.token::<i64>();
+    }
+
+    nums.sort();
+
+    let mut diffs = vec![0; n - 1];
+
+    for i in 1..n {
+        diffs[i - 1] = nums[i] - nums[i - 1];
+    }
+
+    let mut max = diffs[0];
+
+    for i in 1..n - 1 {
+        max = gcd(max, diffs[i]);
+    }
+
+    let mut idx = 2;
+    let mut ret = Vec::new();
+
+    while idx * idx <= max {
+        if max % idx == 0 {
+            ret.push(idx);
+
+            if idx * idx != max {
+                ret.push(max / idx);
+            }
+        }
+
+        idx += 1;
+    }
+
+    ret.sort();
+
+    for val in ret {
+        write!(out, "{val} ").unwrap();
+    }
+
+    writeln!(out, "{max}").unwrap();
+}

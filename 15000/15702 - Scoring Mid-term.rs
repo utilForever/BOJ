d4ff7baa232d5b4de58149d2d0a8b6054739a689
Chris Ohk <utilforever@gmail.com>
2024-10-15 22:24:59 +0900

    Upload 15702 - Scoring Mid-term

---
+++ 15000/15702 - Scoring Mid-term.rs
@@ -0,0 +1,76 @@
+use io::Write;
+use std::{io, str};
+
+pub struct UnsafeScanner<R> {
+    reader: R,
+    buf_str: Vec<u8>,
+    buf_iter: str::SplitAsciiWhitespace<'static>,
+}
+
+impl<R: io::BufRead> UnsafeScanner<R> {
+    pub fn new(reader: R) -> Self {
+        Self {
+            reader,
+            buf_str: vec![],
+            buf_iter: "".split_ascii_whitespace(),
+        }
+    }
+
+    pub fn token<T: str::FromStr>(&mut self) -> T {
+        loop {
+            if let Some(token) = self.buf_iter.next() {
+                return token.parse().ok().expect("Failed parse");
+            }
+            self.buf_str.clear();
+            self.reader
+                .read_until(b'\n', &mut self.buf_str)
+                .expect("Failed read");
+            self.buf_iter = unsafe {
+                let slice = str::from_utf8_unchecked(&self.buf_str);
+                std::mem::transmute(slice.split_ascii_whitespace())
+            }
+        }
+    }
+
+    pub fn line(&mut self) -> String {
+        let mut input = String::new();
+        self.reader.read_line(&mut input).expect("Failed read");
+        input
+    }
+}
+
+fn main() {
+    let (stdin, stdout) = (io::stdin(), io::stdout());
+    let mut scan = UnsafeScanner::new(stdin.lock());
+    let mut out = io::BufWriter::new(stdout.lock());
+
+    let (n, m) = (scan.token::<usize>(), scan.token::<usize>());
+    let mut scores = vec![0; n];
+    let mut students = vec![(0, 0); m];
+
+    for i in 0..n {
+        scores[i] = scan.token::<i64>();
+    }
+
+    for i in 0..m {
+        students[i].0 = scan.token::<i64>();
+
+        for j in 0..n {
+            let ret = scan.token::<char>();
+
+            if ret == 'O' {
+                students[i].1 += scores[j];
+            }
+        }
+    }
+
+    students.sort_by(|a, b| {
+        if a.1 == b.1 {
+            a.0.cmp(&b.0)
+        } else {
+            b.1.cmp(&a.1)
+        }
+    });
+
+    writeln!(out, "{} {}", students[0].0, students[0].1).unwrap();
+}

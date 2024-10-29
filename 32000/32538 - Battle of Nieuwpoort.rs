b269cc3f13baa492e2113f5681c76141d271d4c0
Chris Ohk <utilforever@gmail.com>
2024-10-29 23:33:32 +0900

    Upload 32538 - Battle of Nieuwpoort

---
+++ 32000/32538 - Battle of Nieuwpoort.rs
@@ -0,0 +1,83 @@
+use io::Write;
+use std::{io, str};
+
+pub struct UnsafeScanner<R> {
+    reader: R,
+    buf_str: Vec<u8>,
+    buf_iter: str::SplitAsciiWhitespace<'static>,
+}
+
+impl<R: io::BufRead> UnsafeScanner<R> {
+    pub fn new(reader: R) -> Self {
+        Self {
+            reader,
+            buf_str: vec![],
+            buf_iter: "".split_ascii_whitespace(),
+        }
+    }
+
+    pub fn token<T: str::FromStr>(&mut self) -> T {
+        loop {
+            if let Some(token) = self.buf_iter.next() {
+                return token.parse().ok().expect("Failed parse");
+            }
+            self.buf_str.clear();
+            self.reader
+                .read_until(b'\n', &mut self.buf_str)
+                .expect("Failed read");
+            self.buf_iter = unsafe {
+                let slice = str::from_utf8_unchecked(&self.buf_str);
+                std::mem::transmute(slice.split_ascii_whitespace())
+            }
+        }
+    }
+}
+
+fn main() {
+    let (stdin, stdout) = (io::stdin(), io::stdout());
+    let mut scan = UnsafeScanner::new(stdin.lock());
+    let mut out = io::BufWriter::new(stdout.lock());
+
+    let y = scan.token::<i64>();
+
+    for _ in 0..3 {
+        let _ = scan.token::<String>();
+    }
+
+    for b in 2..=16 {
+        let mut val = y;
+        let mut ret = Vec::new();
+
+        while val > 0 {
+            ret.push(match val % b {
+                0 => '0',
+                1 => '1',
+                2 => '2',
+                3 => '3',
+                4 => '4',
+                5 => '5',
+                6 => '6',
+                7 => '7',
+                8 => '8',
+                9 => '9',
+                10 => 'a',
+                11 => 'b',
+                12 => 'c',
+                13 => 'd',
+                14 => 'e',
+                15 => 'f',
+                _ => unreachable!(),
+            });
+            val /= b;
+        }
+
+        ret.reverse();
+
+        if ret.ends_with(&['0', '0']) {
+            writeln!(out, "{b} {}", ret.iter().collect::<String>()).unwrap();
+            return;
+        }
+    }
+
+    writeln!(out, "impossible").unwrap();
+}

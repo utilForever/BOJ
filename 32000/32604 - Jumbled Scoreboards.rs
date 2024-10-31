af9207121a450289aa4f4662df22d422aecb03a5
Chris Ohk <utilforever@gmail.com>
2024-10-31 18:14:40 +0900

    Upload 32604 - Jumbled Scoreboards

---
+++ 32000/32604 - Jumbled Scoreboards.rs
@@ -0,0 +1,57 @@
+use io::Write;
+use std::{io, str};
+
+pub struct UnsafeScanner<R> {
+    reader: R,
+    buf_str: Vec<u8>,
+    buf_iter: str::SplitAsciiWhitespace<'static>,
+}
+
+impl<R: io::BufRead> UnsafeScanner<R> {
+    pub fn new(reader: R) -> Self {
+        Self {
+            reader,
+            buf_str: vec![],
+            buf_iter: "".split_ascii_whitespace(),
+        }
+    }
+
+    pub fn token<T: str::FromStr>(&mut self) -> T {
+        loop {
+            if let Some(token) = self.buf_iter.next() {
+                return token.parse().ok().expect("Failed parse");
+            }
+            self.buf_str.clear();
+            self.reader
+                .read_until(b'\n', &mut self.buf_str)
+                .expect("Failed read");
+            self.buf_iter = unsafe {
+                let slice = str::from_utf8_unchecked(&self.buf_str);
+                std::mem::transmute(slice.split_ascii_whitespace())
+            }
+        }
+    }
+}
+
+fn main() {
+    let (stdin, stdout) = (io::stdin(), io::stdout());
+    let mut scan = UnsafeScanner::new(stdin.lock());
+    let mut out = io::BufWriter::new(stdout.lock());
+
+    let n = scan.token::<usize>();
+    let (mut a_prev, mut b_prev) = (0, 0);
+    let mut ret = true;
+
+    for _ in 0..n {
+        let (a, b) = (scan.token::<i64>(), scan.token::<i64>());
+
+        if a < a_prev || b < b_prev {
+            ret = false;
+        }
+
+        a_prev = a;
+        b_prev = b;
+    }
+
+    writeln!(out, "{}", if ret { "yes" } else { "no" }).unwrap();
+}

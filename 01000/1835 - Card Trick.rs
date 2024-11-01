912098d5810a7f9967712eba5d38f7ac9d047cf4
Chris Ohk <utilforever@gmail.com>
2024-11-01 10:33:47 +0900

    Upload 1835 - Card Trick

---
+++ 01000/1835 - Card Trick.rs
@@ -0,0 +1,60 @@
+use io::Write;
+use std::{collections::VecDeque, io, str};
+
+pub struct UnsafeScanner<R> {
+    reader: R,
+    buf_str: Vec<u8>,
+    buf_iter: str::SplitAsciiWhitespace<'static>,
+}
+
+impl<R: io::BufRead> UnsafeScanner<R> {
+    pub fn new(reader: R) -> Self {
+        Self {
+            reader,
+            buf_str: vec![],
+            buf_iter: "".split_ascii_whitespace(),
+        }
+    }
+
+    pub fn token<T: str::FromStr>(&mut self) -> T {
+        loop {
+            if let Some(token) = self.buf_iter.next() {
+                return token.parse().ok().expect("Failed parse");
+            }
+            self.buf_str.clear();
+            self.reader
+                .read_until(b'\n', &mut self.buf_str)
+                .expect("Failed read");
+            self.buf_iter = unsafe {
+                let slice = str::from_utf8_unchecked(&self.buf_str);
+                std::mem::transmute(slice.split_ascii_whitespace())
+            }
+        }
+    }
+}
+
+fn main() {
+    let (stdin, stdout) = (io::stdin(), io::stdout());
+    let mut scan = UnsafeScanner::new(stdin.lock());
+    let mut out = io::BufWriter::new(stdout.lock());
+
+    let n = scan.token::<i64>();
+    let mut deque = VecDeque::new();
+
+    deque.push_back(n);
+
+    for i in (1..n).rev() {
+        deque.push_front(i);
+
+        for _ in 0..i {
+            let num = deque.pop_back().unwrap();
+            deque.push_front(num);
+        }
+    }
+
+    for num in deque {
+        write!(out, "{num} ").unwrap();
+    }
+
+    writeln!(out).unwrap();
+}

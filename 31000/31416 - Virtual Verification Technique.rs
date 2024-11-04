71fb88110e98970f07e73498d13ba420047517a1
Chris Ohk <utilforever@gmail.com>
2024-11-04 22:27:28 +0900

    Upload 31416 - Virtual Verification Technique

---
+++ 31000/31416 - Virtual Verification Technique.rs
@@ -0,0 +1,71 @@
+use io::Write;
+use std::{io, str};
+
+pub struct UnsafeScanner<R> {
+    reader: R,
+    buf_str: Vec<u8>,
+    buf_iter: str::SplitAsciiWhitespace<'static>,
+}
+
+impl<R: io::BufRead> UnsafeScanner<R> {
+    pub fn new(reader: R) -> Self {
+        Self {
+            reader,
+            buf_str: vec![],
+            buf_iter: "".split_ascii_whitespace(),
+        }
+    }
+
+    pub fn token<T: str::FromStr>(&mut self) -> T {
+        loop {
+            if let Some(token) = self.buf_iter.next() {
+                return token.parse().ok().expect("Failed parse");
+            }
+            self.buf_str.clear();
+            self.reader
+                .read_until(b'\n', &mut self.buf_str)
+                .expect("Failed read");
+            self.buf_iter = unsafe {
+                let slice = str::from_utf8_unchecked(&self.buf_str);
+                std::mem::transmute(slice.split_ascii_whitespace())
+            }
+        }
+    }
+}
+
+fn main() {
+    let (stdin, stdout) = (io::stdin(), io::stdout());
+    let mut scan = UnsafeScanner::new(stdin.lock());
+    let mut out = io::BufWriter::new(stdout.lock());
+
+    let q = scan.token::<i64>();
+
+    for _ in 0..q {
+        let (ta, tb, va, vb) = (
+            scan.token::<i64>(),
+            scan.token::<i64>(),
+            scan.token::<i64>(),
+            scan.token::<i64>(),
+        );
+
+        if ta * va <= tb * vb {
+            writeln!(out, "{}", tb * vb).unwrap();
+            continue;
+        }
+
+        let curr = tb * vb;
+        let remain = va - (tb * vb) / ta;
+
+        writeln!(
+            out,
+            "{}",
+            if remain % 2 == 0 {
+                curr + (remain / 2) * ta
+            } else {
+                let extra = ta - curr % ta;
+                curr + (remain / 2) * ta + extra
+            }
+        )
+        .unwrap();
+    }
+}

6ab23ec50b9a143517b4848152ef0278161fce26
Chris Ohk <utilforever@gmail.com>
2024-11-10 19:16:45 +0900

    Upload 32629 - Rectangle

---
+++ 32000/32629 - Rectangle.rs
@@ -0,0 +1,59 @@
+use io::Write;
+use std::{io, str};
+
+pub struct UnsafeScanner<R> {
+    reader: R,
+    buf_str: Vec<u8>,
+    buf_iter: str::SplitAsciiWhitespace<'static>,
+}
+
+impl<R: io::BufRead> UnsafeScanner<R> {
+    pub fn new(reader: R) -> Self {
+        Self {
+            reader,
+            buf_str: vec![],
+            buf_iter: "".split_ascii_whitespace(),
+        }
+    }
+
+    pub fn token<T: str::FromStr>(&mut self) -> T {
+        loop {
+            if let Some(token) = self.buf_iter.next() {
+                return token.parse().ok().expect("Failed parse");
+            }
+            self.buf_str.clear();
+            self.reader
+                .read_until(b'\n', &mut self.buf_str)
+                .expect("Failed read");
+            self.buf_iter = unsafe {
+                let slice = str::from_utf8_unchecked(&self.buf_str);
+                std::mem::transmute(slice.split_ascii_whitespace())
+            }
+        }
+    }
+}
+
+fn main() {
+    let (stdin, stdout) = (io::stdin(), io::stdout());
+    let mut scan = UnsafeScanner::new(stdin.lock());
+    let mut out = io::BufWriter::new(stdout.lock());
+
+    let t = scan.token::<i64>();
+
+    for _ in 0..t {
+        let n = scan.token::<i64>();
+        let n_sqrt = (n as f64).sqrt() as i64;
+        let mut ret = i64::MAX;
+
+        for width in n_sqrt - 1..=n_sqrt + 2 {
+            if width <= 0 {
+                continue;
+            }
+
+            let height = (n + width - 1) / width;
+            ret = ret.min(2 * (width + height));
+        }
+
+        writeln!(out, "{ret}").unwrap();
+    }
+}

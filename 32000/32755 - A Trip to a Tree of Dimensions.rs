697f888d5de42661ed5ba66ff6c2c83ab83fcb50
Chris Ohk <utilforever@gmail.com>
2024-12-02 19:53:09 +0900

    Upload 32755 - A Trip to a Tree of Dimensions

---
+++ 32000/32755 - A Trip to a Tree of Dimensions.rs
@@ -0,0 +1,60 @@
+use io::Write;
+use std::{io, str};
+
+pub struct UnsafeScanner<R> {
+    reader: R,
+    buf_str: Vec<u8>,
+    buf_iter: str::SplitAsciiWhitespace<'static>,
+}
+
+impl<R: io::BufRead> UnsafeScanner<R> {
+    pub fn new(reader: R) -> Self {
+        Self {
+            reader,
+            buf_str: vec![],
+            buf_iter: "".split_ascii_whitespace(),
+        }
+    }
+
+    pub fn token<T: str::FromStr>(&mut self) -> T {
+        loop {
+            if let Some(token) = self.buf_iter.next() {
+                return token.parse().ok().expect("Failed parse");
+            }
+            self.buf_str.clear();
+            self.reader
+                .read_until(b'\n', &mut self.buf_str)
+                .expect("Failed read");
+            self.buf_iter = unsafe {
+                let slice = str::from_utf8_unchecked(&self.buf_str);
+                std::mem::transmute(slice.split_ascii_whitespace())
+            }
+        }
+    }
+
+    pub fn line(&mut self) -> String {
+        let mut input = String::new();
+        self.reader.read_line(&mut input).expect("Failed read");
+        input
+    }
+}
+
+fn main() {
+    let (stdin, stdout) = (io::stdin(), io::stdout());
+    let mut scan = UnsafeScanner::new(stdin.lock());
+    let mut out = io::BufWriter::new(stdout.lock());
+
+    let n = scan.token::<usize>();
+    let mut graph = vec![0; n + 1];
+    let mut degree_max = 0;
+
+    for _ in 0..n - 1 {
+        let (u, v) = (scan.token::<usize>(), scan.token::<usize>());
+        graph[u] += 1;
+        graph[v] += 1;
+
+        degree_max = degree_max.max(graph[u]).max(graph[v]);
+    }
+
+    writeln!(out, "{}", if degree_max == n - 1 { n - 1 } else { n }).unwrap();
+}

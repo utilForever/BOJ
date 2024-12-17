ec6da6c20dfeefa807b7e8776b1eddae35382744
Chris Ohk <utilforever@gmail.com>
2024-12-17 23:06:29 +0900

    Upload 12782 - Bit Friendliness Index

---
+++ 12000/12782 - Bit Friendliness Index.rs
@@ -0,0 +1,66 @@
+use io::Write;
+use std::{io, str};
+
+pub struct UnsafeScanner<R> {
+    reader: R,
+    buf_str: Vec<u8>,
+    buf_iter: str::SplitAsciiWhitespace<'static>,
+}
+
+impl<R: io::BufRead> UnsafeScanner<R> {
+    pub fn new(reader: R) -> Self {
+        Self {
+            reader,
+            buf_str: vec![],
+            buf_iter: "".split_ascii_whitespace(),
+        }
+    }
+
+    pub fn token<T: str::FromStr>(&mut self) -> T {
+        loop {
+            if let Some(token) = self.buf_iter.next() {
+                return token.parse().ok().expect("Failed parse");
+            }
+            self.buf_str.clear();
+            self.reader
+                .read_until(b'\n', &mut self.buf_str)
+                .expect("Failed read");
+            self.buf_iter = unsafe {
+                let slice = str::from_utf8_unchecked(&self.buf_str);
+                std::mem::transmute(slice.split_ascii_whitespace())
+            }
+        }
+    }
+
+    pub fn line(&mut self) -> String {
+        let mut input = String::new();
+        self.reader.read_line(&mut input).expect("Failed read");
+        input
+    }
+}
+
+fn main() {
+    let (stdin, stdout) = (io::stdin(), io::stdout());
+    let mut scan = UnsafeScanner::new(stdin.lock());
+    let mut out = io::BufWriter::new(stdout.lock());
+
+    let t = scan.token::<i64>();
+
+    for _ in 0..t {
+        let (a, b) = (scan.token::<String>(), scan.token::<String>());
+        let mut cnt_diff_10 = 0;
+        let mut cnt_diff_01 = 0;
+
+        for (a, b) in a.chars().zip(b.chars()) {
+            if a != b {
+                if a == '1' {
+                    cnt_diff_10 += 1;
+                } else {
+                    cnt_diff_01 += 1;
+                }
+            }
+        }
+
+        writeln!(out, "{}", cnt_diff_10.max(cnt_diff_01)).ok();
+    }
+}

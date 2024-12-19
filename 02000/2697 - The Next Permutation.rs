44bb4ab36055e5137963a22f55fc829054db51d8
Chris Ohk <utilforever@gmail.com>
2024-12-20 01:57:47 +0900

    Upload 2697 - The Next Permutation

---
+++ 02000/2697 - The Next Permutation.rs
@@ -0,0 +1,110 @@
+use io::Write;
+use std::{io, str};
+
+pub struct UnsafeScanner<R> {
+    reader: R,
+    buf_str: Vec<u8>,
+    buf_iter: str::SplitAsciiWhitespace<'static>,
+}
+
+impl<R: io::BufRead> UnsafeScanner<R> {
+    pub fn new(reader: R) -> Self {
+        Self {
+            reader,
+            buf_str: vec![],
+            buf_iter: "".split_ascii_whitespace(),
+        }
+    }
+
+    pub fn token<T: str::FromStr>(&mut self) -> T {
+        loop {
+            if let Some(token) = self.buf_iter.next() {
+                return token.parse().ok().expect("Failed parse");
+            }
+            self.buf_str.clear();
+            self.reader
+                .read_until(b'\n', &mut self.buf_str)
+                .expect("Failed read");
+            self.buf_iter = unsafe {
+                let slice = str::from_utf8_unchecked(&self.buf_str);
+                std::mem::transmute(slice.split_ascii_whitespace())
+            }
+        }
+    }
+
+    pub fn line(&mut self) -> String {
+        let mut input = String::new();
+        self.reader.read_line(&mut input).expect("Failed read");
+        input
+    }
+}
+
+pub fn next_permutation<T>(arr: &mut [T]) -> bool
+where
+    T: std::cmp::Ord,
+{
+    use std::cmp::Ordering;
+
+    // find 1st pair (x, y) from back which satisfies x < y
+    let last_ascending = match arr.windows(2).rposition(|w| w[0] < w[1]) {
+        Some(i) => i,
+        None => {
+            arr.reverse();
+            return false;
+        }
+    };
+
+    // In the remaining later segment, find the one which is just
+    // larger that the index found above.
+    // SAFETY: unwrap_err whill not panic since binary search will
+    // will never succeed since we never return equal ordering
+    let swap_with = arr[last_ascending + 1..]
+        .binary_search_by(|n| match arr[last_ascending].cmp(n) {
+            Ordering::Equal => Ordering::Greater,
+            ord => ord,
+        })
+        .unwrap_err();
+
+    arr.swap(last_ascending, last_ascending + swap_with);
+    arr[last_ascending + 1..].reverse();
+
+    true
+}
+
+fn main() {
+    let (stdin, stdout) = (io::stdin(), io::stdout());
+    let mut scan = UnsafeScanner::new(stdin.lock());
+    let mut out = io::BufWriter::new(stdout.lock());
+
+    let t = scan.token::<i64>();
+
+    for _ in 0..t {
+        let x = scan.token::<String>();
+        let digits = x
+            .chars()
+            .map(|c| c.to_digit(10).unwrap() as usize)
+            .collect::<Vec<usize>>();
+        let mut digits_cloned = digits.clone();
+        let mut check = false;
+
+        while next_permutation(&mut digits_cloned) {
+            if digits < digits_cloned {
+                writeln!(
+                    out,
+                    "{}",
+                    digits_cloned
+                        .iter()
+                        .map(|d| d.to_string())
+                        .collect::<String>()
+                )
+                .unwrap();
+                check = true;
+                break;
+            }
+        }
+
+        if !check {
+            writeln!(out, "BIGGEST").unwrap();
+        }
+    }
+}

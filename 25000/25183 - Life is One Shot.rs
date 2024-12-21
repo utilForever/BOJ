26187d6f88fbdf7c0e508c67797ba1dda58e8698
Chris Ohk <utilforever@gmail.com>
2024-12-21 10:28:59 +0900

    Upload 25183 - Life is One Shot

---
+++ 25000/25183 - Life is One Shot.rs
@@ -0,0 +1,66 @@
+use io::Write;
+use std::{io, str};
+
+pub struct UnsafeScanner<R> {
+    reader: R,
+    buf_str: Vec<u8>,
+    buf_iter: str::SplitAsciiWhitespace<'static>,
+}
+
+impl<R: io::BufRead> UnsafeScanner<R> {
+    pub fn new(reader: R) -> Self {
+        Self {
+            reader,
+            buf_str: vec![],
+            buf_iter: "".split_ascii_whitespace(),
+        }
+    }
+
+    pub fn token<T: str::FromStr>(&mut self) -> T {
+        loop {
+            if let Some(token) = self.buf_iter.next() {
+                return token.parse().ok().expect("Failed parse");
+            }
+            self.buf_str.clear();
+            self.reader
+                .read_until(b'\n', &mut self.buf_str)
+                .expect("Failed read");
+            self.buf_iter = unsafe {
+                let slice = str::from_utf8_unchecked(&self.buf_str);
+                std::mem::transmute(slice.split_ascii_whitespace())
+            }
+        }
+    }
+
+    pub fn line(&mut self) -> String {
+        let mut input = String::new();
+        self.reader.read_line(&mut input).expect("Failed read");
+        input
+    }
+}
+
+fn main() {
+    let (stdin, stdout) = (io::stdin(), io::stdout());
+    let mut scan = UnsafeScanner::new(stdin.lock());
+    let mut out = io::BufWriter::new(stdout.lock());
+
+    let _ = scan.token::<usize>();
+    let lotto = scan.token::<String>().chars().collect::<Vec<_>>();
+    let mut ret = false;
+
+    lotto.windows(5).for_each(|w| {
+        let mut check = true;
+
+        w.windows(2).for_each(|w2| {
+            if (w2[0] as i64 - w2[1] as i64).abs() != 1 {
+                check = false;
+            }
+        });
+
+        if check {
+            ret = true;
+        }
+    });
+
+    writeln!(out, "{}", if ret { "YES" } else { "NO" }).unwrap();
+}

db4267a3622df39285caac49c8005792cf2da269
Chris Ohk <utilforever@gmail.com>
2025-01-03 18:38:40 +0900

    Upload 17481 - Pick Your Favorite

---
+++ 17000/17481 - Pick Your Favorite.rs
@@ -0,0 +1,111 @@
+use io::Write;
+use std::{collections::HashMap, io, str};
+
+pub struct UnsafeScanner<R> {
+    reader: R,
+    buf_str: Vec<u8>,
+    buf_iter: str::SplitAsciiWhitespace<'static>,
+}
+
+impl<R: io::BufRead> UnsafeScanner<R> {
+    pub fn new(reader: R) -> Self {
+        Self {
+            reader,
+            buf_str: vec![],
+            buf_iter: "".split_ascii_whitespace(),
+        }
+    }
+
+    pub fn token<T: str::FromStr>(&mut self) -> T {
+        loop {
+            if let Some(token) = self.buf_iter.next() {
+                return token.parse().ok().expect("Failed parse");
+            }
+            self.buf_str.clear();
+            self.reader
+                .read_until(b'\n', &mut self.buf_str)
+                .expect("Failed read");
+            self.buf_iter = unsafe {
+                let slice = str::from_utf8_unchecked(&self.buf_str);
+                std::mem::transmute(slice.split_ascii_whitespace())
+            }
+        }
+    }
+
+    pub fn line(&mut self) -> String {
+        let mut input = String::new();
+        self.reader.read_line(&mut input).expect("Failed read");
+        input
+    }
+}
+
+fn process_dfs(
+    graph: &Vec<Vec<usize>>,
+    check: &mut Vec<bool>,
+    matched: &mut Vec<i64>,
+    idx: usize,
+) -> bool {
+    for &next in graph[idx].iter() {
+        if check[next] {
+            continue;
+        }
+
+        check[next] = true;
+
+        if matched[next] == -1 || process_dfs(graph, check, matched, matched[next] as usize) {
+            matched[next] = idx as i64;
+            return true;
+        }
+    }
+
+    false
+}
+
+fn main() {
+    let (stdin, stdout) = (io::stdin(), io::stdout());
+    let mut scan = UnsafeScanner::new(stdin.lock());
+    let mut out = io::BufWriter::new(stdout.lock());
+
+    let (n, m) = (scan.token::<usize>(), scan.token::<usize>());
+    let mut members = Vec::with_capacity(m);
+    let mut index_map = HashMap::new();
+
+    for i in 0..m {
+        let name = scan.token::<String>();
+
+        members.push(name.clone());
+        index_map.insert(name, i);
+    }
+
+    let mut graph = vec![Vec::new(); n];
+
+    for i in 0..n {
+        let k = scan.token::<i64>();
+
+        for _ in 0..k {
+            let name = scan.token::<String>();
+            let idx = index_map.get(&name).unwrap();
+
+            graph[i].push(*idx);
+        }
+    }
+
+    let mut check = vec![false; m];
+    let mut matched = vec![-1; m];
+    let mut ret = 0;
+
+    for i in 0..n {
+        check.fill(false);
+
+        if process_dfs(&graph, &mut check, &mut matched, i) {
+            ret += 1;
+        }
+    }
+
+    if ret == n {
+        writeln!(out, "YES").unwrap();
+    } else {
+        writeln!(out, "NO").unwrap();
+        writeln!(out, "{ret}").unwrap();
+    }
+}

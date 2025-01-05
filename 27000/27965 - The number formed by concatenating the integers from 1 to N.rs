9dce57088f9864013fb6dc0203e4419b43357f50
Chris Ohk <utilforever@gmail.com>
2025-01-05 17:12:41 +0900

    Upload 27965 - The number formed by concatenating integers from 1 to N

---
+++ 27000/27965 - The number formed by concatenating the integers from 1 to N.rs
@@ -0,0 +1,65 @@
+use io::Write;
+use std::{io, str};
+
+pub struct UnsafeScanner<R> {
+    reader: R,
+    buf_str: Vec<u8>,
+    buf_iter: str::SplitAsciiWhitespace<'static>,
+}
+
+impl<R: io::BufRead> UnsafeScanner<R> {
+    pub fn new(reader: R) -> Self {
+        Self {
+            reader,
+            buf_str: vec![],
+            buf_iter: "".split_ascii_whitespace(),
+        }
+    }
+
+    pub fn token<T: str::FromStr>(&mut self) -> T {
+        loop {
+            if let Some(token) = self.buf_iter.next() {
+                return token.parse().ok().expect("Failed parse");
+            }
+            self.buf_str.clear();
+            self.reader
+                .read_until(b'\n', &mut self.buf_str)
+                .expect("Failed read");
+            self.buf_iter = unsafe {
+                let slice = str::from_utf8_unchecked(&self.buf_str);
+                std::mem::transmute(slice.split_ascii_whitespace())
+            }
+        }
+    }
+
+    pub fn line(&mut self) -> String {
+        let mut input = String::new();
+        self.reader.read_line(&mut input).expect("Failed read");
+        input
+    }
+}
+
+fn main() {
+    let (stdin, stdout) = (io::stdin(), io::stdout());
+    let mut scan = UnsafeScanner::new(stdin.lock());
+    let mut out = io::BufWriter::new(stdout.lock());
+
+    let (n, k) = (scan.token::<i64>(), scan.token::<i64>());
+    let mut start = 1;
+    let mut len = 1;
+    let mut ret = 0;
+
+    while start <= n {
+        let end = n.min(10i64.pow(len) - 1);
+        let pow = 10i64.pow(len) % k;
+
+        for i in start..=end {
+            ret = (ret * pow + i) % k;
+        }
+
+        start = end + 1;
+        len += 1;
+    }
+
+    writeln!(out, "{ret}").unwrap();
+}

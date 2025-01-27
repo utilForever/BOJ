90bbb0b5b93195690758bf6ca52594201fa6360b
Chris Ohk <utilforever@gmail.com>
2025-01-27 14:53:07 +0900

    Upload 33272 - TAIDADA

---
+++ 33000/33272 - TAIDADA.rs
@@ -0,0 +1,75 @@
+use io::Write;
+use std::{collections::HashSet, io, str};
+
+pub struct UnsafeScanner<R> {
+    reader: R,
+    buf_str: Vec<u8>,
+    buf_iter: str::SplitAsciiWhitespace<'static>,
+}
+
+impl<R: io::BufRead> UnsafeScanner<R> {
+    pub fn new(reader: R) -> Self {
+        Self {
+            reader,
+            buf_str: vec![],
+            buf_iter: "".split_ascii_whitespace(),
+        }
+    }
+
+    pub fn token<T: str::FromStr>(&mut self) -> T {
+        loop {
+            if let Some(token) = self.buf_iter.next() {
+                return token.parse().ok().expect("Failed parse");
+            }
+            self.buf_str.clear();
+            self.reader
+                .read_until(b'\n', &mut self.buf_str)
+                .expect("Failed read");
+            self.buf_iter = unsafe {
+                let slice = str::from_utf8_unchecked(&self.buf_str);
+                std::mem::transmute(slice.split_ascii_whitespace())
+            }
+        }
+    }
+
+    pub fn line(&mut self) -> String {
+        let mut input = String::new();
+        self.reader.read_line(&mut input).expect("Failed read");
+        input
+    }
+}
+
+fn main() {
+    let (stdin, stdout) = (io::stdin(), io::stdout());
+    let mut scan = UnsafeScanner::new(stdin.lock());
+    let mut out = io::BufWriter::new(stdout.lock());
+
+    let (n, m, k) = (
+        scan.token::<usize>(),
+        scan.token::<i64>(),
+        scan.token::<i64>(),
+    );
+    let mut ret = HashSet::with_capacity(n);
+
+    for i in 1..=m {
+        if ret.contains(&i) || ret.contains(&(i ^ k)) {
+            continue;
+        }
+
+        ret.insert(i);
+
+        if ret.len() == n {
+            break;
+        }
+    }
+
+    if ret.len() == n {
+        for val in ret {
+            write!(out, "{val} ").unwrap();
+        }
+
+        writeln!(out).unwrap();
+    } else {
+        writeln!(out, "-1").unwrap();
+    }
+}

6c8b57d12833acf960dee7791743f9c3a870c13d
Chris Ohk <utilforever@gmail.com>
2025-01-30 14:37:35 +0900

    Upload 33244 - Group Activities

---
+++ 33000/33244 - Group Activities.rs
@@ -0,0 +1,93 @@
+use io::Write;
+use std::{io, str};
+
+pub struct UnsafeScanner<R> {
+    reader: R,
+    buf_str: Vec<u8>,
+    buf_iter: str::SplitAsciiWhitespace<'static>,
+}
+
+impl<R: io::BufRead> UnsafeScanner<R> {
+    pub fn new(reader: R) -> Self {
+        Self {
+            reader,
+            buf_str: vec![],
+            buf_iter: "".split_ascii_whitespace(),
+        }
+    }
+
+    pub fn token<T: str::FromStr>(&mut self) -> T {
+        loop {
+            if let Some(token) = self.buf_iter.next() {
+                return token.parse().ok().expect("Failed parse");
+            }
+            self.buf_str.clear();
+            self.reader
+                .read_until(b'\n', &mut self.buf_str)
+                .expect("Failed read");
+            self.buf_iter = unsafe {
+                let slice = str::from_utf8_unchecked(&self.buf_str);
+                std::mem::transmute(slice.split_ascii_whitespace())
+            }
+        }
+    }
+
+    pub fn line(&mut self) -> String {
+        let mut input = String::new();
+        self.reader.read_line(&mut input).expect("Failed read");
+        input
+    }
+}
+
+fn gcd(first: i128, second: i128) -> i128 {
+    let mut max = first;
+    let mut min = second;
+
+    if min == 0 && max == 0 {
+        return 0;
+    } else if min == 0 {
+        return max;
+    } else if max == 0 {
+        return min;
+    }
+
+    if min > max {
+        std::mem::swap(&mut min, &mut max);
+    }
+
+    loop {
+        let res = max % min;
+
+        if res == 0 {
+            return min;
+        }
+
+        max = min;
+        min = res;
+    }
+}
+
+fn lcm(a: i128, b: i128) -> i128 {
+    a * b / gcd(a, b)
+}
+
+fn main() {
+    let (stdin, stdout) = (io::stdin(), io::stdout());
+    let mut scan = UnsafeScanner::new(stdin.lock());
+    let mut out = io::BufWriter::new(stdout.lock());
+
+    let n = scan.token::<usize>();
+    let mut activities = vec![0; n];
+
+    for i in 0..n {
+        activities[i] = scan.token::<i128>();
+    }
+
+    let mut ret = 1;
+
+    for i in 0..n {
+        ret = lcm(ret, activities[i]);
+    }
+
+    writeln!(out, "{ret}").unwrap();
+}

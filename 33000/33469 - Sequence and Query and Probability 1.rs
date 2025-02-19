5d721d289238950357a2a29cac90fdeaac36c2e6
Chris Ohk <utilforever@gmail.com>
2025-02-20 03:17:32 +0900

    Upload 33469 - Sequence and Query and Probability 1

---
+++ 33000/33469 - Sequence and Query and Probability 1.rs
@@ -0,0 +1,89 @@
+use io::Write;
+use std::{io, str};
+
+pub struct UnsafeScanner<R> {
+    reader: R,
+    buf_str: Vec<u8>,
+    buf_iter: str::SplitAsciiWhitespace<'static>,
+}
+
+impl<R: io::BufRead> UnsafeScanner<R> {
+    pub fn new(reader: R) -> Self {
+        Self {
+            reader,
+            buf_str: vec![],
+            buf_iter: "".split_ascii_whitespace(),
+        }
+    }
+
+    pub fn token<T: str::FromStr>(&mut self) -> T {
+        loop {
+            if let Some(token) = self.buf_iter.next() {
+                return token.parse().ok().expect("Failed parse");
+            }
+            self.buf_str.clear();
+            self.reader
+                .read_until(b'\n', &mut self.buf_str)
+                .expect("Failed read");
+            self.buf_iter = unsafe {
+                let slice = str::from_utf8_unchecked(&self.buf_str);
+                std::mem::transmute(slice.split_ascii_whitespace())
+            }
+        }
+    }
+
+    pub fn line(&mut self) -> String {
+        let mut input = String::new();
+        self.reader.read_line(&mut input).expect("Failed read");
+        input
+    }
+}
+
+const MOD: i64 = 1_000_000_007;
+
+fn pow(x: i64, mut y: i64) -> i64 {
+    let mut ret = 1;
+    let mut piv = x % MOD;
+
+    while y != 0 {
+        if y & 1 != 0 {
+            ret = ret * piv % MOD
+        }
+
+        piv = piv * piv % MOD;
+        y >>= 1;
+    }
+
+    ret
+}
+
+fn main() {
+    let (stdin, stdout) = (io::stdin(), io::stdout());
+    let mut scan = UnsafeScanner::new(stdin.lock());
+    let mut out = io::BufWriter::new(stdout.lock());
+
+    let (n, m) = (scan.token::<i64>(), scan.token::<i64>());
+    let mut ret = 1;
+
+    for i in 1..=n {
+        let _ = scan.token::<i64>();
+        ret = (ret * i) % MOD;
+    }
+
+    let c = scan.token::<char>();
+
+    if c == 'S' {
+        writeln!(out, "1").unwrap();
+        return;
+    }
+
+    let inv2 = (MOD + 1) / 2;
+    let inv4 = (MOD + 1) / 4;
+
+    ret = ret * pow(pow(n, n), MOD - 2) % MOD;
+    ret = ret * inv2 % MOD;
+    ret = ret + (n + 1) * inv4 % MOD;
+    ret = pow(ret, m);
+
+    writeln!(out, "{ret}").unwrap();
+}

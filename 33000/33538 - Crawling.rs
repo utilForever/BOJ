21ec26296620a54b477d0af5910521e8a1fae262
Chris Ohk <utilforever@gmail.com>
2025-02-21 00:35:46 +0900

    Upload 33538 - Crawling

---
+++ 33000/33538 - Crawling.rs
@@ -0,0 +1,61 @@
+use io::Write;
+use std::{io, str};
+
+pub struct UnsafeScanner<R> {
+    reader: R,
+    buf_str: Vec<u8>,
+    buf_iter: str::SplitAsciiWhitespace<'static>,
+}
+
+impl<R: io::BufRead> UnsafeScanner<R> {
+    pub fn new(reader: R) -> Self {
+        Self {
+            reader,
+            buf_str: vec![],
+            buf_iter: "".split_ascii_whitespace(),
+        }
+    }
+
+    pub fn token<T: str::FromStr>(&mut self) -> T {
+        loop {
+            if let Some(token) = self.buf_iter.next() {
+                return token.parse().ok().expect("Failed parse");
+            }
+            self.buf_str.clear();
+            self.reader
+                .read_until(b'\n', &mut self.buf_str)
+                .expect("Failed read");
+            self.buf_iter = unsafe {
+                let slice = str::from_utf8_unchecked(&self.buf_str);
+                std::mem::transmute(slice.split_ascii_whitespace())
+            }
+        }
+    }
+
+    pub fn line(&mut self) -> String {
+        let mut input = String::new();
+        self.reader.read_line(&mut input).expect("Failed read");
+        input
+    }
+}
+
+fn main() {
+    let (stdin, stdout) = (io::stdin(), io::stdout());
+    let mut scan = UnsafeScanner::new(stdin.lock());
+    let mut out = io::BufWriter::new(stdout.lock());
+
+    let l = scan.token::<f64>();
+    let n = scan.token::<i64>();
+    let t = scan.token::<f64>();
+    let mut ret = false;
+
+    for _ in 0..n {
+        let (f, b) = (scan.token::<f64>(), scan.token::<f64>());
+
+        if l / f + l / b < t {
+            ret = true;
+        }
+    }
+
+    writeln!(out, "{}", if ret { "HOPE" } else { "DOOMED" }).unwrap();
+}

6f3b621ad4391d49df305d6d8c8b03aa9eabd4c6
Chris Ohk <utilforever@gmail.com>
2025-02-22 11:17:00 +0900

    Upload 14472 - 休憩スペース (Refreshment Area)

---
+++ 14000/14472 - 休憩スペース (Refreshment Area).rs
@@ -0,0 +1,101 @@
+use io::Write;
+use std::{io, str};
+
+pub struct UnsafeScanner<R> {
+    reader: R,
+    buf_str: Vec<u8>,
+    buf_iter: str::SplitAsciiWhitespace<'static>,
+}
+
+impl<R: io::BufRead> UnsafeScanner<R> {
+    pub fn new(reader: R) -> Self {
+        Self {
+            reader,
+            buf_str: vec![],
+            buf_iter: "".split_ascii_whitespace(),
+        }
+    }
+
+    pub fn token<T: str::FromStr>(&mut self) -> T {
+        loop {
+            if let Some(token) = self.buf_iter.next() {
+                return token.parse().ok().expect("Failed parse");
+            }
+            self.buf_str.clear();
+            self.reader
+                .read_until(b'\n', &mut self.buf_str)
+                .expect("Failed read");
+            self.buf_iter = unsafe {
+                let slice = str::from_utf8_unchecked(&self.buf_str);
+                std::mem::transmute(slice.split_ascii_whitespace())
+            }
+        }
+    }
+
+    pub fn line(&mut self) -> String {
+        let mut input = String::new();
+        self.reader.read_line(&mut input).expect("Failed read");
+        input
+    }
+}
+
+fn main() {
+    let (stdin, stdout) = (io::stdin(), io::stdout());
+    let mut scan = UnsafeScanner::new(stdin.lock());
+    let mut out = io::BufWriter::new(stdout.lock());
+
+    let (n, m, d) = (
+        scan.token::<usize>(),
+        scan.token::<usize>(),
+        scan.token::<usize>(),
+    );
+    let mut map = vec![vec![' '; m]; n];
+
+    for i in 0..n {
+        let line = scan.line().trim().to_string();
+
+        for (j, c) in line.chars().enumerate() {
+            map[i][j] = c;
+        }
+    }
+
+    let mut ret = 0;
+
+    for i in 0..n {
+        for j in 0..m {
+            // Horizontal
+            if j + d <= m {
+                let mut check = true;
+
+                for k in 0..d {
+                    if map[i][j + k] == '#' {
+                        check = false;
+                        break;
+                    }
+                }
+
+                if check {
+                    ret += 1;
+                }
+            }
+
+            // Vertical
+            if i + d <= n {
+                let mut check = true;
+
+                for k in 0..d {
+                    if map[i + k][j] == '#' {
+                        check = false;
+                        break;
+                    }
+                }
+
+                if check {
+                    ret += 1;
+                }
+            }
+        }
+    }
+
+    writeln!(out, "{ret}").unwrap();
+}

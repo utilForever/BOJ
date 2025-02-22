8cf4717c933a979d1d710aeee7f273e393d05c4f
Chris Ohk <utilforever@gmail.com>
2025-02-22 11:02:52 +0900

    Upload 12723 - Minimum Scalar Product (Small)

---
+++ 12000/12723 - Minimum Scalar Product (Small).rs
@@ -0,0 +1,72 @@
+use io::Write;
+use std::{io, str};
+
+pub struct UnsafeScanner<R> {
+    reader: R,
+    buf_str: Vec<u8>,
+    buf_iter: str::SplitAsciiWhitespace<'static>,
+}
+
+impl<R: io::BufRead> UnsafeScanner<R> {
+    pub fn new(reader: R) -> Self {
+        Self {
+            reader,
+            buf_str: vec![],
+            buf_iter: "".split_ascii_whitespace(),
+        }
+    }
+
+    pub fn token<T: str::FromStr>(&mut self) -> T {
+        loop {
+            if let Some(token) = self.buf_iter.next() {
+                return token.parse().ok().expect("Failed parse");
+            }
+            self.buf_str.clear();
+            self.reader
+                .read_until(b'\n', &mut self.buf_str)
+                .expect("Failed read");
+            self.buf_iter = unsafe {
+                let slice = str::from_utf8_unchecked(&self.buf_str);
+                std::mem::transmute(slice.split_ascii_whitespace())
+            }
+        }
+    }
+
+    pub fn line(&mut self) -> String {
+        let mut input = String::new();
+        self.reader.read_line(&mut input).expect("Failed read");
+        input
+    }
+}
+
+fn main() {
+    let (stdin, stdout) = (io::stdin(), io::stdout());
+    let mut scan = UnsafeScanner::new(stdin.lock());
+    let mut out = io::BufWriter::new(stdout.lock());
+
+    let t = scan.token::<i64>();
+
+    for i in 1..=t {
+        let n = scan.token::<usize>();
+        let mut a = vec![0; n];
+        let mut b = vec![0; n];
+
+        for j in 0..n {
+            a[j] = scan.token::<i64>();
+        }
+
+        for j in 0..n {
+            b[j] = scan.token::<i64>();
+        }
+
+        a.sort();
+        b.sort_by(|a, b| b.cmp(a));
+
+        writeln!(
+            out,
+            "Case #{i}: {}",
+            a.iter().zip(b.iter()).map(|(x, y)| x * y).sum::<i64>()
+        )
+        .unwrap();
+    }
+}

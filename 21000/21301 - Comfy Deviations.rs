dc3b5a532672c383bcab86d729f4ec8b104730fd
Chris Ohk <utilforever@gmail.com>
2025-02-23 14:52:44 +0900

    Upload 21301 - Comfy Deviations

---
+++ 21000/21301 - Comfy Deviations.rs
@@ -0,0 +1,62 @@
+use io::Write;
+use std::{io, str};
+
+pub struct UnsafeScanner<R> {
+    reader: R,
+    buf_str: Vec<u8>,
+    buf_iter: str::SplitAsciiWhitespace<'static>,
+}
+
+impl<R: io::BufRead> UnsafeScanner<R> {
+    pub fn new(reader: R) -> Self {
+        Self {
+            reader,
+            buf_str: vec![],
+            buf_iter: "".split_ascii_whitespace(),
+        }
+    }
+
+    pub fn token<T: str::FromStr>(&mut self) -> T {
+        loop {
+            if let Some(token) = self.buf_iter.next() {
+                return token.parse().ok().expect("Failed parse");
+            }
+            self.buf_str.clear();
+            self.reader
+                .read_until(b'\n', &mut self.buf_str)
+                .expect("Failed read");
+            self.buf_iter = unsafe {
+                let slice = str::from_utf8_unchecked(&self.buf_str);
+                std::mem::transmute(slice.split_ascii_whitespace())
+            }
+        }
+    }
+
+    pub fn line(&mut self) -> String {
+        let mut input = String::new();
+        self.reader.read_line(&mut input).expect("Failed read");
+        input
+    }
+}
+
+fn main() {
+    let (stdin, stdout) = (io::stdin(), io::stdout());
+    let mut scan = UnsafeScanner::new(stdin.lock());
+    let mut out = io::BufWriter::new(stdout.lock());
+
+    let mut temperatures = [0.0; 10];
+
+    for i in 0..10 {
+        temperatures[i] = scan.token::<f64>();
+    }
+
+    let avg = temperatures.iter().sum::<f64>() / 10.0;
+    let std_dev = (temperatures.iter().map(|x| (x - avg).powi(2)).sum::<f64>() / 9.0).sqrt();
+
+    writeln!(
+        out,
+        "{}",
+        if std_dev <= 1.0 { "COMFY" } else { "NOT COMFY" }
+    )
+    .unwrap();
+}

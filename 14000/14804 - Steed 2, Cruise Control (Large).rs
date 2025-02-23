8612445f594c13cdc6952cb0d074d9af0f1364be
Chris Ohk <utilforever@gmail.com>
2025-02-23 14:40:25 +0900

    Upload 14804 - Steed 2, Cruise Control (Large)

---
+++ 14000/14804 - Steed 2, Cruise Control (Large).rs
@@ -0,0 +1,66 @@
+use io::Write;
+use std::{io, str};
+
+pub struct UnsafeScanner<R> {
+    reader: R,
+    buf_str: Vec<u8>,
+    buf_iter: str::SplitAsciiWhitespace<'static>,
+}
+
+impl<R: io::BufRead> UnsafeScanner<R> {
+    pub fn new(reader: R) -> Self {
+        Self {
+            reader,
+            buf_str: vec![],
+            buf_iter: "".split_ascii_whitespace(),
+        }
+    }
+
+    pub fn token<T: str::FromStr>(&mut self) -> T {
+        loop {
+            if let Some(token) = self.buf_iter.next() {
+                return token.parse().ok().expect("Failed parse");
+            }
+            self.buf_str.clear();
+            self.reader
+                .read_until(b'\n', &mut self.buf_str)
+                .expect("Failed read");
+            self.buf_iter = unsafe {
+                let slice = str::from_utf8_unchecked(&self.buf_str);
+                std::mem::transmute(slice.split_ascii_whitespace())
+            }
+        }
+    }
+
+    pub fn line(&mut self) -> String {
+        let mut input = String::new();
+        self.reader.read_line(&mut input).expect("Failed read");
+        input
+    }
+}
+
+fn main() {
+    let (stdin, stdout) = (io::stdin(), io::stdout());
+    let mut scan = UnsafeScanner::new(stdin.lock());
+    let mut out = io::BufWriter::new(stdout.lock());
+
+    let t = scan.token::<i64>();
+
+    for i in 1..=t {
+        let (d, n) = (scan.token::<i64>(), scan.token::<usize>());
+        let mut horses = vec![(0, 0); n];
+
+        for j in 0..n {
+            let (k, s) = (scan.token::<i64>(), scan.token::<i64>());
+            horses[j] = (k, s);
+        }
+
+        let time_max = horses
+            .iter()
+            .map(|(k, s)| (d - k) as f64 / *s as f64)
+            .max_by(|a, b| a.partial_cmp(b).unwrap())
+            .unwrap();
+
+        writeln!(out, "Case #{i}: {:.6}", d as f64 / time_max).unwrap();
+    }
+}

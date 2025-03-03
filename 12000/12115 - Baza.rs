e1b6e1603f1e614c4f1f22df11ffee0c71841e3e
Chris Ohk <utilforever@gmail.com>
2025-03-03 23:17:11 +0900

    Upload 12115 - Baza

---
+++ 12000/12115 - Baza.rs
@@ -0,0 +1,88 @@
+use io::Write;
+use std::{io, str};
+
+pub struct UnsafeScanner<R> {
+    reader: R,
+    buf_str: Vec<u8>,
+    buf_iter: str::SplitAsciiWhitespace<'static>,
+}
+
+impl<R: io::BufRead> UnsafeScanner<R> {
+    pub fn new(reader: R) -> Self {
+        Self {
+            reader,
+            buf_str: vec![],
+            buf_iter: "".split_ascii_whitespace(),
+        }
+    }
+
+    pub fn token<T: str::FromStr>(&mut self) -> T {
+        loop {
+            if let Some(token) = self.buf_iter.next() {
+                return token.parse().ok().expect("Failed parse");
+            }
+            self.buf_str.clear();
+            self.reader
+                .read_until(b'\n', &mut self.buf_str)
+                .expect("Failed read");
+            self.buf_iter = unsafe {
+                let slice = str::from_utf8_unchecked(&self.buf_str);
+                std::mem::transmute(slice.split_ascii_whitespace())
+            }
+        }
+    }
+
+    pub fn line(&mut self) -> String {
+        let mut input = String::new();
+        self.reader.read_line(&mut input).expect("Failed read");
+        input
+    }
+}
+
+fn main() {
+    let (stdin, stdout) = (io::stdin(), io::stdout());
+    let mut scan = UnsafeScanner::new(stdin.lock());
+    let mut out = io::BufWriter::new(stdout.lock());
+
+    let (n, m) = (scan.token::<usize>(), scan.token::<usize>());
+    let mut database = vec![vec![0; m]; n];
+
+    for i in 0..n {
+        for j in 0..m {
+            database[i][j] = scan.token::<i64>();
+        }
+    }
+
+    let q = scan.token::<i64>();
+
+    for _ in 0..q {
+        let mut nums = vec![0; m];
+
+        for i in 0..m {
+            nums[i] = scan.token::<i64>();
+        }
+
+        let mut ret = 0;
+
+        for i in 0..n {
+            let mut check = true;
+
+            for j in 0..m {
+                if nums[j] == -1 {
+                    continue;
+                }
+
+                if database[i][j] != nums[j] {
+                    check = false;
+                    break;
+                }
+            }
+
+            if check {
+                ret += 1;
+            }
+        }
+
+        writeln!(out, "{ret}").unwrap();
+    }
+}

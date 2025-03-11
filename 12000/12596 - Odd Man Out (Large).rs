ba24930f2199ada6347fc350a327991b0c1f379d
Chris Ohk <utilforever@gmail.com>
2025-03-11 23:06:22 +0900

    Upload 12596 - Odd Man Out (Large)

---
+++ 12000/12596 - Odd Man Out (Large).rs
@@ -0,0 +1,63 @@
+use io::Write;
+use std::{collections::HashMap, io, str};
+
+pub struct UnsafeScanner<R> {
+    reader: R,
+    buf_str: Vec<u8>,
+    buf_iter: str::SplitAsciiWhitespace<'static>,
+}
+
+impl<R: io::BufRead> UnsafeScanner<R> {
+    pub fn new(reader: R) -> Self {
+        Self {
+            reader,
+            buf_str: vec![],
+            buf_iter: "".split_ascii_whitespace(),
+        }
+    }
+
+    pub fn token<T: str::FromStr>(&mut self) -> T {
+        loop {
+            if let Some(token) = self.buf_iter.next() {
+                return token.parse().ok().expect("Failed parse");
+            }
+            self.buf_str.clear();
+            self.reader
+                .read_until(b'\n', &mut self.buf_str)
+                .expect("Failed read");
+            self.buf_iter = unsafe {
+                let slice = str::from_utf8_unchecked(&self.buf_str);
+                std::mem::transmute(slice.split_ascii_whitespace())
+            }
+        }
+    }
+}
+
+fn main() {
+    let (stdin, stdout) = (io::stdin(), io::stdout());
+    let mut scan = UnsafeScanner::new(stdin.lock());
+    let mut out = io::BufWriter::new(stdout.lock());
+
+    let t = scan.token::<i64>();
+
+    for i in 1..=t {
+        let n = scan.token::<i64>();
+        let mut numbers = HashMap::new();
+
+        for _ in 0..n {
+            let num = scan.token::<i64>();
+            numbers.entry(num).and_modify(|e| *e += 1).or_insert(1);
+        }
+
+        let mut ret = 0;
+
+        for (k, v) in numbers.iter() {
+            if *v == 1 {
+                ret = *k;
+                break;
+            }
+        }
+
+        writeln!(out, "Case #{i}: {ret}").unwrap();
+    }
+}

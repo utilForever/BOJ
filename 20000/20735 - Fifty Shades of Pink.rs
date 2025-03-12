54ce49e3a3eb2b5b3c9bbe105ce9466ee7e6a280
Chris Ohk <utilforever@gmail.com>
2025-03-13 08:46:50 +0900

    Upload 20735 - Fifty Shades of Pink

---
+++ 20000/20735 - Fifty Shades of Pink.rs
@@ -0,0 +1,58 @@
+use io::Write;
+use std::{io, str};
+
+pub struct UnsafeScanner<R> {
+    reader: R,
+    buf_str: Vec<u8>,
+    buf_iter: str::SplitAsciiWhitespace<'static>,
+}
+
+impl<R: io::BufRead> UnsafeScanner<R> {
+    pub fn new(reader: R) -> Self {
+        Self {
+            reader,
+            buf_str: vec![],
+            buf_iter: "".split_ascii_whitespace(),
+        }
+    }
+
+    pub fn token<T: str::FromStr>(&mut self) -> T {
+        loop {
+            if let Some(token) = self.buf_iter.next() {
+                return token.parse().ok().expect("Failed parse");
+            }
+            self.buf_str.clear();
+            self.reader
+                .read_until(b'\n', &mut self.buf_str)
+                .expect("Failed read");
+            self.buf_iter = unsafe {
+                let slice = str::from_utf8_unchecked(&self.buf_str);
+                std::mem::transmute(slice.split_ascii_whitespace())
+            }
+        }
+    }
+}
+
+fn main() {
+    let (stdin, stdout) = (io::stdin(), io::stdout());
+    let mut scan = UnsafeScanner::new(stdin.lock());
+    let mut out = io::BufWriter::new(stdout.lock());
+
+    let n = scan.token::<i64>();
+    let mut ret = 0;
+
+    for _ in 0..n {
+        let mut color = scan.token::<String>();
+        color = color.to_ascii_lowercase();
+
+        if color.contains("pink") || color.contains("rose") {
+            ret += 1;
+        }
+    }
+
+    if ret == 0 {
+        writeln!(out, "I must watch Star Wars with my daughter").unwrap();
+    } else {
+        writeln!(out, "{ret}").unwrap();
+    }
+}

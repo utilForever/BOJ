ecf5fdbe0c345d57e2bd8cad42ae22d2d9703ac8
Chris Ohk <utilforever@gmail.com>
2025-03-12 22:10:37 +0900

    Upload 12910 - Handing out Candy

---
+++ 12000/12910 - Handing out Candy.rs
@@ -0,0 +1,61 @@
+use io::Write;
+use std::{io, str};
+
+pub struct UnsafeScanner<R> {
+    reader: R,
+    buf_str: Vec<u8>,
+    buf_iter: str::SplitAsciiWhitespace<'static>,
+}
+
+impl<R: io::BufRead> UnsafeScanner<R> {
+    pub fn new(reader: R) -> Self {
+        Self {
+            reader,
+            buf_str: vec![],
+            buf_iter: "".split_ascii_whitespace(),
+        }
+    }
+
+    pub fn token<T: str::FromStr>(&mut self) -> T {
+        loop {
+            if let Some(token) = self.buf_iter.next() {
+                return token.parse().ok().expect("Failed parse");
+            }
+            self.buf_str.clear();
+            self.reader
+                .read_until(b'\n', &mut self.buf_str)
+                .expect("Failed read");
+            self.buf_iter = unsafe {
+                let slice = str::from_utf8_unchecked(&self.buf_str);
+                std::mem::transmute(slice.split_ascii_whitespace())
+            }
+        }
+    }
+}
+
+fn main() {
+    let (stdin, stdout) = (io::stdin(), io::stdout());
+    let mut scan = UnsafeScanner::new(stdin.lock());
+    let mut out = io::BufWriter::new(stdout.lock());
+
+    let n = scan.token::<usize>();
+    let mut brands = [0; 51];
+
+    for _ in 0..n {
+        let brand = scan.token::<usize>();
+        brands[brand] += 1;
+    }
+
+    let mut ret = [0; 51];
+    ret[0] = 1;
+
+    for i in 1..=50 {
+        if brands[i] == 0 {
+            break;
+        }
+
+        ret[i] = ret[i - 1] * brands[i];
+    }
+
+    writeln!(out, "{}", ret[1..].iter().sum::<i64>()).unwrap();
+}

e614ac03c6978242b6db5f5b350d82f673201a69
Chris Ohk <utilforever@gmail.com>
2025-03-13 21:14:15 +0900

    Upload 21059 - Methodic Multiplication

---
+++ 21000/21059 - Methodic Multiplication.rs
@@ -0,0 +1,57 @@
+use io::Write;
+use std::{io, str};
+
+pub struct UnsafeScanner<R> {
+    reader: R,
+    buf_str: Vec<u8>,
+    buf_iter: str::SplitAsciiWhitespace<'static>,
+}
+
+impl<R: io::BufRead> UnsafeScanner<R> {
+    pub fn new(reader: R) -> Self {
+        Self {
+            reader,
+            buf_str: vec![],
+            buf_iter: "".split_ascii_whitespace(),
+        }
+    }
+
+    pub fn token<T: str::FromStr>(&mut self) -> T {
+        loop {
+            if let Some(token) = self.buf_iter.next() {
+                return token.parse().ok().expect("Failed parse");
+            }
+            self.buf_str.clear();
+            self.reader
+                .read_until(b'\n', &mut self.buf_str)
+                .expect("Failed read");
+            self.buf_iter = unsafe {
+                let slice = str::from_utf8_unchecked(&self.buf_str);
+                std::mem::transmute(slice.split_ascii_whitespace())
+            }
+        }
+    }
+}
+
+fn main() {
+    let (stdin, stdout) = (io::stdin(), io::stdout());
+    let mut scan = UnsafeScanner::new(stdin.lock());
+    let mut out = io::BufWriter::new(stdout.lock());
+
+    let s1 = scan.token::<String>();
+    let s2 = scan.token::<String>();
+    let cnt1 = (s1.len() - 1) / 3;
+    let cnt2 = (s2.len() - 1) / 3;
+
+    for _ in 0..cnt1 * cnt2 {
+        write!(out, "S(").unwrap();
+    }
+
+    write!(out, "0").unwrap();
+
+    for _ in 0..cnt1 * cnt2 {
+        write!(out, ")").unwrap();
+    }
+
+    writeln!(out).unwrap();
+}

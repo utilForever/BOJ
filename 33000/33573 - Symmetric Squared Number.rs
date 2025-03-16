701834cd7e0d1f62ede203fc0dbea5fd3395da6a
Chris Ohk <utilforever@gmail.com>
2025-03-16 10:02:45 +0900

    Upload 33573 - Symmetric Squared Number

---
+++ 33000/33573 - Symmetric Squared Number.rs
@@ -0,0 +1,68 @@
+use io::Write;
+use std::{io, str};
+
+pub struct UnsafeScanner<R> {
+    reader: R,
+    buf_str: Vec<u8>,
+    buf_iter: str::SplitAsciiWhitespace<'static>,
+}
+
+impl<R: io::BufRead> UnsafeScanner<R> {
+    pub fn new(reader: R) -> Self {
+        Self {
+            reader,
+            buf_str: vec![],
+            buf_iter: "".split_ascii_whitespace(),
+        }
+    }
+
+    pub fn token<T: str::FromStr>(&mut self) -> T {
+        loop {
+            if let Some(token) = self.buf_iter.next() {
+                return token.parse().ok().expect("Failed parse");
+            }
+            self.buf_str.clear();
+            self.reader
+                .read_until(b'\n', &mut self.buf_str)
+                .expect("Failed read");
+            self.buf_iter = unsafe {
+                let slice = str::from_utf8_unchecked(&self.buf_str);
+                std::mem::transmute(slice.split_ascii_whitespace())
+            }
+        }
+    }
+
+    pub fn line(&mut self) -> String {
+        let mut input = String::new();
+        self.reader.read_line(&mut input).expect("Failed read");
+        input
+    }
+}
+
+fn main() {
+    let (stdin, stdout) = (io::stdin(), io::stdout());
+    let mut scan = UnsafeScanner::new(stdin.lock());
+    let mut out = io::BufWriter::new(stdout.lock());
+
+    let t = scan.token::<i64>();
+
+    for _ in 0..t {
+        let n = scan.token::<i64>();
+        let n_string = n.to_string();
+        let n_string_rev = n_string.chars().rev().collect::<String>();
+        let n_rev = n_string_rev.parse::<i64>().unwrap();
+
+        writeln!(
+            out,
+            "{}",
+            if ((n as f64).sqrt() as i64).pow(2) == n
+                && ((n_rev as f64).sqrt() as i64).pow(2) == n_rev
+            {
+                "YES"
+            } else {
+                "NO"
+            }
+        )
+        .unwrap();
+    }
+}

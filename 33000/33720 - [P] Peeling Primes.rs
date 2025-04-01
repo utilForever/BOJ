bf767fd36d71db87a16ada0c52e1f4eba66b4003
Chris Ohk <utilforever@gmail.com>
2025-04-01 21:15:52 +0900

    Upload 33720 - [P] Peeling Primes

---
+++ 33000/33720 - [P] Peeling Primes.rs
@@ -0,0 +1,98 @@
+use io::Write;
+use std::{io, str};
+
+pub struct UnsafeScanner<R> {
+    reader: R,
+    buf_str: Vec<u8>,
+    buf_iter: str::SplitAsciiWhitespace<'static>,
+}
+
+impl<R: io::BufRead> UnsafeScanner<R> {
+    pub fn new(reader: R) -> Self {
+        Self {
+            reader,
+            buf_str: vec![],
+            buf_iter: "".split_ascii_whitespace(),
+        }
+    }
+
+    pub fn token<T: str::FromStr>(&mut self) -> T {
+        loop {
+            if let Some(token) = self.buf_iter.next() {
+                return token.parse().ok().expect("Failed parse");
+            }
+            self.buf_str.clear();
+            self.reader
+                .read_until(b'\n', &mut self.buf_str)
+                .expect("Failed read");
+            self.buf_iter = unsafe {
+                let slice = str::from_utf8_unchecked(&self.buf_str);
+                std::mem::transmute(slice.split_ascii_whitespace())
+            }
+        }
+    }
+
+    pub fn line(&mut self) -> String {
+        let mut input = String::new();
+        self.reader.read_line(&mut input).expect("Failed read");
+        input
+    }
+}
+
+fn is_prime(n: i64) -> bool {
+    if n < 2 {
+        return true;
+    }
+
+    let mut i = 2;
+
+    while i * i <= n {
+        if n % i == 0 {
+            return false;
+        }
+
+        i += 1;
+    }
+
+    true
+}
+
+fn smallest_prime_factor(n: i64) -> i64 {
+    if n % 2 == 0 {
+        return 2;
+    }
+
+    let mut i = 3;
+
+    while i * i <= n {
+        if n % i == 0 {
+            return i;
+        }
+
+        i += 2;
+    }
+
+    n
+}
+
+fn main() {
+    let (stdin, stdout) = (io::stdin(), io::stdout());
+    let mut scan = UnsafeScanner::new(stdin.lock());
+    let mut out = io::BufWriter::new(stdout.lock());
+
+    let n = scan.token::<i64>();
+
+    writeln!(
+        out,
+        "{}",
+        if is_prime(n) {
+            1
+        } else if n % 2 == 0 {
+            n / 2
+        } else {
+            let factor = smallest_prime_factor(n);
+            1 + (n - factor) / 2
+        }
+    )
+    .unwrap();
+}

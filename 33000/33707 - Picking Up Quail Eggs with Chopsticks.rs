614d7a798ac949965d61fe167795cefff8df3785
Chris Ohk <utilforever@gmail.com>
2025-04-02 20:29:49 +0900

    Upload 33707 - Picking Up Quail Eggs with Chopsticks

---
+++ 33000/33707 - Picking Up Quail Eggs with Chopsticks.rs
@@ -0,0 +1,62 @@
+use std::{io, str};
+
+pub struct UnsafeScanner<R> {
+    reader: R,
+    buf_str: Vec<u8>,
+    buf_iter: str::SplitAsciiWhitespace<'static>,
+}
+
+impl<R: io::BufRead> UnsafeScanner<R> {
+    pub fn new(reader: R) -> Self {
+        Self {
+            reader,
+            buf_str: vec![],
+            buf_iter: "".split_ascii_whitespace(),
+        }
+    }
+
+    pub fn token<T: str::FromStr>(&mut self) -> T {
+        loop {
+            if let Some(token) = self.buf_iter.next() {
+                return token.parse().ok().expect("Failed parse");
+            }
+            self.buf_str.clear();
+            self.reader
+                .read_until(b'\n', &mut self.buf_str)
+                .expect("Failed read");
+            self.buf_iter = unsafe {
+                let slice = str::from_utf8_unchecked(&self.buf_str);
+                std::mem::transmute(slice.split_ascii_whitespace())
+            }
+        }
+    }
+
+    pub fn line(&mut self) -> String {
+        let mut input = String::new();
+        self.reader.read_line(&mut input).expect("Failed read");
+        input
+    }
+}
+
+fn main() {
+    let stdin = io::stdin();
+    let mut scan = UnsafeScanner::new(stdin.lock());
+
+    let (n, m) = (scan.token::<i64>(), scan.token::<i64>());
+
+    for i in 1..=n {
+        for j in 1..=m {
+            if (i + j) % 2 == 0 {
+                continue;
+            }
+
+            println!("? {i} {j}");
+
+            let ret = scan.token::<i64>();
+
+            if ret == 1 {
+                return;
+            }
+        }
+    }
+}

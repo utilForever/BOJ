1a8948b92309a344106b1968714d2d35b1801066
Chris Ohk <utilforever@gmail.com>
2025-04-04 00:53:49 +0900

    Upload 18311 - Round Trip

---
+++ 18000/18311 - Round Trip.rs
@@ -0,0 +1,86 @@
+use io::Write;
+use std::{io, str};
+
+pub struct UnsafeScanner<R> {
+    reader: R,
+    buf_str: Vec<u8>,
+    buf_iter: str::SplitAsciiWhitespace<'static>,
+}
+
+impl<R: io::BufRead> UnsafeScanner<R> {
+    pub fn new(reader: R) -> Self {
+        Self {
+            reader,
+            buf_str: vec![],
+            buf_iter: "".split_ascii_whitespace(),
+        }
+    }
+
+    pub fn token<T: str::FromStr>(&mut self) -> T {
+        loop {
+            if let Some(token) = self.buf_iter.next() {
+                return token.parse().ok().expect("Failed parse");
+            }
+            self.buf_str.clear();
+            self.reader
+                .read_until(b'\n', &mut self.buf_str)
+                .expect("Failed read");
+            self.buf_iter = unsafe {
+                let slice = str::from_utf8_unchecked(&self.buf_str);
+                std::mem::transmute(slice.split_ascii_whitespace())
+            }
+        }
+    }
+
+    pub fn line(&mut self) -> String {
+        let mut input = String::new();
+        self.reader.read_line(&mut input).expect("Failed read");
+        input
+    }
+}
+
+fn main() {
+    let (stdin, stdout) = (io::stdin(), io::stdout());
+    let mut scan = UnsafeScanner::new(stdin.lock());
+    let mut out = io::BufWriter::new(stdout.lock());
+
+    let (n, k) = (scan.token::<usize>(), scan.token::<i64>());
+    let mut lengths = vec![0; n];
+
+    for i in 0..n {
+        lengths[i] = scan.token::<i64>();
+    }
+
+    let sum = lengths.iter().sum::<i64>();
+
+    if k < sum {
+        let mut accum = 0;
+
+        for i in 0..n {
+            accum += lengths[i];
+
+            if k < accum {
+                writeln!(out, "{}", i + 1).unwrap();
+                return;
+            } else if k == accum {
+                writeln!(out, "{}", i + 2).unwrap();
+                return;
+            }
+        }
+    } else {
+        let remain = k - sum;
+        let mut accum = 0;
+
+        for i in 0..n {
+            accum += lengths[n - 1 - i];
+
+            if remain < accum {
+                writeln!(out, "{}", n - i).unwrap();
+                return;
+            } else if remain == accum {
+                writeln!(out, "{}", n - i - 1).unwrap();
+                return;
+            }
+        }
+    }
+}

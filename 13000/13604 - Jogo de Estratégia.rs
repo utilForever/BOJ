95266e18af19eec50562db1a61fad83cf93b265d
Chris Ohk <utilforever@gmail.com>
2025-04-08 22:47:47 +0900

    Upload 13604 - Jogo de Estratégia

---
+++ 13000/13604 - Jogo de Estratégia.rs
@@ -0,0 +1,62 @@
+use io::Write;
+use std::{io, str};
+
+pub struct UnsafeScanner<R> {
+    reader: R,
+    buf_str: Vec<u8>,
+    buf_iter: str::SplitAsciiWhitespace<'static>,
+}
+
+impl<R: io::BufRead> UnsafeScanner<R> {
+    pub fn new(reader: R) -> Self {
+        Self {
+            reader,
+            buf_str: vec![],
+            buf_iter: "".split_ascii_whitespace(),
+        }
+    }
+
+    pub fn token<T: str::FromStr>(&mut self) -> T {
+        loop {
+            if let Some(token) = self.buf_iter.next() {
+                return token.parse().ok().expect("Failed parse");
+            }
+            self.buf_str.clear();
+            self.reader
+                .read_until(b'\n', &mut self.buf_str)
+                .expect("Failed read");
+            self.buf_iter = unsafe {
+                let slice = str::from_utf8_unchecked(&self.buf_str);
+                std::mem::transmute(slice.split_ascii_whitespace())
+            }
+        }
+    }
+
+    pub fn line(&mut self) -> String {
+        let mut input = String::new();
+        self.reader.read_line(&mut input).expect("Failed read");
+        input
+    }
+}
+
+fn main() {
+    let (stdin, stdout) = (io::stdin(), io::stdout());
+    let mut scan = UnsafeScanner::new(stdin.lock());
+    let mut out = io::BufWriter::new(stdout.lock());
+
+    let (j, r) = (scan.token::<usize>(), scan.token::<usize>());
+    let mut games = vec![vec![0; j]; r];
+    let mut players = vec![0; j];
+
+    for a in 0..r {
+        for b in 0..j {
+            games[a][b] = scan.token::<i64>();
+            players[b] += games[a][b];
+        }
+    }
+
+    let max = *players.iter().max().unwrap();
+    let pos = j - players.iter().rev().position(|&x| x == max).unwrap();
+
+    writeln!(out, "{pos}").unwrap();
+}

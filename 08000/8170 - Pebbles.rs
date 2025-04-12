03d490395d16173701443d1e1ae3d8f67e4dd93e
Chris Ohk <utilforever@gmail.com>
2025-04-13 01:51:47 +0900

    Upload 8170 - Pebbles

---
+++ 08000/8170 - Pebbles.rs
@@ -0,0 +1,75 @@
+use io::Write;
+use std::{io, str, vec};
+
+pub struct UnsafeScanner<R> {
+    reader: R,
+    buf_str: Vec<u8>,
+    buf_iter: str::SplitAsciiWhitespace<'static>,
+}
+
+impl<R: io::BufRead> UnsafeScanner<R> {
+    pub fn new(reader: R) -> Self {
+        Self {
+            reader,
+            buf_str: vec![],
+            buf_iter: "".split_ascii_whitespace(),
+        }
+    }
+
+    pub fn token<T: str::FromStr>(&mut self) -> T {
+        loop {
+            if let Some(token) = self.buf_iter.next() {
+                return token.parse().ok().expect("Failed parse");
+            }
+            self.buf_str.clear();
+            self.reader
+                .read_until(b'\n', &mut self.buf_str)
+                .expect("Failed read");
+            self.buf_iter = unsafe {
+                let slice = str::from_utf8_unchecked(&self.buf_str);
+                std::mem::transmute(slice.split_ascii_whitespace())
+            }
+        }
+    }
+
+    pub fn all(&mut self) -> String {
+        let mut input = String::new();
+        self.reader.read_to_string(&mut input).expect("Failed read");
+        input
+    }
+
+    pub fn line(&mut self) -> String {
+        let mut input = String::new();
+        self.reader.read_line(&mut input).expect("Failed read");
+        input
+    }
+}
+
+fn main() {
+    let (stdin, stdout) = (io::stdin(), io::stdout());
+    let mut scan = UnsafeScanner::new(stdin.lock());
+    let mut out = io::BufWriter::new(stdout.lock());
+
+    let u = scan.token::<i64>();
+
+    for _ in 0..u {
+        let n = scan.token::<usize>();
+        let mut piles = vec![0; n + 1];
+
+        for i in 1..=n {
+            piles[i] = scan.token::<i64>();
+        }
+
+        let mut ret = 0;
+
+        for i in 1..=n {
+            let diff = piles[i] - piles[i - 1];
+
+            if (n - i) % 2 == 0 {
+                ret ^= diff;
+            }
+        }
+
+        writeln!(out, "{}", if ret == 0 { "NIE" } else { "TAK" }).unwrap();
+    }
+}

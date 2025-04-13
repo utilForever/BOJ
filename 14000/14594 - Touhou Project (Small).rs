758cdf36827e306ba0dd4fe037c938445e315823
Chris Ohk <utilforever@gmail.com>
2025-04-13 19:27:04 +0900

    Upload 14594 - Touhou Project (Small)

---
+++ 14000/14594 - Touhou Project (Small).rs
@@ -0,0 +1,116 @@
+use io::Write;
+use std::{io, str};
+
+pub struct UnsafeScanner<R> {
+    reader: R,
+    buf_str: Vec<u8>,
+    buf_iter: str::SplitAsciiWhitespace<'static>,
+}
+
+impl<R: io::BufRead> UnsafeScanner<R> {
+    pub fn new(reader: R) -> Self {
+        Self {
+            reader,
+            buf_str: vec![],
+            buf_iter: "".split_ascii_whitespace(),
+        }
+    }
+
+    pub fn token<T: str::FromStr>(&mut self) -> T {
+        loop {
+            if let Some(token) = self.buf_iter.next() {
+                return token.parse().ok().expect("Failed parse");
+            }
+            self.buf_str.clear();
+            self.reader
+                .read_until(b'\n', &mut self.buf_str)
+                .expect("Failed read");
+            self.buf_iter = unsafe {
+                let slice = str::from_utf8_unchecked(&self.buf_str);
+                std::mem::transmute(slice.split_ascii_whitespace())
+            }
+        }
+    }
+
+    pub fn all(&mut self) -> String {
+        let mut input = String::new();
+        self.reader.read_to_string(&mut input).expect("Failed read");
+        input
+    }
+
+    pub fn line(&mut self) -> String {
+        let mut input = String::new();
+        self.reader.read_line(&mut input).expect("Failed read");
+        input
+    }
+}
+
+struct UnionFind {
+    parent: Vec<usize>,
+}
+
+impl UnionFind {
+    fn new(n: usize) -> Self {
+        UnionFind { parent: vec![0; n] }
+    }
+
+    fn init(&mut self) {
+        for i in 0..self.parent.len() {
+            self.parent[i] = i;
+        }
+    }
+
+    fn find(&mut self, x: usize) -> usize {
+        if self.parent[x] != x {
+            self.parent[x] = self.find(self.parent[x]);
+        }
+
+        self.parent[x]
+    }
+
+    fn union(&mut self, x: usize, y: usize) {
+        let root_x = self.find(x);
+        let root_y = self.find(y);
+
+        if root_x != root_y {
+            self.parent[root_y] = root_x;
+        }
+    }
+}
+
+fn main() {
+    let (stdin, stdout) = (io::stdin(), io::stdout());
+    let mut scan = UnsafeScanner::new(stdin.lock());
+    let mut out = io::BufWriter::new(stdout.lock());
+
+    let n = scan.token::<usize>();
+    let m = scan.token::<usize>();
+
+    let mut union_find = UnionFind::new(n);
+    union_find.init();
+
+    let mut next = (1..=n).collect::<Vec<_>>();
+
+    for _ in 0..m {
+        let (x, y) = (scan.token::<usize>() - 1, scan.token::<usize>() - 1);
+        let mut idx = x;
+
+        while idx < y {
+            union_find.union(idx, idx + 1);
+
+            let temp = idx;
+            idx = next[idx];
+            next[temp] = y;
+        }
+    }
+
+    let mut ret = 0;
+
+    for i in 0..n {
+        if union_find.find(i) == i {
+            ret += 1;
+        }
+    }
+
+    writeln!(out, "{ret}").unwrap();
+}

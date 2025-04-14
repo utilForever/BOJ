d3b959371b047b2eba885bc3c7c14f02045bb3f2
Chris Ohk <utilforever@gmail.com>
2025-04-14 23:09:05 +0900

    Upload 20113 - Emergency Meeting

---
+++ 20000/20113 - Emergency Meeting.rs
@@ -0,0 +1,75 @@
+use io::Write;
+use std::{io, str};
+
+pub struct UnsafeScanner<R> {
+    reader: R,
+    buf_str: Vec<u8>,
+    buf_iter: str::SplitAsciiWhitespace<'static>,
+}
+
+impl<R: io::BufRead> UnsafeScanner<R> {
+    pub fn new(reader: R) -> Self {
+        Self {
+            reader,
+            buf_str: vec![],
+            buf_iter: "".split_ascii_whitespace(),
+        }
+    }
+
+    pub fn token<T: str::FromStr>(&mut self) -> T {
+        loop {
+            if let Some(token) = self.buf_iter.next() {
+                return token.parse().ok().expect("Failed parse");
+            }
+            self.buf_str.clear();
+            self.reader
+                .read_until(b'\n', &mut self.buf_str)
+                .expect("Failed read");
+            self.buf_iter = unsafe {
+                let slice = str::from_utf8_unchecked(&self.buf_str);
+                std::mem::transmute(slice.split_ascii_whitespace())
+            }
+        }
+    }
+
+    pub fn all(&mut self) -> String {
+        let mut input = String::new();
+        self.reader.read_to_string(&mut input).expect("Failed read");
+        input
+    }
+
+    pub fn line(&mut self) -> String {
+        let mut input = String::new();
+        self.reader.read_line(&mut input).expect("Failed read");
+        input
+    }
+}
+
+fn main() {
+    let (stdin, stdout) = (io::stdin(), io::stdout());
+    let mut scan = UnsafeScanner::new(stdin.lock());
+    let mut out = io::BufWriter::new(stdout.lock());
+
+    let n = scan.token::<usize>();
+    let mut votes = vec![0; n + 1];
+
+    for _ in 0..n {
+        let player = scan.token::<usize>();
+
+        if player == 0 {
+            continue;
+        }
+
+        votes[player] += 1;
+    }
+
+    let max = *votes.iter().max().unwrap();
+    let cnt = votes.iter().filter(|&&x| x == max).count();
+
+    if cnt > 1 {
+        writeln!(out, "skipped").unwrap();
+    } else {
+        let ret = votes.iter().position(|&x| x == max).unwrap();
+        writeln!(out, "{ret}").unwrap();
+    }
+}

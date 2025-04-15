c18a07e5badea2c50014caef3e4be54ddbd57d81
Chris Ohk <utilforever@gmail.com>
2025-04-15 14:51:25 +0900

    Upload 9612 - Maximum Word Frequency

---
+++ 09000/9612 - Maximum Word Frequency.rs
@@ -0,0 +1,71 @@
+use io::Write;
+use std::{collections::HashMap, io, str};
+
+pub struct UnsafeScanner<R> {
+    reader: R,
+    buf_str: Vec<u8>,
+    buf_iter: str::SplitAsciiWhitespace<'static>,
+}
+
+impl<R: io::BufRead> UnsafeScanner<R> {
+    pub fn new(reader: R) -> Self {
+        Self {
+            reader,
+            buf_str: vec![],
+            buf_iter: "".split_ascii_whitespace(),
+        }
+    }
+
+    pub fn token<T: str::FromStr>(&mut self) -> T {
+        loop {
+            if let Some(token) = self.buf_iter.next() {
+                return token.parse().ok().expect("Failed parse");
+            }
+            self.buf_str.clear();
+            self.reader
+                .read_until(b'\n', &mut self.buf_str)
+                .expect("Failed read");
+            self.buf_iter = unsafe {
+                let slice = str::from_utf8_unchecked(&self.buf_str);
+                std::mem::transmute(slice.split_ascii_whitespace())
+            }
+        }
+    }
+
+    pub fn all(&mut self) -> String {
+        let mut input = String::new();
+        self.reader.read_to_string(&mut input).expect("Failed read");
+        input
+    }
+
+    pub fn line(&mut self) -> String {
+        let mut input = String::new();
+        self.reader.read_line(&mut input).expect("Failed read");
+        input
+    }
+}
+
+fn main() {
+    let (stdin, stdout) = (io::stdin(), io::stdout());
+    let mut scan = UnsafeScanner::new(stdin.lock());
+    let mut out = io::BufWriter::new(stdout.lock());
+
+    let n = scan.token::<i64>();
+    let mut words = HashMap::new();
+
+    for _ in 0..n {
+        let word = scan.token::<String>();
+        *words.entry(word.clone()).or_insert(0) += 1;
+    }
+
+    let mut word = words.into_iter().collect::<Vec<_>>();
+    word.sort_by(|a, b| {
+        if a.1 == b.1 {
+            b.0.cmp(&a.0)
+        } else {
+            b.1.cmp(&a.1)
+        }
+    });
+
+    writeln!(out, "{} {}", word[0].0, word[0].1).unwrap();
+}

724689ba29cf49c041e96a97f020c0387a0a3a63
Chris Ohk <utilforever@gmail.com>
2025-04-15 16:11:17 +0900

    Upload 17254 - Keyboard Event

---
+++ 17000/17254 - Keyboard Event.rs
@@ -0,0 +1,76 @@
+use io::Write;
+use std::{collections::BTreeMap, io, str};
+
+pub struct UnsafeScanner<R> {
+    reader: R,
+    buf_str: Vec<u8>,
+    buf_iter: str::SplitAsciiWhitespace<'static>,
+}
+
+impl<R: io::BufRead> UnsafeScanner<R> {
+    pub fn new(reader: R) -> Self {
+        Self {
+            reader,
+            buf_str: vec![],
+            buf_iter: "".split_ascii_whitespace(),
+        }
+    }
+
+    pub fn token<T: str::FromStr>(&mut self) -> T {
+        loop {
+            if let Some(token) = self.buf_iter.next() {
+                return token.parse().ok().expect("Failed parse");
+            }
+            self.buf_str.clear();
+            self.reader
+                .read_until(b'\n', &mut self.buf_str)
+                .expect("Failed read");
+            self.buf_iter = unsafe {
+                let slice = str::from_utf8_unchecked(&self.buf_str);
+                std::mem::transmute(slice.split_ascii_whitespace())
+            }
+        }
+    }
+
+    pub fn all(&mut self) -> String {
+        let mut input = String::new();
+        self.reader.read_to_string(&mut input).expect("Failed read");
+        input
+    }
+
+    pub fn line(&mut self) -> String {
+        let mut input = String::new();
+        self.reader.read_line(&mut input).expect("Failed read");
+        input
+    }
+}
+
+fn main() {
+    let (stdin, stdout) = (io::stdin(), io::stdout());
+    let mut scan = UnsafeScanner::new(stdin.lock());
+    let mut out = io::BufWriter::new(stdout.lock());
+
+    let (n, m) = (scan.token::<usize>(), scan.token::<i64>());
+    let mut keyboards = vec![BTreeMap::new(); n];
+    let mut time_max = 0;
+
+    for _ in 0..m {
+        let (a, b, c) = (
+            scan.token::<usize>(),
+            scan.token::<i64>(),
+            scan.token::<char>(),
+        );
+        keyboards[a - 1].insert(b, c);
+        time_max = time_max.max(b);
+    }
+
+    for i in 0..=time_max {
+        for j in 0..n {
+            if let Some(&c) = keyboards[j].get(&i) {
+                write!(out, "{c}").unwrap();
+            }
+        }
+    }
+
+    writeln!(out).unwrap();
+}

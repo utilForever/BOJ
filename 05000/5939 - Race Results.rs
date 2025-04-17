73095558b6a06a8f6910127ffd67312f7d4ef33b
Chris Ohk <utilforever@gmail.com>
2025-04-17 19:34:34 +0900

    Upload 5939 - Race Results

---
+++ 05000/5939 - Race Results.rs
@@ -0,0 +1,79 @@
+use io::Write;
+use std::{io, str};
+
+pub struct UnsafeScanner<R> {
+    reader: R,
+    buf_str: Vec<u8>,
+    buf_iter: str::SplitAsciiWhitespace<'static>,
+}
+
+impl<R: io::BufRead> UnsafeScanner<R> {
+    pub fn new(reader: R) -> Self {
+        Self {
+            reader,
+            buf_str: vec![],
+            buf_iter: "".split_ascii_whitespace(),
+        }
+    }
+
+    pub fn token<T: str::FromStr>(&mut self) -> T {
+        loop {
+            if let Some(token) = self.buf_iter.next() {
+                return token.parse().ok().expect("Failed parse");
+            }
+            self.buf_str.clear();
+            self.reader
+                .read_until(b'\n', &mut self.buf_str)
+                .expect("Failed read");
+            self.buf_iter = unsafe {
+                let slice = str::from_utf8_unchecked(&self.buf_str);
+                std::mem::transmute(slice.split_ascii_whitespace())
+            }
+        }
+    }
+
+    pub fn all(&mut self) -> String {
+        let mut input = String::new();
+        self.reader.read_to_string(&mut input).expect("Failed read");
+        input
+    }
+
+    pub fn line(&mut self) -> String {
+        let mut input = String::new();
+        self.reader.read_line(&mut input).expect("Failed read");
+        input
+    }
+}
+
+fn main() {
+    let (stdin, stdout) = (io::stdin(), io::stdout());
+    let mut scan = UnsafeScanner::new(stdin.lock());
+    let mut out = io::BufWriter::new(stdout.lock());
+
+    let n = scan.token::<usize>();
+    let mut times = vec![(0, 0, 0); n];
+
+    for i in 0..n {
+        times[i] = (
+            scan.token::<i64>(),
+            scan.token::<i64>(),
+            scan.token::<i64>(),
+        );
+    }
+
+    times.sort_unstable_by(|a, b| {
+        if a.0 == b.0 {
+            if a.1 == b.1 {
+                a.2.cmp(&b.2)
+            } else {
+                a.1.cmp(&b.1)
+            }
+        } else {
+            a.0.cmp(&b.0)
+        }
+    });
+
+    for time in times {
+        writeln!(out, "{} {} {}", time.0, time.1, time.2).unwrap();
+    }
+}

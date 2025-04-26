cd960cfc224c0e57035c6f3b287eb27cda962914
Chris Ohk <utilforever@gmail.com>
2025-04-26 17:38:15 +0900

    Upload 20170 - Commemorative Dice

---
+++ 20000/20170 - Commemorative Dice.rs
@@ -0,0 +1,105 @@
+use io::Write;
+use std::{io, str};
+
+pub struct UnsafeScanner<R> {
+    reader: R,
+    buf_str: Vec<u8>,
+    buf_iter: str::SplitAsciiWhitespace<'static>,
+}
+
+impl<R: io::BufRead> UnsafeScanner<R> {
+    pub fn new(reader: R) -> Self {
+        Self {
+            reader,
+            buf_str: vec![],
+            buf_iter: "".split_ascii_whitespace(),
+        }
+    }
+
+    pub fn token<T: str::FromStr>(&mut self) -> T {
+        loop {
+            if let Some(token) = self.buf_iter.next() {
+                return token.parse().ok().expect("Failed parse");
+            }
+            self.buf_str.clear();
+            self.reader
+                .read_until(b'\n', &mut self.buf_str)
+                .expect("Failed read");
+            self.buf_iter = unsafe {
+                let slice = str::from_utf8_unchecked(&self.buf_str);
+                std::mem::transmute(slice.split_ascii_whitespace())
+            }
+        }
+    }
+
+    pub fn all(&mut self) -> String {
+        let mut input = String::new();
+        self.reader.read_to_string(&mut input).expect("Failed read");
+        input
+    }
+
+    pub fn line(&mut self) -> String {
+        let mut input = String::new();
+        self.reader.read_line(&mut input).expect("Failed read");
+        input
+    }
+}
+
+fn gcd(first: i64, second: i64) -> i64 {
+    let mut max = first;
+    let mut min = second;
+
+    if min == 0 && max == 0 {
+        return 0;
+    } else if min == 0 {
+        return max;
+    } else if max == 0 {
+        return min;
+    }
+
+    if min > max {
+        std::mem::swap(&mut min, &mut max);
+    }
+
+    loop {
+        let res = max % min;
+
+        if res == 0 {
+            return min;
+        }
+
+        max = min;
+        min = res;
+    }
+}
+
+fn main() {
+    let (stdin, stdout) = (io::stdin(), io::stdout());
+    let mut scan = UnsafeScanner::new(stdin.lock());
+    let mut out = io::BufWriter::new(stdout.lock());
+
+    let mut first = vec![0; 6];
+    let mut second = vec![0; 6];
+
+    for i in 0..6 {
+        first[i] = scan.token::<i64>();
+    }
+
+    for i in 0..6 {
+        second[i] = scan.token::<i64>();
+    }
+
+    let mut cnt = 0;
+
+    for i in 0..6 {
+        for j in 0..6 {
+            if first[i] > second[j] {
+                cnt += 1;
+            }
+        }
+    }
+
+    let g = gcd(cnt, 36);
+
+    writeln!(out, "{}/{}", cnt / g, 36 / g).unwrap();
+}

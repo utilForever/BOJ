bc13900ba3b18317343512f36d626aea969cf66f
Chris Ohk <utilforever@gmail.com>
2025-05-05 14:23:07 +0900

    Upload 9414 - A Minimum Land Price

---
+++ 09000/9414 - A Minimum Land Price.rs
@@ -0,0 +1,87 @@
+use io::Write;
+use std::{io, str};
+
+pub struct UnsafeScanner<R> {
+    reader: R,
+    buf_str: Vec<u8>,
+    buf_iter: str::SplitAsciiWhitespace<'static>,
+}
+
+impl<R: io::BufRead> UnsafeScanner<R> {
+    pub fn new(reader: R) -> Self {
+        Self {
+            reader,
+            buf_str: vec![],
+            buf_iter: "".split_ascii_whitespace(),
+        }
+    }
+
+    pub fn token<T: str::FromStr>(&mut self) -> T {
+        loop {
+            if let Some(token) = self.buf_iter.next() {
+                return token.parse().ok().expect("Failed parse");
+            }
+            self.buf_str.clear();
+            self.reader
+                .read_until(b'\n', &mut self.buf_str)
+                .expect("Failed read");
+            self.buf_iter = unsafe {
+                let slice = str::from_utf8_unchecked(&self.buf_str);
+                std::mem::transmute(slice.split_ascii_whitespace())
+            }
+        }
+    }
+
+    pub fn all(&mut self) -> String {
+        let mut input = String::new();
+        self.reader.read_to_string(&mut input).expect("Failed read");
+        input
+    }
+
+    pub fn line(&mut self) -> String {
+        let mut input = String::new();
+        self.reader.read_line(&mut input).expect("Failed read");
+        input
+    }
+}
+
+fn main() {
+    let (stdin, stdout) = (io::stdin(), io::stdout());
+    let mut scan = UnsafeScanner::new(stdin.lock());
+    let mut out = io::BufWriter::new(stdout.lock());
+
+    let t = scan.token::<i64>();
+
+    for _ in 0..t {
+        let mut prices = Vec::new();
+
+        loop {
+            let price = scan.token::<i64>();
+
+            if price == 0 {
+                break;
+            }
+
+            prices.push(price);
+        }
+
+        prices.sort_unstable_by(|a, b| b.cmp(a));
+
+        let mut check = true;
+        let mut ret = 0;
+
+        for i in 0..prices.len() {
+            ret += 2 * prices[i].pow(i as u32 + 1);
+
+            if ret > 5_000_000 {
+                writeln!(out, "Too expensive").unwrap();
+                check = false;
+                break;
+            }
+        }
+
+        if check {
+            writeln!(out, "{ret}").unwrap();
+        }
+    }
+}

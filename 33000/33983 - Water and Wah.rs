9f23481c12cf7c68821e645749377e963d15df6b
Chris Ohk <utilforever@gmail.com>
2025-05-27 23:00:11 +0900

    Upload 33983 - Water and Wah

---
+++ 33000/33983 - Water and Wah.rs
@@ -0,0 +1,94 @@
+use io::Write;
+use std::{io, str};
+
+pub struct UnsafeScanner<R> {
+    reader: R,
+    buf_str: Vec<u8>,
+    buf_iter: str::SplitAsciiWhitespace<'static>,
+}
+
+impl<R: io::BufRead> UnsafeScanner<R> {
+    pub fn new(reader: R) -> Self {
+        Self {
+            reader,
+            buf_str: vec![],
+            buf_iter: "".split_ascii_whitespace(),
+        }
+    }
+
+    pub fn token<T: str::FromStr>(&mut self) -> T {
+        loop {
+            if let Some(token) = self.buf_iter.next() {
+                return token.parse().ok().expect("Failed parse");
+            }
+            self.buf_str.clear();
+            self.reader
+                .read_until(b'\n', &mut self.buf_str)
+                .expect("Failed read");
+            self.buf_iter = unsafe {
+                let slice = str::from_utf8_unchecked(&self.buf_str);
+                std::mem::transmute(slice.split_ascii_whitespace())
+            }
+        }
+    }
+
+    pub fn all(&mut self) -> String {
+        let mut input = String::new();
+        self.reader.read_to_string(&mut input).expect("Failed read");
+        input
+    }
+
+    pub fn line(&mut self) -> String {
+        let mut input = String::new();
+        self.reader.read_line(&mut input).expect("Failed read");
+        input
+    }
+}
+
+fn main() {
+    let (stdin, stdout) = (io::stdin(), io::stdout());
+    let mut scan = UnsafeScanner::new(stdin.lock());
+    let mut out = io::BufWriter::new(stdout.lock());
+
+    let _ = scan.token::<String>().chars().collect::<Vec<_>>();
+    let s = scan.token::<String>().chars().collect::<Vec<_>>();
+    let mut cnt_h = 0;
+    let mut cnt_o = 0;
+
+    for &c in s.iter() {
+        if c == 'H' {
+            cnt_h += 1;
+        } else {
+            cnt_o += 1;
+        }
+    }
+
+    if cnt_h != 2 * cnt_o {
+        writeln!(out, "mix").unwrap();
+        return;
+    }
+
+    let mut diff = 0;
+
+    for &c in s.iter() {
+        diff += if c == 'H' { 1 } else { -1 };
+
+        if diff < 0 {
+            writeln!(out, "mix").unwrap();
+            return;
+        }
+    }
+
+    let mut diff = 0;
+
+    for &c in s.iter().rev() {
+        diff += if c == 'H' { 1 } else { -1 };
+
+        if diff < 0 {
+            writeln!(out, "mix").unwrap();
+            return;
+        }
+    }
+
+    writeln!(out, "pure").unwrap();
+}

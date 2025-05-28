1a209852546018912094630443af63b71bfcddec
Chris Ohk <utilforever@gmail.com>
2025-05-29 01:28:52 +0900

    Upload 33992 - Desert Exploration

---
+++ 33000/33992 - Desert Exploration.rs
@@ -0,0 +1,71 @@
+use io::Write;
+use std::{io, str};
+
+pub struct UnsafeScanner<R> {
+    reader: R,
+    buf_str: Vec<u8>,
+    buf_iter: str::SplitAsciiWhitespace<'static>,
+}
+
+impl<R: io::BufRead> UnsafeScanner<R> {
+    pub fn new(reader: R) -> Self {
+        Self {
+            reader,
+            buf_str: vec![],
+            buf_iter: "".split_ascii_whitespace(),
+        }
+    }
+
+    pub fn token<T: str::FromStr>(&mut self) -> T {
+        loop {
+            if let Some(token) = self.buf_iter.next() {
+                return token.parse().ok().expect("Failed parse");
+            }
+            self.buf_str.clear();
+            self.reader
+                .read_until(b'\n', &mut self.buf_str)
+                .expect("Failed read");
+            self.buf_iter = unsafe {
+                let slice = str::from_utf8_unchecked(&self.buf_str);
+                std::mem::transmute(slice.split_ascii_whitespace())
+            }
+        }
+    }
+
+    pub fn all(&mut self) -> String {
+        let mut input = String::new();
+        self.reader.read_to_string(&mut input).expect("Failed read");
+        input
+    }
+
+    pub fn line(&mut self) -> String {
+        let mut input = String::new();
+        self.reader.read_line(&mut input).expect("Failed read");
+        input
+    }
+}
+
+fn main() {
+    let (stdin, stdout) = (io::stdin(), io::stdout());
+    let mut scan = UnsafeScanner::new(stdin.lock());
+    let mut out = io::BufWriter::new(stdout.lock());
+
+    let (ax, ay, bx, by, px, py, r) = (
+        scan.token::<i64>(),
+        scan.token::<i64>(),
+        scan.token::<i64>(),
+        scan.token::<i64>(),
+        scan.token::<i64>(),
+        scan.token::<i64>(),
+        scan.token::<i64>(),
+    );
+    let dx = bx - ax;
+    let dy = by - ay;
+    let dist_ab = ((dx * dx + dy * dy) as f64).sqrt();
+    let dist_ap =
+        ((((px - ax) * (px - ax) + (py - ay) * (py - ay)) as f64).sqrt() - r as f64).max(0.0);
+    let dist_bp =
+        ((((px - bx) * (px - bx) + (py - by) * (py - by)) as f64).sqrt() - r as f64).max(0.0);
+
+    writeln!(out, "{:.10}", dist_ab.min(dist_ap + dist_bp)).unwrap();
+}

91d588a8f76620970d1b323a8b694648a529e119
Chris Ohk <utilforever@gmail.com>
2025-05-31 03:21:44 +0900

    Update 1019 - Book Page

--- 01000/1019 - Book Page.rs
+++ 01000/1019 - Book Page.rs
@@ -1,17 +1,48 @@
-use std::io;
+use io::Write;
+use std::{io, str};
 
-fn input_integers() -> Vec<i64> {
-    let mut s = String::new();
+pub struct UnsafeScanner<R> {
+    reader: R,
+    buf_str: Vec<u8>,
+    buf_iter: str::SplitAsciiWhitespace<'static>,
+}
 
-    io::stdin().read_line(&mut s).unwrap();
+impl<R: io::BufRead> UnsafeScanner<R> {
+    pub fn new(reader: R) -> Self {
+        Self {
+            reader,
+            buf_str: vec![],
+            buf_iter: "".split_ascii_whitespace(),
+        }
+    }
 
-    let values: Vec<i64> = s
-        .as_mut_str()
-        .split_whitespace()
-        .map(|s| s.parse().unwrap())
-        .collect();
+    pub fn token<T: str::FromStr>(&mut self) -> T {
+        loop {
+            if let Some(token) = self.buf_iter.next() {
+                return token.parse().ok().expect("Failed parse");
+            }
+            self.buf_str.clear();
+            self.reader
+                .read_until(b'\n', &mut self.buf_str)
+                .expect("Failed read");
+            self.buf_iter = unsafe {
+                let slice = str::from_utf8_unchecked(&self.buf_str);
+                std::mem::transmute(slice.split_ascii_whitespace())
+            }
+        }
+    }
 
-    values
+    pub fn all(&mut self) -> String {
+        let mut input = String::new();
+        self.reader.read_to_string(&mut input).expect("Failed read");
+        input
+    }
+
+    pub fn line(&mut self) -> String {
+        let mut input = String::new();
+        self.reader.read_line(&mut input).expect("Failed read");
+        input
+    }
 }
 
 fn calc_nums(nums: &mut Vec<i64>, mut n: i64, cnt: i64) {
@@ -22,8 +53,11 @@ fn calc_nums(nums: &mut Vec<i64>, mut n: i64, cnt: i64) {
 }
 
 fn main() {
-    let mut end = input_integers()[0];
+    let (stdin, stdout) = (io::stdin(), io::stdout());
+    let mut scan = UnsafeScanner::new(stdin.lock());
+    let mut out = io::BufWriter::new(stdout.lock());
 
+    let mut end = scan.token::<i64>();
     let mut nums = vec![0; 10];
     let mut start = 1;
     let mut multiplier = 1;
@@ -54,8 +88,8 @@ fn main() {
     }
 
     for i in 0..10 {
-        print!("{} ", nums[i]);
+        write!(out, "{} ", nums[i]).unwrap();
     }
 
-    println!();
+    writeln!(out).unwrap();
 }

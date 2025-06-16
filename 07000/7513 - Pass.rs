2f9821d66c6f398d256381ad35bbe8411283be5d
Chris Ohk <utilforever@gmail.com>
2025-06-17 08:53:07 +0900

    Upload 7513 - Pass

---
+++ 07000/7513 - Pass.rs
@@ -0,0 +1,82 @@
+use io::Write;
+use std::{io, str, vec};
+
+pub struct UnsafeScanner<R> {
+    reader: R,
+    buf_str: Vec<u8>,
+    buf_iter: str::SplitAsciiWhitespace<'static>,
+}
+
+impl<R: io::BufRead> UnsafeScanner<R> {
+    pub fn new(reader: R) -> Self {
+        Self {
+            reader,
+            buf_str: vec![],
+            buf_iter: "".split_ascii_whitespace(),
+        }
+    }
+
+    pub fn token<T: str::FromStr>(&mut self) -> T {
+        loop {
+            if let Some(token) = self.buf_iter.next() {
+                return token.parse().ok().expect("Failed parse");
+            }
+            self.buf_str.clear();
+            self.reader
+                .read_until(b'\n', &mut self.buf_str)
+                .expect("Failed read");
+            self.buf_iter = unsafe {
+                let slice = str::from_utf8_unchecked(&self.buf_str);
+                std::mem::transmute(slice.split_ascii_whitespace())
+            }
+        }
+    }
+
+    pub fn all(&mut self) -> String {
+        let mut input = String::new();
+        self.reader.read_to_string(&mut input).expect("Failed read");
+        input
+    }
+
+    pub fn line(&mut self) -> String {
+        let mut input = String::new();
+        self.reader.read_line(&mut input).expect("Failed read");
+        input
+    }
+}
+
+fn main() {
+    let (stdin, stdout) = (io::stdin(), io::stdout());
+    let mut scan = UnsafeScanner::new(stdin.lock());
+    let mut out = io::BufWriter::new(stdout.lock());
+
+    let t = scan.token::<i64>();
+
+    for i in 1..=t {
+        let m = scan.token::<usize>();
+        let mut words = vec![String::new(); m];
+
+        for j in 0..m {
+            words[j] = scan.token::<String>();
+        }
+
+        writeln!(out, "Scenario #{i}:").unwrap();
+
+        let n = scan.token::<i64>();
+
+        for _ in 0..n {
+            let m = scan.token::<usize>();
+
+            for _ in 0..m {
+                let idx = scan.token::<usize>();
+                write!(out, "{}", words[idx]).unwrap();
+            }
+
+            writeln!(out).unwrap();
+        }
+
+        if i != t {
+            writeln!(out).unwrap();
+        }
+    }
+}

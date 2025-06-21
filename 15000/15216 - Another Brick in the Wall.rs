6b274f940d2eb344293f4742dc5dd5f7f89fee2b
Chris Ohk <utilforever@gmail.com>
2025-06-21 14:24:10 +0900

    Upload 15216 - Another Brick in the Wall

---
+++ 15000/15216 - Another Brick in the Wall.rs
@@ -0,0 +1,77 @@
+use io::Write;
+use std::{io, str, vec};
+
+pub struct UnsafeScanner<R> {
+    reader: R,
+    buf_str: Vec<u8>,
+    buf_iter: str::SplitAsciiWhitespace<'static>,
+}
+
+impl<R: io::BufRead> UnsafeScanner<R> {
+    pub fn new(reader: R) -> Self {
+        Self {
+            reader,
+            buf_str: vec![],
+            buf_iter: "".split_ascii_whitespace(),
+        }
+    }
+
+    pub fn token<T: str::FromStr>(&mut self) -> T {
+        loop {
+            if let Some(token) = self.buf_iter.next() {
+                return token.parse().ok().expect("Failed parse");
+            }
+            self.buf_str.clear();
+            self.reader
+                .read_until(b'\n', &mut self.buf_str)
+                .expect("Failed read");
+            self.buf_iter = unsafe {
+                let slice = str::from_utf8_unchecked(&self.buf_str);
+                std::mem::transmute(slice.split_ascii_whitespace())
+            }
+        }
+    }
+
+    pub fn all(&mut self) -> String {
+        let mut input = String::new();
+        self.reader.read_to_string(&mut input).expect("Failed read");
+        input
+    }
+
+    pub fn line(&mut self) -> String {
+        let mut input = String::new();
+        self.reader.read_line(&mut input).expect("Failed read");
+        input
+    }
+}
+
+fn main() {
+    let (stdin, stdout) = (io::stdin(), io::stdout());
+    let mut scan = UnsafeScanner::new(stdin.lock());
+    let mut out = io::BufWriter::new(stdout.lock());
+
+    let (h, w, n) = (
+        scan.token::<i64>(),
+        scan.token::<i64>(),
+        scan.token::<i64>(),
+    );
+    let mut width = 0;
+    let mut height = 0;
+    let mut ret = false;
+
+    for _ in 0..n {
+        let x = scan.token::<i64>();
+        width += x;
+
+        if width == w {
+            height += 1;
+            width = 0;
+
+            if height == h {
+                ret = true;
+            }
+        }
+    }
+
+    writeln!(out, "{}", if ret { "YES" } else { "NO" }).unwrap();
+}

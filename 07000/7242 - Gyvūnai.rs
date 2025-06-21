d84722869758377ae51941b83d6dd248eeb60277
Chris Ohk <utilforever@gmail.com>
2025-06-21 14:38:39 +0900

    Upload 7242 - Gyvūnai

---
+++ 07000/7242 - Gyvūnai.rs
@@ -0,0 +1,79 @@
+use io::Write;
+use std::{io, str, vec};
+
+pub struct UnsafeScanner<R> {
+    reader: R,
+    buf_str: Vec<u8>,
+    buf_iter: str::SplitAsciiWhitespace<'static>,
+}
+
+impl<R: io::BufRead> UnsafeScanner<R> {
+    pub fn new(reader: R) -> Self {
+        Self {
+            reader,
+            buf_str: vec![],
+            buf_iter: "".split_ascii_whitespace(),
+        }
+    }
+
+    pub fn token<T: str::FromStr>(&mut self) -> T {
+        loop {
+            if let Some(token) = self.buf_iter.next() {
+                return token.parse().ok().expect("Failed parse");
+            }
+            self.buf_str.clear();
+            self.reader
+                .read_until(b'\n', &mut self.buf_str)
+                .expect("Failed read");
+            self.buf_iter = unsafe {
+                let slice = str::from_utf8_unchecked(&self.buf_str);
+                std::mem::transmute(slice.split_ascii_whitespace())
+            }
+        }
+    }
+
+    pub fn all(&mut self) -> String {
+        let mut input = String::new();
+        self.reader.read_to_string(&mut input).expect("Failed read");
+        input
+    }
+
+    pub fn line(&mut self) -> String {
+        let mut input = String::new();
+        self.reader.read_line(&mut input).expect("Failed read");
+        input
+    }
+}
+
+fn main() {
+    let (stdin, stdout) = (io::stdin(), io::stdout());
+    let mut scan = UnsafeScanner::new(stdin.lock());
+    let mut out = io::BufWriter::new(stdout.lock());
+
+    let n = scan.token::<i64>();
+    let mut min = 0;
+    let mut max = i64::MAX;
+
+    for _ in 0..n {
+        let (start_h, start_m, end_h, end_m) = (
+            scan.token::<i64>(),
+            scan.token::<i64>(),
+            scan.token::<i64>(),
+            scan.token::<i64>(),
+        );
+        let start = start_h * 60 + start_m;
+        let end = end_h * 60 + end_m;
+
+        min = min.max(start);
+        max = max.min(end);
+    }
+
+    if min >= max {
+        writeln!(out, "NE").unwrap();
+        return;
+    }
+
+    writeln!(out, "TAIP").unwrap();
+    write!(out, "{} {} ", min / 60, min % 60).unwrap();
+    writeln!(out, "{} {}", max / 60, max % 60).unwrap();
+}

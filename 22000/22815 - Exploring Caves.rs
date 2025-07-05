3ce513e57778f924f94f899f9dbf414841d9b1b8
Chris Ohk <utilforever@gmail.com>
2025-07-05 11:45:16 +0900

    Upload 22815 - Exploring Caves

---
+++ 22000/22815 - Exploring Caves.rs
@@ -0,0 +1,80 @@
+use io::Write;
+use std::{io, str};
+
+pub struct UnsafeScanner<R> {
+    reader: R,
+    buf_str: Vec<u8>,
+    buf_iter: str::SplitAsciiWhitespace<'static>,
+}
+
+impl<R: io::BufRead> UnsafeScanner<R> {
+    pub fn new(reader: R) -> Self {
+        Self {
+            reader,
+            buf_str: vec![],
+            buf_iter: "".split_ascii_whitespace(),
+        }
+    }
+
+    pub fn token<T: str::FromStr>(&mut self) -> T {
+        loop {
+            if let Some(token) = self.buf_iter.next() {
+                return token.parse().ok().expect("Failed parse");
+            }
+            self.buf_str.clear();
+            self.reader
+                .read_until(b'\n', &mut self.buf_str)
+                .expect("Failed read");
+            self.buf_iter = unsafe {
+                let slice = str::from_utf8_unchecked(&self.buf_str);
+                std::mem::transmute(slice.split_ascii_whitespace())
+            }
+        }
+    }
+
+    pub fn all(&mut self) -> String {
+        let mut input = String::new();
+        self.reader.read_to_string(&mut input).expect("Failed read");
+        input
+    }
+
+    pub fn line(&mut self) -> String {
+        let mut input = String::new();
+        self.reader.read_line(&mut input).expect("Failed read");
+        input
+    }
+}
+
+fn main() {
+    let (stdin, stdout) = (io::stdin(), io::stdout());
+    let mut scan = UnsafeScanner::new(stdin.lock());
+    let mut out = io::BufWriter::new(stdout.lock());
+
+    let n = scan.token::<i64>();
+
+    for _ in 0..n {
+        let (mut x, mut y) = (0, 0);
+        let mut dist_max = 0;
+        let mut ret = (0, 0);
+
+        loop {
+            let (dx, dy) = (scan.token::<i64>(), scan.token::<i64>());
+
+            if dx == 0 && dy == 0 {
+                break;
+            }
+
+            x += dx;
+            y += dy;
+
+            let dist = x * x + y * y;
+
+            if dist > dist_max || (dist == dist_max && x > ret.0) {
+                dist_max = dist;
+                ret = (x, y);
+            }
+        }
+
+        writeln!(out, "{} {}", ret.0, ret.1).unwrap();
+    }
+}

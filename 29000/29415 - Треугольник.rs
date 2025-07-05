c9f490b602e5d10fa8e914380e22ca679946e17e
Chris Ohk <utilforever@gmail.com>
2025-07-05 13:03:13 +0900

    Upload 29415 - Треугольник

---
+++ 29000/29415 - Треугольник.rs
@@ -0,0 +1,78 @@
+use io::Write;
+use std::{io, str};
+
+pub struct UnsafeScanner<R> {
+    reader: R,
+    buf_str: Vec<u8>,
+    buf_iter: str::SplitAsciiWhitespace<'static>,
+}
+
+impl<R: io::BufRead> UnsafeScanner<R> {
+    pub fn new(reader: R) -> Self {
+        Self {
+            reader,
+            buf_str: vec![],
+            buf_iter: "".split_ascii_whitespace(),
+        }
+    }
+
+    pub fn token<T: str::FromStr>(&mut self) -> T {
+        loop {
+            if let Some(token) = self.buf_iter.next() {
+                return token.parse().ok().expect("Failed parse");
+            }
+            self.buf_str.clear();
+            self.reader
+                .read_until(b'\n', &mut self.buf_str)
+                .expect("Failed read");
+            self.buf_iter = unsafe {
+                let slice = str::from_utf8_unchecked(&self.buf_str);
+                std::mem::transmute(slice.split_ascii_whitespace())
+            }
+        }
+    }
+
+    pub fn all(&mut self) -> String {
+        let mut input = String::new();
+        self.reader.read_to_string(&mut input).expect("Failed read");
+        input
+    }
+
+    pub fn line(&mut self) -> String {
+        let mut input = String::new();
+        self.reader.read_line(&mut input).expect("Failed read");
+        input
+    }
+}
+
+fn is_perfect_square(val: i64) -> bool {
+    let root = (val as f64).sqrt() as i64;
+    root * root == val || (root + 1) * (root + 1) == val
+}
+
+fn main() {
+    let (stdin, stdout) = (io::stdin(), io::stdout());
+    let mut scan = UnsafeScanner::new(stdin.lock());
+    let mut out = io::BufWriter::new(stdout.lock());
+
+    let s = scan.token::<i64>();
+    let mut a = 1;
+    let mut ret = 0;
+
+    while a * a <= 2 * s {
+        if (2 * s) % a != 0 {
+            a += 1;
+            continue;
+        }
+
+        let b = (2 * s) / a;
+
+        if a <= b && is_perfect_square(a * a + b * b) {
+            ret += 1;
+        }
+
+        a += 1;
+    }
+
+    writeln!(out, "{ret}").unwrap();
+}

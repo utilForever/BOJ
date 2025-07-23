b3cf2d1341810ea1b04918d0a0c29134f15990ce
Chris Ohk <utilforever@gmail.com>
2025-07-23 19:53:19 +0900

    Upload 12646 - The Next Number (Large)

---
+++ 12000/12646 - The Next Number (Large).rs
@@ -0,0 +1,93 @@
+use io::Write;
+use std::{io, str};
+
+pub struct UnsafeScanner<R> {
+    reader: R,
+    buf_str: Vec<u8>,
+    buf_iter: str::SplitAsciiWhitespace<'static>,
+}
+
+impl<R: io::BufRead> UnsafeScanner<R> {
+    pub fn new(reader: R) -> Self {
+        Self {
+            reader,
+            buf_str: vec![],
+            buf_iter: "".split_ascii_whitespace(),
+        }
+    }
+
+    pub fn token<T: str::FromStr>(&mut self) -> T {
+        loop {
+            if let Some(token) = self.buf_iter.next() {
+                return token.parse().ok().expect("Failed parse");
+            }
+            self.buf_str.clear();
+            self.reader
+                .read_until(b'\n', &mut self.buf_str)
+                .expect("Failed read");
+            self.buf_iter = unsafe {
+                let slice = str::from_utf8_unchecked(&self.buf_str);
+                std::mem::transmute(slice.split_ascii_whitespace())
+            }
+        }
+    }
+
+    pub fn all(&mut self) -> String {
+        let mut input = String::new();
+        self.reader.read_to_string(&mut input).expect("Failed read");
+        input
+    }
+
+    pub fn line(&mut self) -> String {
+        let mut input = String::new();
+        self.reader.read_line(&mut input).expect("Failed read");
+        input
+    }
+}
+
+pub fn next_permutation<T>(arr: &mut [T]) -> bool
+where
+    T: std::cmp::Ord,
+{
+    use std::cmp::Ordering;
+
+    let last_ascending = match arr.windows(2).rposition(|w| w[0] < w[1]) {
+        Some(i) => i,
+        None => {
+            arr.reverse();
+            return false;
+        }
+    };
+
+    let swap_with = arr[last_ascending + 1..]
+        .binary_search_by(|n| match arr[last_ascending].cmp(n) {
+            Ordering::Equal => Ordering::Greater,
+            ord => ord,
+        })
+        .unwrap_err();
+    arr.swap(last_ascending, last_ascending + swap_with);
+    arr[last_ascending + 1..].reverse();
+    true
+}
+
+fn main() {
+    let (stdin, stdout) = (io::stdin(), io::stdout());
+    let mut scan = UnsafeScanner::new(stdin.lock());
+    let mut out = io::BufWriter::new(stdout.lock());
+
+    let t = scan.token::<i64>();
+
+    for i in 1..=t {
+        let mut s = scan.token::<String>().chars().collect::<Vec<_>>();
+
+        s.insert(0, '0');
+        
+        next_permutation(&mut s);
+
+        if s[0] == '0' {
+            s.remove(0);
+        }
+
+        writeln!(out, "Case #{i}: {}", s.iter().collect::<String>()).unwrap();
+    }
+}

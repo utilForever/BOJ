cf2872ac58c53800327476a819c379a7778fb2b3
Chris Ohk <utilforever@gmail.com>
2025-08-14 02:28:42 +0900

    Upload 34125 - Raehwan's IVE Concert Story

---
+++ 34000/34125 - Raehwan's IVE Concert Story.rs
@@ -0,0 +1,85 @@
+use io::Write;
+use std::{io, str};
+
+pub struct UnsafeScanner<R> {
+    reader: R,
+    buf_str: Vec<u8>,
+    buf_iter: str::SplitAsciiWhitespace<'static>,
+}
+
+impl<R: io::BufRead> UnsafeScanner<R> {
+    pub fn new(reader: R) -> Self {
+        Self {
+            reader,
+            buf_str: vec![],
+            buf_iter: "".split_ascii_whitespace(),
+        }
+    }
+
+    pub fn token<T: str::FromStr>(&mut self) -> T {
+        loop {
+            if let Some(token) = self.buf_iter.next() {
+                return token.parse().ok().expect("Failed parse");
+            }
+            self.buf_str.clear();
+            self.reader
+                .read_until(b'\n', &mut self.buf_str)
+                .expect("Failed read");
+            self.buf_iter = unsafe {
+                let slice = str::from_utf8_unchecked(&self.buf_str);
+                std::mem::transmute(slice.split_ascii_whitespace())
+            }
+        }
+    }
+
+    pub fn all(&mut self) -> String {
+        let mut input = String::new();
+        self.reader.read_to_string(&mut input).expect("Failed read");
+        input
+    }
+
+    pub fn line(&mut self) -> String {
+        let mut input = String::new();
+        self.reader.read_line(&mut input).expect("Failed read");
+        input
+    }
+}
+
+fn main() {
+    let (stdin, stdout) = (io::stdin(), io::stdout());
+    let mut scan = UnsafeScanner::new(stdin.lock());
+    let mut out = io::BufWriter::new(stdout.lock());
+
+    let (n, m) = (scan.token::<usize>(), scan.token::<usize>());
+    let mut seats = vec![vec![0; m + 1]; n + 1];
+
+    for i in 1..=n {
+        for j in 1..=m {
+            seats[i][j] = scan.token::<i64>();
+        }
+    }
+
+    let mut dist_min = i64::MAX;
+    let mut ret = (0, 0);
+
+    for i in 1..=n {
+        for j in 1..=m {
+            if seats[i][j] == 1 {
+                continue;
+            }
+
+            let dist = i as i64 + ((m as i64 + 1) / 2 - j as i64).abs();
+
+            if dist_min > dist {
+                dist_min = dist;
+                ret = (i, j);
+            }
+        }
+    }
+
+    if dist_min == i64::MAX {
+        writeln!(out, "-1").unwrap();
+    } else {
+        writeln!(out, "{} {}", ret.0, ret.1).unwrap();
+    }
+}

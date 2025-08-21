777f49723d99592af020958712b5c43fe02175fc
Chris Ohk <utilforever@gmail.com>
2025-08-22 01:37:49 +0900

    Upload 25323 - Sorting numbers, but now with squares

---
+++ 25000/25323 - Sorting numbers, but now with squares.rs
@@ -0,0 +1,119 @@
+use io::Write;
+use std::{io, str};
+
+pub struct UnsafeScanner<R> {
+    reader: R,
+    buf_str: Vec<u8>,
+    buf_iter: str::SplitAsciiWhitespace<'static>,
+}
+
+impl<R: io::BufRead> UnsafeScanner<R> {
+    pub fn new(reader: R) -> Self {
+        Self {
+            reader,
+            buf_str: vec![],
+            buf_iter: "".split_ascii_whitespace(),
+        }
+    }
+
+    pub fn token<T: str::FromStr>(&mut self) -> T {
+        loop {
+            if let Some(token) = self.buf_iter.next() {
+                return token.parse().ok().expect("Failed parse");
+            }
+            self.buf_str.clear();
+            self.reader
+                .read_until(b'\n', &mut self.buf_str)
+                .expect("Failed read");
+            self.buf_iter = unsafe {
+                let slice = str::from_utf8_unchecked(&self.buf_str);
+                std::mem::transmute(slice.split_ascii_whitespace())
+            }
+        }
+    }
+
+    pub fn all(&mut self) -> String {
+        let mut input = String::new();
+        self.reader.read_to_string(&mut input).expect("Failed read");
+        input
+    }
+
+    pub fn line(&mut self) -> String {
+        let mut input = String::new();
+        self.reader.read_line(&mut input).expect("Failed read");
+        input
+    }
+}
+
+fn gcd(first: i64, second: i64) -> i64 {
+    let mut max = first;
+    let mut min = second;
+
+    if min == 0 && max == 0 {
+        return 0;
+    } else if min == 0 {
+        return max;
+    } else if max == 0 {
+        return min;
+    }
+
+    if min > max {
+        let val = max;
+
+        max = min;
+        min = val;
+    }
+
+    loop {
+        let res = max % min;
+
+        if res == 0 {
+            return min;
+        }
+
+        max = min;
+        min = res;
+    }
+}
+
+fn is_square(n: i64) -> bool {
+    let mut r = (n as f64).sqrt() as i64;
+
+    while (r + 1) * (r + 1) <= n {
+        r += 1;
+    }
+
+    while r * r > n {
+        r -= 1;
+    }
+
+    r * r == n
+}
+
+fn main() {
+    let (stdin, stdout) = (io::stdin(), io::stdout());
+    let mut scan = UnsafeScanner::new(stdin.lock());
+    let mut out = io::BufWriter::new(stdout.lock());
+
+    let n = scan.token::<usize>();
+    let mut nums1 = vec![0; n];
+
+    for i in 0..n {
+        nums1[i] = scan.token::<i64>();
+    }
+
+    let mut nums2 = nums1.clone();
+    nums2.sort_unstable();
+
+    for i in 0..n {
+        let g = gcd(nums1[i], nums2[i]);
+        let (ax, by) = (nums1[i] / g, nums2[i] / g);
+
+        if !is_square(ax) || !is_square(by) {
+            writeln!(out, "NO").unwrap();
+            return;
+        }
+    }
+
+    writeln!(out, "YES").unwrap();
+}

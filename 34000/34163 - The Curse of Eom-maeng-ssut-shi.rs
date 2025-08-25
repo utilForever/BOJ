7faa6ef9e315be65f87262d9d3a219d4f35b2d7e
Chris Ohk <utilforever@gmail.com>
2025-08-25 22:58:29 +0900

    Update 34163 - The Curse of Eom-maeng-ssut-shi

--- 34000/34163 - The Curse of Eom-maeng-ssut-shi.rs
+++ 34000/34163 - The Curse of Eom-maeng-ssut-shi.rs
@@ -1,4 +1,5 @@
 use io::Write;
+use std::f64::consts::{PI, TAU};
 use std::{io, str};
 
 pub struct UnsafeScanner<R> {
@@ -45,8 +46,92 @@ impl<R: io::BufRead> UnsafeScanner<R> {
     }
 }
 
-fn calculate(_n: i64, _m: i64) -> f64 {
-    0.0
+fn gcd(mut first: i64, mut second: i64) -> i64 {
+    if first < 0 {
+        first = -first;
+    }
+
+    if second < 0 {
+        second = -second;
+    }
+
+    let mut max = first;
+    let mut min = second;
+
+    if min == 0 && max == 0 {
+        return 0;
+    } else if min == 0 {
+        return max;
+    } else if max == 0 {
+        return min;
+    }
+
+    if min > max {
+        std::mem::swap(&mut min, &mut max);
+    }
+
+    loop {
+        let res = max % min;
+
+        if res == 0 {
+            return min;
+        }
+
+        max = min;
+        min = res;
+    }
+}
+
+fn calculate(n: i64, m: i64) -> f64 {
+    if n == 1 {
+        return (PI / m as f64).cos();
+    }
+
+    if m == 1 {
+        return 1.0;
+    }
+
+    let k = gcd(m, n);
+
+    if k > 1 {
+        return (PI / m as f64).cos() / (k as f64 * PI / (n as f64 * m as f64)).cos();
+    }
+
+    if m == 3 {
+        let numerator = 1.5;
+        let denominator = (PI / n as f64).cos()
+            - 2.0 * (PI / n as f64 + TAU * (n / 3) as f64 / n as f64 + PI / 3.0).cos();
+
+        return numerator / denominator;
+    }
+
+    if n == 3 {
+        let angle_offset = if m % 3 == 1 { -PI / 3.0 } else { PI / 3.0 };
+        let angle_common = PI / (3.0 * m as f64);
+        let numerator = (PI / m as f64).cos() + (angle_common + angle_offset).cos();
+        let denominator = angle_common.cos() + (angle_common + angle_offset).cos();
+
+        return numerator / denominator;
+    }
+
+    if m == 4 {
+        return 1.0
+            / (2.0f64.sqrt() * (PI / (2.0 * n as f64)).cos() * (PI / (4.0 * n as f64)).cos());
+    }
+
+    if n == 4 {
+        if m % 4 == 1 {
+            return ((1.0 - (PI / (2.0 * m as f64)).sin())
+                * ((PI / (2.0 * m as f64)).cos() + (PI / (2.0 * m as f64)).sin()))
+                / (PI / (4.0 * m as f64)).cos();
+        } else {
+            return ((1.0 + (PI / (2.0 * m as f64)).sin())
+                * ((PI / (2.0 * m as f64)).cos() - (PI / (2.0 * m as f64)).sin()))
+                / (PI / (4.0 * m as f64)).cos();
+        }
+    }
+
+    -100.0
 }
 
 fn main() {

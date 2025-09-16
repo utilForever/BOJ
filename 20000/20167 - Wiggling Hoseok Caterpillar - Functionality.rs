7bd71095d87088cd03521c2088cc35d1ff39adb2
Chris Ohk <utilforever@gmail.com>
2025-09-17 00:58:27 +0900

    Upload 20167 - Wiggling Hoseok Caterpillar - Functionality

---
+++ 20000/20167 - Wiggling Hoseok Caterpillar - Functionality.rs
@@ -0,0 +1,94 @@
+use io::Write;
+use std::{io, str};
+
+pub struct UnsafeScanner<R> {
+    reader: R,
+    buf_str: Vec<u8>,
+    buf_iter: str::SplitAsciiWhitespace<'static>,
+}
+
+impl<R: io::BufRead> UnsafeScanner<R> {
+    pub fn new(reader: R) -> Self {
+        Self {
+            reader,
+            buf_str: vec![],
+            buf_iter: "".split_ascii_whitespace(),
+        }
+    }
+
+    pub fn token<T: str::FromStr>(&mut self) -> T {
+        loop {
+            if let Some(token) = self.buf_iter.next() {
+                return token.parse().ok().expect("Failed parse");
+            }
+            self.buf_str.clear();
+            self.reader
+                .read_until(b'\n', &mut self.buf_str)
+                .expect("Failed read");
+            self.buf_iter = unsafe {
+                let slice = str::from_utf8_unchecked(&self.buf_str);
+                std::mem::transmute(slice.split_ascii_whitespace())
+            }
+        }
+    }
+
+    pub fn all(&mut self) -> String {
+        let mut input = String::new();
+        self.reader.read_to_string(&mut input).expect("Failed read");
+        input
+    }
+
+    pub fn line(&mut self) -> String {
+        let mut input = String::new();
+        self.reader.read_line(&mut input).expect("Failed read");
+        input
+    }
+}
+
+fn main() {
+    let (stdin, stdout) = (io::stdin(), io::stdout());
+    let mut scan = UnsafeScanner::new(stdin.lock());
+    let mut out = io::BufWriter::new(stdout.lock());
+
+    let (n, k) = (scan.token::<usize>(), scan.token::<i64>());
+    let mut feeds = vec![0; n];
+
+    for i in 0..n {
+        feeds[i] = scan.token::<i64>();
+    }
+
+    let mut positions = vec![n; n];
+    let mut exceeds = vec![0; n];
+    let mut j = 0;
+    let mut sum = 0;
+
+    for i in 0..n {
+        while j < n && sum < k {
+            sum += feeds[j];
+            j += 1;
+        }
+
+        if sum >= k {
+            positions[i] = j - 1;
+            exceeds[i] = sum - k;
+        }
+
+        if i < j {
+            sum -= feeds[i];
+        }
+    }
+
+    let mut dp = vec![0; n + 1];
+
+    for i in (0..n).rev() {
+        let mut val_max = dp[i + 1];
+
+        if positions[i] < n {
+            val_max = val_max.max(exceeds[i] + dp[positions[i] + 1]);
+        }
+
+        dp[i] = val_max;
+    }
+
+    writeln!(out, "{}", dp[0]).unwrap();
+}

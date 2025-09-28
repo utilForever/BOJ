e8ac0087396d0224c52e826e70e5aef26d2b1e94
Chris Ohk <utilforever@gmail.com>
2025-09-28 13:27:56 +0900

    Upload 34541 - Moving Formation

---
+++ 34000/34541 - Moving Formation.rs
@@ -0,0 +1,100 @@
+use io::Write;
+use std::{io, str};
+
+pub struct UnsafeScanner<R> {
+    reader: R,
+    buf_str: Vec<u8>,
+    buf_iter: str::SplitAsciiWhitespace<'static>,
+}
+
+impl<R: io::BufRead> UnsafeScanner<R> {
+    pub fn new(reader: R) -> Self {
+        Self {
+            reader,
+            buf_str: vec![],
+            buf_iter: "".split_ascii_whitespace(),
+        }
+    }
+
+    pub fn token<T: str::FromStr>(&mut self) -> T {
+        loop {
+            if let Some(token) = self.buf_iter.next() {
+                return token.parse().ok().expect("Failed parse");
+            }
+            self.buf_str.clear();
+            self.reader
+                .read_until(b'\n', &mut self.buf_str)
+                .expect("Failed read");
+            self.buf_iter = unsafe {
+                let slice = str::from_utf8_unchecked(&self.buf_str);
+                std::mem::transmute(slice.split_ascii_whitespace())
+            }
+        }
+    }
+
+    pub fn all(&mut self) -> String {
+        let mut input = String::new();
+        self.reader.read_to_string(&mut input).expect("Failed read");
+        input
+    }
+
+    pub fn line(&mut self) -> String {
+        let mut input = String::new();
+        self.reader.read_line(&mut input).expect("Failed read");
+        input
+    }
+}
+
+fn main() {
+    let (stdin, stdout) = (io::stdin(), io::stdout());
+    let mut scan = UnsafeScanner::new(stdin.lock());
+    let mut out = io::BufWriter::new(stdout.lock());
+
+    let (n, x, y) = (
+        scan.token::<i64>(),
+        scan.token::<i64>(),
+        scan.token::<i64>(),
+    );
+    let mut pos = (0, 0);
+    let mut ret = Vec::new();
+
+    for _ in 0..x.min(y) {
+        ret.push(('A', pos.0 + 1, pos.1 + 1));
+        ret.push(('B', pos.0 + 1, pos.1 + n + 1));
+        ret.push(('D', pos.0 + n + 1, pos.1 + 1));
+        ret.push(('C', pos.0 + n + 1, pos.1 + n + 1));
+
+        pos.0 += 1;
+        pos.1 += 1;
+    }
+
+    if x > y {
+        for _ in 0..x - y {
+            ret.push(('B', pos.0 + 1, pos.1 + n - 1));
+            ret.push(('A', pos.0 + 1, pos.1));
+            ret.push(('D', pos.0 + n, pos.1 + 1));
+            ret.push(('B', pos.0 + 1, pos.1 + n));
+            ret.push(('C', pos.0 + n + 1, pos.1 + n));
+            ret.push(('D', pos.0 + n + 1, pos.1));
+
+            pos.0 += 1;
+        }
+    } else if x < y {
+        for _ in 0..y - x {
+            ret.push(('D', pos.0 + n - 1, pos.1 + 1));
+            ret.push(('A', pos.0, pos.1 + 1));
+            ret.push(('B', pos.0 + 1, pos.1 + n));
+            ret.push(('D', pos.0 + n, pos.1 + 1));
+            ret.push(('C', pos.0 + n, pos.1 + n + 1));
+            ret.push(('B', pos.0, pos.1 + n + 1));
+
+            pos.1 += 1;
+        }
+    }
+
+    writeln!(out, "{}", ret.len()).unwrap();
+
+    for (name, x, y) in ret {
+        writeln!(out, "{name} {x} {y}").unwrap();
+    }
+}

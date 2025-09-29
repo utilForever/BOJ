fb610783ae53811e4bcc2dd904ec4289fa3637f5
Chris Ohk <utilforever@gmail.com>
2025-09-29 21:42:34 +0900

    Upload 34521 - Permutation of Not Square Numbers

---
+++ 34000/34521 - Permutation of Not Square Numbers.rs
@@ -0,0 +1,76 @@
+use io::Write;
+use std::{io, str};
+
+pub struct UnsafeScanner<R> {
+    reader: R,
+    buf_str: Vec<u8>,
+    buf_iter: str::SplitAsciiWhitespace<'static>,
+}
+
+impl<R: io::BufRead> UnsafeScanner<R> {
+    pub fn new(reader: R) -> Self {
+        Self {
+            reader,
+            buf_str: vec![],
+            buf_iter: "".split_ascii_whitespace(),
+        }
+    }
+
+    pub fn token<T: str::FromStr>(&mut self) -> T {
+        loop {
+            if let Some(token) = self.buf_iter.next() {
+                return token.parse().ok().expect("Failed parse");
+            }
+            self.buf_str.clear();
+            self.reader
+                .read_until(b'\n', &mut self.buf_str)
+                .expect("Failed read");
+            self.buf_iter = unsafe {
+                let slice = str::from_utf8_unchecked(&self.buf_str);
+                std::mem::transmute(slice.split_ascii_whitespace())
+            }
+        }
+    }
+
+    pub fn all(&mut self) -> String {
+        let mut input = String::new();
+        self.reader.read_to_string(&mut input).expect("Failed read");
+        input
+    }
+
+    pub fn line(&mut self) -> String {
+        let mut input = String::new();
+        self.reader.read_line(&mut input).expect("Failed read");
+        input
+    }
+}
+
+fn is_square(num: usize) -> bool {
+    let root = (num as f64).sqrt() as usize;
+    root * root == num
+}
+
+fn main() {
+    let (stdin, stdout) = (io::stdin(), io::stdout());
+    let mut scan = UnsafeScanner::new(stdin.lock());
+    let mut out = io::BufWriter::new(stdout.lock());
+
+    let t = scan.token::<i64>();
+
+    for _ in 0..t {
+        let n = scan.token::<usize>();
+        let mut nums = (1..=n).collect::<Vec<_>>();
+
+        for i in 1..n - 1 {
+            if is_square(nums[i] + nums[i + 1]) {
+                nums.swap(i, i - 1);
+            }
+        }
+
+        for i in 0..n {
+            write!(out, "{} ", nums[i]).unwrap();
+        }
+
+        writeln!(out).unwrap();
+    }
+}

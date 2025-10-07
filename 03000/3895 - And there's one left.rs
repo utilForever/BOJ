b16065d1259fbd6bd407ce406ace1b868390b2e9
Chris Ohk <utilforever@gmail.com>
2025-10-08 01:53:01 +0900

    Upload 3895 - And there's one left

---
+++ 03000/3895 - And there's one left.rs
@@ -0,0 +1,74 @@
+use io::Write;
+use std::{io, str};
+
+pub struct UnsafeScanner<R> {
+    reader: R,
+    buf_str: Vec<u8>,
+    buf_iter: str::SplitAsciiWhitespace<'static>,
+}
+
+impl<R: io::BufRead> UnsafeScanner<R> {
+    pub fn new(reader: R) -> Self {
+        Self {
+            reader,
+            buf_str: vec![],
+            buf_iter: "".split_ascii_whitespace(),
+        }
+    }
+
+    pub fn token<T: str::FromStr>(&mut self) -> T {
+        loop {
+            if let Some(token) = self.buf_iter.next() {
+                return token.parse().ok().expect("Failed parse");
+            }
+            self.buf_str.clear();
+            self.reader
+                .read_until(b'\n', &mut self.buf_str)
+                .expect("Failed read");
+            self.buf_iter = unsafe {
+                let slice = str::from_utf8_unchecked(&self.buf_str);
+                std::mem::transmute(slice.split_ascii_whitespace())
+            }
+        }
+    }
+
+    pub fn all(&mut self) -> String {
+        let mut input = String::new();
+        self.reader.read_to_string(&mut input).expect("Failed read");
+        input
+    }
+
+    pub fn line(&mut self) -> String {
+        let mut input = String::new();
+        self.reader.read_line(&mut input).expect("Failed read");
+        input
+    }
+}
+
+fn main() {
+    let (stdin, stdout) = (io::stdin(), io::stdout());
+    let mut scan = UnsafeScanner::new(stdin.lock());
+    let mut out = io::BufWriter::new(stdout.lock());
+
+    loop {
+        let (n, k, m) = (
+            scan.token::<i64>(),
+            scan.token::<usize>(),
+            scan.token::<usize>(),
+        );
+
+        if n == 0 && k == 0 && m == 0 {
+            break;
+        }
+
+        let mut stones = (1..=n).collect::<Vec<i64>>();
+        let mut pointer = m - 1;
+
+        while stones.len() > 1 {
+            stones.remove(pointer);
+            pointer = (pointer + k - 1) % stones.len();
+        }
+
+        writeln!(out, "{}", stones[0]).unwrap();
+    }
+}

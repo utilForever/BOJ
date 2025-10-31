8cb457555dde5e309b757d376438a5e6149b611c
Chris Ohk <utilforever@gmail.com>
2025-11-01 03:41:30 +0900

    Upload 13176 - Coin

---
+++ 13000/13176 - Coin.rs
@@ -0,0 +1,71 @@
+use io::Write;
+use std::{io, str};
+
+pub struct UnsafeScanner<R> {
+    reader: R,
+    buf_str: Vec<u8>,
+    buf_iter: str::SplitAsciiWhitespace<'static>,
+}
+
+impl<R: io::BufRead> UnsafeScanner<R> {
+    pub fn new(reader: R) -> Self {
+        Self {
+            reader,
+            buf_str: vec![],
+            buf_iter: "".split_ascii_whitespace(),
+        }
+    }
+
+    pub fn token<T: str::FromStr>(&mut self) -> T {
+        loop {
+            if let Some(token) = self.buf_iter.next() {
+                return token.parse().ok().expect("Failed parse");
+            }
+            self.buf_str.clear();
+            self.reader
+                .read_until(b'\n', &mut self.buf_str)
+                .expect("Failed read");
+            self.buf_iter = unsafe {
+                let slice = str::from_utf8_unchecked(&self.buf_str);
+                std::mem::transmute(slice.split_ascii_whitespace())
+            }
+        }
+    }
+
+    pub fn all(&mut self) -> String {
+        let mut input = String::new();
+        self.reader.read_to_string(&mut input).expect("Failed read");
+        input
+    }
+
+    pub fn line(&mut self) -> String {
+        let mut input = String::new();
+        self.reader.read_line(&mut input).expect("Failed read");
+        input
+    }
+}
+
+const MOD: i64 = 1_000_000_007;
+
+fn main() {
+    let (stdin, stdout) = (io::stdin(), io::stdout());
+    let mut scan = UnsafeScanner::new(stdin.lock());
+    let mut out = io::BufWriter::new(stdout.lock());
+
+    let n = scan.token::<usize>();
+    let mut dp = vec![vec![vec![0; 256]; 2]; n + 1];
+
+    dp[0][0][0] = 1;
+    dp[0][1][0] = 1;
+
+    for i in 1..=n {
+        for j in 1..=i {
+            for k in 0..=250 {
+                dp[i][1][j ^ k] = (dp[i][1][j ^ k] + dp[i - j][0][k]) % MOD;
+                dp[i][0][k] = (dp[i][0][k] + dp[i - j][1][k]) % MOD;
+            }
+        }
+    }
+
+    writeln!(out, "{}", (dp[n][0][0] + dp[n][1][0]) % MOD).unwrap();
+}

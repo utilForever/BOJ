64b63fbd89205f90c87ab3d915beb25711917d34
Chris Ohk <utilforever@gmail.com>
2025-11-19 21:42:45 +0900

    Upload 34692 - Oh, I want to play Maimai

---
+++ 34000/34692 - Oh, I want to play Maimai.rs
@@ -0,0 +1,86 @@
+use io::Write;
+use std::{cmp::Reverse, collections::BinaryHeap, io, str};
+
+pub struct UnsafeScanner<R> {
+    reader: R,
+    buf_str: Vec<u8>,
+    buf_iter: str::SplitAsciiWhitespace<'static>,
+}
+
+impl<R: io::BufRead> UnsafeScanner<R> {
+    pub fn new(reader: R) -> Self {
+        Self {
+            reader,
+            buf_str: vec![],
+            buf_iter: "".split_ascii_whitespace(),
+        }
+    }
+
+    pub fn token<T: str::FromStr>(&mut self) -> T {
+        loop {
+            if let Some(token) = self.buf_iter.next() {
+                return token.parse().ok().expect("Failed parse");
+            }
+            self.buf_str.clear();
+            self.reader
+                .read_until(b'\n', &mut self.buf_str)
+                .expect("Failed read");
+            self.buf_iter = unsafe {
+                let slice = str::from_utf8_unchecked(&self.buf_str);
+                std::mem::transmute(slice.split_ascii_whitespace())
+            }
+        }
+    }
+
+    pub fn all(&mut self) -> String {
+        let mut input = String::new();
+        self.reader.read_to_string(&mut input).expect("Failed read");
+        input
+    }
+
+    pub fn line(&mut self) -> String {
+        let mut input = String::new();
+        self.reader.read_line(&mut input).expect("Failed read");
+        input
+    }
+}
+
+fn main() {
+    let (stdin, stdout) = (io::stdin(), io::stdout());
+    let mut scan = UnsafeScanner::new(stdin.lock());
+    let mut out = io::BufWriter::new(stdout.lock());
+
+    let (n, m, k) = (
+        scan.token::<usize>(),
+        scan.token::<usize>(),
+        scan.token::<i64>(),
+    );
+    let mut times = vec![0; n];
+
+    for i in 0..n {
+        times[i] = scan.token::<i64>();
+    }
+
+    let mut maimais = BinaryHeap::new();
+
+    for _ in 0..m {
+        maimais.push(Reverse(0));
+    }
+
+    for i in 0..n {
+        maimais.peek_mut().map(|mut x| {
+            x.0 += times[i];
+        });
+    }
+
+    writeln!(
+        out,
+        "{}",
+        if maimais.peek().unwrap().0 <= k {
+            "WAIT"
+        } else {
+            "GO"
+        }
+    )
+    .unwrap();
+}

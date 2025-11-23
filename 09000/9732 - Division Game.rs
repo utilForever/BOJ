1091030b78716a9755521cbef31679262fde164f
Chris Ohk <utilforever@gmail.com>
2025-11-24 01:49:34 +0900

    Upload 9732 - Division Game

---
+++ 09000/9732 - Division Game.rs
@@ -0,0 +1,102 @@
+use io::Write;
+use std::{io, str};
+
+pub struct UnsafeScanner<R> {
+    reader: R,
+    buf_str: Vec<u8>,
+    buf_iter: str::SplitAsciiWhitespace<'static>,
+}
+
+impl<R: io::BufRead> UnsafeScanner<R> {
+    pub fn new(reader: R) -> Self {
+        Self {
+            reader,
+            buf_str: vec![],
+            buf_iter: "".split_ascii_whitespace(),
+        }
+    }
+
+    pub fn token<T: str::FromStr>(&mut self) -> T {
+        loop {
+            if let Some(token) = self.buf_iter.next() {
+                return token.parse().ok().expect("Failed parse");
+            }
+            self.buf_str.clear();
+            self.reader
+                .read_until(b'\n', &mut self.buf_str)
+                .expect("Failed read");
+            self.buf_iter = unsafe {
+                let slice = str::from_utf8_unchecked(&self.buf_str);
+                std::mem::transmute(slice.split_ascii_whitespace())
+            }
+        }
+    }
+
+    pub fn all(&mut self) -> String {
+        let mut input = String::new();
+        self.reader.read_to_string(&mut input).expect("Failed read");
+        input
+    }
+
+    pub fn line(&mut self) -> String {
+        let mut input = String::new();
+        self.reader.read_line(&mut input).expect("Failed read");
+        input
+    }
+}
+
+fn main() {
+    let (stdin, stdout) = (io::stdin(), io::stdout());
+    let mut scan = UnsafeScanner::new(stdin.lock());
+    let mut out = io::BufWriter::new(stdout.lock());
+
+    let t = scan.token::<i64>();
+
+    let mut prime_factor = vec![0; 10001];
+
+    for i in 2..=10000 {
+        if prime_factor[i] == 0 {
+            prime_factor[i] = i;
+
+            if i * i > 10000 {
+                continue;
+            }
+
+            let mut j = i * i;
+
+            while j <= 10000 {
+                if prime_factor[j] == 0 {
+                    prime_factor[j] = i;
+                }
+
+                j += i;
+            }
+        }
+    }
+
+    let mut cnt = vec![0; 10001];
+    cnt[1] = 0;
+
+    for n in 2..=10000 {
+        let p = prime_factor[n];
+        cnt[n] = cnt[n / p] + 1;
+    }
+
+    for i in 1..=t {
+        let (n, m) = (scan.token::<usize>(), scan.token::<usize>());
+        let mut ret = 0;
+
+        for _ in 0..n {
+            let mut sum = 0;
+
+            for _ in 0..m {
+                let val = scan.token::<usize>();
+                sum += cnt[val];
+            }
+
+            ret ^= sum;
+        }
+
+        writeln!(out, "Case #{i}: {}", if ret != 0 { "YES" } else { "NO" }).unwrap();
+    }
+}

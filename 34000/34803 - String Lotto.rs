e07fe554911d5c9e12bccaf9ec920e5788a7ef60
Chris Ohk <utilforever@gmail.com>
2025-11-23 21:08:39 +0900

    Upload 34803 - String Lotto

---
+++ 34000/34803 - String Lotto.rs
@@ -0,0 +1,74 @@
+use io::Write;
+use std::{collections::HashMap, io, str};
+
+pub struct UnsafeScanner<R> {
+    reader: R,
+    buf_str: Vec<u8>,
+    buf_iter: str::SplitAsciiWhitespace<'static>,
+}
+
+impl<R: io::BufRead> UnsafeScanner<R> {
+    pub fn new(reader: R) -> Self {
+        Self {
+            reader,
+            buf_str: vec![],
+            buf_iter: "".split_ascii_whitespace(),
+        }
+    }
+
+    pub fn token<T: str::FromStr>(&mut self) -> T {
+        loop {
+            if let Some(token) = self.buf_iter.next() {
+                return token.parse().ok().expect("Failed parse");
+            }
+            self.buf_str.clear();
+            self.reader
+                .read_until(b'\n', &mut self.buf_str)
+                .expect("Failed read");
+            self.buf_iter = unsafe {
+                let slice = str::from_utf8_unchecked(&self.buf_str);
+                std::mem::transmute(slice.split_ascii_whitespace())
+            }
+        }
+    }
+
+    pub fn all(&mut self) -> String {
+        let mut input = String::new();
+        self.reader.read_to_string(&mut input).expect("Failed read");
+        input
+    }
+
+    pub fn line(&mut self) -> String {
+        let mut input = String::new();
+        self.reader.read_line(&mut input).expect("Failed read");
+        input
+    }
+}
+
+fn main() {
+    let (stdin, stdout) = (io::stdin(), io::stdout());
+    let mut scan = UnsafeScanner::new(stdin.lock());
+    let mut out = io::BufWriter::new(stdout.lock());
+
+    let (l, n) = (scan.token::<usize>(), scan.token::<usize>());
+    let mut strings = vec![Vec::new(); n];
+
+    for i in 0..n {
+        let line = scan.token::<String>();
+
+        for c in line.chars() {
+            strings[i].push(c);
+        }
+    }
+
+    let k = scan.token::<usize>();
+    let mut counts = HashMap::new();
+
+    for i in 0..n {
+        for j in 0..l - k + 1 {
+            *counts.entry(&strings[i][j..j + k]).or_insert(0) += 1;
+        }
+    }
+
+    writeln!(out, "{}", counts.iter().map(|(_, &v)| v).max().unwrap()).unwrap();
+}

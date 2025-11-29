9d8d13c9858b8cde25cc7c11a4c70aa64945c84d
Chris Ohk <utilforever@gmail.com>
2025-11-29 21:57:55 +0900

    Upload 34851 - Elementary Arithmetic Game

---
+++ 34000/34851 - Elementary Arithmetic Game.rs
@@ -0,0 +1,75 @@
+use io::Write;
+use std::{io, str};
+
+pub struct UnsafeScanner<R> {
+    reader: R,
+    buf_str: Vec<u8>,
+    buf_iter: str::SplitAsciiWhitespace<'static>,
+}
+
+impl<R: io::BufRead> UnsafeScanner<R> {
+    pub fn new(reader: R) -> Self {
+        Self {
+            reader,
+            buf_str: vec![],
+            buf_iter: "".split_ascii_whitespace(),
+        }
+    }
+
+    pub fn token<T: str::FromStr>(&mut self) -> T {
+        loop {
+            if let Some(token) = self.buf_iter.next() {
+                return token.parse().ok().expect("Failed parse");
+            }
+            self.buf_str.clear();
+            self.reader
+                .read_until(b'\n', &mut self.buf_str)
+                .expect("Failed read");
+            self.buf_iter = unsafe {
+                let slice = str::from_utf8_unchecked(&self.buf_str);
+                std::mem::transmute(slice.split_ascii_whitespace())
+            }
+        }
+    }
+
+    pub fn all(&mut self) -> String {
+        let mut input = String::new();
+        self.reader.read_to_string(&mut input).expect("Failed read");
+        input
+    }
+
+    pub fn line(&mut self) -> String {
+        let mut input = String::new();
+        self.reader.read_line(&mut input).expect("Failed read");
+        input
+    }
+}
+
+const MOD: i64 = 1_000_000_007;
+
+fn main() {
+    let (stdin, stdout) = (io::stdin(), io::stdout());
+    let mut scan = UnsafeScanner::new(stdin.lock());
+    let mut out = io::BufWriter::new(stdout.lock());
+
+    let n = scan.token::<usize>();
+    let mut nums = vec![0; n + 1];
+
+    for i in 0..=n {
+        nums[i] = scan.token::<i64>();
+    }
+
+    let mut ret = nums[0];
+    let mut is_one = nums[0] == 1;
+
+    for i in 1..=n {
+        if is_one || nums[i] == 1 {
+            ret = (ret + nums[i]) % MOD;
+            is_one = false;
+        } else {
+            ret = (ret * nums[i]) % MOD;
+        }
+    }
+
+    writeln!(out, "{ret}").unwrap();
+}

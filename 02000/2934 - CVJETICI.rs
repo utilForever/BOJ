e698d9c61f9caeded61eacbe2d3baaeea981575b
Chris Ohk <utilforever@gmail.com>
2025-12-16 02:41:19 +0900

    Upload 2934 - CVJETICI

---
+++ 02000/2934 - CVJETICI.rs
@@ -0,0 +1,104 @@
+use io::Write;
+use std::{io, str};
+
+pub struct UnsafeScanner<R> {
+    reader: R,
+    buf_str: Vec<u8>,
+    buf_iter: str::SplitAsciiWhitespace<'static>,
+}
+
+impl<R: io::BufRead> UnsafeScanner<R> {
+    pub fn new(reader: R) -> Self {
+        Self {
+            reader,
+            buf_str: vec![],
+            buf_iter: "".split_ascii_whitespace(),
+        }
+    }
+
+    pub fn token<T: str::FromStr>(&mut self) -> T {
+        loop {
+            if let Some(token) = self.buf_iter.next() {
+                return token.parse().ok().expect("Failed parse");
+            }
+            self.buf_str.clear();
+            self.reader
+                .read_until(b'\n', &mut self.buf_str)
+                .expect("Failed read");
+            self.buf_iter = unsafe {
+                let slice = str::from_utf8_unchecked(&self.buf_str);
+                std::mem::transmute(slice.split_ascii_whitespace())
+            }
+        }
+    }
+
+    pub fn all(&mut self) -> String {
+        let mut input = String::new();
+        self.reader.read_to_string(&mut input).expect("Failed read");
+        input
+    }
+
+    pub fn line(&mut self) -> String {
+        let mut input = String::new();
+        self.reader.read_line(&mut input).expect("Failed read");
+        input
+    }
+}
+
+struct FenwickTree {
+    n: usize,
+    data: Vec<i64>,
+}
+
+impl FenwickTree {
+    fn new(n: usize) -> Self {
+        FenwickTree {
+            n,
+            data: vec![0; n + 1],
+        }
+    }
+
+    fn update(&mut self, mut idx: usize, delta: i64) {
+        while idx <= self.n {
+            self.data[idx] += delta;
+            idx += idx & (!idx + 1);
+        }
+    }
+
+    fn query(&self, mut idx: usize) -> i64 {
+        let mut ret = 0;
+
+        while idx > 0 {
+            ret += self.data[idx];
+            idx -= idx & (!idx + 1);
+        }
+
+        ret
+    }
+}
+
+fn main() {
+    let (stdin, stdout) = (io::stdin(), io::stdout());
+    let mut scan = UnsafeScanner::new(stdin.lock());
+    let mut out = io::BufWriter::new(stdout.lock());
+
+    let n = scan.token::<usize>();
+    let mut fenwick_tree = FenwickTree::new(100000);
+    let mut cnt = vec![0; 100001];
+
+    for _ in 0..n {
+        let (l, r) = (scan.token::<usize>(), scan.token::<usize>());
+        let cnt_left = fenwick_tree.query(l);
+        let cnt_right = fenwick_tree.query(r);
+
+        writeln!(out, "{}", cnt_left - cnt[l] + cnt_right - cnt[r]).unwrap();
+
+        cnt[l] = cnt_left;
+        cnt[r] = cnt_right;
+
+        if l + 1 < r {
+            fenwick_tree.update(l + 1, 1);
+            fenwick_tree.update(r, -1);
+        }
+    }
+}

1aeaf4aeca0ddcd73af605d38045bb88e562f0e4
Chris Ohk <utilforever@gmail.com>
2025-12-24 00:52:44 +0900

    Upload 34996 - Sequence and Integral

---
+++ 34000/34996 - Sequence and Integral.rs
@@ -0,0 +1,79 @@
+use io::Write;
+use std::{io, str};
+
+pub struct UnsafeScanner<R> {
+    reader: R,
+    buf_str: Vec<u8>,
+    buf_iter: str::SplitAsciiWhitespace<'static>,
+}
+
+impl<R: io::BufRead> UnsafeScanner<R> {
+    pub fn new(reader: R) -> Self {
+        Self {
+            reader,
+            buf_str: vec![],
+            buf_iter: "".split_ascii_whitespace(),
+        }
+    }
+
+    pub fn token<T: str::FromStr>(&mut self) -> T {
+        loop {
+            if let Some(token) = self.buf_iter.next() {
+                return token.parse().ok().expect("Failed parse");
+            }
+            self.buf_str.clear();
+            self.reader
+                .read_until(b'\n', &mut self.buf_str)
+                .expect("Failed read");
+            self.buf_iter = unsafe {
+                let slice = str::from_utf8_unchecked(&self.buf_str);
+                std::mem::transmute(slice.split_ascii_whitespace())
+            }
+        }
+    }
+
+    pub fn all(&mut self) -> String {
+        let mut input = String::new();
+        self.reader.read_to_string(&mut input).expect("Failed read");
+        input
+    }
+
+    pub fn line(&mut self) -> String {
+        let mut input = String::new();
+        self.reader.read_line(&mut input).expect("Failed read");
+        input
+    }
+}
+
+fn main() {
+    let (stdin, stdout) = (io::stdin(), io::stdout());
+    let mut scan = UnsafeScanner::new(stdin.lock());
+    let mut out = io::BufWriter::new(stdout.lock());
+
+    let n = scan.token::<i64>();
+    let area = (n * (n + 1) - 1) as f64;
+
+    writeln!(out, "{:.9}", area).unwrap();
+
+    let mut a = Vec::new();
+
+    a.push(-1);
+
+    for x in (2..=n).rev() {
+        a.push(-x);
+    }
+
+    a.push(0);
+
+    for x in 2..=n {
+        a.push(x);
+    }
+
+    a.push(1);
+
+    for val in a {
+        write!(out, "{val} ").unwrap();
+    }
+
+    writeln!(out).unwrap();
+}

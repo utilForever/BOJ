438879e078a81dffdb0f51b3b8c821d4a317aaee
Chris Ohk <utilforever@gmail.com>
2025-12-25 10:25:36 +0900

    Upload 19813 - Dates

---
+++ 19000/19813 - Dates.rs
@@ -0,0 +1,72 @@
+use io::Write;
+use std::{io, str};
+
+pub struct UnsafeScanner<R> {
+    reader: R,
+    buf_str: Vec<u8>,
+    buf_iter: str::SplitAsciiWhitespace<'static>,
+}
+
+impl<R: io::BufRead> UnsafeScanner<R> {
+    pub fn new(reader: R) -> Self {
+        Self {
+            reader,
+            buf_str: vec![],
+            buf_iter: "".split_ascii_whitespace(),
+        }
+    }
+
+    pub fn token<T: str::FromStr>(&mut self) -> T {
+        loop {
+            if let Some(token) = self.buf_iter.next() {
+                return token.parse().ok().expect("Failed parse");
+            }
+            self.buf_str.clear();
+            self.reader
+                .read_until(b'\n', &mut self.buf_str)
+                .expect("Failed read");
+            self.buf_iter = unsafe {
+                let slice = str::from_utf8_unchecked(&self.buf_str);
+                std::mem::transmute(slice.split_ascii_whitespace())
+            }
+        }
+    }
+
+    pub fn all(&mut self) -> String {
+        let mut input = String::new();
+        self.reader.read_to_string(&mut input).expect("Failed read");
+        input
+    }
+
+    pub fn line(&mut self) -> String {
+        let mut input = String::new();
+        self.reader.read_line(&mut input).expect("Failed read");
+        input
+    }
+}
+
+fn main() {
+    let (stdin, stdout) = (io::stdin(), io::stdout());
+    let mut scan = UnsafeScanner::new(stdin.lock());
+    let mut out = io::BufWriter::new(stdout.lock());
+
+    let n = scan.token::<i64>();
+
+    for _ in 0..n {
+        let date = scan.token::<String>();
+
+        let (year, month, day) = if date.contains('.') {
+            let parts = date.split('.').collect::<Vec<_>>();
+            (parts[2], parts[1], parts[0])
+        } else {
+            let parts = date.split('/').collect::<Vec<_>>();
+            (parts[2], parts[0], parts[1])
+        };
+
+        writeln!(
+            out,
+            "{day:0>2}.{month:0>2}.{year:0>4} {month:0>2}/{day:0>2}/{year:0>4}"
+        )
+        .unwrap();
+    }
+}

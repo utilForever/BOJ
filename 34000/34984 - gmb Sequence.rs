2fae3138516534af92dde6b61726ae4bf5df1c92
Chris Ohk <utilforever@gmail.com>
2025-12-29 14:46:18 +0900

    Upload 34984 - gmb Sequence

---
+++ 34000/34984 - gmb Sequence.rs
@@ -0,0 +1,75 @@
+use io::Write;
+use std::{io, str};
+
+pub struct UnsafeScanner<R> {
+    reader: R,
+    buf_str: Vec<u8>,
+    buf_iter: str::SplitAsciiWhitespace<'static>,
+}
+
+impl<R: io::BufRead> UnsafeScanner<R> {
+    pub fn new(reader: R) -> Self {
+        Self {
+            reader,
+            buf_str: vec![],
+            buf_iter: "".split_ascii_whitespace(),
+        }
+    }
+
+    pub fn token<T: str::FromStr>(&mut self) -> T {
+        loop {
+            if let Some(token) = self.buf_iter.next() {
+                return token.parse().ok().expect("Failed parse");
+            }
+            self.buf_str.clear();
+            self.reader
+                .read_until(b'\n', &mut self.buf_str)
+                .expect("Failed read");
+            self.buf_iter = unsafe {
+                let slice = str::from_utf8_unchecked(&self.buf_str);
+                std::mem::transmute(slice.split_ascii_whitespace())
+            }
+        }
+    }
+
+    pub fn all(&mut self) -> String {
+        let mut input = String::new();
+        self.reader.read_to_string(&mut input).expect("Failed read");
+        input
+    }
+
+    pub fn line(&mut self) -> String {
+        let mut input = String::new();
+        self.reader.read_line(&mut input).expect("Failed read");
+        input
+    }
+}
+
+const MOD: i64 = 1_000_000_007;
+
+fn main() {
+    let (stdin, stdout) = (io::stdin(), io::stdout());
+    let mut scan = UnsafeScanner::new(stdin.lock());
+    let mut out = io::BufWriter::new(stdout.lock());
+
+    let n = scan.token::<usize>();
+    let mut dp = vec![[0; 300]; 300];
+    let mut cnt = vec![0; 300];
+    let mut ret = 0;
+
+    for _ in 0..n {
+        let b = scan.token::<usize>();
+
+        for i in 0..300 {
+            let ext = if i + b < 300 { dp[i + b][i] } else { 0 };
+            ret = (ret + ext) % MOD;
+
+            let add = (ext + cnt[i]) % MOD;
+            dp[i][b] = (dp[i][b] + add) % MOD;
+        }
+
+        cnt[b] += 1;
+    }
+
+    writeln!(out, "{ret}").unwrap();
+}

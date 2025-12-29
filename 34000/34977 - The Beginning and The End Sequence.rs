0cb9b5c14c1adb467ec0b00f2cdf3d9501dffbe2
Chris Ohk <utilforever@gmail.com>
2025-12-29 14:45:37 +0900

    Upload 34977 - The Beginning and The End Sequence

---
+++ 34000/34977 - The Beginning and The End Sequence.rs
@@ -0,0 +1,73 @@
+use io::Write;
+use std::{io, str};
+
+pub struct UnsafeScanner<R> {
+    reader: R,
+    buf_str: Vec<u8>,
+    buf_iter: str::SplitAsciiWhitespace<'static>,
+}
+
+impl<R: io::BufRead> UnsafeScanner<R> {
+    pub fn new(reader: R) -> Self {
+        Self {
+            reader,
+            buf_str: vec![],
+            buf_iter: "".split_ascii_whitespace(),
+        }
+    }
+
+    pub fn token<T: str::FromStr>(&mut self) -> T {
+        loop {
+            if let Some(token) = self.buf_iter.next() {
+                return token.parse().ok().expect("Failed parse");
+            }
+            self.buf_str.clear();
+            self.reader
+                .read_until(b'\n', &mut self.buf_str)
+                .expect("Failed read");
+            self.buf_iter = unsafe {
+                let slice = str::from_utf8_unchecked(&self.buf_str);
+                std::mem::transmute(slice.split_ascii_whitespace())
+            }
+        }
+    }
+
+    pub fn all(&mut self) -> String {
+        let mut input = String::new();
+        self.reader.read_to_string(&mut input).expect("Failed read");
+        input
+    }
+
+    pub fn line(&mut self) -> String {
+        let mut input = String::new();
+        self.reader.read_line(&mut input).expect("Failed read");
+        input
+    }
+}
+
+fn main() {
+    let (stdin, stdout) = (io::stdin(), io::stdout());
+    let mut scan = UnsafeScanner::new(stdin.lock());
+    let mut out = io::BufWriter::new(stdout.lock());
+
+    let l = scan.token::<usize>();
+    let mut arr = vec![0; l];
+
+    for i in 0..l {
+        arr[i] = scan.token::<i64>();
+    }
+
+    if l == 1 {
+        writeln!(out, "no").unwrap();
+        return;
+    }
+
+    for i in 1..=(l + 1) / 2 {
+        if arr[0..i] == arr[l - i..l] {
+            writeln!(out, "yes").unwrap();
+            return;
+        }
+    }
+
+    writeln!(out, "no").unwrap();
+}

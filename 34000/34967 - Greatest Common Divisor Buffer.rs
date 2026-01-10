08d3385129383ec30c5271de553d6450f5ae2e74
Chris Ohk <utilforever@gmail.com>
2026-01-11 02:32:49 +0900

    Upload 34967 - Greatest Common Divisor Buffer

---
+++ 34000/34967 - Greatest Common Divisor Buffer.rs
@@ -0,0 +1,79 @@
+use io::Write;
+use std::{io, str};
+
+pub struct UnsafeScanner<R> {
+    reader: R,
+    buf_str: Vec<u8>,
+    buf_iter: str::SplitAsciiWhitespace<'static>,
+}
+
+impl<R: io::BufRead> UnsafeScanner<R> {
+    pub fn new(reader: R) -> Self {
+        Self {
+            reader,
+            buf_str: vec![],
+            buf_iter: "".split_ascii_whitespace(),
+        }
+    }
+
+    pub fn token<T: str::FromStr>(&mut self) -> T {
+        loop {
+            if let Some(token) = self.buf_iter.next() {
+                return token.parse().ok().expect("Failed parse");
+            }
+            self.buf_str.clear();
+            self.reader
+                .read_until(b'\n', &mut self.buf_str)
+                .expect("Failed read");
+            self.buf_iter = unsafe {
+                let slice = str::from_utf8_unchecked(&self.buf_str);
+                std::mem::transmute(slice.split_ascii_whitespace())
+            }
+        }
+    }
+
+    pub fn all(&mut self) -> String {
+        let mut input = String::new();
+        self.reader.read_to_string(&mut input).expect("Failed read");
+        input
+    }
+
+    pub fn line(&mut self) -> String {
+        let mut input = String::new();
+        self.reader.read_line(&mut input).expect("Failed read");
+        input
+    }
+}
+
+fn main() {
+    let (stdin, stdout) = (io::stdin(), io::stdout());
+    let mut scan = UnsafeScanner::new(stdin.lock());
+    let mut out = io::BufWriter::new(stdout.lock());
+
+    let n = scan.token::<usize>();
+    let mut g = Vec::with_capacity(n - 1);
+
+    for i in 1..n {
+        g.push(if i % 2 == 1 {
+            9999 + (i + 1) as i64 / 2
+        } else {
+            10000 - (i / 2) as i64
+        });
+    }
+
+    let mut ret = vec![0; n];
+
+    ret[0] = g[0];
+
+    for i in 1..n - 1 {
+        ret[i] = g[i - 1] * g[i];
+    }
+
+    ret[n - 1] = g[n - 2];
+
+    for val in ret {
+        write!(out, "{val} ").unwrap();
+    }
+
+    writeln!(out).unwrap();
+}

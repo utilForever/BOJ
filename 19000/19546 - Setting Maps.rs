6394a025e131a33354c21512ba7dd9d2ab5f6cd6
Chris Ohk <utilforever@gmail.com>
2026-01-24 04:59:46 +0900

    Upload 19546 - Setting Maps

---
+++ 19000/19546 - Setting Maps.rs
@@ -0,0 +1,269 @@
+use io::Write;
+use std::{collections::VecDeque, io, str};
+
+pub struct UnsafeScanner<R> {
+    reader: R,
+    buf_str: Vec<u8>,
+    buf_iter: str::SplitAsciiWhitespace<'static>,
+}
+
+impl<R: io::BufRead> UnsafeScanner<R> {
+    pub fn new(reader: R) -> Self {
+        Self {
+            reader,
+            buf_str: vec![],
+            buf_iter: "".split_ascii_whitespace(),
+        }
+    }
+
+    pub fn token<T: str::FromStr>(&mut self) -> T {
+        loop {
+            if let Some(token) = self.buf_iter.next() {
+                return token.parse().ok().expect("Failed parse");
+            }
+            self.buf_str.clear();
+            self.reader
+                .read_until(b'\n', &mut self.buf_str)
+                .expect("Failed read");
+            self.buf_iter = unsafe {
+                let slice = str::from_utf8_unchecked(&self.buf_str);
+                std::mem::transmute(slice.split_ascii_whitespace())
+            }
+        }
+    }
+
+    pub fn all(&mut self) -> String {
+        let mut input = String::new();
+        self.reader.read_to_string(&mut input).expect("Failed read");
+        input
+    }
+
+    pub fn line(&mut self) -> String {
+        let mut input = String::new();
+        self.reader.read_line(&mut input).expect("Failed read");
+        input
+    }
+}
+
+#[derive(Clone, Copy)]
+struct Edge {
+    to: usize,
+    rev: usize,
+    capacity: i64,
+}
+
+impl Edge {
+    fn new(to: usize, rev: usize, capacity: i64) -> Self {
+        Self { to, rev, capacity }
+    }
+}
+
+struct Dinic {
+    graph: Vec<Vec<Edge>>,
+    source: usize,
+    sink: usize,
+    check: Vec<i64>,
+    done: Vec<usize>,
+}
+
+impl Dinic {
+    fn new(n: usize, source: usize, sink: usize) -> Self {
+        Self {
+            graph: vec![Vec::new(); n],
+            source,
+            sink,
+            check: vec![-1; n],
+            done: vec![0; n],
+        }
+    }
+
+    fn add_edge(&mut self, u: usize, v: usize, capacity: i64) {
+        let orig = self.graph[v].len();
+        let dest = self.graph[u].len();
+
+        self.graph[u].push(Edge::new(v, orig, capacity));
+        self.graph[v].push(Edge::new(u, dest, 0));
+    }
+
+    fn process_bfs(&mut self) -> bool {
+        self.check.fill(-1);
+
+        let mut queue = VecDeque::new();
+
+        queue.push_back(self.source);
+        self.check[self.source] = 0;
+
+        while let Some(u) = queue.pop_front() {
+            for &e in self.graph[u].iter() {
+                if e.capacity > 0 && self.check[e.to] < 0 {
+                    queue.push_back(e.to);
+                    self.check[e.to] = self.check[u] + 1;
+                }
+            }
+        }
+
+        self.check[self.sink] >= 0
+    }
+
+    fn process_dfs(&mut self, idx: usize, flow: i64) -> i64 {
+        if idx == self.sink {
+            return flow;
+        }
+
+        let n = self.graph[idx].len();
+
+        while self.done[idx] < n {
+            let edge = self.graph[idx][self.done[idx]];
+
+            if edge.capacity > 0 && self.check[edge.to] == self.check[idx] + 1 {
+                let flow_current = self.process_dfs(edge.to, flow.min(edge.capacity));
+
+                if flow_current > 0 {
+                    self.graph[idx][self.done[idx]].capacity -= flow_current;
+                    self.graph[edge.to][edge.rev].capacity += flow_current;
+
+                    return flow_current;
+                }
+            }
+
+            self.done[idx] += 1;
+        }
+
+        0
+    }
+
+    fn get_flow(&mut self) -> i64 {
+        let mut flow_total = 0;
+
+        while self.process_bfs() {
+            self.done.fill(0);
+
+            loop {
+                let flow_current = self.process_dfs(self.source, i64::MAX);
+
+                if flow_current == 0 {
+                    break;
+                }
+
+                flow_total += flow_current;
+            }
+        }
+
+        flow_total
+    }
+
+    fn reachable_from_source(&self) -> Vec<bool> {
+        let n = self.graph.len();
+        let mut visited = vec![false; n];
+        let mut queue = VecDeque::new();
+
+        visited[self.source] = true;
+        queue.push_back(self.source);
+
+        while let Some(u) = queue.pop_front() {
+            for &edge in self.graph[u].iter() {
+                if edge.capacity > 0 && !visited[edge.to] {
+                    visited[edge.to] = true;
+                    queue.push_back(edge.to);
+                }
+            }
+        }
+
+        visited
+    }
+}
+
+const INF: i64 = 1_000_000_000_000;
+
+fn main() {
+    let (stdin, stdout) = (io::stdin(), io::stdout());
+    let mut scan = UnsafeScanner::new(stdin.lock());
+    let mut out = io::BufWriter::new(stdout.lock());
+
+    let (n, m, k) = (
+        scan.token::<usize>(),
+        scan.token::<usize>(),
+        scan.token::<usize>(),
+    );
+    let (s, e) = (scan.token::<usize>(), scan.token::<usize>());
+    let mut costs = vec![0; n + 1];
+
+    for i in 1..=n {
+        costs[i] = scan.token::<i64>();
+    }
+
+    let mut edges = vec![(0, 0); m];
+
+    for i in 0..m {
+        edges[i] = (scan.token::<usize>(), scan.token::<usize>());
+    }
+
+    let idx_in = |u: usize, v: usize| -> usize { (u * n + (v - 1)) * 2 };
+    let idx_out = |u: usize, v: usize| -> usize { (u * n + (v - 1)) * 2 + 1 };
+
+    let source = n * k * 2;
+    let sink = n * k * 2 + 1;
+    let mut maximum_flow = Dinic::new(n * k * 2 + 2, source, sink);
+
+    for i in 0..k {
+        for j in 1..=n {
+            maximum_flow.add_edge(idx_in(i, j), idx_out(i, j), costs[j]);
+        }
+    }
+
+    for &(u, v) in edges.iter() {
+        for i in 0..k {
+            maximum_flow.add_edge(idx_out(i, u), idx_in(i, v), INF);
+        }
+    }
+
+    if k >= 2 {
+        for i in 0..k - 1 {
+            for j in 1..=n {
+                maximum_flow.add_edge(idx_in(i, j), idx_out(i + 1, j), INF);
+            }
+        }
+    }
+
+    for i in 0..k {
+        maximum_flow.add_edge(source, idx_in(i, s), INF);
+        maximum_flow.add_edge(idx_out(i, e), sink, INF);
+    }
+
+    let flow = maximum_flow.get_flow();
+
+    if flow >= INF / 2 {
+        println!("-1");
+        return;
+    }
+
+    let reach = maximum_flow.reachable_from_source();
+    let mut selected = vec![false; n + 1];
+
+    for i in 0..k {
+        for v in 1..=n {
+            let a = idx_in(i, v);
+            let b = idx_out(i, v);
+
+            if reach[a] && !reach[b] {
+                selected[v] = true;
+            }
+        }
+    }
+
+    let mut ret = Vec::new();
+
+    for i in 1..=n {
+        if selected[i] {
+            ret.push(i);
+        }
+    }
+
+    writeln!(out, "{}", ret.len()).unwrap();
+
+    for val in ret {
+        write!(out, "{val} ").unwrap();
+    }
+
+    writeln!(out).unwrap();
+}

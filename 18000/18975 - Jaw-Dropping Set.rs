cfa8ba488d4754ee6dbbf884b21ba8649ca62b2f
Chris Ohk <utilforever@gmail.com>
2026-02-10 03:01:04 +0900

    Upload 18975 - Jaw-Dropping Set

---
+++ 18000/18975 - Jaw-Dropping Set.rs
@@ -0,0 +1,73 @@
+use io::Write;
+use std::{io, str};
+
+pub struct UnsafeScanner<R> {
+    reader: R,
+    buf_str: Vec<u8>,
+    buf_iter: str::SplitAsciiWhitespace<'static>,
+}
+
+impl<R: io::BufRead> UnsafeScanner<R> {
+    pub fn new(reader: R) -> Self {
+        Self {
+            reader,
+            buf_str: vec![],
+            buf_iter: "".split_ascii_whitespace(),
+        }
+    }
+
+    pub fn token<T: str::FromStr>(&mut self) -> T {
+        loop {
+            if let Some(token) = self.buf_iter.next() {
+                return token.parse().ok().expect("Failed parse");
+            }
+            self.buf_str.clear();
+            self.reader
+                .read_until(b'\n', &mut self.buf_str)
+                .expect("Failed read");
+            self.buf_iter = unsafe {
+                let slice = str::from_utf8_unchecked(&self.buf_str);
+                std::mem::transmute(slice.split_ascii_whitespace())
+            }
+        }
+    }
+
+    pub fn all(&mut self) -> String {
+        let mut input = String::new();
+        self.reader.read_to_string(&mut input).expect("Failed read");
+        input
+    }
+
+    pub fn line(&mut self) -> String {
+        let mut input = String::new();
+        self.reader.read_line(&mut input).expect("Failed read");
+        input
+    }
+}
+
+fn main() {
+    let (stdin, stdout) = (io::stdin(), io::stdout());
+    let mut scan = UnsafeScanner::new(stdin.lock());
+    let mut out = io::BufWriter::new(stdout.lock());
+
+    let t = scan.token::<i64>();
+
+    for _ in 0..t {
+        let n = scan.token::<i64>();
+        let mut left = n / 3;
+        let mut right = n;
+        let mut pow2 = 1;
+        let mut ret = 0;
+
+        while right > 0 {
+            let sum = ((right + 1) / 2).pow(2) - ((left + 1) / 2).pow(2);
+            ret += sum * pow2;
+
+            right = left;
+            left /= 3;
+            pow2 *= 2;
+        }
+
+        writeln!(out, "{ret}").unwrap();
+    }
+}

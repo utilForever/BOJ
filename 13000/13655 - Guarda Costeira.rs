470437fe3c0c0339522a603b991c07f83ddf7ae8
Chris Ohk <utilforever@gmail.com>
2026-02-17 08:47:10 +0900

    Upload 13655 - Guarda Costeira

---
+++ 13000/13655 - Guarda Costeira.rs
@@ -0,0 +1,72 @@
+use io::Write;
+use std::{io, str};
+
+pub struct UnsafeScanner<R> {
+    reader: R,
+    buf_str: Vec<u8>,
+    buf_iter: str::SplitAsciiWhitespace<'static>,
+}
+
+impl<R: io::BufRead> UnsafeScanner<R> {
+    pub fn new(reader: R) -> Self {
+        Self {
+            reader,
+            buf_str: vec![],
+            buf_iter: "".split_ascii_whitespace(),
+        }
+    }
+
+    pub fn token<T: str::FromStr>(&mut self) -> T {
+        loop {
+            if let Some(token) = self.buf_iter.next() {
+                return token.parse().ok().expect("Failed parse");
+            }
+            self.buf_str.clear();
+            self.reader
+                .read_until(b'\n', &mut self.buf_str)
+                .expect("Failed read");
+            self.buf_iter = unsafe {
+                let slice = str::from_utf8_unchecked(&self.buf_str);
+                std::mem::transmute(slice.split_ascii_whitespace())
+            }
+        }
+    }
+
+    pub fn all(&mut self) -> String {
+        let mut input = String::new();
+        self.reader.read_to_string(&mut input).expect("Failed read");
+        input
+    }
+
+    pub fn line(&mut self) -> String {
+        let mut input = String::new();
+        self.reader.read_line(&mut input).expect("Failed read");
+        input
+    }
+}
+
+fn main() {
+    let (stdin, stdout) = (io::stdin(), io::stdout());
+    let mut scan = UnsafeScanner::new(stdin.lock());
+    let mut out = io::BufWriter::new(stdout.lock());
+
+    loop {
+        let line = scan.line().trim().to_string();
+
+        if line.is_empty() {
+            break;
+        }
+
+        let words = line.split_whitespace().collect::<Vec<_>>();
+        let (d, vf, vg) = (
+            words[0].parse::<f64>().unwrap(),
+            words[1].parse::<f64>().unwrap(),
+            words[2].parse::<f64>().unwrap(),
+        );
+
+        let time_thief = 12.0 / vf;
+        let time_guard = (144.0 + d * d).sqrt() / vg;
+
+        writeln!(out, "{}", if time_thief >= time_guard { "S" } else { "N" }).unwrap();
+    }
+}

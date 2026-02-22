ba921407b9d94877d27ed020997fd4c68ff87877
Chris Ohk <utilforever@gmail.com>
2026-02-23 01:44:30 +0900

    Upload 11580 - Footprint

---
+++ 11000/11580 - Footprint.rs
@@ -0,0 +1,73 @@
+use io::Write;
+use std::{collections::HashSet, io, str};
+
+pub struct UnsafeScanner<R> {
+    reader: R,
+    buf_str: Vec<u8>,
+    buf_iter: str::SplitAsciiWhitespace<'static>,
+}
+
+impl<R: io::BufRead> UnsafeScanner<R> {
+    pub fn new(reader: R) -> Self {
+        Self {
+            reader,
+            buf_str: vec![],
+            buf_iter: "".split_ascii_whitespace(),
+        }
+    }
+
+    pub fn token<T: str::FromStr>(&mut self) -> T {
+        loop {
+            if let Some(token) = self.buf_iter.next() {
+                return token.parse().ok().expect("Failed parse");
+            }
+            self.buf_str.clear();
+            self.reader
+                .read_until(b'\n', &mut self.buf_str)
+                .expect("Failed read");
+            self.buf_iter = unsafe {
+                let slice = str::from_utf8_unchecked(&self.buf_str);
+                std::mem::transmute(slice.split_ascii_whitespace())
+            }
+        }
+    }
+
+    pub fn all(&mut self) -> String {
+        let mut input = String::new();
+        self.reader.read_to_string(&mut input).expect("Failed read");
+        input
+    }
+
+    pub fn line(&mut self) -> String {
+        let mut input = String::new();
+        self.reader.read_line(&mut input).expect("Failed read");
+        input
+    }
+}
+
+fn main() {
+    let (stdin, stdout) = (io::stdin(), io::stdout());
+    let mut scan = UnsafeScanner::new(stdin.lock());
+    let mut out = io::BufWriter::new(stdout.lock());
+
+    let _ = scan.token::<i64>();
+    let commands = scan.token::<String>().chars().collect::<Vec<_>>();
+    let mut pos = (0, 0);
+    let mut coords = HashSet::new();
+
+    coords.insert(pos);
+
+    for command in commands {
+        match command {
+            'N' => pos.1 += 1,
+            'S' => pos.1 -= 1,
+            'W' => pos.0 -= 1,
+            'E' => pos.0 += 1,
+            _ => unreachable!(),
+        }
+
+        coords.insert(pos);
+    }
+
+    writeln!(out, "{}", coords.len()).unwrap();
+}

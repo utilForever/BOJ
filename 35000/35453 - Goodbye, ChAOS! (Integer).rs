d5d108b9b17c7e9bbaa437b9ad5d20e7fe74fcc7
Chris Ohk <utilforever@gmail.com>
2026-03-30 14:58:55 +0900

    Upload 35453 - Goodbye, ChAOS! (Integer)

---
+++ 35000/35453 - Goodbye, ChAOS! (Integer).rs
@@ -0,0 +1,82 @@
+use io::Write;
+use std::{io, str};
+
+pub struct UnsafeScanner<R> {
+    reader: R,
+    buf_str: Vec<u8>,
+    buf_iter: str::SplitAsciiWhitespace<'static>,
+}
+
+impl<R: io::BufRead> UnsafeScanner<R> {
+    pub fn new(reader: R) -> Self {
+        Self {
+            reader,
+            buf_str: vec![],
+            buf_iter: "".split_ascii_whitespace(),
+        }
+    }
+
+    pub fn token<T: str::FromStr>(&mut self) -> T {
+        loop {
+            if let Some(token) = self.buf_iter.next() {
+                return token.parse().ok().expect("Failed parse");
+            }
+            self.buf_str.clear();
+            self.reader
+                .read_until(b'\n', &mut self.buf_str)
+                .expect("Failed read");
+            self.buf_iter = unsafe {
+                let slice = str::from_utf8_unchecked(&self.buf_str);
+                std::mem::transmute(slice.split_ascii_whitespace())
+            }
+        }
+    }
+
+    pub fn all(&mut self) -> String {
+        let mut input = String::new();
+        self.reader.read_to_string(&mut input).expect("Failed read");
+        input
+    }
+
+    pub fn line(&mut self) -> String {
+        let mut input = String::new();
+        self.reader.read_line(&mut input).expect("Failed read");
+        input
+    }
+}
+
+fn main() {
+    let (stdin, stdout) = (io::stdin(), io::stdout());
+    let mut scan = UnsafeScanner::new(stdin.lock());
+    let mut out = io::BufWriter::new(stdout.lock());
+
+    let t = scan.token::<i64>();
+
+    for _ in 0..t {
+        let n = scan.token::<i64>();
+        let mut len = 1;
+
+        for _ in 0..n {
+            let s = scan.token::<String>();
+            len *= s.len() as i64;
+        }
+
+        writeln!(
+            out,
+            "{}",
+            match len {
+                232 => "2017",
+                88 => "2018",
+                754 => "2019",
+                29 => "2020",
+                28 => "2021",
+                1015 => "2022",
+                1295 => "2023",
+                1073 => "2024",
+                348 => "2025",
+                _ => "Goodbye, ChAOS!",
+            }
+        )
+        .unwrap();
+    }
+}
